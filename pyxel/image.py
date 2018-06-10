96d1e26831ee031ce3d41ccf573d8f8df1a881eb
Takashi Kitao <takashi.kitao@gmail.com>
2018-06-10 16:37:02 +0900

    Renamed Bank to Image

--- pyxel/image.py
+++ pyxel/image.py
@@ -1,7 +1,7 @@
 from .glwrapper import GLTexture
 
 
-class Bank:
+class Image:
     def __init__(self, width, height):
         self._tex = GLTexture(width, height, 1, nearest=True)
         self._data = self._tex.data
@@ -12,7 +12,9 @@ class Bank:
         return self._data
 
     def save(self):
+        # todo
         pass
 
     def load(self):
         self._tex.refresh()
+        # todo
