334732d9c90bfb5b2dce5caafdfd7e6928fcb227
Takashi Kitao <takashi.kitao@gmail.com>
2018-06-10 21:39:56 +0900

    Added the size properties

--- pyxel/image.py
+++ pyxel/image.py
@@ -3,8 +3,18 @@ from .glwrapper import GLTexture
 
 class Image:
     def __init__(self, width, height):
+        self._width = width
+        self._height = height
         self._tex = GLTexture(width, height, 1, nearest=True)
         self._data = self._tex.data
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
 
     @property
     def data(self):
