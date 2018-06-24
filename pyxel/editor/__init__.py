27c21fbec6057e4da492be9307abcc338f385e55
Takashi Kitao <takashi.kitao@gmail.com>
2018-06-24 19:21:14 +0900

    Modified the pyxel editor (not implemented yet)

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -5,46 +5,12 @@ class Editor(pyxel.App):
     def __init__(self):
         super().__init__(160, 120)
 
-        self.image = pyxel.Image(16, 16)
-
-        data = self.image.data
-        data[0, 0] = 7
-        data[0, 1] = 3
-        data[0, 2] = 7
-        data[1, 0] = 8
-        data[2, 0] = 7
-        data[7, 7] = 7
-        self.bank(0, self.image)
-
-        self.x = 0
-
-        self.count = 0
-        self.time = 0
-
     def update(self):
-        self.cls(2)
+        self.text(9, 8, "Hello, Pyxel Editor!", 8)
+        self.text(8, 8, "Hello, Pyxel Editor!", 7)
 
-        self.pix(0, 0, 8)
-        self.pix(159, 0, 8)
-        self.pix(0, 119, 8)
-        self.pix(159, 119, 8)
-
-        self.blt(80, 40, 7, 0, 0, 64, 64, 0)
-
-        self.rectb(50, 50, 30, 40, 3)
-
-        self.rectb(90, 90, 21, 21, 8)
-        self.circb(100, 100, 30, 7)
-        self.circ(30, 100, 4, 7)
-
-        self.rect(30, 30, 30, 10, 0)
-        self.line(29, 29, 60, 20, 8)
-
-        self.x = (self.x + 1) % 160
-        self.pix(self.x, 0, 7)
-
-        self.text(11, 11, "Hello, Pyxel!", 8)
-        self.text(10, 10, "Hello, Pyxel!", 7)
+        if self.btnp(pyxel.KEY_Q):
+            exit()
 
 
 def run():
