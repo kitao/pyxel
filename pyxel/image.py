76c1ab769d4b5234a2588631fad72113cb6eea36
Takashi Kitao <takashi.kitao@gmail.com>
2018-06-26 22:43:55 +0900

    Renamed the method name

--- pyxel/image.py
+++ pyxel/image.py
@@ -18,19 +18,19 @@ class Image:
 
     @property
     def data(self):
-        self._tex.refresh()
+        self._tex.update()
         return self._data
 
     def set(self, x, y, width, height, data):
         self._data[y:y + height, x:x + width] = [
             list(map(lambda x: int(x, 16), line)) for line in data
         ]
-        self._tex.refresh()
+        self._tex.update()
 
     def save(self):
         # todo
         pass
 
     def load(self):
-        self._tex.refresh()
+        self._tex.update()
         # todo
