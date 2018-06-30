aae43793ee6737a7ce621d80235a86dbff127779
Takashi Kitao <takashi.kitao@gmail.com>
2018-06-30 11:32:04 +0900

    Added new examples (WIP)

---
+++ examples/hello_pyxel.py
@@ -0,0 +1,15 @@
+import pyxel
+
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        exit()
+
+
+def draw():
+    pyxel.cls(0)
+    pyxel.text(40, 60, 'Hello, Pyxel!', pyxel.frame_count % 16)
+
+
+pyxel.init(128, 128, caption='Hello Pyxel')
+pyxel.run(update, draw)
---
+++ examples/simple_app.py
@@ -0,0 +1,18 @@
+import pyxel
+
+
+class App():
+    def __init__(self):
+        pyxel.init(160, 120, caption='Simple App')
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            exit()
+
+    def draw(self):
+        pyxel.cls(2)
+
+
+App()
