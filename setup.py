a750f42b95f5ce819d5b6205e088524f48e9b0da
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-02 08:37:02 +0900

    Updated the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyxel',
-    version='0.1.0',
+    version='0.2.0',
     description='A fantasy retro gaming console in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
