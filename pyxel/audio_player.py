a923758f6168c07373e06a12a19840605d31bcd7
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-04 22:44:51 +0900

    Renamed sequencer module to audio_player and implemented the play method (WIP)

--- pyxel/audio_player.py
+++ pyxel/audio_player.py
@@ -8,7 +8,7 @@ NOTE_PITCH = [440 * pow(2, (note - 69) / 12) for note in range(128)]
 
 
 def osc_triangle(x):
-    return (abs((x % 1) * 2 - 1) * 2 - 1) * 0.7
+    return (abs((x % 1) * 4 - 2) - 1) * 0.7
 
 
 def osc_tilted_saw(x):
@@ -63,8 +63,9 @@ class Track:
 
         self._is_playing = False
 
-        self._pattern = [0x45, 0x46, 0x47, 0x48, 0x49]
         self._cur_note = 0
+
+        self._sound = None
 
         self._speed = 1
         self._time = 0
@@ -76,27 +77,36 @@ class Track:
         if not self._is_playing:
             return 0
 
-        pitch = NOTE_PITCH[self._pattern[self._cur_note]]
+        sound = self._sound
 
-        data = OSCILLATOR[0](pitch / SAMPLE_RATE * self._time) * self._volume
+        no = self._cur_note
+        note = sound.note[no]
+
+        if note:
+            pitch = NOTE_PITCH[note]
+            data = (OSCILLATOR[sound.tone[no]](
+                pitch / SAMPLE_RATE * self._time) * self._volume)
+        else:
+            data = 0
 
         self._time += 1
 
         if self._time == 3000:
             self._time = 0
             self._cur_note += 1
-            if self._cur_note >= len(self._pattern):
+            if self._cur_note >= sound.length:
                 self._is_playing = False
 
         return data
 
     def play(self, sound, loop):
         self._is_playing = True
+        self._sound = sound
         self._cur_note = 0
         self._time = 0
 
 
-class Sequencer:
+class AudioPlayer:
     def __init__(self):
         self._step = 0
 
@@ -128,34 +138,3 @@ class Sequencer:
                 data += track.next_data()
 
             outdata[i] = data
-
-
-'''
-[NOTES]
-
-00: C -1    0C: C  0    18: C  1    24: C  2    30: C  3
-01: C#-1    0D: C# 0    19: C# 1    25: C# 2    31: C# 3
-02: D -1    0E: D  0    1A: D  1    26: D  2    32: D  3
-03: Eb-1    0F: Eb 0    1B: Eb 1    27: Eb 2    33: Eb 3
-04: E -1    10: E  0    1C: E  1    28: E  2    34: E  3
-05: F -1    11: F  0    1D: F  1    29: F  2    35: F  3
-06: F#-1    12: F# 0    1E: F# 1    2A: F# 2    36: F# 3
-07: G -1    13: G  0    1F: G  1    2B: G  2    37: G  3
-08: G#-1    14: G# 0    20: G# 1    2C: G# 2    38: G# 3
-09: A -1    15: A  0    21: A  1    2D: A  2    39: A  3
-0A: Bb-1    16: Bb 0    22: Bb 1    2E: Bb 2    3A: Bb 3
-0B: B -1    17: B  0    23: B  1    2F: B  2    3B: B  3
-
-3C: C  4    48: C  5    54: C  6    60: C  7    6C: C  8
-3D: C# 4    49: C# 5    55: C# 6    61: C# 7    6D: C# 8
-3E: D  4    4A: D  5    56: D  6    62: D  7    6E: D  8
-3F: Eb 4    4B: Eb 5    57: Eb 6    63: Eb 7    6F: Eb 8
-40: E  4    4C: E  5    58: E  6    64: E  7    70: E  8
-41: F  4    4D: F  5    59: F  6    65: F  7    71: F  8
-42: F# 4    4E: F# 5    5A: F# 6    66: F# 7    72: F# 8
-43: G  4    4F: G  5    5B: G  6    67: G  7    73: G  8
-44: G# 4    50: G# 5    5C: G# 6    68: G# 7    74: G# 8
-45: A  4    51: A  5    5D: A  6    69: A  7    75: A  8
-46: Bb 4    52: Bb 5    5E: Bb 6    6A: Bb 7    76: Bb 8
-47: B  4    53: B  5    5F: B  6    6B: B  7    77: B  8
-'''
