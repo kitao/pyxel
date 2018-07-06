f7cf2276ab1d6ff84cbcb0f1f1be82694abf59e7
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-06 17:08:08 +0900

    Modified the sound data syntax

--- examples/sound_test.py
+++ examples/sound_test.py
@@ -5,7 +5,7 @@ class App:
     def __init__(self):
         pyxel.init(128, 128)
 
-        self.sound = pyxel.Sound(1, ['c5...d5e5f5g5', '0', '7', '7'])
+        self.sound = pyxel.Sound(16, ['a5.....d5e5f5g5', 't', '7', '-'])
 
         pyxel.run(self.update, self.draw)
 
--- pyxel/sound.py
+++ pyxel/sound.py
@@ -1,68 +1,118 @@
+NOTE_TABLE = {'c': 0, 'd': 2, 'e': 4, 'f': 5, 'g': 7, 'a': 9, 'b': 11}
+TONE_TABLE = {'t': 0, 's': 1, 'p': 2, 'n': 3}
+EFFECT_TABLE = {'-': 0, 's': 1, 'v': 2, 'f': 3}
+
+
 class Sound:
     def __init__(self, speed, data):
         self.speed = max(speed, 1)
         self.note = self._parse_note(data[0])
         self.length = len(self.note)
-        self.inst = self._parse_param(data[1])
-        self.volume = self._parse_param(data[2])
-        self.effect = self._parse_param(data[3])
+        self.tone = self._parse_tone(data[1])
+        self.volume = self._parse_volume(data[2])
+        self.effect = self._parse_effect(data[3])
 
     def _parse_note(self, data):
-        note = []
-        data = data.replace(' ', '').upper()
-        last_n = None
+        param_list = []
+        last_param = None
+        data = data.replace(' ', '').lower()
 
         while data:
             c = data[0]
             data = data[1:]
 
-            if c >= 'A' and c <= 'G':
-                n = [9, 11, 0, 2, 4, 5, 7][ord(c) - ord('A')]
+            param = NOTE_TABLE.get(c, None)
 
+            if param is not None:
                 c = data[0]
                 data = data[1:]
 
                 if c == '#':
-                    n += 1
+                    param += 1
 
                     c = data[0]
                     data = data[1:]
 
                 if c >= '0' and c <= '9':
-                    n += int(c) * 12
+                    param += int(c) * 12
                 else:
                     raise ValueError('invalid sound note')
 
             elif c == '.':
-                n = last_n
-            elif c == 'R':
-                n = None
+                param = last_param
+            elif c == 'r':
+                param = None
             else:
                 raise ValueError('invalid sound note')
 
-            note.append(n)
-            last_n = n
+            param_list.append(param)
+            last_param = param
 
-        return note
+        return param_list
 
-    def _parse_param(self, data):
-        param = []
-        data = data.replace(' ', '').upper()
+    def _parse_tone(self, data):
+        param_list = []
+        data = data.replace(' ', '').lower()
+
+        while data:
+            c = data[0]
+            data = data[1:]
+
+            param = TONE_TABLE.get(c, None)
+
+            if param is None:
+                raise ValueError('invalid sound data')
+
+            param_list.append(param)
+
+        param_list = self._complement_param_list(param_list)
+        return param_list
+
+    def _parse_volume(self, data):
+        param_list = []
+        last_param = 0
+        data = data.replace(' ', '')
 
         while data:
             c = data[0]
             data = data[1:]
 
             if c >= '0' and c <= '7':
-                param.append(int(c))
+                param = int(c)
+            elif c == '.':
+                param = last_param
             else:
                 raise ValueError('invalid sound data')
 
-        diff = len(param) - self.length
+            param_list.append(param)
+
+        param_list = self._complement_param_list(param_list)
+        return param_list
+
+    def _parse_effect(self, data):
+        param_list = []
+        data = data.replace(' ', '').lower()
+
+        while data:
+            c = data[0]
+            data = data[1:]
+
+            param = EFFECT_TABLE.get(c, None)
+
+            if param is None:
+                raise ValueError('invalid sound data')
+
+            param_list.append(param)
+
+        param_list = self._complement_param_list(param_list)
+        return param_list
+
+    def _complement_param_list(self, param_list):
+        diff = len(param_list) - self.length
 
         if diff < 0:
-            param += [param[-1]] * -diff
+            param_list += [param_list[-1]] * -diff
         elif diff > 0:
-            param = param[:self._length]
+            param_list = param_list[:self._length]
 
-        return param
+        return param_list
