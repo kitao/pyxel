4e46d510531cca8c25b2d8fdc0eda2a06c52528a
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-11 22:01:26 +0900

    Added the image command

--- pyxel/image.py
+++ pyxel/image.py
@@ -1,44 +1,12 @@
 import os
 import PIL.Image
 from .glwrapper import GLTexture
+import pyxel
 
 
 class Image:
-    _palette_image = None
-
     def __init__(self, width, height):
-        self._tex = GLTexture(width, height, 1, nearest=True)
-        self._data = self._tex.data
-
-    @staticmethod
-    def fromstring(data):
-        width = len(data[0])
-        height = len(data)
-
-        image = Image(width, height)
-        image.set(0, 0, data)
-
-        return image
-
-    @staticmethod
-    def fromfile(filename):
-        if not Image._palette_image:
-            return
-
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, filename)
-
-        pil_image = PIL.Image.open(filename).convert('RGB')
-        pil_image.load()
-
-        im = pil_image.im.convert('P', 0, Image._palette_image.im)
-        pil_image = pil_image._new(im)
-
-        width, height = pil_image.size
-        image = Image(width, height)
-        image._data.reshape(width * height)[:] = pil_image.getdata()
-
-        return image
+        self.resize(width, height)
 
     @property
     def width(self):
@@ -53,6 +21,10 @@ class Image:
         self._tex.update()
         return self._data
 
+    def resize(self, width, height):
+        self._tex = GLTexture(width, height, 1, nearest=True)
+        self._data = self._tex.data
+
     def set(self, x, y, data):
         width = len(data[0])
         height = len(data)
@@ -61,17 +33,27 @@ class Image:
             list(map(lambda x: int(x, 16), line)) for line in data
         ]
 
-    @staticmethod
-    def set_palette(palette):
-        pil_palette = []
-
-        for color in palette:
+    def load(self, filename):
+        palette = []
+        for color in pyxel._app._palette:
             r = (color >> 16) & 0xff
             g = (color >> 8) & 0xff
             b = color & 0xff
-            pil_palette.extend((r, g, b))
+            palette.extend((r, g, b))
+        palette += [0] * 240 * 3
 
-        pil_palette += [0] * 240 * 3
+        palette_image = PIL.Image.new('P', (1, 1), 0)
+        palette_image.putpalette(palette)
 
-        Image._palette_image = PIL.Image.new('P', (1, 1), 0)
-        Image._palette_image.putpalette(pil_palette)
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, filename)
+
+        pil_image = PIL.Image.open(filename).convert('RGB')
+        pil_image.load()
+
+        im = pil_image.im.convert('P', 0, palette_image.im)
+        pil_image = pil_image._new(im)
+
+        width, height = pil_image.size
+        self.resize(width, height)
+        self._data.reshape(width * height)[:] = pil_image.getdata()
