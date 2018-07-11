f62d41024aa09f4a359bfc2404f1c698213a74d6
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-12 00:14:00 +0900

    Added the image loading code

--- pyxel/examples/02_simple_app.py
+++ pyxel/examples/02_simple_app.py
@@ -5,6 +5,10 @@ class App():
     def __init__(self):
         pyxel.init(160, 120, caption='Simple App')
 
+        pyxel.image(0).load('assets/sample_tile.png')
+        # Pico 8 tiles by Kicked-in-Teeth
+        # https://kicked-in-teeth.itch.io/pico-8-tiles
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -13,6 +17,8 @@ class App():
 
     def draw(self):
         pyxel.cls(2)
+
+        pyxel.blt(0, 0, 0, 0, 0, 96, 96)
 
 
 App()
