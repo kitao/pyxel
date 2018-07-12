6969cb87125f8c1baec421abf0b5f5a11b0e6467
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-13 07:51:20 +0900

    Added the offset arguments to the load method

--- pyxel/examples/02_simple_app.py
+++ pyxel/examples/02_simple_app.py
@@ -5,7 +5,7 @@ class App():
     def __init__(self):
         pyxel.init(160, 120, caption='Simple App')
 
-        pyxel.image(0).load('assets/sample_tile.png')
+        pyxel.image(0).load(32, 16, 'assets/sample_tile.png')
         # Pico 8 tiles by Kicked-in-Teeth
         # https://kicked-in-teeth.itch.io/pico-8-tiles
 
@@ -18,7 +18,7 @@ class App():
     def draw(self):
         pyxel.cls(2)
 
-        pyxel.blt(0, 0, 0, 0, 0, 96, 96)
+        pyxel.blt(0, 0, 0, 0, 0, 128, 128)
 
 
 App()
--- pyxel/image.py
+++ pyxel/image.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+import numpy as np
 import PIL.Image
 from .glwrapper import GLTexture
 import pyxel
@@ -7,7 +8,8 @@ import pyxel
 
 class Image:
     def __init__(self, width, height):
-        self.resize(width, height)
+        self._tex = GLTexture(width, height, 1, nearest=True)
+        self._data = self._tex.data
 
     @property
     def width(self):
@@ -22,19 +24,15 @@ class Image:
         self._tex.update()
         return self._data
 
-    def resize(self, width, height):
-        self._tex = GLTexture(width, height, 1, nearest=True)
-        self._data = self._tex.data
-
     def set(self, x, y, data):
         width = len(data[0])
         height = len(data)
 
-        self._data[y:height + y, x:width + x] = [
+        self._data[y:y + height, x:x + width] = [
             list(map(lambda x: int(x, 16), line)) for line in data
         ]
 
-    def load(self, filename):
+    def load(self, x, y, filename):
         palette = []
         for color in pyxel._app._palette:
             r = (color >> 16) & 0xff
@@ -56,5 +54,12 @@ class Image:
         pil_image = pil_image._new(im)
 
         width, height = pil_image.size
-        self.resize(width, height)
-        self._data.reshape(width * height)[:] = pil_image.getdata()
+        width = min(width, self._tex.width - x)
+        height = min(height, self._tex.height - y)
+        pil_image = pil_image.crop((0, 0, width, height))
+
+        self._data[y:y + height, x:x + width] = np.array(
+            pil_image.getdata()).reshape(height, width)
+
+    def copy(self, x, y, img):
+        pass
