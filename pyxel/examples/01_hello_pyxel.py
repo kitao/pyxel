18d69807ce4ab553e073924c6ef302654088a35f
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-14 17:21:43 +0900

    Added the logo

--- pyxel/examples/01_hello_pyxel.py
+++ pyxel/examples/01_hello_pyxel.py
@@ -8,7 +8,8 @@ def update():
 
 def draw():
     pyxel.cls(0)
-    pyxel.text(40, 60, 'Hello, Pyxel!', pyxel.frame_count % 16)
+    pyxel.text(40, 48, 'Hello, Pyxel!', pyxel.frame_count % 16)
+    pyxel.logo(46, 70)
 
 
 pyxel.init(128, 128, caption='Hello Pyxel')
