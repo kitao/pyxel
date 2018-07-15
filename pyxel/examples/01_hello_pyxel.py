4c8a32092492890412746ad1639bbb0095b237ae
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-15 23:04:40 +0900

    Changed to use the App class

--- pyxel/examples/01_hello_pyxel.py
+++ pyxel/examples/01_hello_pyxel.py
@@ -1,16 +1,19 @@
 import pyxel
 
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+class App:
+    def __init__(self):
+        pyxel.init(128, 128, caption='Hello Pyxel')
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(40, 48, 'Hello, Pyxel!', pyxel.frame_count % 16)
+        pyxel.logo(46, 70)
 
 
-def draw():
-    pyxel.cls(0)
-    pyxel.text(40, 48, 'Hello, Pyxel!', pyxel.frame_count % 16)
-    pyxel.logo(46, 70)
-
-
-pyxel.init(128, 128, caption='Hello Pyxel')
-pyxel.run(update, draw)
+App()
