eb0475a454581cc44f4a649e6f15a62ebc93f891
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-22 23:41:41 +0900

    Updated (WIP)

--- pyxel/examples/02_jump_game.py
+++ pyxel/examples/02_jump_game.py
@@ -8,20 +8,23 @@ class App:
 
         pyxel.image(0).load(0, 0, 'assets/jump_game_128x128.png')
 
-        def loop_list(l):
+        def circulate_list(l):
             return l + list(map(lambda pos: (pos[0] + 240, pos[1]), l))
 
-        self.cloud1 = loop_list(
+        self.cloud1 = circulate_list(
             [(i * 80 + randint(-20, 20), randint(-10, 30)) for i in range(3)])
-        self.cloud2 = loop_list([(i * 40 + randint(-15, 15), randint(10, 80))
-                                 for i in range(6)] * 2)
-        self.hill1 = loop_list([(i * 60 + randint(-30, 30), randint(50, 80))
-                                for i in range(4)] * 2)
-        self.hill2 = loop_list([(i * 30 + randint(-20, 20), randint(70, 100))
-                                for i in range(8)] * 2)
+        self.cloud2 = circulate_list(
+            [(i * 40 + randint(-15, 15), randint(10, 80)) for i in range(6)])
+        self.hill1 = circulate_list(
+            [(i * 60 + randint(-30, 30), randint(50, 80)) for i in range(4)])
+        self.hill2 = circulate_list(
+            [(i * 30 + randint(-10, 10), randint(70, 100)) for i in range(8)])
 
-        self.frame = [(i * 60, randint(0, 120)) for i in range(4)]
+        self.frame = [(i * 60, randint(10, 110), 40, False) for i in range(4)]
 
+        self.player_x = 10
+        self.player_y = 30
+        self.player_vy = 1
         self.score = 0
 
         pyxel.run(self.update, self.draw)
@@ -30,11 +33,35 @@ class App:
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
-        for i, (x, y) in enumerate(self.frame):
-            x -= 3
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.player_x -= 2
+
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.player_x += 2
+
+        self.player_y += self.player_vy
+        self.player_vy = min(self.player_vy + 1, 8)
+
+        if self.player_y > 104:
+            self.player_vy = -12
+
+        for i, (x, y, w, f) in enumerate(self.frame):
+            if f:
+                y += 6
+
+            if (not f and self.player_x + 16 >= x and self.player_x <= x + w
+                    and self.player_y + 16 >= y and self.player_y <= y + 8
+                    and self.player_vy > 0):
+                self.player_vy = -12
+                f = True
+
+            x -= 4
+
             if x < -40:
                 x += 240
-            self.frame[i] = (x, y)
+                y = randint(10, 110)
+                f = False
+            self.frame[i] = (x, y, w, f)
 
     def draw(self):
         pyxel.cls(0)
@@ -55,12 +82,14 @@ class App:
             x -= (pyxel.frame_count // 2) % 240
             pyxel.blt(x, y, 0, 0, 56, 33, 72, 1)
 
-        for x, y in self.frame:
-            pyxel.blt(x, y, 0, 0, 16, 40, 8, 1)
+        pyxel.blt(self.player_x, self.player_y, 0, 16
+                  if self.player_vy > 0 else 0, 0, 16, 16, 1)
 
-        pyxel.blt(30, 30, 0, 0, 0, 16, 16, 1)
+        for x, y, w, f in self.frame:
+            pyxel.blt(x, y, 0, 0, 16, w, 8, 1)
 
         pyxel.blt(100, 30, 0, 32, 0, 16, 16, 1)
+        pyxel.blt(120, 40, 0, 48, 0, 16, 16, 1)
         pyxel.blt(120, 60, 0, 64, 0, 16, 16, 1)
 
         s = 'SCORE {:>4}'.format(self.score)
