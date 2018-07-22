082982a356c996108fe3421f448b9416dcfbd322
Takashi Kitao <takashi.kitao@gmail.com>
2018-07-22 22:59:29 +0900

    Changed the tempo of the music

--- pyxel/examples/04_sound_api.py
+++ pyxel/examples/04_sound_api.py
@@ -20,23 +20,23 @@ class App:
         pyxel.sound(0).set(
             'e2e2c2g1 g1g1c2e2 d2d2d2g2 g2g2rr'
             'c2c2a1e1 e1e1a1c2 b1b1b1e2 e2e2rr', 'p', '6',
-            'vffn fnff vffs vfnn', 30)
+            'vffn fnff vffs vfnn', 25)
 
         pyxel.sound(1).set(
             'r a1b1c2 b1b1c2d2 g2g2g2g2 c2c2d2e2'
             'f2f2f2e2 f2e2d2c2 d2d2d2d2 g2g2r r ', 's', '6',
-            'nnff vfff vvvv vfff svff vfff vvvv svnn', 30)
+            'nnff vfff vvvv vfff svff vfff vvvv svnn', 25)
 
         pyxel.sound(2).set(
             'c1g1c1g1 c1g1c1g1 b0g1b0g1 b0g1b0g1'
-            'a0e1a0e1 a0e1a0e1 g0d1g0d1 g0d1g0d1', 't', '7', 'n', 30)
+            'a0e1a0e1 a0e1a0e1 g0d1g0d1 g0d1g0d1', 't', '7', 'n', 25)
 
         pyxel.sound(3).set(
             'f0c1f0c1 g0d1g0d1 c1g1c1g1 a0e1a0e1'
-            'f0c1f0c1 f0c1f0c1 g0d1g0d1 g0d1g0d1', 't', '7', 'n', 30)
+            'f0c1f0c1 f0c1f0c1 g0d1g0d1 g0d1g0d1', 't', '7', 'n', 25)
 
         pyxel.sound(4).set('f0ra4r f0ra4r f0ra4r f0f0a4r', 'n',
-                           '6622 6622 6622 6422', 'f', 30)
+                           '6622 6622 6622 6422', 'f', 25)
 
         self.is_playing = [True] * 3
 
