fa5d47375b6f5ec543b52457097e234ddbb20cbd
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-05 23:41:21 +0900

    Updaed the size of the editor

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -3,7 +3,7 @@ import pyxel
 
 class Editor:
     def __init__(self):
-        pyxel.init(160, 120, caption='Pyxel')
+        pyxel.init(240, 180, caption='Pyxel')
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -11,8 +11,18 @@ class Editor:
             pyxel.quit()
 
     def draw(self):
-        pyxel.text(9, 8, "Hello, Pyxel Editor!", 8)
-        pyxel.text(8, 8, "Hello, Pyxel Editor!", 7)
+        self.draw_menu()
+        self.draw_help()
+
+    def draw_menu(self):
+        pyxel.rect(0, 0, 239, 6, 6)
+        pyxel.text(2, 1, 'MENU', 0)
+
+        pyxel.rect(230, 1, 234, 5, 13)
+
+    def draw_help(self):
+        pyxel.rect(0, 173, 239, 179, 13)
+        pyxel.text(2, 174, 'Help Message', 0)
 
 
 def run():
