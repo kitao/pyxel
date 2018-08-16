bc22f32f25ed40d959680cce00689301a84f67c4
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-16 23:49:27 +0900

    Removed unnecessary code

--- pyxel/editor/radio_button.py
+++ pyxel/editor/radio_button.py
@@ -17,8 +17,6 @@ class RadioButton(Widget):
         self._interval = interval
 
     def on_press(self, key, mx, my):
-        super().on_press(key, mx, my)
-
         if (mx % self._interval < BUTTON_SIZE
                 and my % self._interval < BUTTON_SIZE):
             value = (my // self._interval) * self._col + mx // self._interval
@@ -28,8 +26,6 @@ class RadioButton(Widget):
                 self.on_value_change(value)
 
     def on_draw(self):
-        super().on_draw()
-
         pyxel.pal(13, 7)
 
         x = self.x + self._interval * (self.value % self._col)
