42d103da30dff74624ffcb84b5d1d3f9f1fd725c
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-16 19:51:11 +0900

    Implemented drawing feature (WIP)

--- pyxel/editor/image_editor.py
+++ pyxel/editor/image_editor.py
@@ -1,97 +1,67 @@
 import pyxel
 
-from pyxel.editor.screen import Screen
 from .radio_button import RadioButton
+from .screen import Screen
+from .widget import Widget
 
-COLOR_BUTTON_X = 12
-COLOR_BUTTON_Y1 = 157
-COLOR_BUTTON_Y2 = 165
-COLOR_BUTTON_INTERVAL = 8
-COLOR_BUTTON_COUNT = 8
 
-TOOL_BUTTON_X = 81
-TOOL_BUTTON_Y = 161
-TOOL_BUTTON_INTERVAL = 9
-TOOL_BUTTON_COUNT = 7
+class CanvasWindow(Widget):
+    def __init__(self, parent):
+        super().__init__(parent, 12, 17, 128, 128)
 
-IMAGE_BUTTON_X = 191
-IMAGE_BUTTON_Y = 161
-IMAGE_BUTTON_INTERVAL = 10
-IMAGE_BUTTON_COUNT = 3
+    def on_draw(self):
+        for i in range(16):
+            y = self.y + i * 8
+            for j in range(16):
+                x = self.x + j * 8
+                col = pyxel.image(self.parent._image_button.value).data[i, j]
+                pyxel.rect(x, y, x + 7, y + 7, col)
 
-CANVAS_X = 12
-CANVAS_Y = 17
+        pyxel.line(self.x, self.y + 63, self.x + 127, self.y + 63, 1)
+        pyxel.line(self.x + 63, self.y, self.x + 63, self.y + 127, 1)
 
-PREVIEW_X = 158
-PREVIEW_Y = 17
+    def on_click(self, key, mx, my):
+        if key == pyxel.KEY_RIGHT_BUTTON:
+            img = self.parent._image_button.value
+            x = mx // 8
+            y = my // 8
+            self.parent._color_button.value = pyxel.image(img).data[y, x]
+
+    def on_drag(self, key, mx, my, dx, dy):
+        if key == pyxel.KEY_LEFT_BUTTON:
+            img = self.parent._image_button.value
+            x = mx // 8
+            y = my // 8
+            col = self.parent._color_button.value
+            pyxel.image(img).data[y, x] = col
+
+
+class PreviewWindow(Widget):
+    def __init__(self, parent):
+        super().__init__(parent, 158, 17, 64, 128)
+
+    def on_draw(self):
+        img = self.parent._image_button.value
+        pyxel.blt(self.x, self.y, img, 0, 0, 64, 128)
 
 
 class ImageEditor(Screen):
     def __init__(self, parent):
         super().__init__(parent, 'image_editor.png')
 
-        self._color = 0
-        self._tool = 0
-        self._img = 0
+        self._color_button = RadioButton(self, 12, 157, 8, 2, 8)
 
-    def on_update(self):
-        super().on_update()
+        def on_draw():
+            widget = self._color_button
+            x = widget.x + (widget.value % 8) * 8
+            y = widget.y + (widget.value // 8) * 8
+            col = 7 if widget.value < 6 else 0
+            pyxel.text(x + 2, y + 1, '+', col)
 
-        #col = self.check_button(COLOR_BUTTON_X, COLOR_BUTTON_Y1,
-        #                        COLOR_BUTTON_INTERVAL, COLOR_BUTTON_COUNT)
-        #if col is not None:
-        #    self._color = col
-        #else:
-        #    col = self.check_button(COLOR_BUTTON_X, COLOR_BUTTON_Y2,
-        #                            COLOR_BUTTON_INTERVAL, COLOR_BUTTON_COUNT)
-        #    if col is not None:
-        #        self._color = col + COLOR_BUTTON_COUNT
+        self._color_button.on_draw = on_draw
 
-        #tool = self.check_button(TOOL_BUTTON_X, TOOL_BUTTON_Y,
-        #                         TOOL_BUTTON_INTERVAL, TOOL_BUTTON_INTERVAL)
-        #if tool is not None:
-        #    self._tool = tool
+        self._tool_button = RadioButton(self, 81, 161, 7, 1, 9)
+        self._image_button = RadioButton(self, 191, 161, 3, 1, 10)
 
-        #img = self.check_button(IMAGE_BUTTON_X, IMAGE_BUTTON_Y,
-        #                        IMAGE_BUTTON_INTERVAL, IMAGE_BUTTON_COUNT)
-        #if img is not None:
-        #    self._img = img
-
-        #if pyxel.btn(pyxel.KEY_LEFT_BUTTON):
-        #    mx = pyxel.mouse_x
-        #    my = pyxel.mouse_y
-
-        #    if (mx >= CANVAS_X and mx < CANVAS_X + 8 * 16 and my >= CANVAS_Y
-        #            and my < CANVAS_Y + 8 * 16):
-        #        x = (mx - CANVAS_X) // 8
-        #        y = (my - CANVAS_Y) // 8
-        #        pyxel.image(self._img).data[y, x] = self._color
-
-    def on_draw(self):
-        super().on_draw()
-        #for i in range(16):
-        #    y = CANVAS_Y + i * 8
-        #    for j in range(16):
-        #        x = CANVAS_X + j * 8
-        #        col = pyxel.image(self._img).data[i, j]
-        #        pyxel.rect(x, y, x + 7, y + 7, col)
-
-        #pyxel.blt(PREVIEW_X, PREVIEW_Y, self._img, 0, 0, 64, 128)
-
-        #super().draw()
-
-        #if self._color < COLOR_BUTTON_COUNT:
-        #    x = COLOR_BUTTON_X + self._color * COLOR_BUTTON_INTERVAL
-        #    y = COLOR_BUTTON_Y1
-        #else:
-        #    x = COLOR_BUTTON_X + (
-        #        self._color - COLOR_BUTTON_COUNT) * COLOR_BUTTON_INTERVAL
-        #    y = COLOR_BUTTON_Y2
-        #col = 7 if self._color < 6 else 0
-        #pyxel.text(x + 2, y + 1, '+', col)
-
-        #self.draw_button(TOOL_BUTTON_X, TOOL_BUTTON_Y, TOOL_BUTTON_INTERVAL,
-        #                 self._tool)
-
-        #self.draw_button(IMAGE_BUTTON_X, IMAGE_BUTTON_Y, IMAGE_BUTTON_INTERVAL,
-        #                 self._img)
+        self._canvas_window = CanvasWindow(self)
+        self._preview_window = PreviewWindow(self)
