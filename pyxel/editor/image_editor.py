389201a39e1b20f0a240c5c87fa13eef9e955680
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-16 23:49:54 +0900

    Implemented scroll function

--- pyxel/editor/image_editor.py
+++ pyxel/editor/image_editor.py
@@ -5,49 +5,95 @@ from .screen import Screen
 from .widget import Widget
 
 
-class CanvasWindow(Widget):
+class EditWindow(Widget):
     def __init__(self, parent):
         super().__init__(parent, 12, 17, 128, 128)
+
+    def on_click(self, key, mx, my):
+        if key == pyxel.KEY_RIGHT_BUTTON:
+            img = self.parent._image_button.value
+            x = self.parent.edit_x + mx // 8
+            y = self.parent.edit_y + my // 8
+            self.parent._color_button.value = pyxel.image(img).data[y, x]
+
+    def on_drag(self, key, mx, my, dx, dy):
+        if key == pyxel.KEY_LEFT_BUTTON:
+            img = self.parent._image_button.value
+            x = self.parent.edit_x + mx // 8
+            y = self.parent.edit_y + my // 8
+            col = self.parent._color_button.value
+            pyxel.image(img).data[y, x] = col
 
     def on_draw(self):
         for i in range(16):
             y = self.y + i * 8
             for j in range(16):
                 x = self.x + j * 8
-                col = pyxel.image(self.parent._image_button.value).data[i, j]
+                data = pyxel.image(self.parent._image_button.value).data
+                col = data[self.parent.edit_y + i, self.parent.edit_x + j]
                 pyxel.rect(x, y, x + 7, y + 7, col)
 
         pyxel.line(self.x, self.y + 63, self.x + 127, self.y + 63, 1)
         pyxel.line(self.x + 63, self.y, self.x + 63, self.y + 127, 1)
-
-    def on_click(self, key, mx, my):
-        if key == pyxel.KEY_RIGHT_BUTTON:
-            img = self.parent._image_button.value
-            x = mx // 8
-            y = my // 8
-            self.parent._color_button.value = pyxel.image(img).data[y, x]
-
-    def on_drag(self, key, mx, my, dx, dy):
-        if key == pyxel.KEY_LEFT_BUTTON:
-            img = self.parent._image_button.value
-            x = mx // 8
-            y = my // 8
-            col = self.parent._color_button.value
-            pyxel.image(img).data[y, x] = col
 
 
 class PreviewWindow(Widget):
     def __init__(self, parent):
         super().__init__(parent, 158, 17, 64, 128)
 
+        self.preview_x = 0
+        self.preview_y = 0
+        self.offset_x = 0
+        self.offset_y = 0
+
+    def on_press(self, key, mx, my):
+        if key == pyxel.KEY_LEFT_BUTTON:
+            self.parent.edit_x = self.preview_x + ((mx - 4) // 8) * 8
+            self.parent.edit_y = self.preview_y + ((my - 4) // 8) * 8
+
+            self.parent.edit_x = min(max(self.parent.edit_x, 0), 240)
+            self.parent.edit_y = min(max(self.parent.edit_y, 0), 240)
+
+        if key == pyxel.KEY_RIGHT_BUTTON:
+            self.offset_x = 0
+            self.offset_y = 0
+
+    def on_drag(self, key, mx, my, dx, dy):
+        if key == pyxel.KEY_RIGHT_BUTTON:
+            self.offset_x -= dx
+            self.offset_y -= dy
+
+            if abs(self.offset_x) >= 8:
+                offset = (self.offset_x // 8) * 8
+                self.preview_x += offset
+                self.offset_x -= offset
+
+            if abs(self.offset_y) >= 8:
+                offset = (self.offset_y // 8) * 8
+                self.preview_y += offset
+                self.offset_y -= offset
+
+            self.preview_x = min(max(self.preview_x, 0), 192)
+            self.preview_y = min(max(self.preview_y, 0), 128)
+
     def on_draw(self):
         img = self.parent._image_button.value
-        pyxel.blt(self.x, self.y, img, 0, 0, 64, 128)
+        pyxel.blt(self.x, self.y, img, self.preview_x, self.preview_y, 64, 128)
+
+        pyxel.clip(self.x - 1, self.y - 1, self.x + self.width,
+                   self.y + self.height)
+        x = self.x + self.parent.edit_x - self.preview_x
+        y = self.y + self.parent.edit_y - self.preview_y
+        pyxel.rectb(x - 1, y - 1, x + 16, y + 16, 7)
+        pyxel.clip()
 
 
 class ImageEditor(Screen):
     def __init__(self, parent):
         super().__init__(parent, 'image_editor.png')
+
+        self.edit_x = 0
+        self.edit_y = 0
 
         self._color_button = RadioButton(self, 12, 157, 8, 2, 8)
 
@@ -59,9 +105,9 @@ class ImageEditor(Screen):
             pyxel.text(x + 2, y + 1, '+', col)
 
         self._color_button.on_draw = on_draw
+        self._color_button.value = 7
 
         self._tool_button = RadioButton(self, 81, 161, 7, 1, 9)
         self._image_button = RadioButton(self, 191, 161, 3, 1, 10)
-
-        self._canvas_window = CanvasWindow(self)
+        self._canvas_window = EditWindow(self)
         self._preview_window = PreviewWindow(self)
