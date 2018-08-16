3486d96b635f122906735c96215cf685479d3984
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-16 23:48:44 +0900

    Added the button class

---
+++ pyxel/editor/button.py
@@ -0,0 +1,17 @@
+import pyxel
+
+from .widget import Widget
+
+
+class RadioButton(Widget):
+    def __init__(self, parent, x, y, width, height):
+        super().__init__(parent, x, y, width, height)
+
+    def on_draw(self):
+        pyxel.pal(13, 7)
+
+        x = self.x + self._interval * (self.value % self._col)
+        y = self.y + self._interval * (self.value // self._col)
+        pyxel.blt(x, y, 3, x, y + 16, self.width, self.height)
+
+        pyxel.pal()
