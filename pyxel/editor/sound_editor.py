d47d6139271dc776f8ea3d1f056b91909bc8faa1
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-22 07:20:02 +0900

    Added the "not implemented yet" message to the resource editor

--- pyxel/editor/sound_editor.py
+++ pyxel/editor/sound_editor.py
@@ -4,3 +4,5 @@ from .screen import Screen
 class SoundEditor(Screen):
     def __init__(self, parent):
         super().__init__(parent, 'sound_editor.png')
+
+        self.add_event_handler('draw', self.draw_not_implemented_message)
--- pyxel/editor/tilemap_editor.py
+++ pyxel/editor/tilemap_editor.py
@@ -4,3 +4,5 @@ from .screen import Screen
 class TileMapEditor(Screen):
     def __init__(self, parent):
         super().__init__(parent, 'tilemap_editor.png')
+
+        self.add_event_handler('draw', self.draw_not_implemented_message)
