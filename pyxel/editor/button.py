38b7d4efb72f8d17e2a6af2f6592e53a32edccfe
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-25 18:19:24 +0900

    Implemented the button widget

--- pyxel/editor/button.py
+++ pyxel/editor/button.py
@@ -1,19 +1,33 @@
 import pyxel
 
+from .editor_constants import BUTTON_BLINK_TIME
 from .widget import Widget
 
 
-class RadioButton(Widget):
+class Button(Widget):
     def __init__(self, parent, x, y, width, height):
         super().__init__(parent, x, y, width, height)
 
+        self._blink_time = 0
+
+        self.add_event_handler('press', self.on_press)
+        self.add_event_handler('update', self.on_update)
         self.add_event_handler('draw', self.on_draw)
 
+    def on_press(self, key, x, y):
+        if key == pyxel.KEY_LEFT_BUTTON:
+            self._blink_time = BUTTON_BLINK_TIME + 1
+
+    def on_update(self):
+        if self._blink_time > 0:
+            self._blink_time -= 1
+
     def on_draw(self):
-        pyxel.pal(13, 7)
+        if self._blink_time > 0:
+            pyxel.pal(13, 7)
 
-        x = self.x + self._interval * (self.value % self._col)
-        y = self.y + self._interval * (self.value // self._col)
-        pyxel.blt(x, y, 3, x, y + 16, self.width, self.height)
+            x = self.x
+            y = self.y
+            pyxel.blt(x, y, 3, x, y + 16, self.width, self.height)
 
-        pyxel.pal()
+            pyxel.pal()
