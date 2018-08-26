e08a64615f210bb92cc0f2ce6557d410dd081ec0
Takashi Kitao <takashi.kitao@gmail.com>
2018-08-26 15:14:30 +0900

    Added static methods for event processing

--- pyxel/editor/widget.py
+++ pyxel/editor/widget.py
@@ -17,13 +17,30 @@ class Widget:
         self.parent = parent
         self.children = []
         self._event_handler = {}
+        self.is_enabled = True
         self._is_visible = False
 
         if is_visible:
-            self.set_visible(True)
+            self.is_visible = True
 
         if parent:
             parent.children.append(self)
+
+    @property
+    def is_visible(self):
+        return self._is_visible
+
+    @is_visible.setter
+    def is_visible(self, value):
+        if self._is_visible == value:
+            return
+
+        self._is_visible = value
+
+        if value:
+            self.call_event_handler('show')
+        else:
+            self.call_event_handler('hide')
 
     def _capture_mouse(self, key):
         Widget._capture_widget = self
@@ -38,17 +55,6 @@ class Widget:
         Widget._capture_time = None
         Widget._capture_press_pos = None
         Widget._capture_last_pos = None
-
-    def set_visible(self, is_visible):
-        if self._is_visible == is_visible:
-            return
-
-        self._is_visible = is_visible
-
-        if is_visible:
-            self.call_event_handler('show')
-        else:
-            self.call_event_handler('hide')
 
     def _get_event_handler(self, event):
         if event not in self._event_handler:
@@ -66,82 +72,89 @@ class Widget:
         for handler in self._get_event_handler(event):
             handler(*args)
 
-    def process_input(self):
-        if not self._is_visible:
-            return False
+    @staticmethod
+    def process_input(root):
+        capturer = Widget._capture_widget
 
-        widget = Widget._capture_widget
-
-        if widget:
+        if capturer:
             mx = pyxel.mouse_x
             my = pyxel.mouse_y
             last_mx, last_my = Widget._capture_last_pos
 
             if mx != last_mx or my != last_my:
-                widget.call_event_handler('drag', Widget._capture_key,
-                                          mx - widget.x, my - widget.y,
-                                          mx - last_mx, my - last_my)
+                capturer.call_event_handler('drag', Widget._capture_key,
+                                            mx - capturer.x, my - capturer.y,
+                                            mx - last_mx, my - last_my)
                 Widget._capture_last_pos = (mx, my)
 
             if pyxel.btnr(Widget._capture_key):
-                widget.call_event_handler('release', Widget._capture_key,
-                                          mx - widget.x, my - widget.y)
+                capturer.call_event_handler('release', Widget._capture_key,
+                                            mx - capturer.x, my - capturer.y)
 
                 press_x, press_y = Widget._capture_press_pos
                 if (pyxel.frame_count <=
                         Widget._capture_time + WIDGET_CLICK_TIME
                         and abs(pyxel.mouse_x - press_x) <= WIDGET_CLICK_DIST
                         and abs(pyxel.mouse_y - press_y) <= WIDGET_CLICK_DIST):
-                    widget.call_event_handler('click', Widget._capture_key,
-                                              mx - widget.x, my - widget.y)
+                    capturer.call_event_handler('click', Widget._capture_key,
+                                                mx - capturer.x,
+                                                my - capturer.y)
 
-                self._release_mouse()
+                root._release_mouse()
 
-            return True
+        root._process_input()
 
-        for widget in reversed(self.children):
-            if widget.process_input():
-                return True
+    def _process_input(self):
+        if not self._is_visible:
+            return False
+
+        if self.is_enabled:
+            for widget in reversed(self.children):
+                if widget._process_input():
+                    return True
 
         mx = pyxel.mouse_x
         my = pyxel.mouse_y
 
         if (mx >= self.x and mx < self.x + self.width and my >= self.y
                 and my < self.y + self.height):
-            key = None
-            if pyxel.btnp(pyxel.KEY_LEFT_BUTTON):
-                key = pyxel.KEY_LEFT_BUTTON
-            elif pyxel.btnp(pyxel.KEY_RIGHT_BUTTON):
-                key = pyxel.KEY_RIGHT_BUTTON
+            if self.is_enabled:
+                key = None
+                if pyxel.btnp(pyxel.KEY_LEFT_BUTTON):
+                    key = pyxel.KEY_LEFT_BUTTON
+                elif pyxel.btnp(pyxel.KEY_RIGHT_BUTTON):
+                    key = pyxel.KEY_RIGHT_BUTTON
 
-            if key is not None:
-                self._capture_mouse(key)
-                x = mx - self.x
-                y = my - self.y
-                self.call_event_handler('press', key, x, y)
-                return True
+                if key is not None:
+                    self._capture_mouse(key)
+                    x = mx - self.x
+                    y = my - self.y
+                    self.call_event_handler('press', key, x, y)
+                    return True
 
             self.call_event_handler('hover', mx - self.x, my - self.y)
 
         return False
 
-    def update(self):
-        if not self._is_visible:
+    @staticmethod
+    def update(root):
+        if not root._is_visible:
             return
 
-        self.call_event_handler('update')
+        root.call_event_handler('update')
 
-        for widget in self.children:
-            widget.update()
+        for child in root.children:
+            Widget.update(child)
 
-    def draw(self):
-        if not self._is_visible:
+    @staticmethod
+    def draw(root):
+        if not root._is_visible:
             return
 
-        self.call_event_handler('draw')
+        root.call_event_handler('draw')
 
-        for widget in self.children:
-            widget.draw()
+        for child in root.children:
+            Widget.draw(child)
 
     def on_show(self):
         pass
