c0ea5e11b0c78a530fdc00e2d457051c794f634d
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-15 15:47:05 +0900

    Formatted the code with black

--- pyxel/editor/console.py
+++ pyxel/editor/console.py
@@ -3,6 +3,6 @@ from .screen import Screen
 
 class Console(Screen):
     def __init__(self, parent):
-        super().__init__(parent, 'console.png')
+        super().__init__(parent, "console.png")
 
-        self.add_event_handler('draw', self.draw_not_implemented_message)
+        self.add_event_handler("draw", self.draw_not_implemented_message)
