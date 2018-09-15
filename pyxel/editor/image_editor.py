19723817ff405929ed61f3114303a92ea513735e
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-15 16:19:25 +0900

    Formatted the code with black

--- pyxel/editor/image_editor.py
+++ pyxel/editor/image_editor.py
@@ -3,7 +3,6 @@ import numpy as np
 import pyxel
 from pyxel.constants import RENDERER_IMAGE_COUNT
 
-from .draw_window import DrawWindow
 from .radio_button import RadioButton
 from .screen import Screen
 from .scroll_bar import ScrollBar
@@ -18,9 +17,9 @@ TOOL_CIRC = 5
 TOOL_BUCKET = 6
 
 
-class EditWindow(DrawWindow):
+class EditWindow(Widget):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, 12, 17, 128, 128)
 
         self.edit_x = 0
         self.edit_y = 0
@@ -49,6 +48,59 @@ class EditWindow(DrawWindow):
         self.add_event_handler("drag", self.on_drag)
         self.add_event_handler("update", self.on_update)
         self.add_event_handler("draw", self.on_draw)
+
+        self.overlay = np.ndarray((16, 16), np.int8)
+        self.clear_overlay()
+
+    def clear_overlay(self):
+        self.overlay[:, :] = -1
+
+    def draw_line_on_overlay(self, x1, y1, x2, y2, val):
+        if x1 == x2 and y1 == y2:
+            if x1 >= 0 and x1 < 16 and y1 >= 0 and y1 < 16:
+                self.overlay[y1, x1] = val
+            return
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        if abs(dx) > abs(dy):
+            if dx < 0:
+                x1, y1 = x2, y2
+                dx, dy = -dx, -dy
+
+            for i in range(dx + 1):
+                x = x1 + i
+                y = int(y1 + i * dy / dx + 0.5)
+
+                if x >= 0 and x < 16 and y >= 0 and y < 16:
+                    self.overlay[y, x] = val
+        else:
+            if dy < 0:
+                x1, y1 = x2, y2
+                dx, dy = -dx, -dy
+
+            for i in range(dy + 1):
+                x = int(x1 + i * dx / dy + 0.5)
+                y = y1 + i
+
+                if x >= 0 and x < 16 and y >= 0 and y < 16:
+                    self.overlay[y, x] = val
+
+    def draw_rectb_on_overlay(self, x1, y1, x2, y2, val):
+        self.overlay[y1 : y1 + 1, x1 : x2 + 1] = val
+        self.overlay[y2 : y2 + 1, x1 : x2 + 1] = val
+        self.overlay[y1 : y2 + 1, x1 : x1 + 1] = val
+        self.overlay[y1 : y2 + 1, x2 : x2 + 1] = val
+
+    def draw_rect_on_overlay(self, x1, y1, x2, y2, val):
+        self.overlay[y1 : y2 + 1, x1 : x2 + 1] = val
+
+    def draw_circb_on_overlay(self, x1, y1, x2, y2, val):
+        pass
+
+    def draw_circ_on_overlay(self, x1, y1, x2, y2, val):
+        pass
 
     def on_change_x(self, value):
         self.edit_x = value * 8
@@ -272,7 +324,7 @@ class PreviewWindow(Widget):
 
 
 class ImageEditor(Screen):
-    def __init__(self, parent):
+    def __init__(self, parent, is_tilemap_mode=False):
         super().__init__(parent, "image_editor.png")
 
         def on_draw():
