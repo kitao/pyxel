57daa4fdd6be886e7acbe89814b691041cdffaa8
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-23 20:03:46 +0900

    Removed an unnecessary option

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -8,24 +8,15 @@ from pyxel.editor.editor_app import EditorApp
 def run():
     parser = argparse.ArgumentParser(
         prog="pyxel",
-        usage="pyxel resource_file [-h] [-w app_file]",
+        usage="pyxel resource_file [-h]",
         description="Pyxel Resource Editor {}".format(pyxel.VERSION),
         add_help=True,
     )
     parser.add_argument("resource_file", help="Pyxel resource file")
-    parser.add_argument(
-        "-w",
-        "--watch",
-        help="Pyxel app file to be watched",
-        metavar="app_file",
-        dest="app_file",
-    )
-
     args = parser.parse_args()
-
     EditorApp(args.resource_file, args.app_file)
 
 
 if __name__ == "__main__":
-    sys.argv[1:] = ["test_resource", "-w", "test_app"]
+    sys.argv[1:] = ["test_resource"]
     run()
