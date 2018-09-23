054d5f2692c8029d80ced1d5fa7b84cec360ad29
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-23 18:52:30 +0900

    Removed the console screen

--- pyxel/editor/editor_app.py
+++ pyxel/editor/editor_app.py
@@ -1,7 +1,6 @@
 import pyxel
 from pyxel.ui import Widget
 
-from .console import Console
 from .editor_button import EditorButton
 from .editor_constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from .editor_radio_button import EditorRadioButton
@@ -13,29 +12,33 @@ from .tilemap_editor import TileMapEditor
 
 class EditorApp:
     def __init__(self, resoure_file, app_file):
-        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, caption="pyxel")
+        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, caption="Pyxel Resource Editor")
 
         self._root_widget = Widget(None, 0, 0, 0, 0)
 
         self._screen_list = [
-            Console(self._root_widget),
             ImageEditor(self._root_widget),
             TileMapEditor(self._root_widget),
             SoundEditor(self._root_widget),
             MusicEditor(self._root_widget),
         ]
 
-        self._screen_button = EditorRadioButton(self._root_widget, 3, 1, 5, 1, 2)
+        self._screen_button = EditorRadioButton(self._root_widget, 3, 1, 4, 1, 2)
         self._screen_button.add_event_handler(
             "change", lambda value: self.set_screen(value)
         )
-        self.set_screen(1)
+        self.set_screen(0)
 
-        self._undo_button = EditorButton(self._root_widget, 57, 1, 7, 7)
-        self._undo_button.add_event_handler("press", self.on_undo_press)
+        self._undo_button = EditorButton(self._root_widget, 48, 1, 7, 7)
+        self._undo_button.add_event_handler("press", self.__on_undo_press)
 
-        self._redo_button = EditorButton(self._root_widget, 66, 1, 7, 7)
-        self._redo_button.add_event_handler("press", self.on_redo_press)
+        self._redo_button = EditorButton(self._root_widget, 57, 1, 7, 7)
+        self._redo_button.add_event_handler("press", self.__on_redo_press)
+
+        self._save_button = EditorButton(
+            self._root_widget, 75, 1, 7, 7, is_enabled=False
+        )
+        self._save_button.add_event_handler("press", self.__on_save_press)
 
         pyxel.run(self.update, self.draw)
 
@@ -44,12 +47,6 @@ class EditorApp:
 
         for i, widget in enumerate(self._screen_list):
             widget.is_visible = i == screen
-
-    def on_undo_press(self):
-        self._screen_list[self._screen_button.value].undo()
-
-    def on_redo_press(self):
-        self._screen_list[self._screen_button.value].redo()
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
@@ -79,3 +76,12 @@ class EditorApp:
     def draw(self):
         pyxel.cls(6)
         Widget.draw(self._root_widget)
+
+    def __on_undo_press(self):
+        self._screen_list[self._screen_button.value].undo()
+
+    def __on_redo_press(self):
+        self._screen_list[self._screen_button.value].redo()
+
+    def __on_save_press(self):
+        pass
