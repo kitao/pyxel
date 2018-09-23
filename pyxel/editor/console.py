773271ca7fb9ba2e8cb421307f4b7153a61950db
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-23 18:52:30 +0900

    Removed the console screen

--- pyxel/editor/console.py
+++
@@ -1,8 +0,0 @@
-from .screen import Screen
-
-
-class Console(Screen):
-    def __init__(self, parent):
-        super().__init__(parent, "console.png")
-
-        self.add_event_handler("draw", self.draw_not_implemented_message)
