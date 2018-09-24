f40e49d1566d79383118f81fc7a5b86171aeac42
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-24 09:24:36 +0900

    Change to show help when no argument specified

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -7,12 +7,17 @@ from pyxel.editor.editor_app import EditorApp
 
 def run():
     parser = argparse.ArgumentParser(
-        prog="pyxel",
-        usage="pyxel resource_file [-h]",
+        prog="pyxeleditor",
+        usage="pyxeleditor resource_file [-h]",
         description="Pyxel Editor {}".format(pyxel.VERSION),
         add_help=True,
     )
-    parser.add_argument("resource_file", help="Pyxel resource file")
+    parser.add_argument("resource_file", help="Pyxel resource file (.pyxel)")
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
     args = parser.parse_args()
     EditorApp(args.resource_file)
 
