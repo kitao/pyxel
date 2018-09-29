11271118fe26767d7e1adf6b65c079155ab1581c
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-29 15:54:53 +0900

    Added the number picker

--- pyxel/ui/__init__.py
+++ pyxel/ui/__init__.py
@@ -1,4 +1,5 @@
 from .button import Button  # noqa: F401
+from .number_picker import NumberPicker  # noqa: F401
 from .radio_button import RadioButton  # noqa: F401
 from .scroll_bar import ScrollBar  # noqa: F401
 from .widget import Widget  # noqa: F401
