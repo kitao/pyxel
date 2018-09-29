c8daeb3e19e761c2b6bbe49bfc1119ba82e2418e
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-29 21:47:44 +0900

    Splitted the selece fame class

--- pyxel/editor/image_frame.py
+++ pyxel/editor/image_frame.py
@@ -4,17 +4,18 @@ from pyxel.ui import Widget
 from .editor_scroll_bar import EditorScrollBar
 
 
-class SelectFrame(Widget):
+class ImageFrame(Widget):
     def __init__(self, parent, *, is_tilemap_mode):
-        super().__init__(parent, 158, 17, 64, 128)
+        y, height = (81, 64) if is_tilemap_mode else (17, 128)
+        super().__init__(parent, 158, y, 64, height)
 
         self._is_tilemap_mode = is_tilemap_mode
 
+        self.viewport_x = 0
+        self.viewport_y = 0
+
         self.select_x = 0
         self.select_y = 0
-
-        self.cursor_x = 0
-        self.cursor_y = 0
 
         self._drag_offset_x = 0
         self._drag_offset_y = 0
@@ -22,7 +23,11 @@ class SelectFrame(Widget):
         self._h_scroll_bar = EditorScrollBar(self, 157, 145, 66, 7, 32, 8)
         self._h_scroll_bar.add_event_handler("change", self.__on_change_x)
 
-        self._v_scroll_bar = EditorScrollBar(self, 222, 16, 7, 130, 32, 16)
+        if is_tilemap_mode:
+            self._v_scroll_bar = EditorScrollBar(self, 222, 80, 7, 66, 32, 8)
+        else:
+            self._v_scroll_bar = EditorScrollBar(self, 222, 16, 7, 130, 32, 16)
+
         self._v_scroll_bar.add_event_handler("change", self.__on_change_y)
 
         self.add_event_handler("mouse_down", self.__on_mouse_down)
@@ -31,10 +36,10 @@ class SelectFrame(Widget):
         self.add_event_handler("draw", self.__on_draw)
 
     def __on_change_x(self, value):
-        self.select_x = value * 8
+        self.viewport_x = value * 8
 
     def __on_change_y(self, value):
-        self.select_y = value * 8
+        self.viewport_y = value * 8
 
     def __on_mouse_down(self, key, x, y):
         if key == pyxel.KEY_LEFT_BUTTON:
@@ -42,14 +47,14 @@ class SelectFrame(Widget):
             y -= self.y
 
             if self._is_tilemap_mode:
-                self.cursor_x = min(max(self.select_x + (x // 8) * 8, 0), 248)
-                self.cursor_y = min(max(self.select_y + (y // 8) * 8, 0), 248)
+                self.select_x = min(max(self.viewport_x + (x // 8) * 8, 0), 248)
+                self.select_y = min(max(self.viewport_y + (y // 8) * 8, 0), 248)
             else:
-                self.cursor_x = min(max(self.select_x + ((x - 4) // 8) * 8, 0), 240)
-                self.cursor_y = min(max(self.select_y + ((y - 4) // 8) * 8, 0), 240)
+                self.select_x = min(max(self.viewport_x + ((x - 4) // 8) * 8, 0), 240)
+                self.select_y = min(max(self.viewport_y + ((y - 4) // 8) * 8, 0), 240)
 
-                self.parent.edit_x = self.cursor_x
-                self.parent.edit_y = self.cursor_y
+                self.parent.edit_x = self.select_x
+                self.parent.edit_y = self.select_y
 
         if key == pyxel.KEY_RIGHT_BUTTON:
             self._drag_offset_x = 0
@@ -57,34 +62,43 @@ class SelectFrame(Widget):
 
     def __on_mouse_drag(self, key, x, y, dx, dy):
         if key == pyxel.KEY_RIGHT_BUTTON:
-            self._drag_offset_x -= dx * 2
-            self._drag_offset_y -= dy * 2
+            self._drag_offset_x -= dx
+            self._drag_offset_y -= dy
 
             if abs(self._drag_offset_x) >= 8:
                 offset = (self._drag_offset_x // 8) * 8
-                self.select_x += offset
                 self._drag_offset_x -= offset
+                self.viewport_x += offset
 
             if abs(self._drag_offset_y) >= 8:
                 offset = (self._drag_offset_y // 8) * 8
-                self.select_y += offset
                 self._drag_offset_y -= offset
+                self.viewport_y += offset
 
-            self.select_x = min(max(self.select_x, 0), 192)
-            self.select_y = min(max(self.select_y, 0), 128)
+            self.viewport_x = min(max(self.viewport_x, 0), 192)
+            self.viewport_y = min(
+                max(self.viewport_y, 0), 192 if self._is_tilemap_mode else 128
+            )
 
     def __on_update(self):
-        self._h_scroll_bar.value = self.select_x // 8
-        self._v_scroll_bar.value = self.select_y // 8
+        self._h_scroll_bar.value = self.viewport_x // 8
+        self._v_scroll_bar.value = self.viewport_y // 8
 
     def __on_draw(self):
         pyxel.blt(
-            self.x, self.y, self.parent.image, self.select_x, self.select_y, 64, 128
+            self.x,
+            self.y,
+            self.parent.image,
+            self.viewport_x,
+            self.viewport_y,
+            self.width,
+            self.height,
         )
 
         pyxel.clip(self.x - 1, self.y - 1, self.x + self.width, self.y + self.height)
-        x = self.x + self.cursor_x - self.select_x
-        y = self.y + self.cursor_y - self.select_y
+
+        x = self.x + self.select_x - self.viewport_x
+        y = self.y + self.select_y - self.viewport_y
 
         if self._is_tilemap_mode:
             pyxel.rectb(x - 1, y - 1, x + 8, y + 8, 7)
---
+++ pyxel/editor/tilemap_frame.py
@@ -0,0 +1,26 @@
+import pyxel
+from pyxel.ui import Widget
+
+
+class TilemapFrame(Widget):
+    def __init__(self, parent):
+        super().__init__(parent, 158, 17, 64, 63)
+
+        self.add_event_handler("mouse_down", self.__on_mouse_down)
+        self.add_event_handler("draw", self.__on_draw)
+
+    def __on_mouse_down(self, key, x, y):
+        if key == pyxel.KEY_LEFT_BUTTON:
+            x -= self.x
+            y -= self.y
+
+            self.parent.edit_x = min(max((x - 1) // 2, 0), 30) * 8
+            self.parent.edit_y = min(max((y - 1) // 2, 0), 30) * 8
+
+    def __on_draw(self):
+        pyxel.blt(self.x, self.y, 3, self.parent.tilemap * 64, 192, 64, 63)
+
+        x = self.x + self.parent.edit_x // 4
+        y = self.y + self.parent.edit_y // 4
+        height = 4 if self.parent.edit_y < 240 else 3
+        pyxel.rectb(x - 1, y - 1, x + 4, y + height, 7)
