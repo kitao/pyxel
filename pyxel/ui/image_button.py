6ca16118db3a9f5fb68ce7e4d8b3297209886025
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-30 15:44:36 +0900

    Added standard widgets

---
+++ pyxel/ui/image_button.py
@@ -0,0 +1,38 @@
+import pyxel
+
+from .button import Button
+from .ui_constants import (
+    BUTTON_DISABLED_COLOR,
+    BUTTON_ENABLED_COLOR,
+    BUTTON_PRESSED_COLOR,
+)
+
+
+class ImageButton(Button):
+    """
+    Events:
+        __on_press()
+        __on_release()
+    """
+
+    def __init__(self, parent, x, y, img, sx, sy, w, h, **kwargs):
+        super().__init__(parent, x, y, w, h, **kwargs)
+
+        self._img = img
+        self._sx = sx
+        self._sy = sy
+
+        self.add_event_handler("draw", self.__on_draw)
+
+    def __on_draw(self):
+        color = (
+            BUTTON_PRESSED_COLOR
+            if self.is_pressed
+            else (BUTTON_ENABLED_COLOR if self.is_enabled else BUTTON_DISABLED_COLOR)
+        )
+
+        pyxel.pal(BUTTON_ENABLED_COLOR, color)
+        pyxel.blt(
+            self.x, self.y, self._img, self._sx, self._sy, self.width, self.height, 0
+        )
+        pyxel.pal()
