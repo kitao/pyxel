fb5ef8011ea0192afa4699fb80e79ecaae538b17
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-30 21:50:38 +0900

    Changed the Pyxel Editor usage

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 
 import pyxel
@@ -6,20 +5,14 @@ from pyxel.editor.editor_app import EditorApp
 
 
 def run():
-    parser = argparse.ArgumentParser(
-        prog="pyxeleditor",
-        usage="pyxeleditor resource_file [-h]",
-        description="Pyxel Editor {}".format(pyxel.VERSION),
-        add_help=True,
-    )
-    parser.add_argument("resource_file", help="Pyxel resource file (.pyxel)")
-
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit()
-
-    args = parser.parse_args()
-    EditorApp(args.resource_file)
+    if len(sys.argv) == 2:
+        EditorApp(sys.argv[1])
+    else:
+        print("usage: pyxeleditor pyxe_resource_file")
+        print("\n")
+        print("Pyxel Editor {}".format(pyxel.VERSION))
+        print("Please specify a Pyxel resource file (.pyxel)")
+        print("e.g. pyxeleditor my_resource.pyxel")
 
 
 if __name__ == "__main__":
