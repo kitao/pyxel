83a7152b18b3a4c4316b7a85cb000ddaa0127a4e
Takashi Kitao <takashi.kitao@gmail.com>
2018-09-30 15:45:03 +0900

    Added standard widgets

--- pyxel/ui/__init__.py
+++ pyxel/ui/__init__.py
@@ -1,5 +1,8 @@
 from .button import Button  # noqa: F401
+from .color_picker import ColorPicker  # noqa: F401
+from .image_button import ImageButton  # noqa: F401
 from .number_picker import NumberPicker  # noqa: F401
 from .radio_button import RadioButton  # noqa: F401
 from .scroll_bar import ScrollBar  # noqa: F401
+from .text_button import TextButton  # noqa: F401
 from .widget import Widget  # noqa: F401
