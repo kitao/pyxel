95b71843af55d7f716824bf93008796b2da0b300
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-02 23:47:04 +0900

    Added standard widgets

---
+++ pyxel/ui/image_label.py
@@ -0,0 +1,27 @@
+import pyxel
+
+from .widget import Widget
+
+
+class ImageLabel(Widget):
+    def __init__(self, parent, x, y, img, sx, sy, w, h, colkey=None, **kwargs):
+        super().__init__(parent, x, y, w, h, **kwargs)
+
+        self._img = img
+        self._sx = sx
+        self._sy = sy
+        self._colkey = colkey
+
+        self.add_event_handler("draw", self.__on_draw)
+
+    def __on_draw(self):
+        pyxel.blt(
+            self._x,
+            self._y,
+            self._img,
+            self._sx,
+            self._sy,
+            self._width,
+            self.height,
+            self._colkey,
+        )
