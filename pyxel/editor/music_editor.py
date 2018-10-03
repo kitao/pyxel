bc3bdef81ea7b89207e41fe2fd710999d1583376
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-03 23:14:40 +0900

    Changed to use standard widgets

--- pyxel/editor/music_editor.py
+++ pyxel/editor/music_editor.py
@@ -1,3 +1,5 @@
+import pyxel
+
 from .editor import Editor
 
 
@@ -5,4 +7,14 @@ class MusicEditor(Editor):
     def __init__(self, parent):
         super().__init__(parent, "music_editor.png")
 
+        self.add_event_handler("draw", self.__on_draw)
         self.add_event_handler("draw", self.draw_not_implemented_message)
+
+    def __on_draw(self):
+        self.draw_frame(11, 16, 228, 24)
+        pyxel.text(23, 18, "MUSIC", 6)
+
+        for i in range(4):
+            x = i * 59 + 11
+            self.draw_frame(x, 30, x + 40, 172)
+            pyxel.text(x + 15, 32, "CH{}".format(i), 6)
--- pyxel/editor/sound_editor.py
+++ pyxel/editor/sound_editor.py
@@ -1,3 +1,5 @@
+import pyxel
+
 from .editor import Editor
 
 
@@ -5,4 +7,10 @@ class SoundEditor(Editor):
     def __init__(self, parent):
         super().__init__(parent, "sound_editor.png")
 
+        self.add_event_handler("draw", self.__on_draw)
         self.add_event_handler("draw", self.draw_not_implemented_message)
+
+    def __on_draw(self):
+        self.draw_frame(11, 16, 228, 172)
+        pyxel.text(23, 18, "SOUND", 6)
+        pyxel.text(83, 18, "SPEED", 6)
