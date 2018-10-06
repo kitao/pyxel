496c2abdb01d3a52c8bd162b0162fdce46460a06
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-06 13:18:07 +0900

    Add help messages

--- pyxel/ui/radio_button.py
+++ pyxel/ui/radio_button.py
@@ -20,9 +20,11 @@ class RadioButton(Widget):
         self._sy = sy
         self._btn_count = btn_count
         self._value = None
+        self._hover_value = None
 
         self.add_event_handler("mouse_down", self.__on_mouse_down)
         self.add_event_handler("mouse_drag", self.__on_mouse_drag)
+        self.add_event_handler("mouse_hover", self.__on_mouse_hover)
         self.add_event_handler("draw", self.__on_draw)
 
         self.value = value
@@ -36,6 +38,10 @@ class RadioButton(Widget):
         if self._value != value:
             self._value = value
             self.call_event_handler("change", value)
+
+    @property
+    def hover_value(self):
+        return self._hover_value
 
     def __on_mouse_down(self, key, x, y):
         if key != pyxel.KEY_LEFT_BUTTON:
@@ -56,6 +62,23 @@ class RadioButton(Widget):
 
     def __on_mouse_drag(self, key, x, y, dx, dy):
         self.__on_mouse_down(key, x, y)
+
+    def __on_mouse_hover(self, x, y):
+        # TODO
+        x -= self.x
+        y -= self.y
+
+        index = min(max(x // 9, 0), self._btn_count - 1)
+
+        x1 = index * 9
+        y1 = 0
+        x2 = x1 + 6
+        y2 = y1 + 6
+
+        if x >= x1 and x <= x2 and y >= y1 and y <= y2:
+            self._hover_value = index
+        else:
+            self._hover_value = None
 
     def __on_draw(self):
         pyxel.blt(
