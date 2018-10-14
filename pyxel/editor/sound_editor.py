84a546a9dc6c3b44d60c96c8eab859f1bdc1de8b
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-14 14:24:09 +0900

    Modified the design of the sound editor

--- pyxel/editor/sound_editor.py
+++ pyxel/editor/sound_editor.py
@@ -1,7 +1,6 @@
 import pyxel
 from pyxel.constants import AUDIO_SOUND_COUNT
-from pyxel.ui import NumberPicker, ScrollBar
-from pyxel.ui.constants import WIDGET_FRAME_COLOR
+from pyxel.ui import ImageButton, NumberPicker
 
 from .constants import EDITOR_IMAGE_X, EDITOR_IMAGE_Y
 from .editor import Editor
@@ -14,11 +13,14 @@ class SoundEditor(Editor):
         self._sound_picker = NumberPicker(self, 45, 17, 0, AUDIO_SOUND_COUNT - 1, 0)
         self._speed_picker = NumberPicker(self, 105, 17, 1, 99, 0)
 
-        self._v_scroll_bar = ScrollBar(
-            self, 222, 24, 119, ScrollBar.VERTICAL, 239, 117, 60, with_shadow=False
+        self._play_button = ImageButton(
+            self, 185, 17, 3, EDITOR_IMAGE_X + 126, EDITOR_IMAGE_Y
         )
-        self._h_scroll_bar = ScrollBar(
-            self, 30, 166, 193, ScrollBar.HORIZONTAL, 8, 4, 0, with_shadow=False
+        self._stop_button = ImageButton(
+            self, 195, 17, 3, EDITOR_IMAGE_X + 135, EDITOR_IMAGE_Y
+        )
+        self._loop_button = ImageButton(
+            self, 205, 17, 3, EDITOR_IMAGE_X + 144, EDITOR_IMAGE_Y
         )
 
         self.add_event_handler("draw", self.__on_draw)
@@ -36,37 +38,27 @@ class SoundEditor(Editor):
         pyxel.text(23, 18, "SOUND", 6)
         pyxel.text(83, 18, "SPEED", 6)
 
-        offset = (-self._v_scroll_bar.value) % 48 - 48
-
-        pyxel.clip(12, 25, 221, 141)
-
-        pyxel.blt(12, offset + 25, 3, EDITOR_IMAGE_X, EDITOR_IMAGE_Y + 8, 115, 96)
-        pyxel.blt(127, offset + 25, 3, EDITOR_IMAGE_X + 19, EDITOR_IMAGE_Y + 8, 96, 96)
-        pyxel.blt(12, offset + 121, 3, EDITOR_IMAGE_X, EDITOR_IMAGE_Y + 8, 115, 96)
-        pyxel.blt(127, offset + 121, 3, EDITOR_IMAGE_X + 19, EDITOR_IMAGE_Y + 8, 96, 96)
+        pyxel.blt(17, 25, 3, EDITOR_IMAGE_X, EDITOR_IMAGE_Y + 8, 109, 147)
+        pyxel.blt(126, 25, 3, EDITOR_IMAGE_X + 13, EDITOR_IMAGE_Y + 8, -97, 147)
+        pyxel.rect(12, 25, 15, 147, 6)
+        pyxel.rect(224, 25, 227, 147, 6)
 
         sound = pyxel.sound(self.sound)
-        offset_x = self._h_scroll_bar.value * 8
 
-        for i, note in enumerate(sound.note[offset_x : offset_x + 32]):
-            x = i * 6 + 31
-            y = 261 - note * 4 - self._v_scroll_bar.value
-            pyxel.rect(x + 1, y, x + 4, y + 2, 2)
+        for i, note in enumerate(sound.note):
+            x = i * 4 + 31
+            y = 143 - note * 2
+            pyxel.rect(x, y, x + 2, y + 2, 8 if note >= 0 else 12)
 
-        pyxel.clip()
+        pyxel.text(17, 150, "TON", 6)
+        pyxel.text(17, 158, "VOL", 6)
+        pyxel.text(17, 166, "EFX", 6)
 
-        pyxel.blt(31, 143, 3, EDITOR_IMAGE_X + 19, EDITOR_IMAGE_Y + 104, 96, 23)
-        pyxel.blt(127, 143, 3, EDITOR_IMAGE_X + 19, EDITOR_IMAGE_Y + 104, 95, 23)
+        for i, tone in enumerate(sound.tone):
+            pyxel.text(31 + i * 4, 150, "TSPN"[tone], 1)
 
-        pyxel.text(17, 144, "TON", 6)
-        pyxel.text(17, 152, "VOL", 6)
-        pyxel.text(17, 160, "EFX", 6)
+        for i, volume in enumerate(sound.volume):
+            pyxel.text(31 + i * 4, 158, str(volume), 1)
 
-        for i, tone in enumerate(sound.tone[offset_x : offset_x + 32]):
-            pyxel.text(32 + i * 6, 144, "TSPN"[tone], 1)
-
-        for i, volume in enumerate(sound.volume[offset_x : offset_x + 32]):
-            pyxel.text(32 + i * 6, 152, str(volume), 1)
-
-        for i, effect in enumerate(sound.effect[offset_x : offset_x + 32]):
-            pyxel.text(32 + i * 6, 160, "NSVF"[effect], 1)
+        for i, effect in enumerate(sound.effect):
+            pyxel.text(31 + i * 4, 166, "NSVF"[effect], 1)
