262373fce74ac532c0c282e50028bd6a19341975
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-15 23:13:10 +0900

    Splitted the widgets for the Sound Editor

--- pyxel/editor/sound_editor.py
+++ pyxel/editor/sound_editor.py
@@ -5,6 +5,10 @@ from pyxel.ui.constants import WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
 
 from .constants import EDITOR_IMAGE_X, EDITOR_IMAGE_Y
 from .editor import Editor
+from .octave_bar import OctaveBar
+from .piano_keyboard import PianoKeyboard
+from .piano_roll import PianoRoll
+from .sound_input import SoundInput
 
 
 class SoundEditor(Editor):
@@ -27,6 +31,16 @@ class SoundEditor(Editor):
             self, 205, 17, 3, EDITOR_IMAGE_X + 144, EDITOR_IMAGE_Y
         )
 
+        self._piano_keyboard = PianoKeyboard(self)
+        self._piano_roll = PianoRoll(self)
+        self._sound_input = SoundInput(self)
+
+        self._left_octave_bar = OctaveBar(self, 12, 25)
+        self._right_octave_bar = OctaveBar(self, 224, 25)
+
+        # self.add_event_handler("mouse_down", self.__on_update)
+        # self.add_event_handler("mouse_drag", self.__on_update)
+        # self.add_event_handler("mouse_hover", self.__on_update)
         self.add_event_handler("update", self.__on_update)
         self.add_event_handler("draw", self.__on_draw)
 
@@ -61,44 +75,8 @@ class SoundEditor(Editor):
 
     def __on_draw(self):
         self.draw_frame(11, 16, 218, 157)
-        pyxel.rect(12, 25, 227, 147, 6)
-
         pyxel.text(23, 18, "SOUND", 6)
         pyxel.text(83, 18, "SPEED", 6)
-
-        x = self._cursor_x * 4 + 31
-        if self._cursor_y == 0:
-            pyxel.rect(x, 25, x + 2, 147, 1)
-
-        pyxel.blt(16, 25, 3, EDITOR_IMAGE_X, EDITOR_IMAGE_Y + 8, 110, 147, 6)
-        pyxel.blt(126, 25, 3, EDITOR_IMAGE_X + 13, EDITOR_IMAGE_Y + 8, -98, 147, 6)
-
-        if self._cursor_y > 0:
-            y = self._cursor_y * 8 + 142
-            pyxel.rect(x, y - 1, x + 2, y + 5, 1)
-
-        sound = pyxel.sound(self.sound)
-
-        for i, note in enumerate(sound.note):
-            x = i * 4 + 31
-            y = 143 - note * 2
-            pyxel.rect(x, y, x + 2, y + 2, 8 if note >= 0 else 12)
-
         pyxel.text(17, 150, "TON", 6)
         pyxel.text(17, 158, "VOL", 6)
         pyxel.text(17, 166, "EFX", 6)
-
-        pyxel.rect(13, 26, 14, 72, 13)
-        pyxel.rect(225, 26, 226, 72, 13)
-
-        for i, tone in enumerate(sound.tone):
-            col = 7 if self._cursor_y == 1 and self._cursor_x == i else 1
-            pyxel.text(31 + i * 4, 150, "TSPN"[tone], col)
-
-        for i, volume in enumerate(sound.volume):
-            col = 7 if self._cursor_y == 2 and self._cursor_x == i else 1
-            pyxel.text(31 + i * 4, 158, str(volume), col)
-
-        for i, effect in enumerate(sound.effect):
-            col = 7 if self._cursor_y == 3 and self._cursor_x == i else 1
-            pyxel.text(31 + i * 4, 166, "NSVF"[effect], col)
