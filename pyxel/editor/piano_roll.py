3f25ddd3aff1d30e0f200e06e5170acf4e3d5d6c
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-20 15:00:47 +0900

    Implemented the sound editor (WIP)

--- pyxel/editor/piano_roll.py
+++ pyxel/editor/piano_roll.py
@@ -1,5 +1,7 @@
 import pyxel
+from pyxel.constants import SOUND_MAX_LENGTH
 from pyxel.ui import Widget
+from pyxel.ui.constants import WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
 
 from .constants import EDITOR_IMAGE_X, EDITOR_IMAGE_Y
 
@@ -8,20 +10,120 @@ class PianoRoll(Widget):
     def __init__(self, parent):
         super().__init__(parent, 30, 25, 193, 123)
 
+        self._press_x = 0
+        self._press_y = 0
+
+        self.add_event_handler("mouse_down", self.__on_mouse_down)
+        self.add_event_handler("mouse_up", self.__on_mouse_up)
+        self.add_event_handler("mouse_drag", self.__on_mouse_drag)
+        self.add_event_handler("mouse_hover", self.__on_mouse_hover)
         self.add_event_handler("update", self.__on_update)
         self.add_event_handler("draw", self.__on_draw)
 
-    def __on_update(self):
+    def _screen_to_view(self, x, y):
+        x = min(max((x - self.x - 1) // 4, 0), SOUND_MAX_LENGTH - 1)
+        y = min(max(59 - (y - self.y - 1) // 2, -1), 59)
+        return x, y
+
+    def __on_mouse_down(self, key, x, y):
+        if key != pyxel.KEY_LEFT_BUTTON:
+            return
+
+        x, y = self._screen_to_view(x, y)
+
+        self._press_x = x
+        self._press_y = y
+
+        self.parent.cursor_x = x
+        self.parent.cursor_y = 0
+
+        data = self.parent.sound_data
+
+        padding_length = x + 1 - len(data)
+        if padding_length > 0:
+            data.extend([-1] * padding_length)
+
+        data[x] = y
+
+    def __on_mouse_up(self, key, x, y):
         pass
+
+    def __on_mouse_drag(self, key, x, y, dx, dy):
+        if key != pyxel.KEY_LEFT_BUTTON:
+            return
+
+        x, y = self._screen_to_view(x, y)
+
+        if x > self._press_x:
+            x1 = self._press_x
+            y1 = self._press_y
+            x2 = x
+            y2 = y
+        elif x < self._press_x:
+            x1 = x
+            y1 = y
+            x2 = self._press_x
+            y2 = self._press_y
+        else:
+            return
+
+        self._press_x = x
+        self._press_y = y
+
+        self.parent.cursor_x = x
+        self.parent.cursor_y = 0
+
+        data = self.parent.sound_data
+        dx = x2 - x1
+        dy = y2 - y1
+
+        for i in range(dx + 1):
+            value = round(y1 + (dy / dx) * i)
+
+            if x1 + i >= len(data):
+                data.append(value)
+            else:
+                data[x1 + i] = value
+
+    def __on_mouse_hover(self, x, y):
+        self.parent.help_message = "EDIT:BS/DEL/ENTER"
+
+    def __on_update(self):
+        cursor_y = self.parent.cursor_y
+
+        if cursor_y > 0:
+            return
+
+        edit_x = self.parent.edit_x
+        data = self.parent.sound_data
+
+        if pyxel.btnp(pyxel.KEY_BACKSPACE, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+            if edit_x > 0:
+                del data[edit_x - 1]
+                self.parent.cursor_x = edit_x - 1
+            return
+
+        if pyxel.btnp(pyxel.KEY_DELETE, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+            if edit_x < len(data):
+                del data[edit_x]
+            return
+
+        if pyxel.btnp(
+            pyxel.KEY_ENTER, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
+        ) or pyxel.btnp(pyxel.KEY_KP_ENTER, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+            data.insert(edit_x, -1)
+            data[:] = data[:SOUND_MAX_LENGTH]
+
+            self.parent.cursor_x = edit_x
+            if edit_x < SOUND_MAX_LENGTH - 1:
+                self.parent.cursor_x += 1
+            return
 
     def __on_draw(self):
         pyxel.rect(self.x, self.y, self.x + self.width - 1, self.y + self.height - 1, 6)
 
-        self._cursor_x = 0
-        self._cursor_y = 0
-
-        x = self._cursor_x * 4 + 31
-        if self._cursor_y == 0:
+        if self.parent.cursor_y == 0:
+            x = self.parent.edit_x * 4 + 31
             pyxel.rect(x, 25, x + 2, 147, 1)
 
         pyxel.blt(
@@ -32,7 +134,6 @@ class PianoRoll(Widget):
         )
 
         sound = pyxel.sound(self.parent.sound)
-
         for i, note in enumerate(sound.note):
             x = i * 4 + 31
             y = 143 - note * 2
--- pyxel/editor/sound_input.py
+++ pyxel/editor/sound_input.py
@@ -1,5 +1,7 @@
 import pyxel
+from pyxel.constants import SOUND_MAX_LENGTH
 from pyxel.ui import Widget
+from pyxel.ui.constants import WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
 
 from .constants import EDITOR_IMAGE_X, EDITOR_IMAGE_Y
 
@@ -8,33 +10,133 @@ class SoundInput(Widget):
     def __init__(self, parent):
         super().__init__(parent, 30, 149, 193, 23)
 
+        self.add_event_handler("mouse_down", self.__on_mouse_down)
+        self.add_event_handler("mouse_up", self.__on_mouse_up)
+        self.add_event_handler("mouse_hover", self.__on_mouse_hover)
         self.add_event_handler("update", self.__on_update)
         self.add_event_handler("draw", self.__on_draw)
 
-    def __on_update(self):
+    def _screen_to_view(self, x, y):
+        x = min(max((x - self.x - 1) // 4, 0), SOUND_MAX_LENGTH - 1)
+        y = min(max((y - self.y) // 8, 0), 2)
+        return x, y
+
+    def __on_mouse_down(self, key, x, y):
+        if key != pyxel.KEY_LEFT_BUTTON:
+            return
+
+        x, y = self._screen_to_view(x, y)
+        self.parent.cursor_x = x
+        self.parent.cursor_y = y + 1
+
+    def __on_mouse_up(self, key, x, y):
         pass
+
+    def __on_mouse_hover(self, x, y):
+        x, y = self._screen_to_view(x, y)
+        if y == 0:
+            self.parent.help_message = "TONE:T/S/P/N EDIT:BS/DEL/ENTER"
+        elif y == 1:
+            self.parent.help_message = "VOLUME:0-7 EDIT:BS/DEL/ENTER"
+        elif y == 2:
+            self.parent.help_message = "EFFECT:N/S/V/F EDIT:BS/DEL/ENTER"
+
+    def __on_update(self):
+        cursor_y = self.parent.cursor_y
+
+        if cursor_y < 1:
+            return
+
+        edit_x = self.parent.edit_x
+        data = self.parent.sound_data
+
+        if pyxel.btnp(pyxel.KEY_BACKSPACE, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+            if edit_x > 0:
+                del data[edit_x - 1]
+                self.parent.cursor_x = edit_x - 1
+            return
+
+        if pyxel.btnp(pyxel.KEY_DELETE, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+            if edit_x < len(data):
+                del data[edit_x]
+            return
+
+        if pyxel.btnp(
+            pyxel.KEY_ENTER, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
+        ) or pyxel.btnp(pyxel.KEY_KP_ENTER, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+            data.insert(edit_x, 0)
+            data[:] = data[:SOUND_MAX_LENGTH]
+
+            self.parent.cursor_x = edit_x
+            if edit_x < SOUND_MAX_LENGTH - 1:
+                self.parent.cursor_x += 1
+            return
+
+        value = None
+        if cursor_y == 1:
+            if pyxel.btnp(pyxel.KEY_T, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 0
+            elif pyxel.btnp(pyxel.KEY_S, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 1
+            elif pyxel.btnp(pyxel.KEY_P, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 2
+            elif pyxel.btnp(pyxel.KEY_N, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 3
+        elif cursor_y == 2:
+            for i in range(8):
+                if pyxel.btnp(
+                    pyxel.KEY_0 + i, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
+                ) or pyxel.btnp(
+                    pyxel.KEY_KP_0 + i, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME
+                ):
+                    value = i
+                    break
+        elif cursor_y == 3:
+            if pyxel.btnp(pyxel.KEY_N, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 0
+            elif pyxel.btnp(pyxel.KEY_S, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 1
+            elif pyxel.btnp(pyxel.KEY_V, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 2
+            elif pyxel.btnp(pyxel.KEY_F, WIDGET_HOLD_TIME, WIDGET_REPEAT_TIME):
+                value = 3
+
+        if value is None:
+            return
+
+        edit_x = self.parent.edit_x
+        data = self.parent.sound_data
+
+        if edit_x >= len(data):
+            data.append(value)
+        else:
+            data[edit_x] = value
+
+        self.parent.cursor_x = edit_x
+        if edit_x < SOUND_MAX_LENGTH - 1:
+            self.parent.cursor_x += 1
 
     def __on_draw(self):
         pyxel.blt(self.x, self.y, 3, EDITOR_IMAGE_X, EDITOR_IMAGE_Y + 132, 97, 23)
         pyxel.blt(self.x + 97, self.y, 3, EDITOR_IMAGE_X, EDITOR_IMAGE_Y + 132, -96, 23)
 
-        self._cursor_x = 0
-        self._cursor_y = 0
-
-        # if self._cursor_y > 0:
-        #    y = self._cursor_y * 8 + 142
-        #    pyxel.rect(x, y - 1, x + 2, y + 5, 1)
-
         sound = pyxel.sound(self.parent.sound)
+        cursor_x = self.parent.cursor_x
+        cursor_y = self.parent.cursor_y
+
+        if cursor_y > 0:
+            x = self.parent.edit_x * 4 + 31
+            y = cursor_y * 8 + 142
+            pyxel.rect(x, y - 1, x + 2, y + 5, 1)
 
         for i, tone in enumerate(sound.tone):
-            col = 7 if self._cursor_y == 1 and self._cursor_x == i else 1
+            col = 7 if cursor_y == 1 and cursor_x == i else 1
             pyxel.text(31 + i * 4, 150, "TSPN"[tone], col)
 
         for i, volume in enumerate(sound.volume):
-            col = 7 if self._cursor_y == 2 and self._cursor_x == i else 1
+            col = 7 if cursor_y == 2 and cursor_x == i else 1
             pyxel.text(31 + i * 4, 158, str(volume), col)
 
         for i, effect in enumerate(sound.effect):
-            col = 7 if self._cursor_y == 3 and self._cursor_x == i else 1
+            col = 7 if cursor_y == 3 and cursor_x == i else 1
             pyxel.text(31 + i * 4, 166, "NSVF"[effect], col)
