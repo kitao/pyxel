57bf232d48aa10f30c9073ef7671618f8a9f37f8
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-21 20:16:27 +0900

    Implemented the octave button

--- pyxel/editor/octave_bar.py
+++ pyxel/editor/octave_bar.py
@@ -6,11 +6,25 @@ class OctaveBar(Widget):
     def __init__(self, parent, x, y):
         super().__init__(parent, x, y, 4, 123)
 
-        self.add_event_handler("update", self.__on_update)
+        self.add_event_handler("mouse_down", self.__on_mouse_down)
+        self.add_event_handler("mouse_drag", self.__on_mouse_drag)
+        self.add_event_handler("mouse_hover", self.__on_mouse_hover)
         self.add_event_handler("draw", self.__on_draw)
 
-    def __on_update(self):
-        pass
+    def __on_mouse_down(self, key, x, y):
+        if key != pyxel.KEY_LEFT_BUTTON:
+            return
+
+        x -= self.x
+        y -= self.y
+
+        self.parent.octave = min(max(3 - ((y - 12) // 24), 0), 3)
+
+    def __on_mouse_drag(self, key, x, y, dx, dy):
+        self.__on_mouse_down(key, x, y)
+
+    def __on_mouse_hover(self, x, y):
+        self.parent.help_message = "OCTAVE:PAGEUP/PAGEDOWN"
 
     def __on_draw(self):
         pyxel.rect(self.x, self.y, self.x + self.width - 1, self.y + self.height - 1, 6)
