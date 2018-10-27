a34edc687a5d6cd5a615780f9a9ca906623f8ddd
Takashi Kitao <takashi.kitao@gmail.com>
2018-10-27 17:00:14 +0900

    Fixed the bug when outside the sound button is pressed

--- pyxel/editor/sound_selector.py
+++ pyxel/editor/sound_selector.py
@@ -50,7 +50,8 @@ class SoundSelector(Widget):
 
         self._pressed_sound = self._hit_sound_button(x, y)
 
-        self.parent.field_editor.insert(self._pressed_sound)
+        if self._pressed_sound is not None:
+            self.parent.field_editor.insert(self._pressed_sound)
 
     def __on_mouse_up(self, key, x, y):
         if key != pyxel.KEY_LEFT_BUTTON:
