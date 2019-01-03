36d152d8e2f45bff28c16a60cc44988fed954b52
Takashi Kitao <takashi.kitao@gmail.com>
2019-01-03 19:09:22 +0900

    Added the DLLEXPORT macro

--- pyxel/core/include/pyxelcore.h
+++ pyxel/core/include/pyxelcore.h
@@ -1,4 +1,12 @@
 #ifndef PYXELCORE_H_
 #define PYXELCORE_H_
 
+#ifdef WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+DLLEXPORT int test(int width, int height);
+
 #endif // PYXELCORE_H_
