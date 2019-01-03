a56cccdac40ec51ef30f1fbfa0a81b3895daaf66
Takashi Kitao <takashi.kitao@gmail.com>
2019-01-03 19:10:15 +0900

    Refactored the code

--- pyxel/core/__init__.py
+++ pyxel/core/__init__.py
@@ -4,26 +4,25 @@ def init_module():
     import platform
     import sys
 
-    lib_name = "libpyxelcore"
-    system = platform.system()
+    lib_path = os.path.join(os.path.dirname(__file__), "libpyxelcore")
 
+    system = platform.system()
     if system == "Darwin":
-        lib_name += "_darwin"
+        lib_path += "_darwin"
         lib_ext = ".dylib"
     elif system == "Windows":
-        lib_name += "_windows"
+        lib_path += "_windows"
         lib_ext = ".dll"
     elif system == "Linux":
-        lib_name += "_linux"
+        lib_path += "_linux"
         lib_ext = ".so"
     else:
         raise RuntimeError("unsupported platform: {}".format(system))
 
-    lib_name = os.path.join(os.path.dirname(__file__), lib_name)
-    lib_name += "_amd64" if platform.architecture()[0] == "64bit" else "_386"
-    lib_name += lib_ext
+    lib_path += "_amd64" if platform.architecture()[0] == "64bit" else "_i386"
+    lib_path += lib_ext
 
-    lib = ctypes.cdll.LoadLibrary(lib_name)
+    lib = ctypes.cdll.LoadLibrary(lib_path)
 
     module = sys.modules[__name__]
     module.test = lib.test
@@ -32,4 +31,4 @@ def init_module():
 init_module()
 
 if __name__ == "__main__":
-    test(400, 300)  # noqa: F821
+    print(test(400, 300))  # noqa: F821
