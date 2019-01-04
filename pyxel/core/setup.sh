09eba17e2f818c8d814e5f61d74bad27e1fc33d5
Takashi Kitao <takashi.kitao@gmail.com>
2019-01-05 00:50:02 +0900

    Renamed the script to setup headers and libraries

--- pyxel/core/setup.sh
+++ pyxel/core/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Preparation
+#
+# Mac:
+#   brew install sdl2 sdl2_image mingw-w64
+#
+# Linux:
+#   sudo apt install libsdl2-dev libsdl2-image_dev
+
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
 SDL2_INCDIR=$SCRIPT_DIR/include/SDL2
@@ -18,29 +26,27 @@ cd $SCRIPT_DIR
 rm -rf $SDL2_INCDIR $SDL2_LIBDIR
 mkdir -p $SDL2_INCDIR $SDL2_LIBDIR
 
-brew reinstall sdl2 sdl2_image
-cp /usr/local/include/SDL2/*.h $SDL2_INCDIR
 cp /usr/local/lib/libSDL2.a $SDL2_LIBDIR/libSDL2_darwin_amd64.a
 cp /usr/local/lib/libSDL2_image.a $SDL2_LIBDIR/libSDL2_image_darwin_amd64.a
-brew uninstall sdl2 sdl2_image
 
 rm -rf temp
 mkdir -p temp
 cd temp
 
-curl -L $MINGW_SDL2_URL -o sdl2.tar.gz
-tar xzf sdl2.tar.gz
+curl -L $MINGW_SDL2_URL -o SDL2.tar.gz
+tar xzf SDL2.tar.gz
 cd SDL2-*
-pwd
+cp i686-w64-mingw32/include/SDL2/*.h $SDL2_INCDIR
 cp i686-w64-mingw32/lib/libSDL2.a $SDL2_LIBDIR/libSDL2_windows_386.a
 cp i686-w64-mingw32/lib/libSDL2main.a $SDL2_LIBDIR/libSDL2main_windows_386.a
 cp x86_64-w64-mingw32/lib/libSDL2.a $SDL2_LIBDIR/libSDL2_windows_amd64.a
 cp x86_64-w64-mingw32/lib/libSDL2main.a $SDL2_LIBDIR/libSDL2main_windows_amd64.a
 cd ..
 
-curl -L $MINGW_SDL2_IMAGE_URL -o sdl2_image.tar.gz
-tar xzf sdl2_image.tar.gz
+curl -L $MINGW_SDL2_IMAGE_URL -o SDL2_image.tar.gz
+tar xzf SDL2_image.tar.gz
 cd SDL2_image-*
+cp i686-w64-mingw32/include/SDL2/*.h $SDL2_INCDIR
 cp i686-w64-mingw32/lib/libSDL2_image.a $SDL2_LIBDIR/libSDL2_image_windows_386.a
 cp x86_64-w64-mingw32/lib/libSDL2_image.a $SDL2_LIBDIR/libSDL2_image_windows_amd64.a
 cd ..
