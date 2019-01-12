484519546f637b5a78f606982e8825616c0ee626
Takashi Kitao <takashi.kitao@gmail.com>
2019-01-12 22:54:19 +0900

    Changed to use C++ instead of C

--- pyxel/core/include/pyxelcore.h
+++ pyxel/core/include/pyxelcore.h
@@ -1,12 +1,24 @@
 #ifndef PYXELCORE_H_
 #define PYXELCORE_H_
 
-#ifdef WIN32
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-DLLEXPORT int test(int width, int height);
+#ifdef WIN32
+#ifdef PYXEL_DLL
+#define PYXEL_API __declspec(dllexport)
+#else
+#define PYXEL_API __declspec(dllimport)
+#endif
+#else
+#define PYXEL_API
+#endif
+
+PYXEL_API int test(int width, int height);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // PYXELCORE_H_
