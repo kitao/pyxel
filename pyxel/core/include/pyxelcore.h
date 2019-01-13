f7d0c01934905eb6cf8a5dde884725b1af3bbf3b
Takashi Kitao <takashi.kitao@gmail.com>
2019-01-13 17:56:13 +0900

    Added API declarations

--- pyxel/core/include/pyxelcore.h
+++ pyxel/core/include/pyxelcore.h
@@ -16,9 +16,52 @@ extern "C" {
 #endif
 
 PYXEL_API int test(int width, int height);
-PYXEL_API void init(int width, int height, const char *caption, int scale,
+
+//
+// System
+//
+PYXEL_API int Width();
+PYXEL_API int Height();
+PYXEL_API int FrameCount();
+
+PYXEL_API void Init(int width, int height, const char *caption, int scale,
                     const int *palette, int fps, int border_width,
                     int border_color);
+PYXEL_API void Run(void (*update)(), void (*draw)());
+PYXEL_API void Quit();
+
+//
+// Resource
+//
+PYXEL_API void Save(const char *filename);
+PYXEL_API void Load(const char *filename);
+
+//
+// Input
+//
+PYXEL_API int MouseX();
+PYXEL_API int MouseY();
+
+PYXEL_API bool Btn(int key);
+PYXEL_API bool Btnp(int key, int hold, int period);
+PYXEL_API bool Btnr(int key);
+PYXEL_API void Mouse(bool visible);
+
+//
+// Graphics
+//
+PYXEL_API int Image(int img, bool system);
+PYXEL_API int Tilemap(int tm);
+PYXEL_API void Clip(int x1, int y1, int x2, int y2);
+
+//
+// Audio
+//
+PYXEL_API int Sound(int snd, bool system);
+PYXEL_API int Music(int msc);
+PYXEL_API void Play(int ch, int snd, bool loop);
+PYXEL_API void Playm(int msc, bool loop);
+PYXEL_API void Stop(int ch);
 
 #ifdef __cplusplus
 }
---
+++ pyxel/core/include/pyxelcore/app.h
@@ -0,0 +1,10 @@
+#ifndef PYXELCORE_APP_H_
+#define PYXELCORE_APP_H_
+
+namespace pyxelcore {
+
+class App {}
+
+} // namespace pyxelcore
+
+#endif // PYXELCORE_APP_H_
