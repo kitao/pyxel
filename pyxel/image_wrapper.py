37af6d4bc4d8798391528d9ad110f1b255625e36
Takashi Kitao <takashi.kitao@gmail.com>
2019-03-21 22:39:30 +0900

    Removed unnecessary ctype definitions

--- pyxel/image_wrapper.py
+++ pyxel/image_wrapper.py
@@ -7,18 +7,17 @@ def setup_apis(module, lib):
             self._c_obj = c_obj
 
             self._data = np.ctypeslib.as_array(
-                lib.Image_data_getter(c_obj), shape=(self.width * self.height,)
+                lib.Image_data_getter(ctypes.c_void_p(c_obj)),
+                shape=(self.width * self.height,),
             )
-
-            print(type(c_obj), self._data.shape)
 
         @property
         def width(self):
-            return lib.Image_width_getter(self._c_obj)
+            return lib.Image_width_getter(ctypes.c_void_p(self._c_obj))
 
         @property
         def height(self):
-            return lib.Image_height_getter(self._c_obj)
+            return lib.Image_height_getter(ctypes.c_void_p(self._c_obj))
 
         @property
         def data(self):
@@ -51,40 +50,5 @@ def setup_apis(module, lib):
 
         def copy(self, x, y, img, u, v, w, h):
             lib.Image_copy(self._c_obj, x, y, img, u, v, w, h)
-
-    lib.Image_width_getter.argtypes = [ctypes.c_void_p]
-    lib.Image_height_getter.argtypes = [ctypes.c_void_p]
-    lib.Image_data_getter.argtypes = [ctypes.c_void_p]
-    lib.Image_get.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int32]
-    lib.Image_set1.argtypes = [
-        ctypes.c_void_p,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_int32,
-    ]
-    lib.Image_set.argtypes = [
-        ctypes.c_void_p,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.POINTER(ctypes.c_int32),
-        ctypes.c_int32,
-        ctypes.c_int32,
-    ]
-    lib.Image_load.argtypes = [
-        ctypes.c_void_p,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_char_p,
-    ]
-    lib.Image_copy.argtypes = [
-        ctypes.c_void_p,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_int32,
-        ctypes.c_int32,
-    ]
 
     module.Image = Image
