207bfe74c85bf8957e28465a045ae565d21afa02
Takashi Kitao <takashi.kitao@gmail.com>
2019-03-21 15:23:53 +0900

    Moved the sources

---
+++ pyxel/core/src/pyxelcore/constants.cc
@@ -0,0 +1,19 @@
+#include <cstddef>
+
+#include "pyxelcore/constants.h"
+
+namespace pyxelcore {
+
+Constants::Constants() {}
+
+Constants::~Constants() {}
+
+int Constants::get_constant_number(char* name) {
+  return 0;
+}
+
+const char* Constants::get_constant_string(char* name) {
+  return "a";
+}
+
+}  // namespace pyxelcore
