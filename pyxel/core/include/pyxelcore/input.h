f102e0e2e0145d8f173ad614d8dde2a45d42d069
Takashi Kitao <takashi.kitao@gmail.com>
2019-03-22 01:15:12 +0900

    Refined API names

--- pyxel/core/include/pyxelcore/input.h
+++ pyxel/core/include/pyxelcore/input.h
@@ -10,13 +10,13 @@ class Input {
   Input();
   ~Input();
 
-  int32_t mouse_x_getter() { return mouse_x_; }
-  int32_t mouse_y_getter() { return mouse_y_; }
+  int32_t MouseX() { return mouse_x_; }
+  int32_t MouseY() { return mouse_y_; }
 
-  int32_t btn(int32_t key);
-  int32_t btnp(int32_t key, int32_t hold, int32_t period);
-  int32_t btnr(int32_t key);
-  void mouse(int32_t visible);
+  int32_t Btn(int32_t key);
+  int32_t Btnp(int32_t key, int32_t hold, int32_t period);
+  int32_t Btnr(int32_t key);
+  void Mouse(int32_t visible);
 
  private:
   int32_t mouse_x_;
