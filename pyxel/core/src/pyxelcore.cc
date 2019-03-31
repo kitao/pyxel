9a06235b8603ae674a6ff5d027eaf235a9a03f15
Takashi Kitao <takashi.kitao@gmail.com>
2019-03-31 18:38:17 +0900

    Merged classes

--- pyxel/core/src/pyxelcore.cc
+++ pyxel/core/src/pyxelcore.cc
@@ -2,18 +2,14 @@
 
 #include "pyxelcore.h"
 #include "pyxelcore/audio.h"
+#include "pyxelcore/canvas.h"
 #include "pyxelcore/constants.h"
-#include "pyxelcore/graphics.h"
-#include "pyxelcore/image.h"
 #include "pyxelcore/input.h"
-#include "pyxelcore/resource.h"
 #include "pyxelcore/system.h"
 
 static pyxelcore::Constants* s_constants = NULL;
 static pyxelcore::System* s_system = NULL;
-static pyxelcore::Resource* s_resource = NULL;
 static pyxelcore::Input* s_input = NULL;
-static pyxelcore::Graphics* s_graphics = NULL;
 static pyxelcore::Audio* s_audio = NULL;
 
 //
@@ -51,26 +47,22 @@ void init(int32_t width,
           int32_t border_width,
           int32_t border_color) {
   s_constants = new pyxelcore::Constants();
-  s_resource = new pyxelcore::Resource();
   s_input = new pyxelcore::Input();
-  s_graphics = new pyxelcore::Graphics(width, height);
   s_audio = new pyxelcore::Audio();
-  s_system = new pyxelcore::System(s_graphics, width, height, caption, scale,
-                                   palette, fps, border_width, border_color);
+  s_system = new pyxelcore::System(width, height, caption, scale, palette, fps,
+                                   border_width, border_color);
 }
 
 void run(void (*update)(), void (*draw)()) {
-  s_system->Run(update, draw);
+  s_system->RunApplication(update, draw);
 }
 
 void quit() {
-  s_system->Quit();
+  s_system->QuitApplication();
 
   delete s_system;
   delete s_audio;
-  delete s_graphics;
   delete s_input;
-  delete s_resource;
   delete s_constants;
 }
 
@@ -78,11 +70,11 @@ void quit() {
 // Resource
 //
 void save(const char* filename) {
-  s_resource->Save(filename);
+  s_system->SaveAsset(filename);
 }
 
 void load(const char* filename) {
-  s_resource->Load(filename);
+  s_system->LoadAsset(filename);
 }
 
 //
@@ -116,55 +108,55 @@ void mouse(int32_t visible) {
 // Graphics
 //
 void* image(int32_t img, int32_t system) {
-  return s_graphics->GetImage(img, system);
+  return s_system->GetImage(img, system);
 }
 
 void* tilemap(int32_t tm) {
-  return s_graphics->GetTilemap(tm);
+  return s_system->GetTilemap(tm);
 }
 
 void clip0() {
-  s_graphics->Screen()->ResetClippingArea();
+  s_system->Screen()->ResetClippingArea();
 }
 
 void clip(int32_t x1, int32_t y1, int32_t x2, int32_t y2) {
-  s_graphics->Screen()->SetClippingArea(x1, y1, x2, y2);
+  s_system->Screen()->SetClippingArea(x1, y1, x2, y2);
 }
 
 void pal0() {
-  s_graphics->Screen()->ResetPalette();
+  s_system->Screen()->ResetPalette();
 }
 
 void pal(int32_t col1, int32_t col2) {
-  s_graphics->Screen()->SetPalette(col1, col2);
+  s_system->Screen()->SetPalette(col1, col2);
 }
 
 void cls(int32_t col) {
-  s_graphics->Screen()->Clear(col);
+  s_system->Screen()->Clear(col);
 }
 
 void pix(int32_t x, int32_t y, int32_t col) {
-  s_graphics->Screen()->DrawPoint(x, y, col);
+  s_system->Screen()->DrawPoint(x, y, col);
 }
 
 void line(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t col) {
-  s_graphics->Screen()->DrawLine(x1, y1, x2, y2, col);
+  s_system->Screen()->DrawLine(x1, y1, x2, y2, col);
 }
 
 void rect(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t col) {
-  s_graphics->Screen()->DrawRectangle(x1, y1, x2, y2, col);
+  s_system->Screen()->DrawRectangle(x1, y1, x2, y2, col);
 }
 
 void rectb(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t col) {
-  s_graphics->Screen()->DrawRectangleBorder(x1, y1, x2, y2, col);
+  s_system->Screen()->DrawRectangleBorder(x1, y1, x2, y2, col);
 }
 
 void circ(int32_t x, int32_t y, int32_t r, int32_t col) {
-  s_graphics->Screen()->DrawCircle(x, y, r, col);
+  s_system->Screen()->DrawCircle(x, y, r, col);
 }
 
 void circb(int32_t x, int32_t y, int32_t r, int32_t col) {
-  s_graphics->Screen()->DrawCircleBorder(x, y, r, col);
+  s_system->Screen()->DrawCircleBorder(x, y, r, col);
 }
 
 void blt(int32_t x,
@@ -175,8 +167,8 @@ void blt(int32_t x,
          int32_t w,
          int32_t h,
          int32_t colkey) {
-  s_graphics->Screen()->DrawImage(x, y, s_graphics->GetImage(img), u, v, w, h,
-                                  colkey);
+  s_system->Screen()->DrawImage(x, y, s_system->GetImage(img), u, v, w, h,
+                                colkey);
 }
 
 void bltm(int32_t x,
@@ -187,12 +179,12 @@ void bltm(int32_t x,
           int32_t w,
           int32_t h,
           int32_t colkey) {
-  s_graphics->Screen()->DrawTilemap(x, y, s_graphics->GetTilemap(tm), u, v, w,
-                                    h, colkey);
+  s_system->Screen()->DrawTilemap(x, y, s_system->GetTilemap(tm), u, v, w, h,
+                                  colkey);
 }
 
 void text(int32_t x, int32_t y, const char* s, int32_t col) {
-  s_graphics->Screen()->DrawText(x, y, s, col);
+  s_system->Screen()->DrawText(x, y, s, col);
 }
 
 //
@@ -222,23 +214,23 @@ void stop(int32_t ch) {
 // Image class
 //
 int32_t image_width_getter(void* self) {
-  return reinterpret_cast<pyxelcore::Image*>(self)->Width();
+  return reinterpret_cast<pyxelcore::Canvas*>(self)->Width();
 }
 
 int32_t image_height_getter(void* self) {
-  return reinterpret_cast<pyxelcore::Image*>(self)->Height();
+  return reinterpret_cast<pyxelcore::Canvas*>(self)->Height();
 }
 
 int32_t* image_data_getter(void* self) {
-  return reinterpret_cast<pyxelcore::Image*>(self)->Data();
+  return reinterpret_cast<pyxelcore::Canvas*>(self)->Data();
 }
 
 int32_t image_get(void* self, int32_t x, int32_t y) {
-  return reinterpret_cast<pyxelcore::Image*>(self)->GetColor(x, y);
+  return reinterpret_cast<pyxelcore::Canvas*>(self)->GetColor(x, y);
 }
 
 void image_set1(void* self, int32_t x, int32_t y, int32_t color) {
-  reinterpret_cast<pyxelcore::Image*>(self)->SetColor(x, y, color);
+  reinterpret_cast<pyxelcore::Canvas*>(self)->SetColor(x, y, color);
 }
 
 void image_set(void* self,
@@ -247,8 +239,8 @@ void image_set(void* self,
                const int32_t* data,
                int32_t data_width,
                int32_t data_height) {
-  reinterpret_cast<pyxelcore::Image*>(self)->SetData(x, y, data, data_width,
-                                                     data_height);
+  reinterpret_cast<pyxelcore::Canvas*>(self)->SetData(x, y, data, data_width,
+                                                      data_height);
 }
 
 void image_load(void* self, int32_t x, int32_t y, const char* filename) {}
