13f985986ae6f3f23126bb9b6dc4c692b64fc601
Takashi Kitao <takashi.kitao@gmail.com>
2019-03-31 19:14:34 +0900

    Added the methods for constants

---
+++ pyxel/core/src/pyxelcore/constants.cc
@@ -0,0 +1,13 @@
+#include "pyxelcore/app.h"
+
+namespace pyxelcore {
+
+int32_t App::GetConstantNumber(const char* name) {
+  return 0;
+}
+
+const char* App::GetConstantString(const char* name) {
+  return "test";
+}
+
+}  // namespace pyxelcore
