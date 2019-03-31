640b95cca4ce03102790ac8f3f651fb09b2bd9ce
Takashi Kitao <takashi.kitao@gmail.com>
2019-03-31 18:38:17 +0900

    Merged classes

--- pyxel/core/include/pyxelcore/system.h
+++ pyxel/core/include/pyxelcore/system.h
@@ -10,28 +10,36 @@ class SDL_Texture;
 
 namespace pyxelcore {
 
-class Graphics;
+class Canvas;
+class Tilemap;
 
 class System {
  public:
-  System(Graphics* graphics,
-         int32_t width,
+  System(int32_t width,
          int32_t height,
          const char* caption = NULL,
          int32_t scale = -1,
-         const int* palette = NULL,
+         const int32_t* palette = NULL,
          int32_t fps = -1,
          int32_t border_width = -1,
          int32_t border_color = -1);
   ~System();
 
+  Canvas* Screen() { return screen_; }
+
   int32_t Width() { return width_; }
   int32_t Height() { return height_; }
   int32_t FrameCount() { return frame_count_; }
 
-  void Run(void (*update)(), void (*draw)());
-  void Quit();
-  void Error(const char* func, const char* msg);
+  Canvas* GetImage(int32_t img, bool system = false);
+  Tilemap* GetTilemap(int32_t tm);
+
+  void RunApplication(void (*update)(), void (*draw)());
+  void QuitApplication();
+
+  void LoadImage(Canvas* image, int32_t x, int32_t y, const char* filename);
+  void LoadAsset(const char* filename);
+  void SaveAsset(const char* filename);
 
  private:
   int32_t width_;
@@ -44,11 +52,12 @@ class System {
   int32_t border_color_;
   int32_t frame_count_;
 
-  Graphics* graphics_;
+  Canvas* screen_;
+  Canvas** image_;
+  Tilemap** tilemap_;
 
   SDL_Renderer* renderer_;
   SDL_Window* window_;
-  // SDL_Texture *temp_texture_;
   SDL_Texture* screen_texture_;
 
   void UpdateScreenTexture();
