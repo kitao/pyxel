6f929cbe19a0b034c377c8e610f75f2601ac7172
Takashi Kitao <takashi.kitao@gmail.com>
2019-04-07 11:44:33 +0900

    Implemented module properties

--- pyxel/core/src/pyxelcore/input.cc
+++ pyxel/core/src/pyxelcore/input.cc
@@ -1,10 +1,16 @@
 #include "pyxelcore/input.h"
 
+#include "SDL2/SDL.h"
+
 namespace pyxelcore {
 
 Input::Input() {}
 
 Input::~Input() {}
+
+void Input::UpdateState(int32_t frame_count) {
+  uint32_t mouse_state = SDL_GetMouseState(&mouse_x_, &mouse_y_);
+}
 
 bool Input::IsButtonOn(int32_t key) {
   return false;
