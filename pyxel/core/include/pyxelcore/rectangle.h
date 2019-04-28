4c77c3e94515ade7d31ae587cb2cbc8837076e7a
Takashi Kitao <takashi.kitao@gmail.com>
2019-04-29 07:37:33 +0900

    Changed Rectangle as inline methods

--- pyxel/core/include/pyxelcore/rectangle.h
+++ pyxel/core/include/pyxelcore/rectangle.h
@@ -1,19 +1,13 @@
 #ifndef PYXELCORE_RECTANGLE_H_
 #define PYXELCORE_RECTANGLE_H_
 
-#include <cstdint>
+#include <algorithm>
 
 namespace pyxelcore {
 
 class Rectangle {
  public:
   Rectangle();
-
-  static Rectangle FromPos(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
-  static Rectangle FromSize(int32_t left,
-                            int32_t top,
-                            int32_t width,
-                            int32_t height);
 
   int32_t Left() const { return left_; }
   int32_t Top() const { return top_; }
@@ -27,6 +21,12 @@ class Rectangle {
   Rectangle MoveTo(int32_t x, int32_t y) const;
   Rectangle Intersect(const Rectangle& rect) const;
 
+  static Rectangle FromPos(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
+  static Rectangle FromSize(int32_t left,
+                            int32_t top,
+                            int32_t width,
+                            int32_t height);
+
  protected:
   Rectangle(int32_t left, int32_t top, int32_t width, int32_t height);
 
@@ -38,6 +38,72 @@ class Rectangle {
   int32_t width_;
   int32_t height_;
 };
+
+inline Rectangle::Rectangle()
+    : left_(INT16_MIN),
+      top_(INT16_MIN),
+      right_(INT16_MAX),
+      bottom_(INT16_MAX),
+      width_(INT16_MAX - INT16_MIN),
+      height_(INT16_MAX - INT16_MIN) {}
+
+inline Rectangle::Rectangle(int32_t left,
+                            int32_t top,
+                            int32_t width,
+                            int32_t height) {
+  left_ = left;
+  top_ = top;
+  width_ = std::max(width, 0);
+  height_ = std::max(height, 0);
+  right_ = left_ + width_ - 1;
+  bottom_ = top_ + height_ - 1;
+}
+
+inline Rectangle Rectangle::FromPos(int32_t x1,
+                                    int32_t y1,
+                                    int32_t x2,
+                                    int32_t y2) {
+  int32_t left = std::min(x1, x2);
+  int32_t top = std::min(y1, y2);
+  int32_t width = std::abs(x1 - x2);
+  int32_t height = std::abs(y1 - y2);
+
+  return Rectangle(left, top, width, height);
+}
+
+inline Rectangle Rectangle::FromSize(int32_t left,
+                                     int32_t top,
+                                     int32_t width,
+                                     int32_t height) {
+  return Rectangle(left, top, width, height);
+}
+
+inline bool Rectangle::IsEmpty() const {
+  return width_ == 0 && height_ == 0;
+}
+
+inline bool Rectangle::Includes(int32_t x, int32_t y) const {
+  return x >= left_ && x <= right_ && y >= top_ && y <= bottom_;
+}
+
+inline Rectangle Rectangle::MoveTo(int32_t x, int32_t y) const {
+  return Rectangle(x, y, width_, height_);
+}
+
+inline Rectangle Rectangle::Intersect(const Rectangle& rect) const {
+  int32_t left = std::max(left_, rect.left_);
+  int32_t top = std::max(top_, rect.top_);
+  int32_t right = std::min(right_, rect.right_);
+  int32_t bottom = std::min(bottom_, rect.bottom_);
+  int32_t width = right - left + 1;
+  int32_t height = bottom - top + 1;
+
+  if (width > 0 && height > 0) {
+    return Rectangle(left, top, width, height);
+  } else {
+    return Rectangle(0, 0, 0, 0);
+  }
+}
 
 }  // namespace pyxelcore
 
