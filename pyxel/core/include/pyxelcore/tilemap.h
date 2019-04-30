8f7420eb09114a62bc7fc138ec31fdb82e9569c3
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-01 04:19:17 +0900

    Implemented the Tilemap class (WIP)

--- pyxel/core/include/pyxelcore/tilemap.h
+++ pyxel/core/include/pyxelcore/tilemap.h
@@ -1,13 +1,44 @@
 #ifndef PYXELCORE_TILEMAP_H_
 #define PYXELCORE_TILEMAP_H_
 
-#include <cstdint>
+#include "pyxelcore/rectangle.h"
 
 namespace pyxelcore {
 
-class Tilemap {
+class Tilemap : public Rectangle {
  public:
+  Tilemap(int32_t width, int32_t height);
+  ~Tilemap();
+
+  int32_t* Data() const { return data_; }
+
+  int32_t GetValue(int32_t x, int32_t y) const;
+
+  /*
+ PYXEL_API int32_t tilemap_get(void* self, int32_t x, int32_t y);
+ PYXEL_API void timemap_set1(void* self,
+                             int32_t x,
+                             int32_t y,
+                             int32_t data,
+                             int32_t refimg);
+ PYXEL_API void timemap_set(void* self,
+                            int32_t x,
+                            int32_t y,
+                            const int32_t* data,
+                            int32_t data_width,
+                            int32_t data_height,
+                            int32_t refimg);
+ PYXEL_API void timemap_copy(void* self,
+                             int32_t x,
+                             int32_t y,
+                             int32_t tm,
+                             int32_t u,
+                             int32_t v,
+                             int32_t w,
+                             int32_t h);
+                             */
  private:
+  int32_t* data_;
 };
 
 }  // namespace pyxelcore
