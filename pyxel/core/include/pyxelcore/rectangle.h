8727795f3358bc263c8d5142110d000ba7959a39
Takashi Kitao <takashi.kitao@gmail.com>
2019-04-30 11:20:19 +0900

    Changed to use utility functions

--- pyxel/core/include/pyxelcore/rectangle.h
+++ pyxel/core/include/pyxelcore/rectangle.h
@@ -1,7 +1,7 @@
 #ifndef PYXELCORE_RECTANGLE_H_
 #define PYXELCORE_RECTANGLE_H_
 
-#include <algorithm>
+#include "pyxelcore/utilities.h"
 
 namespace pyxelcore {
 
@@ -53,8 +53,8 @@ inline Rectangle::Rectangle(int32_t left,
                             int32_t height) {
   left_ = left;
   top_ = top;
-  width_ = std::max(width, 0);
-  height_ = std::max(height, 0);
+  width_ = Max(width, 0);
+  height_ = Max(height, 0);
   right_ = left_ + width_ - 1;
   bottom_ = top_ + height_ - 1;
 }
@@ -63,10 +63,10 @@ inline Rectangle Rectangle::FromPos(int32_t x1,
                                     int32_t y1,
                                     int32_t x2,
                                     int32_t y2) {
-  int32_t left = std::min(x1, x2);
-  int32_t top = std::min(y1, y2);
-  int32_t width = std::abs(x1 - x2);
-  int32_t height = std::abs(y1 - y2);
+  int32_t left = Min(x1, x2);
+  int32_t top = Min(y1, y2);
+  int32_t width = Abs(x1 - x2);
+  int32_t height = Abs(y1 - y2);
 
   return Rectangle(left, top, width, height);
 }
@@ -91,10 +91,10 @@ inline Rectangle Rectangle::MoveTo(int32_t x, int32_t y) const {
 }
 
 inline Rectangle Rectangle::Intersect(const Rectangle& rect) const {
-  int32_t left = std::max(left_, rect.left_);
-  int32_t top = std::max(top_, rect.top_);
-  int32_t right = std::min(right_, rect.right_);
-  int32_t bottom = std::min(bottom_, rect.bottom_);
+  int32_t left = Max(left_, rect.left_);
+  int32_t top = Max(top_, rect.top_);
+  int32_t right = Min(right_, rect.right_);
+  int32_t bottom = Min(bottom_, rect.bottom_);
   int32_t width = right - left + 1;
   int32_t height = bottom - top + 1;
 
