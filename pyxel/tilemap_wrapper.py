36b2098e6562d92f5b9156626d8b6bbc9da9930b
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-03 11:35:05 +0900

    Refactored variable names

--- pyxel/tilemap_wrapper.py
+++ pyxel/tilemap_wrapper.py
@@ -27,24 +27,24 @@ def setup_apis(module, lib):
             return lib.tilemap_refimg_getter(self._c_obj)
 
         @refimg.setter
-        def refimg(self, value):
-            return lib.tilemap_refimg_setter(self._c_obj, value)
+        def refimg(self, img):
+            return lib.tilemap_refimg_setter(self._c_obj, img)
 
         def get(self, x, y):
             return lib.tilemap_get(self._c_obj, x, y)
 
-        def set(self, x, y, val):
-            if type(val) is int:
-                lib.tilemap_set1(self._c_obj, x, y, val)
+        def set(self, x, y, data):
+            if type(data) is int:
+                lib.tilemap_set1(self._c_obj, x, y, data)
             else:
-                val_count = len(val)
-                c_val = (ctypes.c_char_p * val_count)()
+                data_count = len(data)
+                c_data = (ctypes.c_char_p * data_count)()
 
-                for i in range(val_count):
-                    c_str = ctypes.create_string_buffer(val[i].encode("utf-8"))
-                    c_val[i] = ctypes.cast(c_str, ctypes.c_char_p)
+                for i in range(data_count):
+                    c_str = ctypes.create_string_buffer(data[i].encode("utf-8"))
+                    c_data[i] = ctypes.cast(c_str, ctypes.c_char_p)
 
-                lib.tilemap_set(self._c_obj, x, y, c_val, val_count)
+                lib.tilemap_set(self._c_obj, x, y, c_data, data_count)
 
         def copy(self, x, y, tm, u, v, w, h):
             lib.tilemap_copy(self._c_obj, x, y, tm, u, v, w, h)
