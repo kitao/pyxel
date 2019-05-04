43aaa2d5378ed57ac22dc1a7afe2148bd61ed3e6
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-04 17:01:25 +0900

    Aded the audio and capture related files (WIP)

---
+++ pyxel/core/include/pyxelcore/recorder.h
@@ -0,0 +1,20 @@
+#ifndef PYXELCORE_RECORDER_H_
+#define PYXELCORE_RECORDER_H_
+
+#include "pyxelcore/common.h"
+
+#include "pyxelcore/gif.h"
+
+namespace pyxelcore {
+
+class Recorder {
+ public:
+  Recorder();
+  ~Recorder();
+
+ private:
+};
+
+}  // namespace pyxelcore
+
+#endif  // PYXELCORE_RECORDER_H_
