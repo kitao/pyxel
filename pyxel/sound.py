c9a3a5da6b127961749b2e6f2a3f663c2d21d65b
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-06 17:00:30 +0900

    Implemented temporary load API

---
+++ pyxel/sound.py
@@ -0,0 +1,8 @@
+# dummy class to load old pyxel file
+class Sound:
+    def __init__(self):
+        self._note = []
+        self._tone = []
+        self._volume = []
+        self._effect = []
+        self.speed = 30
