892e63d1584104a713ade6431312326b319d0463
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-06 21:22:53 +0900

    Added test code

--- pyxel/core/src/pyxelcore/resource.cc
+++ pyxel/core/src/pyxelcore/resource.cc
@@ -8,11 +8,24 @@ namespace pyxelcore {
 bool Resource::SaveAsset(const char* filename) {
   // TODO
 
+  miniz_cpp::zip_file file;
+
+  file.writestr("file1.txt", "this is file 1");
+  file.writestr("file2.txt", "this is file 2");
+  file.writestr("file3.txt", "this is file 3");
+  file.writestr("file4.txt", "this is file 4");
+  file.writestr("file5.txt", "this is file 5");
+
+  // file.save(argv[1]);
+
   return true;
 }
 
 bool Resource::LoadAsset(const char* filename) {
   // TODO
+
+  miniz_cpp::zip_file file(filename);
+  file.printdir();
 
   return true;
 }
