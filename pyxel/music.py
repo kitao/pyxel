f65c18ddf6320a8e00ad094586bced3c10c2ca80
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-08 07:51:24 +0900

    Implemented Sound and Music loader

--- pyxel/music.py
+++ pyxel/music.py
@@ -1,7 +1,3 @@
 # dummy class to load old pyxel file
 class Music:
-    def __init__(self):
-        self._ch0 = []
-        self._ch1 = []
-        self._ch2 = []
-        self._ch3 = []
+    pass
--- pyxel/sound.py
+++ pyxel/sound.py
@@ -1,8 +1,3 @@
 # dummy class to load old pyxel file
 class Sound:
-    def __init__(self):
-        self._note = []
-        self._tone = []
-        self._volume = []
-        self._effect = []
-        self.speed = 30
+    pass
