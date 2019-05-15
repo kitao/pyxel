004821d5ccf8391ebd0c3f48c81a8f65719c508f
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-15 23:17:11 +0900

    Implemented the Channel class (WIP)

--- pyxel/core/include/pyxelcore/channel.h
+++ pyxel/core/include/pyxelcore/channel.h
@@ -1,24 +1,47 @@
 #ifndef PYXELCORE_CHANNEL_H_
 #define PYXELCORE_CHANNEL_H_
 
-#include "pyxelcore/common.h"
+#include "pyxelcore/oscillator.h"
 
 namespace pyxelcore {
+
+class Sound;
 
 class Channel {
  public:
   Channel();
-  ~Channel();
 
-  void Play(int32_t* sound, int32_t sound_count, bool loop);
+  void Play(Sound** sound, int32_t sound_count, bool loop);
   void Stop();
   int32_t Output();
 
  private:
+  Oscillator oscillator_;
+
+  bool is_playing_;
+  bool is_loop_;
+  Sound* sound_[MAX_MUSIC_LENGTH];
+  int32_t sound_count_;
+  int32_t sound_index_;
+
+  int32_t time_;
+  int32_t one_note_time_;
+  int32_t total_note_time_;
+
+  int32_t tone_;
+  int32_t note_;
+  int32_t pitch_;
+  int32_t volume_;
+  int32_t effect_;
+
+  int32_t effect_time_;
+  int32_t effect_pitch_;
+  int32_t effect_volume_;
+
   void PlaySound();
   void Update();
   void NextSound();
-  int32_t NoteToPitch();
+  int32_t NoteToPitch(int32_t note);
   int32_t Lfo(int32_t time);
 };
 
