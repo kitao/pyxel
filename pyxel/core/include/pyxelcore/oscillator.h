9fe7b2fafc6cd62ba80b71f337683ec47e13bb13
Takashi Kitao <takashi.kitao@gmail.com>
2019-05-19 01:02:49 +0900

    Implemented the Audio-related classes

--- pyxel/core/include/pyxelcore/oscillator.h
+++ pyxel/core/include/pyxelcore/oscillator.h
@@ -34,6 +34,98 @@ class Oscillator {
   int32_t Noise(int32_t period, int32_t phase);
 };
 
+inline Oscillator::Oscillator() {
+  phase_ = 0;
+  tone_ = nullptr;
+  period_ = 0;
+  volume_ = 0;
+
+  next_tone_ = nullptr;
+  next_period_ = 0;
+  next_volume_ = 0;
+
+  noise_seed_ = 0x8000;
+  noise_last_ = 0;
+}
+
+inline void Oscillator::SetTone(int32_t tone) {
+  if (tone == TONE_TRIANGLE) {
+    next_tone_ = &Oscillator::Triangle;
+  } else if (tone == TONE_SQUARE) {
+    next_tone_ = &Oscillator::Square;
+  } else if (tone == TONE_PULSE) {
+    next_tone_ = &Oscillator::Pulse;
+  } else if (tone == TONE_NOISE) {
+    next_tone_ = &Oscillator::Noise;
+  } else {
+    next_tone_ = nullptr;
+  }
+}
+
+inline void Oscillator::SetPeriod(int32_t period) {
+  next_period_ = period;
+}
+
+inline void Oscillator::SetVolume(int32_t volume) {
+  next_volume_ = volume;
+}
+
+inline void Oscillator::Stop() {
+  next_tone_ = nullptr;
+  next_period_ = 0;
+  next_volume_ = 0;
+}
+
+inline int32_t Oscillator::Output() {
+  if (phase_ == 0) {
+    period_ = next_period_;
+    tone_ = next_tone_;
+    volume_ = next_volume_;
+  }
+
+  int32_t output;
+
+  if (tone_) {
+    output = (this->*tone_)(period_, phase_) * volume_;
+    phase_ = (phase_ + 1) % period_;
+  } else {
+    output = 0;
+  }
+
+  return output;
+}
+
+inline int32_t Oscillator::Triangle(int32_t period, int32_t phase) {
+  float x = static_cast<float>(phase) / period + 0.25f;
+  x -= static_cast<int32_t>(x);
+
+  return Abs(x * 4.0f - 2.0f) - 1.0f;
+}
+
+inline int32_t Oscillator::Square(int32_t period, int32_t phase) {
+  float x = static_cast<float>(phase) / period;
+  x -= static_cast<int32_t>(x);
+
+  return (x < 0.5f ? 1.0f : -1.0f) * 0.2f;
+}
+
+inline int32_t Oscillator::Pulse(int32_t period, int32_t phase) {
+  float x = static_cast<float>(phase) / period;
+  x -= static_cast<int32_t>(x);
+
+  return (x < 0.25f ? 1.0f : -1.0f) * 0.2f;
+}
+
+inline int32_t Oscillator::Noise(int32_t period, int32_t phase) {
+  if (phase % (period / 4) == 0) {
+    noise_seed_ >>= 1;
+    noise_seed_ |= ((noise_seed_ ^ (noise_seed_ >> 1)) & 1) << 15;
+    noise_last_ = noise_seed_ & 1;
+  }
+
+  return noise_last_ * 0.5f;
+}
+
 }  // namespace pyxelcore
 
 #endif  // PYXELCORE_OSCILLATOR_H_
