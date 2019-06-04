8902196fcdcad56e4f92ad0ef1886dcc0b3eaae6
Takashi Kitao <takashi.kitao@gmail.com>
2019-06-05 00:29:20 +0900

    Refactored the code

--- pyxel/core/src/pyxelcore/tilemap.cc
+++ pyxel/core/src/pyxelcore/tilemap.cc
@@ -12,11 +12,17 @@ Tilemap::Tilemap(int32_t width, int32_t height) {
   width_ = width;
   height_ = height;
   rect_ = pyxelcore::Rectangle(0, 0, width, height);
-  data_ = new int32_t[width * height]();
   image_index_ = 0;
+
+  data_ = new int32_t*[height];
+  data_[0] = new int32_t[width * height]();
+  for (int32_t i = 1; i < height; i++) {
+    data_[i] = data_[0] + width * i;
+  }
 }
 
 Tilemap::~Tilemap() {
+  delete data_[0];
   delete data_;
 }
 
@@ -26,7 +32,7 @@ int32_t Tilemap::GetValue(int32_t x, int32_t y) const {
     return 0;
   }
 
-  return data_[width_ * y + x];
+  return data_[y][x];
 }
 
 void Tilemap::SetValue(int32_t x, int32_t y, int32_t value) {
@@ -38,7 +44,7 @@ void Tilemap::SetValue(int32_t x, int32_t y, int32_t value) {
     PRINT_ERROR("invalid value");
   }
 
-  data_[width_ * y + x] = value;
+  data_[y][x] = value;
 }
 
 void Tilemap::SetData(int32_t x,
@@ -53,11 +59,11 @@ void Tilemap::SetData(int32_t x,
   }
 
   Tilemap tilemap = Tilemap(width, height);
-  int32_t* dst_data = tilemap.data_;
+  int32_t** dst_data = tilemap.data_;
 
   for (int32_t i = 0; i < height; i++) {
-    int32_t index = width * i;
     std::string str = tilemap_string[i];
+    int32_t* dst_line = dst_data[i];
 
     for (int32_t j = 0; j < width; j++) {
       int32_t value = std::stoi(str.substr(j * 3, 3), nullptr, 16);
@@ -67,7 +73,7 @@ void Tilemap::SetData(int32_t x,
         value = 0;
       }
 
-      dst_data[index + j] = value;
+      dst_line[j] = value;
     }
   }
 
@@ -88,18 +94,15 @@ void Tilemap::CopyTilemap(int32_t x,
     return;
   }
 
-  int32_t src_width = tilemap->width_;
-  int32_t* src_data = tilemap->data_;
-
-  int32_t dst_width = width_;
-  int32_t* dst_data = data_;
+  int32_t** src_data = tilemap->data_;
+  int32_t** dst_data = data_;
 
   for (int32_t i = 0; i < copy_area.height; i++) {
-    int32_t src_index = src_width * (copy_area.v + i) + copy_area.u;
-    int32_t dst_index = dst_width * (copy_area.y + i) + copy_area.x;
+    int32_t* src_line = src_data[copy_area.v + i];
+    int32_t* dst_line = dst_data[copy_area.y + i];
 
     for (int32_t j = 0; j < copy_area.width; j++) {
-      dst_data[dst_index + j] = src_data[src_index + j];
+      dst_line[copy_area.x + j] = src_line[copy_area.x + j];
     }
   }
 }
