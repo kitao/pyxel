f7ea5f87baf915ab5c90d06976f523e79f8289a5
Takashi Kitao <takashi.kitao@gmail.com>
2019-06-09 22:21:41 +0900

    Implemented the Resource module (WIP)

--- pyxel/core/include/pyxelcore/resource.h
+++ pyxel/core/include/pyxelcore/resource.h
@@ -5,10 +5,34 @@
 
 namespace pyxelcore {
 
+class Graphics;
+class Audio;
+class Image;
+class Tilemap;
+class Sound;
+class Music;
+
 class Resource {
  public:
-  bool SaveAsset(const char* filename);
-  bool LoadAsset(const char* filename);
+  Resource(Graphics* graphics, Audio* audio);
+
+  bool SaveAsset(const std::string& filename);
+  bool LoadAsset(const std::string& filename);
+
+ private:
+  Graphics* graphics_;
+  Audio* audio_;
+
+  void DumpImage(std::stringstream& ss, int32_t image_index);
+  void DumpTilemap(std::stringstream& ss, int32_t tilemap_index);
+  void DumpSound(std::stringstream& ss, int32_t sound_index);
+  void DumpMusic(std::stringstream& ss, int32_t music_index);
+
+  void ParseVersion(std::stringstream& ss);
+  void ParseImage(std::stringstream& ss, int32_t image_index);
+  void ParseTilemap(std::stringstream& ss, int32_t tilemap_index);
+  void ParseSound(std::stringstream& ss, int32_t sound_index);
+  void ParseMusic(std::stringstream& ss, int32_t music_index);
 };
 
 }  // namespace pyxelcore
