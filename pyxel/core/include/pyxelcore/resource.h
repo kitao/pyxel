095eb3da765f15b4bff54a088eab800b847769ef
Takashi Kitao <takashi.kitao@gmail.com>
2019-06-10 21:18:25 +0900

    Implemented the Resource module

--- pyxel/core/include/pyxelcore/resource.h
+++ pyxel/core/include/pyxelcore/resource.h
@@ -23,17 +23,46 @@ class Resource {
   Graphics* graphics_;
   Audio* audio_;
 
-  void DumpImage(std::stringstream& ss, int32_t image_index);
-  void DumpTilemap(std::stringstream& ss, int32_t tilemap_index);
-  void DumpSound(std::stringstream& ss, int32_t sound_index);
-  void DumpMusic(std::stringstream& ss, int32_t music_index);
+  std::string DumpImage(int32_t image_index) const;
+  std::string DumpTilemap(int32_t tilemap_index) const;
+  std::string DumpSound(int32_t sound_index) const;
+  std::string DumpMusic(int32_t music_index) const;
 
-  void ParseVersion(std::stringstream& ss);
-  void ParseImage(std::stringstream& ss, int32_t image_index);
-  void ParseTilemap(std::stringstream& ss, int32_t tilemap_index);
-  void ParseSound(std::stringstream& ss, int32_t sound_index);
-  void ParseMusic(std::stringstream& ss, int32_t music_index);
+  void ParseImage(int32_t image_index, const std::string& str);
+  void ParseTilemap(int32_t tilemap_index, const std::string& str);
+  void ParseSound(int32_t sound_index, const std::string& str);
+  void ParseMusic(int32_t music_index, const std::string& str);
+
+  static std::string GetVersionName();
+  static std::string GetImageName(int32_t image_index);
+  static std::string GetTilemapName(int32_t tilemap_index);
+  static std::string GetSoundName(int32_t sound_index);
+  static std::string GetMusicName(int32_t music_index);
 };
+
+inline std::string Resource::GetVersionName() {
+  return std::string(RESOURCE_ARCHIVE_DIRNAME) + "version";
+}
+
+inline std::string Resource::GetImageName(int32_t image_index) {
+  return std::string(RESOURCE_ARCHIVE_DIRNAME) + "image" +
+         std::to_string(image_index);
+}
+
+inline std::string Resource::GetTilemapName(int32_t tilemap_index) {
+  return std::string(RESOURCE_ARCHIVE_DIRNAME) + "tilemap" +
+         std::to_string(tilemap_index);
+}
+
+inline std::string Resource::GetSoundName(int32_t sound_index) {
+  return std::string(RESOURCE_ARCHIVE_DIRNAME) + "sound" +
+         (sound_index < 10 ? "0" : "") + std::to_string(sound_index);
+}
+
+inline std::string Resource::GetMusicName(int32_t music_index) {
+  return std::string(RESOURCE_ARCHIVE_DIRNAME) + "music" +
+         std::to_string(music_index);
+}
 
 }  // namespace pyxelcore
 
