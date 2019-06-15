63043f7ae10f3c04155a6cfee214e774c1aeb37c
Takashi Kitao <takashi.kitao@gmail.com>
2019-06-15 23:42:11 +0900

    Fixed the way to get strings from C

--- pyxel/core/__init__.py
+++ pyxel/core/__init__.py
@@ -38,7 +38,7 @@ def _setup_api(name, restype, argtypes):
 # Constants
 #
 _setup_api("_get_constant_number", c_int32, [c_char_p])
-_setup_api("_get_constant_string", c_char_p, [c_char_p])
+_setup_api("_get_constant_string", None, [c_char_p, c_int32, c_char_p])
 
 #
 # System
@@ -53,7 +53,7 @@ _setup_api(
 _setup_api("run", None, [CFUNCTYPE(None), CFUNCTYPE(None)])
 _setup_api("quit", None, [])
 
-_setup_api("_drop_file_getter", c_char_p, [])
+_setup_api("_drop_file_getter", None, [c_char_p, c_int32])
 _setup_api("_caption", None, [c_char_p])
 
 #
