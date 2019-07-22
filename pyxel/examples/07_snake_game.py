4fad42a5f41dacea2bb6f13816b35774c1d1e330
Takashi Kitao <takashi.kitao@gmail.com>
2019-07-22 19:48:12 +0900

    Added the example #7

---
+++ pyxel/examples/07_snake_game.py
@@ -0,0 +1,302 @@
+"""Snake implemented with pyxel.
+
+This is the game of snake in pyxel version!
+
+Try and collect the tasty apples without running
+into the side or yourself.
+
+Controls are the arrow keys ← ↑ → ↓
+
+Q: Quit the game
+R: Restart the game
+
+Created by Marcus Croucher in 2018.
+"""
+
+from collections import deque, namedtuple
+from random import randint
+
+import pyxel
+
+Point = namedtuple("Point", ["x", "y"])  # Convenience class for coordinates
+
+#############
+# Constants #
+#############
+
+COL_BACKGROUND = 3
+COL_BODY = 11
+COL_HEAD = 7
+COL_DEATH = 8
+COL_APPLE = 8
+
+TEXT_DEATH = ["GAME OVER", "(Q)UIT", "(R)ESTART"]
+COL_TEXT_DEATH = 0
+HEIGHT_DEATH = 5
+
+WIDTH = 40
+HEIGHT = 50
+
+HEIGHT_SCORE = pyxel.FONT_HEIGHT
+COL_SCORE = 6
+COL_SCORE_BACKGROUND = 5
+
+UP = Point(0, -1)
+DOWN = Point(0, 1)
+RIGHT = Point(1, 0)
+LEFT = Point(-1, 0)
+
+START = Point(5, 5 + HEIGHT_SCORE)
+
+###################
+# The game itself #
+###################
+
+
+class Snake:
+    """The class that sets up and runs the game."""
+
+    def __init__(self):
+        """Initiate pyxel, set up initial game variables, and run."""
+
+        pyxel.init(WIDTH, HEIGHT, caption="Snake!", scale=8, fps=20)
+        self.music = Music()
+        self.reset()
+        pyxel.run(self.update, self.draw)
+
+    def reset(self):
+        """Initiate key variables (direction, snake, apple, score, etc.)"""
+
+        self.direction = RIGHT
+        self.snake = deque()
+        self.snake.append(START)
+        self.death = False
+        self.score = 0
+        self.generate_apple()
+        self.music.start_music()
+
+    ##############
+    # Game logic #
+    ##############
+
+    def update(self):
+        """Update logic of game. Updates the snake and checks for scoring/win condition."""
+
+        if not self.death:
+            self.update_direction()
+            self.update_snake()
+            self.check_death()
+            self.check_apple()
+
+        if pyxel.btn(pyxel.KEY_Q):
+            pyxel.quit()
+
+        if pyxel.btnp(pyxel.KEY_R):
+            self.reset()
+
+    def update_direction(self):
+        """Watch the keys and change direction."""
+
+        if pyxel.btn(pyxel.KEY_UP):
+            if self.direction is not DOWN:
+                self.direction = UP
+        elif pyxel.btn(pyxel.KEY_DOWN):
+            if self.direction is not UP:
+                self.direction = DOWN
+        elif pyxel.btn(pyxel.KEY_LEFT):
+            if self.direction is not RIGHT:
+                self.direction = LEFT
+        elif pyxel.btn(pyxel.KEY_RIGHT):
+            if self.direction is not LEFT:
+                self.direction = RIGHT
+
+    def update_snake(self):
+        """Move the snake based on the direction."""
+
+        old_head = self.snake[0]
+        new_head = Point(old_head.x + self.direction.x, old_head.y + self.direction.y)
+        self.snake.appendleft(new_head)
+        self.popped_point = self.snake.pop()
+
+    def check_apple(self):
+        """Check whether the snake is on an apple."""
+
+        if self.snake[0] == self.apple:
+            self.score += 1
+            self.music.sfx_apple()
+            self.snake.append(self.popped_point)
+            self.generate_apple()
+
+    def generate_apple(self):
+        """Generate an apple randomly."""
+        snake_pixels = set(self.snake)
+
+        self.apple = self.snake[0]
+        while self.apple in snake_pixels:
+            x = randint(0, WIDTH - 1)
+            y = randint(HEIGHT_SCORE + 1, HEIGHT - 1)
+            self.apple = Point(x, y)
+
+    def check_death(self):
+        """Check whether the snake has died (out of bounds or doubled up.)"""
+
+        head = self.snake[0]
+        if head.x < 0 or head.y <= HEIGHT_SCORE or head.x >= WIDTH or head.y >= HEIGHT:
+            self.death_event()
+        elif len(self.snake) != len(set(self.snake)):
+            self.death_event()
+
+    def death_event(self):
+        """Kill the game (bring up end screen)."""
+        self.music.sfx_death()
+        self.music.stop_music()
+        self.death = True  # Check having run into self
+
+    ##############
+    # Draw logic #
+    ##############
+
+    def draw(self):
+        """Draw the background, snake, score, and apple OR the end screen."""
+
+        if not self.death:
+            pyxel.cls(col=COL_BACKGROUND)
+            self.draw_snake()
+            self.draw_score()
+            pyxel.pix(self.apple.x, self.apple.y, col=COL_APPLE)
+
+        else:
+            self.draw_death()
+
+    def draw_snake(self):
+        """Draw the snake with a distinct head by iterating through deque."""
+
+        for i, point in enumerate(self.snake):
+            if i == 0:
+                colour = COL_HEAD
+            else:
+                colour = COL_BODY
+            pyxel.pix(point.x, point.y, col=colour)
+
+    def draw_score(self):
+        """Draw the score at the top."""
+
+        score = "{:04}".format(self.score)
+        pyxel.rect(0, 0, WIDTH, HEIGHT_SCORE, COL_SCORE_BACKGROUND)
+        pyxel.text(1, 1, score, COL_SCORE)
+
+    def draw_death(self):
+        """Draw a blank screen with some text."""
+
+        pyxel.cls(col=COL_DEATH)
+        display_text = TEXT_DEATH[:]
+        display_text.insert(1, "{:04}".format(self.score))
+        for i, text in enumerate(display_text):
+            y_offset = (pyxel.FONT_HEIGHT + 2) * i
+            text_x = self.center_text(text, WIDTH)
+            pyxel.text(text_x, HEIGHT_DEATH + y_offset, text, COL_TEXT_DEATH)
+
+    @staticmethod
+    def center_text(text, page_width, char_width=pyxel.FONT_WIDTH):
+        """Helper function for calcuating the start x value for centered text."""
+
+        text_width = len(text) * char_width
+        return (page_width - text_width) // 2
+
+
+###########################
+# Music and sound effects #
+###########################
+
+
+class Music:
+    def __init__(self):
+        """Define sound and music."""
+
+        # Sound effects
+        pyxel.sound(0).set(
+            note="c3e3g3c4c4", tone="s", volume="4", effect=("n" * 4 + "f"), speed=7
+        )
+        pyxel.sound(1).set(
+            note="f3 b2 f2 b1  f1 f1 f1 f1",
+            tone="p",
+            volume=("4" * 4 + "4321"),
+            effect=("n" * 7 + "f"),
+            speed=9,
+        )
+
+        melody1 = (
+            "c3 c3 c3 d3 e3 r e3 r"
+            + ("r" * 8)
+            + "e3 e3 e3 f3 d3 r c3 r"
+            + ("r" * 8)
+            + "c3 c3 c3 d3 e3 r e3 r"
+            + ("r" * 8)
+            + "b2 b2 b2 f3 d3 r c3 r"
+            + ("r" * 8)
+        )
+
+        melody2 = (
+            "rrrr e3e3e3e3 d3d3c3c3 b2b2c3c3"
+            + "a2a2a2a2 c3c3c3c3 d3d3d3d3 e3e3e3e3"
+            + "rrrr e3e3e3e3 d3d3c3c3 b2b2c3c3"
+            + "a2a2a2a2 g2g2g2g2 c3c3c3c3 g2g2a2a2"
+            + "rrrr e3e3e3e3 d3d3c3c3 b2b2c3c3"
+            + "a2a2a2a2 c3c3c3c3 d3d3d3d3 e3e3e3e3"
+            + "f3f3f3a3 a3a3a3a3 g3g3g3b3 b3b3b3b3"
+            + "b3b3b3b4 rrrr e3d3c3g3 a2g2e2d2"
+        )
+
+        # Music
+        pyxel.sound(2).set(
+            note=melody1 * 2 + melody2 * 2,
+            tone="s",
+            volume=("3"),
+            effect=("nnnsffff"),
+            speed=20,
+        )
+
+        harmony1 = (
+            "a1 a1 a1 b1  f1 f1 c2 c2"
+            "c2 c2 c2 c2  g1 g1 b1 b1" * 3
+            + "f1 f1 f1 f1 f1 f1 f1 f1 g1 g1 g1 g1 g1 g1 g1 g1"
+        )
+        harmony2 = (
+            ("f1" * 8 + "g1" * 8 + "a1" * 8 + ("c2" * 7 + "d2")) * 3
+            + "f1" * 16
+            + "g1" * 16
+        )
+
+        pyxel.sound(3).set(
+            note=harmony1 * 2 + harmony2 * 2, tone="t", volume="5", effect="f", speed=20
+        )
+        pyxel.sound(4).set(
+            note=("f0 r a4 r  f0 f0 a4 r" "f0 r a4 r   f0 f0 a4 f0"),
+            tone="n",
+            volume="6622 6622 6622 6426",
+            effect="f",
+            speed=20,
+        )
+
+    def sfx_apple(self):
+        """Play apple collection sound."""
+        pyxel.play(ch=0, snd=0)
+
+    def sfx_death(self):
+        """Play death collection sound."""
+        pyxel.play(ch=0, snd=1)
+
+    def start_music(self):
+        """Start all music tracks (channels 1 - 3)."""
+        music_tracks = [2, 3, 4]
+        for ch, snd in enumerate(music_tracks):
+            pyxel.play(ch=(ch + 1), snd=snd, loop=True)
+
+    def stop_music(self):
+        """Stop all music tracks (channels 1 - 3)."""
+        for ch in range(1, 4):
+            pyxel.stop(ch=ch)
+
+
+if __name__ == "__main__":
+    Snake()
