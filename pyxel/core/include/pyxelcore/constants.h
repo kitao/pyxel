889ac28537242a0e0ed39c732dfa4e4e861eacbb
Takashi Kitao <takashi.kitao@gmail.com>
2019-08-17 00:10:56 +0900

    Updated the version number

--- pyxel/core/include/pyxelcore/constants.h
+++ pyxel/core/include/pyxelcore/constants.h
@@ -21,7 +21,7 @@ namespace pyxelcore {
 //
 // Common
 //
-const std::string VERSION = "1.2.4";
+const std::string VERSION = "1.2.5";
 const int32_t COLOR_COUNT = 16;
 const std::string WHITESPACE = " \t\v\r\n";
 
