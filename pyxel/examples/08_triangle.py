3ec54e86b46a71896670a7cd44f62802ea26f775
Takashi Kitao <takashi.kitao@gmail.com>
2019-11-12 22:22:19 +0900

    Updated the example #8

--- pyxel/examples/08_triangle.py
+++ pyxel/examples/08_triangle.py
@@ -3,7 +3,7 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(200, 110)
+        pyxel.init(200, 150)
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,7 +12,11 @@ class App:
     def draw(self):
         pyxel.cls(5)
 
-        pyxel.tri(100, 0, 0, 100, 199, 100, 8)
+        pyxel.text(6, 6, "tri(x1,y1,x2,y2,x3,y3,col)", 7)
+        pyxel.text(6, 20, "trib(x1,y1,x2,y2,x3,y3,col)", 7)
+
+        pyxel.tri(150, 0, 0, 100, 180, 100, 8)
+        pyxel.trib(150, 100, 0, 140, 180, 140, 3)
 
 
 App()
