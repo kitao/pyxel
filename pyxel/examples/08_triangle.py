37e2bedeb1618c8276f22cd52656436ff3af22a1
Takashi Kitao <takashi.kitao@gmail.com>
2019-11-17 11:18:13 +0900

    Completed the example #8

--- pyxel/examples/08_triangle.py
+++ pyxel/examples/08_triangle.py
@@ -1,22 +1,39 @@
 import pyxel
 
 
-class App:
-    def __init__(self):
-        pyxel.init(200, 150)
-        pyxel.run(self.update, self.draw)
+def draw_triangle(x1, y1, x2, y2, x3, y3, n):
+    if n == 0:
+        return
 
-    def update(self):
-        pass
+    col = n + 8
 
-    def draw(self):
-        pyxel.cls(5)
+    if n % 2 == 0:
+        pyxel.tri(x1, y1, x2, y2, x3, y3, col)
+    else:
+        pyxel.trib(x1, y1, x2, y2, x3, y3, col)
 
-        pyxel.text(6, 6, "tri(x1,y1,x2,y2,x3,y3,col)", 7)
-        pyxel.text(6, 20, "trib(x1,y1,x2,y2,x3,y3,col)", 7)
+    h1 = (x1 + x2) / 2
+    w1 = (y1 + y2) / 2
+    h2 = (x2 + x3) / 2
+    w2 = (y2 + y3) / 2
+    h3 = (x3 + x1) / 2
+    w3 = (y3 + y1) / 2
 
-        pyxel.tri(150, 0, 0, 100, 180, 100, 8)
-        pyxel.trib(150, 100, 0, 140, 180, 140, 3)
+    triangle_list.append((x1, y1, h1, w1, h3, w3, n - 1))
+    triangle_list.append((h1, w1, x2, y2, h2, w2, n - 1))
+    triangle_list.append((h3, w3, h2, w2, x3, y3, n - 1))
 
 
-App()
+triangle_list = [(100, 24, 7, 143, 193, 143, 7)]
+
+pyxel.init(200, 150)
+pyxel.cls(1)
+pyxel.text(6, 6, "tri(x1,y1,x2,y2,x3,y3,col)", 7)
+pyxel.text(6, 14, "trib(x1,y1,x2,y2,x3,y3,col)", 7)
+
+while True:
+    if triangle_list:
+        t = triangle_list.pop(0)
+        draw_triangle(*t)
+
+    pyxel.flip()
