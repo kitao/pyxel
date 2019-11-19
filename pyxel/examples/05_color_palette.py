fb0c9370211fd321c5f5756f87df71c2a6e3c4dc
Takashi Kitao <takashi.kitao@gmail.com>
2019-11-19 20:31:20 +0900

    Modified the example #5

--- pyxel/examples/05_color_palette.py
+++ pyxel/examples/05_color_palette.py
@@ -1,37 +1,24 @@
 import pyxel
 
 
-class App:
-    def __init__(self):
-        pyxel.init(255, 81, caption="Pyxel Color Palette")
-        pyxel.run(self.update, self.draw)
+def draw_palette(x, y, col):
+    col_val = pyxel.DEFAULT_PALETTE[col]
+    hex_col = "#{:06X}".format(col_val)
+    rgb_col = "{},{},{}".format(col_val >> 16, (col_val >> 8) & 0xFF, col_val & 0xFF)
 
-    def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
+    pyxel.rect(x, y, 13, 13, col)
+    pyxel.text(x + 16, y + 1, hex_col, 7)
+    pyxel.text(x + 16, y + 8, rgb_col, 7)
+    pyxel.text(x + 5 - (col // 10) * 2, y + 4, "{}".format(col), 7 if col < 6 else 0)
 
-    def draw(self):
-        pyxel.cls(0)
-
-        for i in range(16):
-            self.draw_palette(2 + (i % 4) * 64, 4 + (i // 4) * 20, i)
-
-    def draw_palette(self, x, y, col):
-        col_val = pyxel.DEFAULT_PALETTE[col]
-        hex_col = "#{:06X}".format(col_val)
-        rgb_col = "{},{},{}".format(
-            col_val >> 16, (col_val >> 8) & 0xFF, col_val & 0xFF
-        )
-
-        pyxel.rect(x, y, 13, 13, col)
-        pyxel.text(x + 16, y + 1, hex_col, 7)
-        pyxel.text(x + 16, y + 8, rgb_col, 7)
-        pyxel.text(
-            x + 5 - (col // 10) * 2, y + 4, "{}".format(col), 7 if col < 6 else 0
-        )
-
-        if col == 0:
-            pyxel.rectb(x, y, 13, 13, 5)
+    if col == 0:
+        pyxel.rectb(x, y, 13, 13, 5)
 
 
-App()
+pyxel.init(255, 81, caption="Pyxel Color Palette")
+pyxel.cls(0)
+
+for i in range(16):
+    draw_palette(2 + (i % 4) * 64, 4 + (i // 4) * 20, i)
+
+pyxel.show()
