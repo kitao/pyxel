d1e3ad5f4026415713bdaf30307d3ce695d2f4b4
Takashi Kitao <takashi.kitao@gmail.com>
2020-01-11 15:56:01 +0900

    Changed the text color

--- pyxel/examples/05_color_palette.py
+++ pyxel/examples/05_color_palette.py
@@ -3,16 +3,19 @@ import pyxel
 
 def draw_palette(x, y, col):
     col_val = pyxel.DEFAULT_PALETTE[col]
+    r, g, b = col_val >> 16, (col_val >> 8) & 0xFF, col_val & 0xFF
     hex_col = "#{:06X}".format(col_val)
-    rgb_col = "{},{},{}".format(col_val >> 16, (col_val >> 8) & 0xFF, col_val & 0xFF)
+    rgb_col = "{},{},{}".format(r, g, b)
 
     pyxel.rect(x, y, 13, 13, col)
     pyxel.text(x + 16, y + 1, hex_col, 7)
     pyxel.text(x + 16, y + 8, rgb_col, 7)
-    pyxel.text(x + 5 - (col // 10) * 2, y + 4, "{}".format(col), 7 if col < 6 else 0)
+    pyxel.text(
+        x + 5 - (col // 10) * 2, y + 4, "{}".format(col), 0 if (r + g + b) > 192 else 7
+    )
 
     if col == 0:
-        pyxel.rectb(x, y, 13, 13, 5)
+        pyxel.rectb(x, y, 13, 13, 13)
 
 
 pyxel.init(255, 81, caption="Pyxel Color Palette")
