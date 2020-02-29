c8ca8fba79cb51ea04e296b6ed759c93e9b0854c
Takashi Kitao <takashi.kitao@gmail.com>
2020-02-29 11:19:28 +0900

    Changed the way to make an animeted GIF

--- pyxel/core/include/pyxelcore/gif_writer.h
+++ pyxel/core/include/pyxelcore/gif_writer.h
@@ -9,11 +9,14 @@ class Image;
 
 class GifWriter {
  public:
+  static const int32_t TRANSPARENT_COLOR = COLOR_COUNT;
+
   GifWriter(const std::string& filename,
             int32_t width,
             int32_t height,
             const PaletteColor& palette_color,
             int32_t delay_time);
+  ~GifWriter();
 
   void AddFrame(const Image* image);
   void EndFrame();
@@ -23,6 +26,7 @@ class GifWriter {
   int32_t height_;
   int32_t delay_time_;
   std::ofstream ofs_;
+  int32_t* last_frame_data_;
 };
 
 }  // namespace pyxelcore
