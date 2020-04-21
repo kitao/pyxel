98ea18ca09f6c0e9d83da05ad5241b54aa0692c8
Takashi Kitao <takashi.kitao@gmail.com>
2020-04-22 08:12:07 +0900

    Added support for variable frame rate

--- pyxel/core/include/pyxelcore/recorder.h
+++ pyxel/core/include/pyxelcore/recorder.h
@@ -19,17 +19,18 @@ class Recorder {
   void ResetScreenCapture();
   void SaveScreenCapture();
 
-  void Update(const Image* screen_image);
+  void Update(const Image* screen_image, int32_t update_frame_count);
 
  private:
   int32_t width_;
   int32_t height_;
   PaletteColor palette_color_;
-  int32_t delay_time_;
+  int32_t fps_;
   int32_t cur_frame_;
   int32_t start_frame_;
   int32_t frame_count_;
   Image* captured_images_[SCREEN_CAPTURE_COUNT];
+  int32_t captured_frames_[SCREEN_CAPTURE_COUNT];
 
   std::string GetBaseName() const;
 };
