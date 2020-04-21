cb6d24733ea582e1d27800d510cf7519e018df28
Takashi Kitao <takashi.kitao@gmail.com>
2020-04-22 08:10:11 +0900

    Added support for variable frame rate

--- pyxel/core/include/pyxelcore/gif_writer.h
+++ pyxel/core/include/pyxelcore/gif_writer.h
@@ -14,17 +14,15 @@ class GifWriter {
   GifWriter(const std::string& filename,
             int32_t width,
             int32_t height,
-            const PaletteColor& palette_color,
-            int32_t delay_time);
+            const PaletteColor& palette_color);
   ~GifWriter();
 
-  void AddFrame(const Image* image);
+  void AddFrame(const Image* image, int32_t delay_time);
   void EndFrame();
 
  private:
   int32_t width_;
   int32_t height_;
-  int32_t delay_time_;
   std::ofstream ofs_;
   int32_t* last_frame_data_;
 };
