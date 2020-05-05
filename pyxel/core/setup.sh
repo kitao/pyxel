21615b7d4c85091dc934ac7a76acc03156062935
Takashi Kitao <takashi.kitao@gmail.com>
2020-05-05 18:08:47 +0900

    Updated the library downloader script

--- pyxel/core/setup.sh
+++ pyxel/core/setup.sh
@@ -28,34 +28,45 @@
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 DOWNLOAD_DIR=$SCRIPT_DIR/download
-SDL2_INCDIR=$SCRIPT_DIR/include/SDL2
-SDL2_LIBDIR=$SCRIPT_DIR/lib
-SDL2_BINDIR=$SCRIPT_DIR/bin
+INCDIR=$SCRIPT_DIR/include/SDL2
+LIBDIR=$SCRIPT_DIR/lib
+BINDIR=$SCRIPT_DIR/bin
 
-MINGW_SDL2_URL="https://www.libsdl.org/release/SDL2-devel-2.0.10-mingw.tar.gz"
+MINGW_SDL2_URL="https://www.libsdl.org/release/SDL2-devel-2.0.12-mingw.tar.gz"
 MINGW_SDL2_IMAGE_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-mingw.tar.gz"
 
-rm -rf $SDL2_INCDIR $SDL2_LIBDIR $SDL_BINDIR $DOWNLOAD_DIR
-mkdir -p $SDL2_INCDIR $SDL2_LIBDIR/win{32,64} $SDL2_BINDIR/win{32,64} $DOWNLOAD_DIR
+GIFSICLE_WIN32_URL="https://eternallybored.org/misc/gifsicle/releases/gifsicle-1.92-win32.zip"
+GIFSICLE_WIN64_URL="https://eternallybored.org/misc/gifsicle/releases/gifsicle-1.92-win64.zip"
+
+rm -rf $INCDIR $LIBDIR $SDL_BINDIR $DOWNLOAD_DIR
+mkdir -p $INCDIR $LIBDIR/win{32,64} $BINDIR/win{32,64} $DOWNLOAD_DIR
 
 cd $DOWNLOAD_DIR
 curl -L $MINGW_SDL2_URL -o SDL2.tar.gz
 tar xzf SDL2.tar.gz
 cd SDL2-*
-cp i686-w64-mingw32/include/SDL2/*.h $SDL2_INCDIR
-cp i686-w64-mingw32/lib/libSDL2{.dll,main}.a $SDL2_LIBDIR/win32
-cp i686-w64-mingw32/bin/SDL2.dll $SDL2_BINDIR/win32
-cp x86_64-w64-mingw32/lib/libSDL2{.dll,main}.a $SDL2_LIBDIR/win64
-cp x86_64-w64-mingw32/bin/SDL2.dll $SDL2_BINDIR/win64
+cp i686-w64-mingw32/include/SDL2/*.h $INCDIR
+cp i686-w64-mingw32/lib/libSDL2{.dll,main}.a $LIBDIR/win32
+cp i686-w64-mingw32/bin/SDL2.dll $BINDIR/win32
+cp x86_64-w64-mingw32/lib/libSDL2{.dll,main}.a $LIBDIR/win64
+cp x86_64-w64-mingw32/bin/SDL2.dll $BINDIR/win64
 
 cd $DOWNLOAD_DIR
 curl -L $MINGW_SDL2_IMAGE_URL -o SDL2_image.tar.gz
 tar xzf SDL2_image.tar.gz
 cd SDL2_image-*
-cp i686-w64-mingw32/include/SDL2/*.h $SDL2_INCDIR
-cp i686-w64-mingw32/lib/libSDL2_image.dll.a $SDL2_LIBDIR/win32
-cp i686-w64-mingw32/bin/*.dll $SDL2_BINDIR/win32
-cp x86_64-w64-mingw32/lib/libSDL2_image.dll.a $SDL2_LIBDIR/win64
-cp x86_64-w64-mingw32/bin/*.dll $SDL2_BINDIR/win64
+cp i686-w64-mingw32/include/SDL2/*.h $INCDIR
+cp i686-w64-mingw32/lib/libSDL2_image.dll.a $LIBDIR/win32
+cp i686-w64-mingw32/bin/*.dll $BINDIR/win32
+cp x86_64-w64-mingw32/lib/libSDL2_image.dll.a $LIBDIR/win64
+cp x86_64-w64-mingw32/bin/*.dll $BINDIR/win64
+
+cd $DOWNLOAD_DIR
+curl -L $GIFSICLE_WIN32_URL -o gifsicle-win32.zip
+curl -L $GIFSICLE_WIN64_URL -o gifsicle-win64.zip
+unzip -q gifsicle-win32.zip -d gifsicle-win32
+unzip -q gifsicle-win64.zip -d gifsicle-win64
+cp gifsicle-win32/gifsicle-*/gifsicle.exe $BINDIR/win32
+cp gifsicle-win64/gifsicle-*/gifsicle.exe $BINDIR/win64
 
 rm -rf $DOWNLOAD_DIR
