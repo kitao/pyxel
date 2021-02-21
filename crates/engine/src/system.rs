f8f46a4450031578f6e9de687409d47c1e486813
Takashi Kitao <takashi.kitao@gmail.com>
2021-02-21 22:29:06 +0900

    Added modules of Pyxel Engine

--- crates/engine/src/system.rs
+++ crates/engine/src/system.rs
@@ -1,13 +1,20 @@
-use sdl2::render::WindowCanvas as SdlWindowCanvas;
+use sdl2::render::WindowCanvas as SdlCanvas;
+//use sdl2::video::Window as SdlWindow;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use sdl2::EventPump as SdlEventPump;
 use sdl2::Sdl as SdlContext;
+use sdl2::VideoSubsystem as SdlVideo;
 
 global_instance!(System, system);
 
 pub struct System {
     sdl_context: SdlContext,
     sdl_event_pump: SdlEventPump,
-    sdl_canvas: SdlWindowCanvas,
+    sdl_video: SdlVideo,
+    //sdl_window: SdlWindow,
+    sdl_canvas: SdlCanvas,
 }
 
 pub fn init_system(name: &str, width: usize, height: usize) {
@@ -24,6 +31,8 @@ pub fn init_system(name: &str, width: usize, height: usize) {
     let system = System {
         sdl_context: sdl_context,
         sdl_event_pump: sdl_event_pump,
+        sdl_video: sdl_video,
+        //sdl_window: sdl_window,
         sdl_canvas: sdl_canvas,
     };
 
@@ -31,6 +40,30 @@ pub fn init_system(name: &str, width: usize, height: usize) {
 }
 
 impl System {
+    pub fn run(&mut self) {
+        'running: loop {
+            self.sdl_canvas.set_draw_color(Color::RGB(200, 200, 200));
+            self.sdl_canvas.clear();
+
+            //self.sdl_canvas.copy(&texture, None, None).expect("Render failed");
+
+            self.sdl_canvas.present();
+
+            for event in self.sdl_event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    _ => {}
+                }
+            }
+
+            //thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));}
+        }
+    }
+
     pub fn set_window_caption(&self, caption: &str) {
         //
     }
@@ -64,6 +97,70 @@ impl System {
 mod tests {
     #[test]
     fn hoge() {
-        crate::init_system("test", 100, 100);
+        //
     }
 }
+
+/*
+class System {
+ public:
+  System(int32_t width,
+         int32_t height,
+         const std::string& caption,
+         int32_t scale,
+         const pyxelcore::PaletteColor& palette_color,
+         int32_t fps,
+         int32_t quit_key,
+         bool is_fullscreen);
+  ~System();
+
+  pyxelcore::Resource* Resource() const { return resource_; }
+  pyxelcore::Input* Input() const { return input_; }
+  pyxelcore::Graphics* Graphics() const { return graphics_; }
+  pyxelcore::Audio* Audio() const { return audio_; }
+  const pyxelcore::PaletteColor& PaletteColor() const { return palette_color_; }
+
+  int32_t Width() const { return window_->ScreenWidth(); }
+  int32_t Height() const { return window_->ScreenHeight(); }
+  int32_t FrameCount() const { return frame_count_; }
+
+  void Run(void (*update)(), void (*draw)());
+  bool Quit();
+  bool FlipScreen();
+  void ShowScreen();
+
+  std::string DropFile() const { return drop_file_; }
+  void SetCaption(const std::string& caption);
+
+ private:
+  pyxelcore::Resource* resource_;
+  pyxelcore::Input* input_;
+  pyxelcore::Graphics* graphics_;
+  pyxelcore::Audio* audio_;
+  Window* window_;
+  Recorder* recorder_;
+
+  pyxelcore::PaletteColor palette_color_;
+  int32_t quit_key_;
+  int32_t fps_;
+  int32_t frame_count_;
+  double one_frame_time_;
+  double next_update_time_;
+  std::string drop_file_;
+  bool is_loop_running_;
+  bool is_quit_requested_;
+  bool is_update_suspended_;
+
+  Profiler fps_profiler_;
+  Profiler update_profiler_;
+  Profiler draw_profiler_;
+  bool is_performance_monitor_on_;
+
+  int32_t WaitForUpdateTime();
+  bool UpdateFrame(void (*update)());
+  void CheckSpecialInput();
+  void DrawFrame(void (*draw)(), int32_t update_frame_count);
+  void DrawPerformanceMonitor();
+  void DrawMouseCursor();
+}
+*/
