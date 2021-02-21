07e6e1a8f97aa966b140df6e43c6b7ccb4363e29
Takashi Kitao <takashi.kitao@gmail.com>
2021-02-21 22:29:06 +0900

    Added modules of Pyxel Engine

--- crates/engine/src/audio.rs
+++ crates/engine/src/audio.rs
@@ -13,3 +13,29 @@ pub fn init_audio() {
 impl Audio {
     //
 }
+
+/*
+class Audio {
+public:
+Audio();
+~Audio();
+
+Sound* GetSoundBank(int32_t sound_index, bool system = false) const;
+Music* GetMusicBank(int32_t music_index) const;
+int32_t GetPlayPos(int32_t channel) const;
+void PlaySound(int32_t channel, int32_t sound_index, bool loop = false);
+void PlaySound(int32_t channel,
+const SoundIndexList& sound_index_list,
+bool loop = false);
+void PlayMusic(int32_t music_index, bool loop = false);
+void StopPlaying(int32_t channel = -1);
+
+private:
+SDL_AudioDeviceID audio_device_id_;
+Sound** sound_bank_;
+Music** music_bank_;
+Channel channel_[MUSIC_CHANNEL_COUNT];
+
+static void callback(void* userdata, uint8_t* stream, int len);
+}
+*/
--- crates/engine/src/input.rs
+++ crates/engine/src/input.rs
@@ -13,3 +13,40 @@ pub fn init_input() {
 impl Input {
     //
 }
+
+/*
+class Input {
+public:
+Input();
+~Input();
+
+int32_t MouseX() const { return mouse_x_; }
+int32_t MouseY() const { return mouse_y_; }
+int32_t MouseWheel() const { return mouse_wheel_; }
+
+bool IsButtonOn(int32_t key) const;
+bool IsButtonPressed(int32_t key,
+int32_t hold_frame = 0,
+int32_t period_frame = 0) const;
+bool IsButtonReleased(int32_t key) const;
+void SetMouseVisible(int32_t is_visible);
+
+bool IsMouseVisible() const { return is_mouse_visible_; }
+void Update(Window* window, int32_t frame_count);
+
+private:
+SDL_GameController* gamepad1_;
+SDL_GameController* gamepad2_;
+SDL_Cursor* blank_cursor_;
+SDL_Cursor* normal_cursor_;
+
+int32_t frame_count_;
+int32_t mouse_x_;
+int32_t mouse_y_;
+int32_t mouse_wheel_;
+bool is_mouse_visible_;
+int32_t key_state_[KEY_COUNT];
+
+void UpdateKeyState(int32_t key, bool state);
+};
+*/
