8ea37fc0ccd5bf0428ebae006711fe7bb4d6f25e
Takashi Kitao <takashi.kitao@gmail.com>
2021-03-21 23:14:04 +0900

    Modified the RectArea class specs

--- native/src/core/rectarea.nim
+++ native/src/core/rectarea.nim
@@ -1,12 +1,10 @@
-import options
-
 type
-  RectArea = object
+  RectArea* = object
     left, top: int
     right, bottom: int
     width, height: int
 
-proc initRectAreaFromPos*(x1, y1, x2, y2: int): RectArea =
+proc initRectAreaFromPos*(x1, y1, x2, y2: int): RectArea {.inline.} =
   if x1 < x2:
     result.left = x1
     result.right = x2
@@ -25,15 +23,16 @@ proc initRectAreaFromPos*(x1, y1, x2, y2: int): RectArea =
     result.bottom = y1
     result.height = y1 - y2 + 1
 
-proc initRectAreaFromSize*(left, top, width, height: int): RectArea =
-  doAssert(width > 0 and height > 0, "Invalid size")
-
-  result.left = left
-  result.top = top
-  result.right = left + width - 1
-  result.bottom = top + height - 1
-  result.width = width
-  result.height = height
+proc initRectAreaFromSize*(left, top, width, height: int): RectArea {.inline.} =
+  if width > 0 and height > 0:
+    result.left = left
+    result.top = top
+    result.right = left + width - 1
+    result.bottom = top + height - 1
+    result.width = width
+    result.height = height
+  else:
+    result = RectArea()
 
 proc left*(self: RectArea): int {.inline.} = self.left
 proc top*(self: RectArea): int {.inline.} = self.top
@@ -42,19 +41,28 @@ proc bottom*(self: RectArea): int {.inline.} = self.bottom
 proc width*(self: RectArea): int {.inline.} = self.width
 proc height*(self: RectArea): int {.inline.} = self.height
 
+proc isEmpty*(self: RectArea): bool {.inline.} =
+  result = self.width <= 0 or self.height <= 0
+
 proc contains*(self: RectArea, x, y: int): bool {.inline.} =
-  result = x >= self.left and x <= self.right and y >= self.top and y <= self.bottom
-
-proc intersects*(self: RectArea, rect: RectArea): Option[RectArea] {.inline.} =
-  let left = max(self.left, rect.left)
-  let top = max(self.top, rect.top)
-  let right = min(self.right, rect.right)
-  let bottom = min(self.bottom, rect.bottom)
-  let width = right - left + 1
-  let height = bottom - top + 1
-
-  if width <= 0 or height <= 0:
-    result = none(RectArea)
+  if self.isEmpty:
+    result = false
   else:
-    result = some(RectArea(left: left, top: top, right: right, bottom: bottom,
-        width: width, height: height))
+    result = x >= self.left and x <= self.right and y >= self.top and y <= self.bottom
+
+proc intersects*(self: RectArea, rect: RectArea): RectArea {.inline.} =
+  if self.isEmpty or rect.isEmpty:
+    result = RectArea()
+  else:
+    let left = max(self.left, rect.left)
+    let top = max(self.top, rect.top)
+    let right = min(self.right, rect.right)
+    let bottom = min(self.bottom, rect.bottom)
+    let width = right - left + 1
+    let height = bottom - top + 1
+
+    if width <= 0 or height <= 0:
+      result = RectArea()
+    else:
+      result = RectArea(left: left, top: top, right: right, bottom: bottom,
+          width: width, height: height)
