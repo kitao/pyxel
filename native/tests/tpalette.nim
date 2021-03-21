b979c2c088811575c36df4f04b665b9e7a6bb550
Takashi Kitao <takashi.kitao@gmail.com>
2021-03-21 23:13:17 +0900

    Implemented the Palette class

--- native/tests/tpalette.nim
+++ native/tests/tpalette.nim
@@ -1,86 +1,70 @@
 import unittest
 
-import palette
+import core/palette
 
 suite "Palette":
-  setup:
-    var palette = newPalette[3, uint8]()
+  test "types":
+    check(Rgb is uint32)
 
-  test "Constants":
-    check(Rgb24 is uint32)
-    check(palette.ColorCount == 3)
+  test "properties":
+    let pal = newPalette[uint8](3)
 
-    var a: palette.ColorType
-    check(a is uint8)
+    assert(pal.colorCount == 3)
+
+  test "operators":
+    let pal = newPalette[uint8](3)
+
+    for i in 0 ..< pal.colorCount:
+      check(pal[i] == i)
+
+    for i in 0 ..< pal.colorCount:
+      pal[i] = 2
+      check(pal[i] == 2)
+
+  test "reset":
+    let pal = newPalette[uint8](3)
+
+    for i in 0 ..< pal.colorCount:
+      pal[i] = 0
+
+    pal.reset()
+
+    for i in 0 ..< pal.colorCount:
+      check(pal[i] == i)
 
   test "getDisplayColor":
-    for i in 0 ..< palette.ColorCount:
-      check(palette.getDisplayColor(i) == 0)
+    let pal = newPalette[uint8](3)
+
+    for i in 0 ..< pal.colorCount:
+      check(pal.getDisplayColor(i) == 0)
 
     expect AssertionDefect:
-      discard palette.getDisplayColor(-1)
+      discard pal.getDisplayColor(-1)
 
     expect AssertionDefect:
-      discard palette.getDisplayColor(3)
+      discard pal.getDisplayColor(3)
 
   test "setDisplayColor":
-    check(palette.getDisplayColor(0) == 0)
-    palette.setDisplayColor(0, 0x112233)
-    check(palette.getDisplayColor(0) == 0x112233)
+    let pal = newPalette[uint8](3)
 
-    check(palette.getDisplayColor(2) == 0)
-    palette.setDisplayColor(2, 0x445566)
-    check(palette.getDisplayColor(2) == 0x445566)
+    check(pal.getDisplayColor(0) == 0)
+    pal.setDisplayColor(0, 0x112233)
+    check(pal.getDisplayColor(0) == 0x112233)
+
+    check(pal.getDisplayColor(2) == 0)
+    pal.setDisplayColor(2, 0x445566)
+    check(pal.getDisplayColor(2) == 0x445566)
 
     expect AssertionDefect:
-      palette.setDisplayColor(-1, 0)
+      pal.setDisplayColor(-1, 0)
 
     expect AssertionDefect:
-      palette.setDisplayColor(3, 0)
+      pal.setDisplayColor(3, 0)
 
   test "setDisplayColors":
-    palette.setDisplayColors([0x111111, 0x222222, 0x333333])
-    check(palette.getDisplayColor(0) == 0x111111)
-    check(palette.getDisplayColor(1) == 0x222222)
-    check(palette.getDisplayColor(2) == 0x333333)
+    let pal = newPalette[uint8](3)
 
-  test "getReplaceColor":
-    for i in 0 ..< palette.ColorCount:
-      check(palette.getReplaceColor(i) == i)
-
-    expect AssertionDefect:
-      discard palette.getReplaceColor(-1)
-
-    expect AssertionDefect:
-      discard palette.getReplaceColor(8)
-
-  test "setReplaceColor":
-    check(palette.getReplaceColor(0) == 0)
-    palette.setReplaceColor(0, 2)
-    check(palette.getReplaceColor(0) == 2)
-
-    check(palette.getReplaceColor(2) == 2)
-    palette.setReplaceColor(2, 0)
-    check(palette.getReplaceColor(2) == 0)
-
-    expect AssertionDefect:
-      palette.setReplaceColor(-1, 0)
-
-    expect AssertionDefect:
-      palette.setReplaceColor(3, 0)
-
-    expect AssertionDefect:
-      palette.setReplaceColor(0, -1)
-
-    expect AssertionDefect:
-      palette.setReplaceColor(0, 3)
-
-  test "resetReplaceColor":
-    for i in 0 ..< palette.ColorCount:
-      palette.setReplaceColor(i, 0)
-      check(palette.getReplaceColor(i) == 0)
-
-    palette.resetReplaceColor()
-
-    for i in 0 ..< palette.ColorCount:
-      check(palette.getReplaceColor(i) == i)
+    pal.setDisplayColors([0x111111, 0x222222, 0x333333])
+    check(pal.getDisplayColor(0) == 0x111111)
+    check(pal.getDisplayColor(1) == 0x222222)
+    check(pal.getDisplayColor(2) == 0x333333)
