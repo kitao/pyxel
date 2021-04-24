32fee8a9c36003ba7642768b75ba8e9c6d5478be
Takashi Kitao <takashi.kitao@gmail.com>
2021-04-24 21:06:12 +0900

    Modified the Palette class

--- pyxelcore/src/pyxelcore/palette.nim
+++ pyxelcore/src/pyxelcore/palette.nim
@@ -1,47 +1,44 @@
-import
-  sequtils,
-  sugar
+import sequtils
 
 type
-  Rgb* = uint32
-
-  Palette*[T] = ref object
+  Palette* = ref object
     colorCount: int
-    replaceTable: seq[T]
-    displayColors: seq[Rgb]
+    replaceTable: seq[int]
+    displayColors: seq[int]
 
-proc newPalette*[T](colorCount: int): Palette[T] =
-  doAssert(colorCount >= 0, "Invalid colorCount")
+proc newPalette*(colorCount: int): Palette =
+  doAssert(colorCount >= 0, "Invalid color count")
 
   new(result)
   result.colorCount = colorCount
-  result.replaceTable = toSeq(0 ..< colorCount).map(x => T(x))
-  result.displayColors = newSeq[uint32](colorCount)
+  result.replaceTable = toSeq(0 ..< colorCount)
+  result.displayColors = newSeq[int](colorCount)
 
 proc colorCount*(self: Palette): int {.inline.} = self.colorCount
 
 proc `[]`*(self: Palette, index: int): int {.inline.} =
-  assert(index >= 0 and index < self.colorCount, "Invalid index")
+  doAssert(index >= 0 and index < self.colorCount, "Invalid index")
 
-  result = int(self.replaceTable[index])
+  result = self.replaceTable[index]
 
-proc `[]=`*(self: Palette, index: int, value: int) {.inline.} =
-  assert(index >= 0 and index < self.colorCount, "Invalid index")
-  assert(value >= 0 and value < self.colorCount, "Invalid value")
+proc `[]=`*(self: Palette, index, value: int) {.inline.} =
+  doAssert(index >= 0 and index < self.colorCount, "Invalid index")
+  doAssert(value >= 0 and value < self.colorCount, "Invalid value")
 
-  self.replaceTable[index] = self.T(value)
+  self.replaceTable[index] = value
 
 proc reset*(self: Palette) =
   for i in 0 ..< self.colorCount:
-    self.replaceTable[i] = self.T(i)
+    self.replaceTable[i] = i
 
-proc getDisplayColor*(self: Palette, color: int): Rgb =
-  assert(color >= 0 and color < self.colorCount, "Invalid color")
+proc getDisplayColor*(self: Palette, color: int): int =
+  doAssert(color >= 0 and color < self.colorCount, "Invalid color")
 
   result = self.displayColors[color]
 
-proc setDisplayColor*(self: Palette, color: int, rgb: Rgb) =
-  assert(color >= 0 and color < self.colorCount, "Invalid color")
+proc setDisplayColor*(self: Palette, color: int, rgb: int) =
+  doAssert(color >= 0 and color < self.colorCount, "Invalid color")
+  doAssert(rgb >= 0x000000 and rgb <= 0xffffff, "Invalid rgb")
 
   self.displayColors[color] = rgb
 
@@ -49,4 +46,4 @@ proc setDisplayColors*(self: Palette, rgbs: openArray[int]) =
   doAssert(rgbs.len == self.colorCount, "Invalid lengh array")
 
   for i, rgb in rgbs:
-    self.displayColors[i] = Rgb(rgb)
+    self.setDisplayColor(i, rgb)
