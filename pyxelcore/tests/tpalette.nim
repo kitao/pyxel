23fb46241abbdbcb3f27c2cf8fb6f46abb608141
Takashi Kitao <takashi.kitao@gmail.com>
2021-04-24 21:06:12 +0900

    Modified the Palette class

--- pyxelcore/tests/tpalette.nim
+++ pyxelcore/tests/tpalette.nim
@@ -1,18 +1,15 @@
 import
   unittest,
-  core/palette
+  pyxelcore/palette
 
 suite "Palette":
-  test "types":
-    check(Rgb is uint32)
-
   test "properties":
-    let pal = newPalette[uint8](3)
+    let pal = newPalette(3)
 
     check(pal.colorCount == 3)
 
   test "[] and []=":
-    let pal = newPalette[uint8](3)
+    let pal = newPalette(3)
 
     for i in 0 ..< pal.colorCount:
       check(pal[i] == i)
@@ -21,8 +18,20 @@ suite "Palette":
       pal[i] = 2
       check(pal[i] == 2)
 
+    expect AssertionDefect:
+      pal[-1] = 0
+
+    expect AssertionDefect:
+      pal[3] = 0
+
+    expect AssertionDefect:
+      pal[0] = -1
+
+    expect AssertionDefect:
+      pal[0] = 3
+
   test "reset":
-    let pal = newPalette[uint8](3)
+    let pal = newPalette(3)
 
     for i in 0 ..< pal.colorCount:
       pal[i] = 0
@@ -33,7 +42,7 @@ suite "Palette":
       check(pal[i] == i)
 
   test "getDisplayColor":
-    let pal = newPalette[uint8](3)
+    let pal = newPalette(3)
 
     for i in 0 ..< pal.colorCount:
       check(pal.getDisplayColor(i) == 0)
@@ -45,7 +54,7 @@ suite "Palette":
       discard pal.getDisplayColor(3)
 
   test "setDisplayColor":
-    let pal = newPalette[uint8](3)
+    let pal = newPalette(3)
 
     check(pal.getDisplayColor(0) == 0)
     pal.setDisplayColor(0, 0x112233)
@@ -61,10 +70,25 @@ suite "Palette":
     expect AssertionDefect:
       pal.setDisplayColor(3, 0)
 
+    expect AssertionDefect:
+      pal.setDisplayColor(0, -1)
+
+    expect AssertionDefect:
+      pal.setDisplayColor(0, 0x1000000)
+
   test "setDisplayColors":
-    let pal = newPalette[uint8](3)
+    let pal = newPalette(3)
 
     pal.setDisplayColors([0x111111, 0x222222, 0x333333])
     check(pal.getDisplayColor(0) == 0x111111)
     check(pal.getDisplayColor(1) == 0x222222)
     check(pal.getDisplayColor(2) == 0x333333)
+
+    expect AssertionDefect:
+      pal.setDisplayColors([0, 1])
+
+    expect AssertionDefect:
+      pal.setDisplayColors([-1, 0, 0])
+
+    expect AssertionDefect:
+      pal.setDisplayColors([0x1000000, 0, 0])
