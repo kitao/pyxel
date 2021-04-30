b9c6e13d5fdeee9fe0c8bd0954553c7164b6878d
Takashi Kitao <takashi.kitao@gmail.com>
2021-04-30 15:29:01 +0900

    Refined implementations for the Palette trait

--- crates/engine/src/colorpalette.rs
+++ crates/engine/src/colorpalette.rs
@@ -1,21 +1,21 @@
 use crate::settings::{Color, Rgb24, COLOR_COUNT, RGB24_MAX_VALUE};
 use crate::Palette;
 
-pub struct ImagePalette {
+pub struct ColorPalette {
     render_colors: [Color; COLOR_COUNT],
     display_colors: [Rgb24; COLOR_COUNT],
 }
 
-impl Palette<Color> for ImagePalette {
+impl Palette<Color> for ColorPalette {
     #[inline]
-    fn get_render_color(&self, original_color: Color) -> Color {
-        self.render_colors[original_color as usize]
+    fn get_render_value(&self, original_value: Color) -> Color {
+        self.render_colors[original_value as usize]
     }
 }
 
-impl ImagePalette {
-    pub fn new() -> ImagePalette {
-        let mut palette = ImagePalette {
+impl ColorPalette {
+    pub fn new() -> ColorPalette {
+        let mut palette = ColorPalette {
             render_colors: [0; COLOR_COUNT],
             display_colors: [0; COLOR_COUNT],
         };
@@ -23,6 +23,11 @@ impl ImagePalette {
         palette.reset_render_colors();
 
         palette
+    }
+
+    #[inline]
+    pub fn get_render_color(&self, original_color: Color) -> Color {
+        self.get_render_value(original_color)
     }
 
     #[inline]
@@ -62,12 +67,12 @@ impl ImagePalette {
 }
 
 #[cfg(test)]
-mod image_palette_tests {
+mod color_palette_tests {
     use super::*;
 
     #[test]
     fn new() {
-        let palette = ImagePalette::new();
+        let palette = ColorPalette::new();
 
         for i in 0..COLOR_COUNT {
             assert_eq!(palette.get_render_color(i as Color), i as Color);
@@ -77,7 +82,7 @@ mod image_palette_tests {
 
     #[test]
     fn set_render_color() {
-        let mut palette = ImagePalette::new();
+        let mut palette = ColorPalette::new();
 
         for i in 0..COLOR_COUNT {
             assert_eq!(palette.get_render_color(i as Color), i as Color);
@@ -96,8 +101,16 @@ mod image_palette_tests {
     }
 
     #[test]
+    #[should_panic]
+    fn set_render_color_panic() {
+        let mut palette = ColorPalette::new();
+
+        palette.set_render_color(0, COLOR_COUNT as Color);
+    }
+
+    #[test]
     fn reset_render_colors() {
-        let mut palette = ImagePalette::new();
+        let mut palette = ColorPalette::new();
 
         for i in 0..COLOR_COUNT {
             palette.set_render_color(i as Color, ((i + 1) % COLOR_COUNT) as Color);
@@ -112,7 +125,7 @@ mod image_palette_tests {
 
     #[test]
     fn set_display_color() {
-        let mut palette = ImagePalette::new();
+        let mut palette = ColorPalette::new();
 
         for i in 0..COLOR_COUNT {
             assert_eq!(palette.get_display_color(i as Color), 0);
@@ -131,8 +144,16 @@ mod image_palette_tests {
     }
 
     #[test]
+    #[should_panic]
+    fn set_display_color_panic() {
+        let mut palette = ColorPalette::new();
+
+        palette.set_display_color(0, RGB24_MAX_VALUE + 1);
+    }
+
+    #[test]
     fn set_display_colors() {
-        let mut palette = ImagePalette::new();
+        let mut palette = ColorPalette::new();
 
         palette.set_display_colors(&[
             0x111111, 0x222222, 0x333333, 0x444444, 0x555555, 0x666666, 0x777777, 0x888888,
@@ -145,5 +166,16 @@ mod image_palette_tests {
                 (0x111111 * ((i + 1) % COLOR_COUNT)) as Rgb24
             );
         }
+    }
+
+    #[test]
+    #[should_panic]
+    fn set_display_colors_panic() {
+        let mut palette = ColorPalette::new();
+
+        palette.set_display_colors(&[
+            0x111111, 0x222222, 0x333333, 0x444444, 0x555555, 0x666666, 0x777777, 0x888888,
+            0x999999, 0xaaaaaa, 0xbbbbbb, 0xcccccc, 0xdddddd, 0xeeeeee, 0xffffff,
+        ]);
     }
 }
