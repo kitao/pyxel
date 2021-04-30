a368f1f537a89d831cd81f72887f6c54a38e0367
Takashi Kitao <takashi.kitao@gmail.com>
2021-04-30 15:12:11 +0900

    Added some implementations in Rust

---
+++ crates/engine/src/lib.rs
@@ -0,0 +1,43 @@
+mod canvas;
+mod imagepalette;
+mod palette;
+mod rectarea;
+mod settings;
+mod tilemappalette;
+
+pub use imagepalette::ImagePalette;
+pub use palette::Palette;
+pub use tilemappalette::TilemapPalette;
+
+/*
+macro_rules! global_instance {
+    ($struct_name:ident, $instance_name:ident) => {
+        static mut INSTANCE: Option<$struct_name> = None;
+
+        fn set_instance($instance_name: $struct_name) {
+            unsafe {
+                if INSTANCE.is_some() {
+                    panic!(concat!(
+                        stringify!($instance_name),
+                        " is already initialized"
+                    ));
+                }
+
+                INSTANCE = Some($instance_name);
+            }
+        }
+
+        #[inline]
+        pub fn $instance_name() -> &'static mut $struct_name {
+            unsafe {
+                INSTANCE
+                    .as_mut()
+                    .expect(concat!(stringify!($struct_name), " is not initialized"))
+            }
+        }
+    };
+}
+
+mod system;
+pub use system::{init_system, system, System};
+*/
