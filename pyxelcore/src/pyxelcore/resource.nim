1f7b0ecadb5ff0c92fdf0b919b8a77a883189356
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-02 18:02:27 +0900

    Removed the codes in Nim

--- pyxelcore/src/pyxelcore/resource.nim
+++
@@ -1,4 +0,0 @@
-proc save(self: Pyxel, filaname: string) = discard
-
-proc load(self: Pyxel, filename: string, image, tilemap, sound,
-    music: int) = discard
--- pyxelcore/tests/tpyxelcore.nim
+++
@@ -1,7 +0,0 @@
-import
-  unittest,
-  pyxelcore
-
-suite "PyxelCore":
-  test "hoge":
-    discard
