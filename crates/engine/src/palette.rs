46ffce42235301c5359d2b56e819adae4b0d1dd3
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-05 23:35:21 +0900

    Change the palette to have 256 colors

--- crates/engine/src/palette.rs
+++ crates/engine/src/palette.rs
@@ -1,27 +1,54 @@
-use crate::settings::{Color, Rgb24, COLOR_COUNT, RGB24_MAX_VALUE};
+pub type Color = u8;
+pub type Rgb24 = u32;
+
+const MAX_COLOR_COUNT: usize = 256;
+
+pub const DEFAULT_COLOR_COUNT: usize = 16;
+pub const DEFAULT_DISPLAY_COLORS: [Rgb24; DEFAULT_COLOR_COUNT] = [
+    0x000000, 0x2b335f, 0x7e2072, 0x19959c, 0x8b4852, 0x395c98, 0xa9c1ff, 0xeeeeee, 0xd4186c,
+    0xd38441, 0xe9c35b, 0x70c6a9, 0x7696de, 0xa3a3a3, 0xFF9798, 0xedc7b0,
+];
+
+pub enum DefaultColor {
+    Black,
+    Navy,
+    Purple,
+    Green,
+    Brown,
+    DarkBlue,
+    LightBlue,
+    White,
+    Red,
+    Orange,
+    Yellow,
+    Lime,
+    Cyan,
+    Gray,
+    Pink,
+    Peach,
+}
 
 #[derive(Debug)]
 pub struct Palette {
-    render_colors: [Color; COLOR_COUNT],
-    display_colors: [Rgb24; COLOR_COUNT],
+    render_colors: [Color; MAX_COLOR_COUNT],
+    display_colors: [Rgb24; MAX_COLOR_COUNT],
 }
 
 impl Palette {
     pub fn new() -> Palette {
         let mut palette = Palette {
-            render_colors: [0; COLOR_COUNT],
-            display_colors: [0; COLOR_COUNT],
+            render_colors: [0; MAX_COLOR_COUNT],
+            display_colors: [0; MAX_COLOR_COUNT],
         };
 
         palette.reset_render_colors();
+        palette.set_display_colors(&DEFAULT_DISPLAY_COLORS);
 
         palette
     }
 
     #[inline]
     pub fn set_render_color(&mut self, original_color: Color, render_color: Color) {
-        assert!((render_color as usize) < COLOR_COUNT);
-
         self.render_colors[original_color as usize] = render_color;
     }
 
@@ -32,7 +59,7 @@ impl Palette {
 
     #[inline]
     pub fn reset_render_colors(&mut self) {
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             self.render_colors[i] = i as Color;
         }
     }
@@ -44,15 +71,11 @@ impl Palette {
 
     #[inline]
     pub fn set_display_color(&mut self, render_color: Color, display_color: Rgb24) {
-        assert!(display_color <= RGB24_MAX_VALUE);
-
         self.display_colors[render_color as usize] = display_color;
     }
 
     #[inline]
     pub fn set_display_colors(&mut self, display_colors: &[Rgb24]) {
-        assert!(display_colors.len() == COLOR_COUNT);
-
         for (i, &display_color) in display_colors.iter().enumerate() {
             self.set_display_color(i as Color, display_color);
         }
@@ -67,9 +90,19 @@ mod color_palette_tests {
     fn new() {
         let palette = Palette::new();
 
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             assert_eq!(palette.get_render_color(i as Color), i as Color);
-            assert_eq!(palette.get_display_color(i as Color), 0);
+
+            for i in 0..MAX_COLOR_COUNT {
+                if i < DEFAULT_COLOR_COUNT {
+                    assert_eq!(
+                        palette.get_display_color(i as Color),
+                        DEFAULT_DISPLAY_COLORS[i]
+                    );
+                } else {
+                    assert_eq!(palette.get_display_color(i as Color), 0);
+                }
+            }
         }
     }
 
@@ -77,41 +110,33 @@ mod color_palette_tests {
     fn set_render_color() {
         let mut palette = Palette::new();
 
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             assert_eq!(palette.get_render_color(i as Color), i as Color);
         }
 
-        for i in 0..COLOR_COUNT {
-            palette.set_render_color(i as Color, ((i + 1) % COLOR_COUNT) as Color);
+        for i in 0..MAX_COLOR_COUNT {
+            palette.set_render_color(i as Color, ((i + 1) % MAX_COLOR_COUNT) as Color);
         }
 
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             assert_eq!(
                 palette.get_render_color(i as Color),
-                ((i + 1) % COLOR_COUNT) as Color
+                ((i + 1) % MAX_COLOR_COUNT) as Color
             );
         }
-    }
-
-    #[test]
-    #[should_panic]
-    fn set_render_color_panic() {
-        let mut palette = Palette::new();
-
-        palette.set_render_color(0, COLOR_COUNT as Color);
     }
 
     #[test]
     fn reset_render_colors() {
         let mut palette = Palette::new();
 
-        for i in 0..COLOR_COUNT {
-            palette.set_render_color(i as Color, ((i + 1) % COLOR_COUNT) as Color);
+        for i in 0..MAX_COLOR_COUNT {
+            palette.set_render_color(i as Color, ((i + 1) % MAX_COLOR_COUNT) as Color);
         }
 
         palette.reset_render_colors();
 
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             assert_eq!(palette.get_render_color(i as Color), i as Color);
         }
     }
@@ -120,15 +145,22 @@ mod color_palette_tests {
     fn set_display_color() {
         let mut palette = Palette::new();
 
-        for i in 0..COLOR_COUNT {
-            assert_eq!(palette.get_display_color(i as Color), 0);
+        for i in 0..MAX_COLOR_COUNT {
+            if i < DEFAULT_COLOR_COUNT {
+                assert_eq!(
+                    palette.get_display_color(i as Color),
+                    DEFAULT_DISPLAY_COLORS[i]
+                );
+            } else {
+                assert_eq!(palette.get_display_color(i as Color), 0);
+            }
         }
 
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             palette.set_display_color(i as Color, (0x111111 * i) as Rgb24);
         }
 
-        for i in 0..COLOR_COUNT {
+        for i in 0..MAX_COLOR_COUNT {
             assert_eq!(
                 palette.get_display_color(i as Color),
                 (0x111111 * i) as Rgb24
@@ -137,38 +169,18 @@ mod color_palette_tests {
     }
 
     #[test]
-    #[should_panic]
-    fn set_display_color_panic() {
-        let mut palette = Palette::new();
-
-        palette.set_display_color(0, RGB24_MAX_VALUE + 1);
-    }
-
-    #[test]
     fn set_display_colors() {
         let mut palette = Palette::new();
 
-        palette.set_display_colors(&[
-            0x111111, 0x222222, 0x333333, 0x444444, 0x555555, 0x666666, 0x777777, 0x888888,
-            0x999999, 0xaaaaaa, 0xbbbbbb, 0xcccccc, 0xdddddd, 0xeeeeee, 0xffffff, 0x000000,
-        ]);
+        let rgbs: [Rgb24; 16] = [
+            0x000000, 0x111111, 0x222222, 0x333333, 0x444444, 0x555555, 0x666666, 0x777777,
+            0x888888, 0x999999, 0xaaaaaa, 0xbbbbbb, 0xcccccc, 0xdddddd, 0xeeeeee, 0xffffff,
+        ];
 
-        for i in 0..COLOR_COUNT {
-            assert_eq!(
-                palette.get_display_color(i as Color),
-                (0x111111 * ((i + 1) % COLOR_COUNT)) as Rgb24
-            );
+        palette.set_display_colors(&rgbs);
+
+        for i in 0..16 {
+            assert_eq!(palette.get_display_color(i as Color), rgbs[i]);
         }
-    }
-
-    #[test]
-    #[should_panic]
-    fn set_display_colors_panic() {
-        let mut palette = Palette::new();
-
-        palette.set_display_colors(&[
-            0x111111, 0x222222, 0x333333, 0x444444, 0x555555, 0x666666, 0x777777, 0x888888,
-            0x999999, 0xaaaaaa, 0xbbbbbb, 0xcccccc, 0xdddddd, 0xeeeeee, 0xffffff,
-        ]);
     }
 }
