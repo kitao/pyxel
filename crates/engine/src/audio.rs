e9922e583d451fbd39cf911fc2f2ea89a20c6091
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-07 03:19:54 +0900

    Added initial files

---
+++ crates/engine/src/audio.rs
@@ -0,0 +1,25 @@
+pub struct Audio {
+    /*
+    SDL_AudioDeviceID audio_device_id_;
+    Sound** sound_bank_;
+    Music** music_bank_;
+    Channel channel_[MUSIC_CHANNEL_COUNT];
+    */
+}
+
+impl Audio {
+    /*
+    Sound* GetSoundBank(int32_t sound_index, bool system = false) const;
+    Music* GetMusicBank(int32_t music_index) const;
+    int32_t GetPlayPos(int32_t channel) const;
+    void PlaySound(int32_t channel, int32_t sound_index, bool loop = false);
+    void PlaySound(int32_t channel,
+    const SoundIndexList& sound_index_list,
+    bool loop = false);
+    void PlayMusic(int32_t music_index, bool loop = false);
+    void StopPlaying(int32_t channel = -1);
+
+    private:
+    static void callback(void* userdata, uint8_t* stream, int len);
+    */
+}
---
+++ crates/engine/src/resource.rs
@@ -0,0 +1,42 @@
+pub struct Resource {
+    /*
+    private:
+    Graphics* graphics_;
+    Audio* audio_;
+
+    void ClearImage(int32_t image_index);
+    void ClearTilemap(int32_t tilemap_index);
+    void ClearSound(int32_t sound_index);
+    void ClearMusic(int32_t music_index);
+
+    std::string DumpImage(int32_t image_index) const;
+    std::string DumpTilemap(int32_t tilemap_index) const;
+    std::string DumpSound(int32_t sound_index) const;
+    std::string DumpMusic(int32_t music_index) const;
+
+    void ParseImage(int32_t image_index, const std::string& str);
+    void ParseTilemap(int32_t tilemap_index, const std::string& str);
+    void ParseSound(int32_t sound_index, const std::string& str);
+    void ParseMusic(int32_t music_index, const std::string& str);
+
+    static std::string GetVersionName();
+    static std::string GetImageName(int32_t image_index);
+    static std::string GetTilemapName(int32_t tilemap_index);
+    static std::string GetSoundName(int32_t sound_index);
+    static std::string GetMusicName(int32_t music_index);
+    */
+}
+
+impl Resource {
+    /*
+    public:
+    Resource(Graphics* graphics, Audio* audio);
+
+    void SaveAsset(const std::string& filename);
+    void LoadAsset(const std::string& filename,
+    bool image = true,
+    bool tilemap = true,
+    bool sound = true,
+    bool music = true);
+    */
+}
