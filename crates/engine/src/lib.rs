99b7916925672deab97b1a7ec027f6996636b4b8
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-07 17:39:54 +0900

    Changed the each module as singleton

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -1,47 +1,54 @@
-pub mod color_palette;
-pub mod graphics;
-pub mod graphics_buffer;
-pub mod image_buffer;
-pub mod rectarea;
-pub mod system;
-pub mod tilemap_buffer;
+mod color_palette;
+mod graphics;
+mod graphics_buffer;
+mod image_buffer;
+mod rectarea;
+mod system;
+mod tilemap_buffer;
 
-use graphics::Graphics;
-use system::System;
+use color_palette::Color;
+use graphics::{graphics, init_graphics};
+use graphics_buffer::GraphicsBuffer;
+use system::{init_system, system};
 
-pub struct Pyxel {
-    system: System,
-    graphics: Graphics,
+//
+// System
+//
+#[inline]
+pub fn init(width: u32, height: u32) {
+    init_system("hoge", width, height);
+    init_graphics(width, height);
 }
-
-static mut PYXEL: Option<Pyxel> = None;
 
 #[inline]
-fn pyxel() -> &'static mut Pyxel {
-    unsafe { PYXEL.as_mut().expect("Pyxel is not initialized") }
+pub fn width() -> u32 {
+    system().screen_width()
 }
 
-//
-//  System
-//
-pub fn init(width: u32, height: u32) {
-    let system = System::new("hoge", width, height);
-    let graphics = Graphics::new(width, height);
-
-    let pyxel = Pyxel {
-        system: system,
-        graphics: graphics,
-    };
-
-    unsafe {
-        PYXEL = Some(pyxel);
-    }
+#[inline]
+pub fn height() -> u32 {
+    system().screen_height()
 }
 
+#[inline]
 pub fn run() {
-    pyxel().system.run();
+    system().run();
 }
 
 //
-//  Graphics
+// Graphics
 //
+#[inline]
+pub fn cls(color: Color) {
+    graphics().screen().clear_buffer(color);
+}
+
+#[inline]
+pub fn pget(x: i32, y: i32) -> Color {
+    graphics().screen().get_color(x, y)
+}
+
+#[inline]
+pub fn pset(x: i32, y: i32, color: Color) {
+    graphics().screen().set_color(x, y, color);
+}
