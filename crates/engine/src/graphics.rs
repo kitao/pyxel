9ad78ddc11cf05e68f6cf07c590f994e62cb5e3b
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-07 21:12:52 +0900

    Refined the code (on going)

--- crates/engine/src/graphics.rs
+++ crates/engine/src/graphics.rs
@@ -1,115 +1,77 @@
-use std::cell::{RefCell, RefMut};
-use std::rc::Rc;
-
-use crate::color_palette::{ColorPalette, Rgb24};
-use crate::image_buffer::ImageBuffer;
-use crate::tilemap_buffer::TilemapBuffer;
-
-const IMAGE_BUFFER_COUNT: usize = 4;
-const IMAGE_BUFFER_SIZE: u32 = 256;
-
-const TILEMAP_BUFFER_COUNT: usize = 8;
-const TILEMAP_BUFFER_SIZE: u32 = 256;
-
-const DEFAULT_COLOR_COUNT: usize = 16;
-const DEFAULT_DISPLAY_COLORS: [Rgb24; DEFAULT_COLOR_COUNT] = [
-    0x000000, 0x2b335f, 0x7e2072, 0x19959c, 0x8b4852, 0x395c98, 0xa9c1ff, 0xeeeeee, 0xd4186c,
-    0xd38441, 0xe9c35b, 0x70c6a9, 0x7696de, 0xa3a3a3, 0xFF9798, 0xedc7b0,
-];
-
-pub enum DefaultColor {
-    Black,
-    Navy,
-    Purple,
-    Green,
-    Brown,
-    DarkBlue,
-    LightBlue,
-    White,
-    Red,
-    Orange,
-    Yellow,
-    Lime,
-    Cyan,
-    Gray,
-    Pink,
-    Peach,
-}
+use crate::imagebank::Imagebank;
+use crate::palette::Palette;
+use crate::settings::{
+    DISPLAY_COLORS, IMAGEBANK_COUNT, IMAGEBANK_SIZE, TILEMAP_COUNT, TILEMAP_SIZE,
+};
+use crate::tilemap::Tilemap;
 
 static mut INSTANCE: Option<Graphics> = None;
 
 #[inline]
 pub fn graphics() -> &'static mut Graphics {
-    unsafe { INSTANCE.as_mut().expect("System is not initialized") }
+    unsafe { INSTANCE.as_mut().expect("Graphics is not initialized") }
 }
 
 pub fn init_graphics(width: u32, height: u32) {
     unsafe {
+        assert!(INSTANCE.is_none(), "Graphics is already initialized");
+
         INSTANCE = Some(Graphics::new(width, height));
     }
 }
 
 pub struct Graphics {
-    palette: Rc<RefCell<ColorPalette>>,
-    screen: ImageBuffer,
-    image_buffers: Vec<ImageBuffer>,
-    tilemap_buffers: Vec<TilemapBuffer>,
+    screen: Imagebank,
+    imagebanks: Vec<Imagebank>,
+    tilemaps: Vec<Tilemap>,
 }
 
 impl Graphics {
     pub fn new(screen_width: u32, screen_height: u32) -> Graphics {
-        let palette = Rc::new(RefCell::new(ColorPalette::new()));
-        let screen = ImageBuffer::new(screen_width, screen_height, Rc::clone(&palette));
-        let mut image_buffers = Vec::new();
-        let mut tilemap_buffers = Vec::new();
+        let screen = Imagebank::new(screen_width, screen_height);
+        let mut imagebanks = Vec::new();
+        let mut tilemaps = Vec::new();
 
-        for _ in 0..IMAGE_BUFFER_COUNT {
-            image_buffers.push(ImageBuffer::new(
-                IMAGE_BUFFER_SIZE,
-                IMAGE_BUFFER_SIZE,
-                Rc::clone(&palette),
-            ));
+        for _ in 0..IMAGEBANK_COUNT {
+            imagebanks.push(Imagebank::new(IMAGEBANK_SIZE, IMAGEBANK_SIZE));
         }
 
-        for _ in 0..TILEMAP_BUFFER_COUNT {
-            tilemap_buffers.push(TilemapBuffer::new(TILEMAP_BUFFER_SIZE, TILEMAP_BUFFER_SIZE));
+        for _ in 0..TILEMAP_COUNT {
+            tilemaps.push(Tilemap::new(TILEMAP_SIZE, TILEMAP_SIZE));
         }
 
-        let graphics = Graphics {
-            palette: palette,
+        let mut graphics = Graphics {
             screen: screen,
-            image_buffers: image_buffers,
-            tilemap_buffers: tilemap_buffers,
+            imagebanks: imagebanks,
+            tilemaps: tilemaps,
         };
 
-        graphics
-            .palette()
-            .set_display_colors(&DEFAULT_DISPLAY_COLORS);
+        graphics.palette().set_display_colors(&DISPLAY_COLORS);
 
         graphics
     }
 
-    pub fn screen(&mut self) -> &mut ImageBuffer {
+    #[inline]
+    pub fn screen(&mut self) -> &mut Imagebank {
         &mut self.screen
     }
 
-    pub fn image(&mut self, no: usize) -> &mut ImageBuffer {
-        if no < self.image_buffers.len() {
-            &mut self.image_buffers[no]
+    #[inline]
+    pub fn palette(&mut self) -> &mut Palette {
+        self.screen.palette_mut()
+    }
+
+    #[inline]
+    pub fn imagebank(&mut self, no: usize) -> &mut Imagebank {
+        if no < self.imagebanks.len() {
+            &mut self.imagebanks[no]
         } else {
             &mut self.screen
         }
     }
 
-    pub fn tilemap(&mut self, no: usize) -> &mut TilemapBuffer {
-        &mut self.tilemap_buffers[no]
-    }
-
-    pub fn palette(&self) -> RefMut<ColorPalette> {
-        self.palette.borrow_mut()
+    #[inline]
+    pub fn tilemap(&mut self, no: usize) -> &mut Tilemap {
+        &mut self.tilemaps[no]
     }
 }
-
-/*
-Image* ScreenImage() const { return image_bank_[IMAGE_BANK_FOR_SCREEN]; }
-*/
