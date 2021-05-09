01f6d0cc103cf037369988b4c761085b1bf5d9ba
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-09 17:47:58 +0900

    Implemented the run method

--- crates/engine/tests/main.rs
+++ crates/engine/tests/main.rs
@@ -1,9 +1,31 @@
-use pyxel::Pyxel;
+use pyxel::*;
+
+pub struct App {
+    x: i32,
+    y: i32,
+}
+
+impl App {
+    fn new() -> App {
+        App { x: 0, y: 0 }
+    }
+}
+
+impl PyxelCallback for App {
+    fn update(&mut self, pyxel: &mut Pyxel) {
+        self.x += 1;
+        self.y -= 1;
+    }
+
+    fn draw(&mut self, pyxel: &mut Pyxel) {
+        pyxel.cls(3);
+        pyxel.pset(10, 20, 8);
+    }
+}
 
 pub fn main() {
-    let mut pyxel = Pyxel::init(200, 150, Some("Hello"), None);
+    let mut pyxel = Pyxel::init(200, 150, Some("Hello"), None, None);
+    let mut app = App::new();
 
-    pyxel.cls(3);
-    pyxel.pset(10, 20, 8);
-    pyxel.run();
+    pyxel.run(&mut app);
 }
