4af44fdd76b17ce3a8ebf7eb3b5d2dcd20a7ca50
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-14 13:31:07 +0900

    Renamed getter methods

--- crates/engine/src/canvas.rs
+++ crates/engine/src/canvas.rs
@@ -8,26 +8,23 @@ pub trait Canvas<T: Copy + Default> {
   fn self_rect(&self) -> Rectarea;
   fn clip_rect(&self) -> Rectarea;
   fn clip_rect_mut(&mut self) -> &mut Rectarea;
-  fn get_render_color(&self, original_color: T) -> T;
+  fn render_color(&self, original_color: T) -> T;
 
   #[inline]
-  fn get_clip_area(&mut self) -> (i32, i32, i32, i32) {
+  fn clip_area(&mut self) -> (i32, i32, u32, u32) {
     (
       self.clip_rect().left(),
       self.clip_rect().top(),
-      self.clip_rect().width() as i32,
-      self.clip_rect().height() as i32,
+      self.clip_rect().width(),
+      self.clip_rect().height(),
     )
   }
 
   #[inline]
-  fn set_clip_area(&mut self, left: i32, top: i32, width: i32, height: i32) {
-    let rect = self.self_rect().intersects(&Rectarea::with_size(
-      left,
-      top,
-      (width as f64) as u32,
-      (height as f64) as u32,
-    ));
+  fn set_clip_area(&mut self, left: i32, top: i32, width: u32, height: u32) {
+    let rect = self
+      .self_rect()
+      .intersects(Rectarea::with_size(left, top, width, height));
 
     *self.clip_rect_mut() = rect;
   }
@@ -39,7 +36,7 @@ pub trait Canvas<T: Copy + Default> {
 
   #[inline]
   fn clear(&mut self, color: T) {
-    let color = self.get_render_color(color);
+    let color = self.render_color(color);
 
     for i in 0..self.height() {
       for j in 0..self.width() {
@@ -49,7 +46,7 @@ pub trait Canvas<T: Copy + Default> {
   }
 
   #[inline]
-  fn get_color(&mut self, x: i32, y: i32) -> T {
+  fn point(&mut self, x: i32, y: i32) -> T {
     if self.self_rect().contains(x, y) {
       self.data()[y as usize][x as usize]
     } else {
@@ -59,7 +56,7 @@ pub trait Canvas<T: Copy + Default> {
 
   #[inline]
   fn draw_point(&mut self, x: i32, y: i32, color: T) {
-    let color = self.get_render_color(color);
+    let color = self.render_color(color);
 
     if self.self_rect().contains(x, y) {
       self.data_mut()[y as usize][x as usize] = color;
@@ -72,22 +69,58 @@ pub trait Canvas<T: Copy + Default> {
   }
 
   #[inline]
-  fn draw_rect(&mut self, x: i32, y: i32, width: i32, height: i32, color: T) {
+  fn draw_rect(&mut self, x: i32, y: i32, width: u32, height: u32, color: T) {
+    let color = self.render_color(color);
+    let rect = Rectarea::with_size(x, y, width, height).intersects(self.clip_rect());
+
+    if rect.is_empty() {
+      return;
+    }
+
+    let left = rect.left();
+    let top = rect.top();
+    let right = rect.right();
+    let bottom = rect.bottom();
+
+    for i in top..=bottom {
+      for j in left..=right {
+        self.data_mut()[i as usize][j as usize] = color;
+      }
+    }
+  }
+
+  #[inline]
+  fn draw_rect_border(&mut self, x: i32, y: i32, width: u32, height: u32, color: T) {
+    let color = self.render_color(color);
+    let rect = Rectarea::with_size(x, y, width, height).intersects(self.clip_rect());
+
+    if rect.is_empty() {
+      return;
+    }
+
+    let left = rect.left();
+    let top = rect.top();
+    let right = rect.right();
+    let bottom = rect.bottom();
+
+    for i in left..=right {
+      self.data_mut()[top as usize][i as usize] = color;
+      self.data_mut()[bottom as usize][i as usize] = color;
+    }
+
+    for i in top..=bottom {
+      self.data_mut()[i as usize][left as usize] = color;
+      self.data_mut()[i as usize][right as usize] = color;
+    }
+  }
+
+  #[inline]
+  fn draw_circle(&mut self, x: i32, y: i32, radius: u32, color: T) {
     //
   }
 
   #[inline]
-  fn draw_rect_border(&mut self, x: i32, y: i32, width: i32, height: i32, color: T) {
-    //
-  }
-
-  #[inline]
-  fn draw_circle(&mut self, x: i32, y: i32, radius: i32, color: T) {
-    //
-  }
-
-  #[inline]
-  fn draw_circle_border(&mut self, x: i32, y: i32, radius: i32, color: T) {
+  fn draw_circle_border(&mut self, x: i32, y: i32, radius: u32, color: T) {
     //
   }
 
@@ -123,8 +156,8 @@ pub trait Canvas<T: Copy + Default> {
     src: &dyn Canvas<T>,
     u: i32,
     v: i32,
-    width: i32,
-    height: i32,
+    width: u32,
+    height: u32,
     color_key: Option<T>,
   ) {
     //
