2f14063a1c4e691d4d7740aa4f3025832390f6d0
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-14 13:29:04 +0900

    Separated the platform dependent code

--- crates/engine/src/system.rs
+++ crates/engine/src/system.rs
@@ -1,40 +1,23 @@
 use std::cmp::min;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color as SdlColor;
-use sdl2::pixels::PixelFormatEnum;
-use sdl2::render::Texture as SdlTexture;
-use sdl2::render::WindowCanvas as SdlCanvas;
-use sdl2::EventPump as SdlEventPump;
-use sdl2::Sdl as SdlContext;
-use sdl2::TimerSubsystem as SdlTimerSubsystem;
-use sdl2::VideoSubsystem as SdlVideoSubsystem;
-
 use crate::canvas::Canvas;
+use crate::event::Event;
 use crate::graphics::Graphics;
-use crate::settings::*;
+use crate::platform::Platform;
+use crate::settings::{BACKGROUND_COLOR, DEFAULT_CAPTION, DEFAULT_FPS, MAX_FRAME_SKIP_COUNT};
 
-pub struct System {
-    sdl_context: SdlContext,
-    sdl_video: SdlVideoSubsystem,
-    sdl_timer: SdlTimerSubsystem,
-    sdl_canvas: SdlCanvas,
-    sdl_texture: SdlTexture,
-    sdl_event_pump: SdlEventPump,
+pub struct System<T: Platform> {
+    platform: T,
 
-    screen_width: u32,
-    screen_height: u32,
-    window_caption: String,
     target_fps: u32,
 
-    elapsed_frame_count: i32,
+    elapsed_frame_count: u32,
     one_frame_time: f64,
     next_update_time: f64,
     waiting_update_count: u32,
 
     should_quit: bool,
-    update_suspended: bool,
+    disable_frame_skip_once: bool,
     performance_monitor_enabled: bool,
     /*
         Recorder* recorder_;
@@ -49,64 +32,23 @@ pub struct System {
     */
 }
 
-impl System {
-    pub fn new(width: u32, height: u32, caption: Option<&str>, fps: Option<u32>) -> System {
+impl<T: Platform> System<T> {
+    pub fn new(
+        platform: T,
+        pwidth: u32,
+        height: u32,
+        caption: Option<&str>,
+        fps: Option<u32>,
+    ) -> System<T> {
         let caption = caption.unwrap_or(DEFAULT_CAPTION);
         let fps = fps.unwrap_or(DEFAULT_FPS);
 
-        let sdl_context = sdl2::init().unwrap();
-        let sdl_video = sdl_context.video().unwrap();
-        let sdl_timer = sdl_context.timer().unwrap();
-        let sdl_window = sdl_video
-            .window(caption, width, height)
-            .position_centered()
-            .build()
-            .unwrap();
-        let sdl_canvas = sdl_window.into_canvas().build().unwrap();
-        let sdl_event_pump = sdl_context.event_pump().unwrap();
-        let sdl_texture_creator = sdl_canvas.texture_creator();
-        let sdl_texture = sdl_texture_creator
-            .create_texture_streaming(PixelFormatEnum::RGB24, width, height)
-            .unwrap();
-
         let one_frame_time = 1000.0 / fps as f64;
-        let next_update_time = sdl_timer.ticks() as f64;
+        let next_update_time = platform.ticks() as f64;
 
-        /*
-          if (screen_scale_ <= 0) {
-            SDL_DisplayMode display_mode;
-            SDL_GetDesktopDisplayMode(0, &display_mode);
+        System::<T> {
+            platform: platform,
 
-            screen_scale_ = Max(
-                Min(display_mode.w / screen_width_, display_mode.h / screen_height_) *
-                    MAX_WINDOW_SIZE_RATIO,
-                1.0f);
-          }
-
-          int32_t window_width = screen_width_ * screen_scale_;
-          unt32_t window_height = screen_height_ * screen_scale_;
-
-          window_ = SDL_CreateWindow(caption.c_str(), SDL_WINDOWPOS_CENTERED,
-                                     SDL_WINDOWPOS_CENTERED, window_width,
-                                     window_height, SDL_WINDOW_RESIZABLE);
-
-          SDL_SetWindowMinimumSize(window_, screen_width_, screen_height_);
-
-          SetupWindowIcon();
-          UpdateWindowInfo();
-        */
-
-        System {
-            sdl_context: sdl_context,
-            sdl_video: sdl_video,
-            sdl_timer: sdl_timer,
-            sdl_canvas: sdl_canvas,
-            sdl_texture: sdl_texture,
-            sdl_event_pump: sdl_event_pump,
-
-            screen_width: width,
-            screen_height: height,
-            window_caption: caption.to_string(),
             target_fps: fps,
 
             elapsed_frame_count: 0,
@@ -115,34 +57,34 @@ impl System {
             waiting_update_count: 0,
 
             should_quit: false,
-            update_suspended: false,
+            disable_frame_skip_once: false,
             performance_monitor_enabled: false,
         }
     }
 
     #[inline]
     pub fn width(&self) -> u32 {
-        self.screen_width
+        self.platform.width()
     }
 
     #[inline]
     pub fn height(&self) -> u32 {
-        self.screen_height
+        self.platform.height()
     }
 
     #[inline]
-    pub fn frame_count(&self) -> i32 {
+    pub fn frame_count(&self) -> u32 {
         self.elapsed_frame_count
     }
 
     #[inline]
-    pub fn get_caption(&self) -> &String {
-        &self.window_caption
+    pub fn caption(&self) -> &str {
+        self.platform.caption()
     }
 
     #[inline]
     pub fn set_caption(&mut self, caption: &str) {
-        self.window_caption = caption.to_string();
+        self.platform.set_caption(caption);
     }
 
     #[inline]
@@ -155,8 +97,71 @@ impl System {
         //
     }
 
+    /*
+    void Window::SetupWindowIcon() const {
+    SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormat(
+        0, ICON_WIDTH * ICON_SCALE, ICON_HEIGHT * ICON_SCALE, 32,
+        SDL_PIXELFORMAT_RGBA8888);
+
+    Image* image = new Image(ICON_WIDTH, ICON_HEIGHT);
+    image->SetData(0, 0, ICON_DATA);
+
+    int32_t** src_data = image->Data();
+    uint32_t* dst_data = reinterpret_cast<uint32_t*>(surface->pixels);
+
+    for (int32_t i = 0; i < ICON_HEIGHT; i++) {
+        int32_t index = ICON_WIDTH * i;
+
+        for (int32_t j = 0; j < ICON_WIDTH; j++) {
+        int32_t color = src_data[i][j];
+        uint32_t argb = color == 0 ? 0 : (DEFAULT_PALETTE[color] << 8) + 0xff;
+
+        for (int32_t y = 0; y < ICON_SCALE; y++) {
+            int32_t index = (ICON_WIDTH * (i * ICON_SCALE + y) + j) * ICON_SCALE;
+
+            for (int32_t x = 0; x < ICON_SCALE; x++) {
+            dst_data[index + x] = argb;
+            }
+        }
+        }
+    }
+
+    SDL_SetWindowIcon(window_, surface);
+    SDL_FreeSurface(surface);
+
+    delete image;
+    }
+
+    void Window::UpdateWindowInfo() {
+    SDL_GetWindowPosition(window_, &window_x_, &window_y_);
+
+    int32_t window_width, window_height;
+    SDL_GetWindowSize(window_, &window_width, &window_height);
+
+    screen_scale_ =
+        Min(window_width / screen_width_, window_height / screen_height_);
+    screen_x_ = (window_width - screen_width_ * screen_scale_) / 2;
+    screen_y_ = (window_height - screen_height_ * screen_scale_) / 2;
+    }
+
+    void Window::ToggleFullscreen() {
+    is_fullscreen_ = !is_fullscreen_;
+
+    SDL_SetWindowFullscreen(window_, is_fullscreen_ ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
+    }
+    */
+
     #[inline]
     fn process_events(&mut self) {
+        while let Some(event) = self.platform.poll_event() {
+            match event {
+                Event::Quit => self.should_quit = true,
+
+                _ => {}
+            }
+        }
+
+        /*
         // TODO
         for event in self.sdl_event_pump.poll_iter() {
             match event {
@@ -169,6 +174,38 @@ impl System {
                 _ => {}
             }
         }
+        */
+
+        /*
+        SDL_Event event;
+        bool window_should_close = false;
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_MOVED ||
+                    event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                UpdateWindowInfo();
+                }
+                break;
+
+            case SDL_MOUSEWHEEL:
+                mouse_wheel_ += event.wheel.y;
+                break;
+
+            case SDL_DROPFILE:
+                drop_file_ = event.drop.file;
+                break;
+
+            case SDL_QUIT:
+                window_should_close = true;
+                break;
+            }
+        }
+
+        return window_should_close;
+        }
+        */
     }
 
     /*
@@ -191,15 +228,46 @@ impl System {
     */
 
     #[inline]
+    pub fn flip(&mut self) {
+        /*
+        next_update_time_ += one_frame_time_;
+
+        fps_profiler_.End();
+        fps_profiler_.Start();
+
+        if (UpdateFrame(nullptr)) {
+            return true;
+        }
+
+        DrawFrame(nullptr, 1);
+
+        return false;
+        */
+    }
+
+    #[inline]
+    pub fn show(&mut self) {
+        /*
+        is_loop_running_ = true;
+
+        while (true) {
+            if (FlipScreen()) {
+            break;
+            }
+        }
+        */
+    }
+
+    #[inline]
     fn wait_for_update_time(&mut self) -> i32 {
         loop {
-            let sleep_time = self.next_update_time - self.sdl_timer.ticks() as f64;
+            let sleep_time = self.next_update_time - self.platform.ticks() as f64;
 
             if sleep_time <= 0.0 {
                 return sleep_time as i32;
             }
 
-            self.sdl_timer.delay((sleep_time / 2.0) as u32);
+            self.platform.delay((sleep_time / 2.0) as u32);
         }
     }
 
@@ -207,34 +275,34 @@ impl System {
     // methods for run macro
     //
     #[inline]
-    pub fn should_update(&self) -> bool {
+    pub fn _should_update(&self) -> bool {
         self.waiting_update_count > 0
     }
 
     #[inline]
-    pub fn should_quit(&self) -> bool {
+    pub fn _should_quit(&self) -> bool {
         self.should_quit
     }
 
     #[inline]
-    pub fn init_run_status(&mut self) {
-        self.next_update_time = self.sdl_timer.ticks() as f64 + self.one_frame_time;
+    pub fn _init_run_status(&mut self) {
+        self.next_update_time = self.platform.ticks() as f64 + self.one_frame_time;
         self.should_quit = false;
-        self.update_suspended = true;
-        self.elapsed_frame_count = -1;
+        self.disable_frame_skip_once = true;
+        self.elapsed_frame_count = 0;
     }
 
     #[inline]
-    pub fn prepare_for_update(&mut self) {
+    pub fn _prepare_for_update(&mut self) {
         let sleep_time = self.wait_for_update_time();
 
         // TODO: fps_profiler_.End();
         // TODO: fps_profiler_.Start();
 
-        if self.update_suspended {
-            self.update_suspended = false;
+        if self.disable_frame_skip_once {
+            self.disable_frame_skip_once = false;
             self.waiting_update_count = 1;
-            self.next_update_time = self.sdl_timer.ticks() as f64 + self.one_frame_time;
+            self.next_update_time = self.platform.ticks() as f64 + self.one_frame_time;
         } else {
             self.waiting_update_count = min(
                 (-sleep_time as f64 / self.one_frame_time) as u32,
@@ -245,9 +313,7 @@ impl System {
     }
 
     #[inline]
-    pub fn start_update(&mut self) {
-        self.elapsed_frame_count += 1;
-
+    pub fn _start_update(&mut self) {
         // TODO: update_profiler_.Start();
 
         self.process_events();
@@ -260,82 +326,71 @@ impl System {
     #[inline]
     pub fn end_update(&mut self) {
         // TODO: update_profiler_.End();
+
+        if self.waiting_update_count > 0 {
+            self.waiting_update_count -= 1;
+            self.elapsed_frame_count += 1;
+        }
     }
 
     #[inline]
-    pub fn render_screen(&mut self, graphics: &Graphics) {
-        let palette = graphics.screen().palette();
-        let data = graphics.screen().data();
-        let width = self.screen_width as usize;
-        let height = self.screen_height as usize;
+    pub fn _start_draw(&mut self) {
+        //
+    }
 
-        self.sdl_texture
-            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
-                for i in 0..height {
-                    for j in 0..width {
-                        let offset = i * pitch + j * 3;
-                        let color = palette.get_display_color(data[i][j]);
+    #[inline]
+    pub fn _end_draw(&mut self, graphics: &Graphics) {
+        self.platform
+            .render_screen(graphics.screen(), BACKGROUND_COLOR);
 
-                        buffer[offset] = ((color >> 16) & 0xff) as u8;
-                        buffer[offset + 1] = ((color >> 8) & 0xff) as u8;
-                        buffer[offset + 2] = (color & 0xff) as u8;
-                    }
-                }
-            })
-            .unwrap();
-
-        self.sdl_canvas.set_draw_color(SdlColor::RGB(
-            ((BACKGROUND_COLOR >> 16) & 0xff) as u8,
-            ((BACKGROUND_COLOR >> 8) & 0xff) as u8,
-            (BACKGROUND_COLOR & 0xff) as u8,
-        ));
-        self.sdl_canvas.clear();
-        self.sdl_canvas
-            .copy(&self.sdl_texture, None, None)
-            .expect("Render failed");
-        self.sdl_canvas.present();
+        self.elapsed_frame_count += 1;
     }
 }
 
+macro_rules! update_frame {
+    ($self: expr, $callback: expr, $context: expr, $quit: stmt) => {
+        $self._start_update();
+
+        if $self._should_quit() {
+            $quit
+        }
+
+        $callback.update($context);
+
+        if $self._should_quit() {
+            $quit
+        }
+
+        $self.end_update();
+    };
+}
+
+macro_rules! draw_frame {
+    ($self: expr, $graphics: expr, $callback: expr, $context: expr) => {
+        $self._start_draw();
+
+        $callback.draw($context);
+
+        $self._end_draw($graphics);
+    };
+}
+
 macro_rules! run {
-    ($system: expr, $graphics: expr, $callback: expr, $context: expr) => {
+    ($self: expr, $graphics: expr, $callback: expr, $context: expr) => {
         'main_loop: loop {
-            $system.init_run_status();
+            $self._init_run_status();
 
-            // update frame
-            $system.start_update();
-            if $system.should_quit() {
-                break 'main_loop;
-            }
-            $callback.update($context);
-            if $system.should_quit() {
-                break 'main_loop;
-            }
-            $system.end_update();
-
-            // draw frame
-            $callback.draw($context);
-            $system.render_screen(&$graphics);
+            update_frame!($self, $callback, $context, break 'main_loop);
+            draw_frame!($self, $graphics, $callback, $context);
 
             loop {
-                $system.prepare_for_update();
+                $self._prepare_for_update();
 
-                while $system.should_update() {
-                    // update frame
-                    $system.start_update();
-                    if $system.should_quit() {
-                        break 'main_loop;
-                    }
-                    $callback.update($context);
-                    if $system.should_quit() {
-                        break 'main_loop;
-                    }
-                    $system.end_update();
+                while $self._should_update() {
+                    update_frame!($self, $callback, $context, break 'main_loop);
                 }
 
-                // draw frame
-                $callback.draw($context);
-                $system.render_screen(&$graphics);
+                draw_frame!($self, $graphics, $callback, $context);
             }
         }
     };
