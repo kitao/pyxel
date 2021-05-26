14483c1e3e80865d273cc7836ca4bcdbcc9bd149
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-26 19:53:49 +0900

    Refined the code

--- crates/engine/src/sdl2.rs
+++ crates/engine/src/sdl2.rs
@@ -21,75 +21,46 @@ use crate::palette::Rgb24;
 use crate::platform::Platform;
 
 pub struct Sdl2 {
-    /*
-    screen_x: i32,
-    screen_y: i32,
-    screen_scale: u32,
-    */
-    sdl_timer: SdlTimerSubsystem,
     sdl_canvas: SdlCanvas,
     sdl_texture: SdlTexture,
+    sdl_timer: SdlTimerSubsystem,
     sdl_event_pump: SdlEventPump,
 }
 
-impl Sdl2 {
-    pub fn new(width: u32, height: u32) -> Sdl2 {
+impl Platform for Sdl2 {
+    #[inline]
+    fn new(title: &str, width: u32, height: u32, scale: u32) -> Self {
         let sdl_context = sdl2::init().unwrap();
         let sdl_video = sdl_context.video().unwrap();
-        let sdl_timer = sdl_context.timer().unwrap();
-        let mut sdl_window = sdl_video
-            .window("", width, height)
+        let sdl_window = sdl_video
+            .window(title, width * scale, height * scale)
             .position_centered()
             .resizable()
             .build()
             .unwrap();
-
-        sdl_window.set_minimum_size(width, height).unwrap();
-
         let sdl_canvas = sdl_window.into_canvas().build().unwrap();
-        let sdl_event_pump = sdl_context.event_pump().unwrap();
-        let sdl_texture_creator = sdl_canvas.texture_creator();
-        let sdl_texture = sdl_texture_creator
+        let sdl_texture = sdl_canvas
+            .texture_creator()
             .create_texture_streaming(PixelFormatEnum::RGB24, width, height)
             .unwrap();
+        let sdl_timer = sdl_context.timer().unwrap();
+        let sdl_event_pump = sdl_context.event_pump().unwrap();
 
-        Sdl2 {
+        let mut sdl2 = Sdl2 {
             sdl_timer: sdl_timer,
             sdl_canvas: sdl_canvas,
             sdl_texture: sdl_texture,
             sdl_event_pump: sdl_event_pump,
-        }
+        };
 
-        /*
-                screen_width_ = screen_width;
-                screen_height_ = screen_height;
-                screen_scale_ = screen_scale;
-                is_fullscreen_ = false;
-                mouse_wheel_ = 0;
+        sdl2.sdl_canvas
+            .window_mut()
+            .set_minimum_size(width, height)
+            .unwrap();
 
-                if (screen_scale_ <= 0) {
-                    SDL_DisplayMode display_mode;
-        e           SDL_GetDesktopDisplayMode(0, &display_mode);
-
-                    screen_scale_ = Max(
-                        Min(display_mode.w / screen_width_, display_mode.h / screen_height_) * MAX_WINDOW_SIZE_RATIO, 1.0f);
-                }
-
-                int32_t window_width = screen_width_ * screen_scale_;
-                int32_t window_height = screen_height_ * screen_scale_;
-
-                window_ = SDL_CreateWindow(caption.c_str(), SDL_WINDOWPOS_CENTERED,
-                                            SDL_WINDOWPOS_CENTERED, window_width,
-                                            window_height, SDL_WINDOW_RESIZABLE);
-
-
-                SetupWindowIcon();
-                UpdateWindowInfo();
-                */
+        sdl2
     }
-}
 
-impl Platform for Sdl2 {
     #[inline]
     fn window_pos(&self) -> (i32, i32) {
         self.sdl_canvas.window().position()
@@ -123,15 +94,15 @@ impl Platform for Sdl2 {
     #[inline]
     fn set_fullscreen(&mut self, is_fullscreen: bool) {
         if is_fullscreen {
-            let _ = self
-                .sdl_canvas
+            self.sdl_canvas
                 .window_mut()
-                .set_fullscreen(SdlFullscreenType::True);
+                .set_fullscreen(SdlFullscreenType::True)
+                .unwrap();
         } else {
-            let _ = self
-                .sdl_canvas
+            self.sdl_canvas
                 .window_mut()
-                .set_fullscreen(SdlFullscreenType::Off);
+                .set_fullscreen(SdlFullscreenType::Off)
+                .unwrap();
         }
     }
 
@@ -175,6 +146,7 @@ impl Platform for Sdl2 {
                         width: width,
                         height: height,
                     },
+
                     _ => continue,
                     /*
                     WindowMinimized,
@@ -308,17 +280,17 @@ impl Platform for Sdl2 {
     }
 
     fn render_screen(&mut self, screen: &Image, bg_color: Rgb24) {
-        let width = screen.width() as usize;
-        let height = screen.height() as usize;
-        let data = screen.data();
-        let palette = screen.palette();
+        let screen_width = screen.width();
+        let screen_height = screen.height();
+        let screen_data = screen.data();
+        let screen_palette = screen.palette();
 
         self.sdl_texture
             .with_lock(None, |buffer: &mut [u8], pitch: usize| {
-                for i in 0..height {
-                    for j in 0..width {
+                for i in 0..screen_height as usize {
+                    for j in 0..screen_width as usize {
                         let offset = i * pitch + j * 3;
-                        let color = palette.display_color(data[i][j]);
+                        let color = screen_palette.display_color(screen_data[i][j]);
 
                         buffer[offset] = ((color >> 16) & 0xff) as u8;
                         buffer[offset + 1] = ((color >> 8) & 0xff) as u8;
@@ -334,9 +306,9 @@ impl Platform for Sdl2 {
             (bg_color & 0xff) as u8,
         ));
 
-        let screen_width = screen.width();
-        let screen_height = screen.height();
-        let (window_width, window_height) = self.sdl_canvas.window().size();
+        self.sdl_canvas.clear();
+
+        let (window_width, window_height) = self.window_size();
         let screen_scale = min(window_width / screen_width, window_height / screen_height);
         let screen_x = (window_width - screen_width * screen_scale) / 2;
         let screen_y = (window_height - screen_height * screen_scale) / 2;
@@ -348,10 +320,10 @@ impl Platform for Sdl2 {
             screen_height * screen_scale,
         );
 
-        self.sdl_canvas.clear();
         self.sdl_canvas
             .copy(&self.sdl_texture, None, Some(dst))
             .unwrap();
+
         self.sdl_canvas.present();
     }
 }
