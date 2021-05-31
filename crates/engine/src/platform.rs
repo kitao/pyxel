cd6ab3af13e38c5337c50998d7713b9d56cc21a5
Takashi Kitao <takashi.kitao@gmail.com>
2021-05-31 23:24:28 +0900

    Removed unnecessary methods

--- crates/engine/src/platform.rs
+++ crates/engine/src/platform.rs
@@ -12,11 +12,9 @@ pub trait Platform {
     fn new(title: &str, width: u32, height: u32, scale: u32) -> Self;
     fn window_pos(&self) -> (i32, i32);
     fn window_size(&self) -> (u32, u32);
-    fn window_title(&self) -> &str;
     fn set_window_title(&mut self, title: &str);
     fn set_window_icon(&mut self, icon: &Image, scale: u32);
-    fn is_fullscreen(&self) -> bool;
-    fn set_fullscreen(&mut self, is_fullscreen: bool);
+    fn toggle_fullscreen(&mut self);
     fn ticks(&self) -> u32;
     fn delay(&mut self, ms: u32);
     fn poll_event(&mut self) -> Option<Event>;
