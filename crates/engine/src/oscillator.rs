75e1516ab4527223ef7635b46b74c4cd8ec937a6
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-04 16:36:22 +0900

    Implemented the oscillator (WIP)

--- crates/engine/src/oscillator.rs
+++ crates/engine/src/oscillator.rs
@@ -1,9 +1,11 @@
 use blip_buf::BlipBuf;
 
 use crate::settings::{
-    MASTER_VOLUME_FACTOR, NOISE_VOLUME_FACTOR, PULSE_VOLUME_FACTOR, SQUARE_VOLUME_FACTOR,
-    TRIANGLE_VOLUME_FACTOR,
+    CLOCK_RATE, MASTER_VOLUME_FACTOR, NOISE_VOLUME_FACTOR, PULSE_VOLUME_FACTOR,
+    SQUARE_VOLUME_FACTOR, TICK_CLOCK_COUNT, TRIANGLE_VOLUME_FACTOR,
 };
+
+pub const WAVE_RESOLUTION: u32 = 32;
 
 pub enum Tone {
     Triangle,
@@ -20,152 +22,122 @@ pub enum Effect {
 }
 
 pub struct Oscillator {
-    elapsed: u32,
-    noise_seed: u32,
-
+    frequency: f64,
     period: u32,
     duration: u32,
     tone: Tone,
     volume: f64,
     effect: Effect,
+    time: u32,
     phase: u32,
-    amplitude: u16,
+    amplitude: i16,
+    noise: u32,
 }
 
 impl Oscillator {
     pub fn new() -> Oscillator {
         Oscillator {
-            elapsed: 0,
-            noise_seed: 0x8000,
-
+            frequency: 0.0,
             period: 0,
             duration: 0,
             tone: Tone::Triangle,
             volume: 0.0,
             effect: Effect::None,
+            time: 0,
             phase: 0,
             amplitude: 0,
+            noise: 0x8000,
         }
     }
 
     #[inline]
-    pub fn play(&mut self, period: u32, duration: u32, tone: Tone, volume: u8, effect: Effect) {
-        let last_period = self.period;
-
-        self.period = period / 32;
+    pub fn play(&mut self, frequency: f64, duration: u32, tone: Tone, volume: f64, effect: Effect) {
+        self.frequency = frequency;
+        self.period = (CLOCK_RATE as f64 / frequency / WAVE_RESOLUTION as f64) as u32;
         self.duration = duration;
         self.tone = tone;
-        self.volume = volume as f64;
+        self.volume = volume;
         self.effect = effect;
-
-        if self.period != last_period {
-            self.phase = 0;
-            self.elapsed = 0;
-        }
     }
 
     #[inline]
-    pub fn update(&mut self, blip_buf: &mut BlipBuf, start_time: u32, clocks: u32) {
-        assert!(clocks > 0);
-
-        let mut cur_time = start_time;
-        let end_time = start_time + clocks;
-
-        while cur_time < end_time {
-            if self.duration == 0 {
-                if self.amplitude > 0 {
-                    blip_buf.add_delta(cur_time, -(self.amplitude as i32));
-                }
-
-                break;
+    pub fn update(&mut self, blip_buf: &mut BlipBuf) {
+        if self.duration == 0 {
+            if self.amplitude != 0 {
+                blip_buf.add_delta(0, -(self.amplitude as i32));
+                self.amplitude = 0;
             }
 
-            let remaining = end_time - cur_time;
+            return;
+        }
 
-            if remaining < self.period && remaining < self.duration {
-                if self.duration < self.period {
-                    cur_time += self.duration;
-                    self.duration = 0;
-                } else {
-                    cur_time += self.period;
-                    self.duration -= self.period;
-                }
-
-                continue;
-            }
-
-            if self.duration < self.period && self.duration < remaining {
-                cur_time += self.duration;
-                self.duration = 0;
-
-                continue;
-            }
+        while self.time < TICK_CLOCK_COUNT {
+            self.phase = (self.phase + 1) % WAVE_RESOLUTION;
 
             let last_amplitude = self.amplitude;
-
             match self.tone {
                 Tone::Triangle => self.update_triangle(),
                 Tone::Square => self.update_square(),
                 Tone::Pulse => self.update_pulse(),
                 Tone::Noise => self.update_noise(),
             };
+            blip_buf.add_delta(self.time, self.amplitude as i32 - last_amplitude as i32);
 
-            blip_buf.add_delta(cur_time, self.amplitude as i32 - last_amplitude as i32);
-
-            self.phase = (self.phase + 1) % 32;
-            self.duration -= self.period;
-            cur_time += self.period;
-
-            match self.effect {
-                Effect::None => break,
-                Effect::Slide => break,
-                Effect::Vibrato => break,
-                Effect::FadeOut => break,
-            }
+            self.time += self.period;
         }
 
-        self.elapsed = cur_time - end_time;
+        match self.effect {
+            Effect::None => (),
+            Effect::Slide => self.update_slide(),
+            Effect::Vibrato => self.update_vibrato(),
+            Effect::FadeOut => self.update_fadeout(),
+        };
+
+        self.duration -= 1;
+        self.time -= TICK_CLOCK_COUNT;
     }
 
     #[inline]
     fn update_triangle(&mut self) {
-        self.amplitude = ((if self.phase < 16 {
-            self.phase
+        let volume = self.volume * MASTER_VOLUME_FACTOR * TRIANGLE_VOLUME_FACTOR * i16::MAX as f64;
+
+        self.amplitude = ((if self.phase < WAVE_RESOLUTION / 2 {
+            self.phase as f64 / (WAVE_RESOLUTION / 4) as f64 - 1.0
         } else {
-            31 - self.phase
+            3.0 - self.phase as f64 / (WAVE_RESOLUTION / 4) as f64
         }) as f64
-            / 15.0
-            * u16::MAX as f64
-            * self.volume as f64
-            / u8::MAX as f64
-            * MASTER_VOLUME_FACTOR
-            * TRIANGLE_VOLUME_FACTOR) as u16;
+            * volume) as i16;
     }
 
     #[inline]
     fn update_square(&mut self) {
-        self.amplitude = ((if self.phase < 16 { u16::MAX } else { 0 }) as f64 * self.volume as f64
-            / u8::MAX as f64
-            * MASTER_VOLUME_FACTOR
-            * SQUARE_VOLUME_FACTOR) as u16;
+        let volume = self.volume * MASTER_VOLUME_FACTOR * SQUARE_VOLUME_FACTOR * i16::MAX as f64;
+
+        self.amplitude = ((if self.phase < WAVE_RESOLUTION / 2 {
+            1.0
+        } else {
+            -1.0
+        }) * volume) as i16;
     }
 
     #[inline]
     fn update_pulse(&mut self) {
-        self.amplitude = ((if self.phase < 8 { u16::MAX } else { 0 }) as f64 * self.volume as f64
-            / u8::MAX as f64
-            * MASTER_VOLUME_FACTOR
-            * PULSE_VOLUME_FACTOR) as u16;
+        let volume = self.volume * MASTER_VOLUME_FACTOR * PULSE_VOLUME_FACTOR * i16::MAX as f64;
+
+        self.amplitude = ((if self.phase < WAVE_RESOLUTION / 4 {
+            1.0
+        } else {
+            -1.0
+        }) * volume) as i16;
     }
 
     #[inline]
     fn update_noise(&mut self) {
-        self.noise_seed >>= 1;
-        self.noise_seed |= ((self.noise_seed ^ (self.noise_seed >> 1)) & 1) << 15;
+        let volume = self.volume * MASTER_VOLUME_FACTOR * NOISE_VOLUME_FACTOR * i16::MAX as f64;
 
-        self.amplitude = ((self.noise_seed & 1) as f64 * self.volume as f64 / u8::MAX as f64
-            * MASTER_VOLUME_FACTOR
-            * NOISE_VOLUME_FACTOR) as u16;
+        self.noise >>= 1;
+        self.noise |= ((self.noise ^ (self.noise >> 1)) & 1) << 15;
+        self.amplitude = (((self.noise & 1) as f64 * 2.0 - 1.0) * volume) as i16;
     }
 
     #[inline]
@@ -191,6 +163,14 @@ impl Oscillator {
         pitch = pitch_ + Lfo(time_) * effect_pitch_;
         oscillator_.SetPeriod(AUDIO_SAMPLE_RATE / pitch);
         */
+        /*
+        inline float Channel::Lfo(int32_t time) {
+          float x = (time * 8.0f / AUDIO_SAMPLE_RATE + 0.25f);
+          x -= static_cast<int32_t>(x);
+
+          return Abs(x * 4.0f - 2.0f) - 1.0f;
+        }
+        */
     }
 
     #[inline]
@@ -205,13 +185,4 @@ impl Oscillator {
             (1.0f - static_cast<float>(time_ - effect_time_) / one_note_time_));
         */
     }
-
-    /*
-    float Channel::Lfo(int32_t time) {
-        float x = (time * 8.0f / AUDIO_SAMPLE_RATE + 0.25f);
-        x -= static_cast<int32_t>(x);
-
-        return Abs(x * 4.0f - 2.0f) - 1.0f;
-    }
-    */
 }
