419d2c4cad9bb4d71344dce89defa874c123ca77
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-07 15:01:03 +0900

    Changed the indent size

--- crates/engine/src/canvas.rs
+++ crates/engine/src/canvas.rs
@@ -1,178 +1,178 @@
 use crate::rectarea::RectArea;
 
 pub trait Canvas<T: Copy + Default> {
-  fn width(&self) -> u32;
-  fn height(&self) -> u32;
-  fn data(&self) -> &Vec<Vec<T>>;
-  fn data_mut(&mut self) -> &mut Vec<Vec<T>>;
-  fn self_rect(&self) -> RectArea;
-  fn clip_rect(&self) -> RectArea;
-  fn clip_rect_mut(&mut self) -> &mut RectArea;
-  fn render_color(&self, original_color: T) -> T;
+    fn width(&self) -> u32;
+    fn height(&self) -> u32;
+    fn data(&self) -> &Vec<Vec<T>>;
+    fn data_mut(&mut self) -> &mut Vec<Vec<T>>;
+    fn self_rect(&self) -> RectArea;
+    fn clip_rect(&self) -> RectArea;
+    fn clip_rect_mut(&mut self) -> &mut RectArea;
+    fn render_color(&self, original_color: T) -> T;
 
-  #[inline]
-  fn clip_area(&mut self) -> (i32, i32, u32, u32) {
-    (
-      self.clip_rect().left(),
-      self.clip_rect().top(),
-      self.clip_rect().width(),
-      self.clip_rect().height(),
-    )
-  }
-
-  #[inline]
-  fn set_clip_area(&mut self, left: i32, top: i32, width: u32, height: u32) {
-    let rect = RectArea::with_size(left, top, width, height).intersects(self.self_rect());
-
-    *self.clip_rect_mut() = rect;
-  }
-
-  #[inline]
-  fn reset_clip_area(&mut self) {
-    *self.clip_rect_mut() = self.self_rect();
-  }
-
-  #[inline]
-  fn clear(&mut self, color: T) {
-    let color = self.render_color(color);
-
-    for i in 0..self.height() {
-      for j in 0..self.width() {
-        self.data_mut()[i as usize][j as usize] = color;
-      }
-    }
-  }
-
-  #[inline]
-  fn point(&mut self, x: i32, y: i32) -> T {
-    if self.self_rect().contains(x, y) {
-      self.data()[y as usize][x as usize]
-    } else {
-      T::default()
-    }
-  }
-
-  #[inline]
-  fn draw_point(&mut self, x: i32, y: i32, color: T) {
-    let color = self.render_color(color);
-
-    if self.self_rect().contains(x, y) {
-      self.data_mut()[y as usize][x as usize] = color;
-    }
-  }
-
-  #[inline]
-  fn draw_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, color: T) {
-    //
-  }
-
-  #[inline]
-  fn draw_rect(&mut self, x: i32, y: i32, width: u32, height: u32, color: T) {
-    let color = self.render_color(color);
-    let rect = RectArea::with_size(x, y, width, height).intersects(self.clip_rect());
-
-    if rect.is_empty() {
-      return;
+    #[inline]
+    fn clip_area(&mut self) -> (i32, i32, u32, u32) {
+        (
+            self.clip_rect().left(),
+            self.clip_rect().top(),
+            self.clip_rect().width(),
+            self.clip_rect().height(),
+        )
     }
 
-    let left = rect.left();
-    let top = rect.top();
-    let right = rect.right();
-    let bottom = rect.bottom();
+    #[inline]
+    fn set_clip_area(&mut self, left: i32, top: i32, width: u32, height: u32) {
+        let rect = RectArea::with_size(left, top, width, height).intersects(self.self_rect());
 
-    for i in top..=bottom {
-      for j in left..=right {
-        self.data_mut()[i as usize][j as usize] = color;
-      }
-    }
-  }
-
-  #[inline]
-  fn draw_rect_border(&mut self, x: i32, y: i32, width: u32, height: u32, color: T) {
-    let color = self.render_color(color);
-    let rect = RectArea::with_size(x, y, width, height).intersects(self.clip_rect());
-
-    if rect.is_empty() {
-      return;
+        *self.clip_rect_mut() = rect;
     }
 
-    let left = rect.left();
-    let top = rect.top();
-    let right = rect.right();
-    let bottom = rect.bottom();
-
-    for i in left..=right {
-      self.data_mut()[top as usize][i as usize] = color;
-      self.data_mut()[bottom as usize][i as usize] = color;
+    #[inline]
+    fn reset_clip_area(&mut self) {
+        *self.clip_rect_mut() = self.self_rect();
     }
 
-    for i in top..=bottom {
-      self.data_mut()[i as usize][left as usize] = color;
-      self.data_mut()[i as usize][right as usize] = color;
+    #[inline]
+    fn clear(&mut self, color: T) {
+        let color = self.render_color(color);
+
+        for i in 0..self.height() {
+            for j in 0..self.width() {
+                self.data_mut()[i as usize][j as usize] = color;
+            }
+        }
     }
-  }
 
-  #[inline]
-  fn draw_circle(&mut self, x: i32, y: i32, radius: u32, color: T) {
-    let color = self.render_color(color);
+    #[inline]
+    fn point(&mut self, x: i32, y: i32) -> T {
+        if self.self_rect().contains(x, y) {
+            self.data()[y as usize][x as usize]
+        } else {
+            T::default()
+        }
+    }
 
-    //
-  }
+    #[inline]
+    fn draw_point(&mut self, x: i32, y: i32, color: T) {
+        let color = self.render_color(color);
 
-  #[inline]
-  fn draw_circle_border(&mut self, x: i32, y: i32, radius: u32, color: T) {
-    let color = self.render_color(color);
+        if self.self_rect().contains(x, y) {
+            self.data_mut()[y as usize][x as usize] = color;
+        }
+    }
 
-    //
-  }
+    #[inline]
+    fn draw_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, color: T) {
+        //
+    }
 
-  #[inline]
-  fn draw_triangle(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, x3: i32, y3: i32, color: T) {
-    let color = self.render_color(color);
+    #[inline]
+    fn draw_rect(&mut self, x: i32, y: i32, width: u32, height: u32, color: T) {
+        let color = self.render_color(color);
+        let rect = RectArea::with_size(x, y, width, height).intersects(self.clip_rect());
 
-    //
-  }
+        if rect.is_empty() {
+            return;
+        }
 
-  #[inline]
-  fn draw_triangle_border(
-    &mut self,
-    x1: i32,
-    y1: i32,
-    x2: i32,
-    y2: i32,
-    x3: i32,
-    y3: i32,
-    color: T,
-  ) {
-    let color = self.render_color(color);
+        let left = rect.left();
+        let top = rect.top();
+        let right = rect.right();
+        let bottom = rect.bottom();
 
-    //
-  }
+        for i in top..=bottom {
+            for j in left..=right {
+                self.data_mut()[i as usize][j as usize] = color;
+            }
+        }
+    }
 
-  #[inline]
-  fn paint(&mut self, x: i32, y: i32, color: T) {
-    let color = self.render_color(color);
+    #[inline]
+    fn draw_rect_border(&mut self, x: i32, y: i32, width: u32, height: u32, color: T) {
+        let color = self.render_color(color);
+        let rect = RectArea::with_size(x, y, width, height).intersects(self.clip_rect());
 
-    //
-  }
+        if rect.is_empty() {
+            return;
+        }
 
-  #[inline]
-  fn paint_rec(&mut self) {
-    //
-  }
+        let left = rect.left();
+        let top = rect.top();
+        let right = rect.right();
+        let bottom = rect.bottom();
 
-  #[inline]
-  fn copy(
-    &mut self,
-    x: i32,
-    y: i32,
-    src: &Self,
-    u: i32,
-    v: i32,
-    width: u32,
-    height: u32,
-    color_key: Option<T>,
-  ) {
-    //
-  }
+        for i in left..=right {
+            self.data_mut()[top as usize][i as usize] = color;
+            self.data_mut()[bottom as usize][i as usize] = color;
+        }
+
+        for i in top..=bottom {
+            self.data_mut()[i as usize][left as usize] = color;
+            self.data_mut()[i as usize][right as usize] = color;
+        }
+    }
+
+    #[inline]
+    fn draw_circle(&mut self, x: i32, y: i32, radius: u32, color: T) {
+        let color = self.render_color(color);
+
+        //
+    }
+
+    #[inline]
+    fn draw_circle_border(&mut self, x: i32, y: i32, radius: u32, color: T) {
+        let color = self.render_color(color);
+
+        //
+    }
+
+    #[inline]
+    fn draw_triangle(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, x3: i32, y3: i32, color: T) {
+        let color = self.render_color(color);
+
+        //
+    }
+
+    #[inline]
+    fn draw_triangle_border(
+        &mut self,
+        x1: i32,
+        y1: i32,
+        x2: i32,
+        y2: i32,
+        x3: i32,
+        y3: i32,
+        color: T,
+    ) {
+        let color = self.render_color(color);
+
+        //
+    }
+
+    #[inline]
+    fn paint(&mut self, x: i32, y: i32, color: T) {
+        let color = self.render_color(color);
+
+        //
+    }
+
+    #[inline]
+    fn paint_rec(&mut self) {
+        //
+    }
+
+    #[inline]
+    fn copy(
+        &mut self,
+        x: i32,
+        y: i32,
+        src: &Self,
+        u: i32,
+        v: i32,
+        width: u32,
+        height: u32,
+        color_key: Option<T>,
+    ) {
+        //
+    }
 }
--- crates/engine/src/image.rs
+++ crates/engine/src/image.rs
@@ -3,99 +3,99 @@ use crate::palette::{Color, Palette};
 use crate::rectarea::RectArea;
 
 pub struct Image {
-  width: u32,
-  height: u32,
-  data: Vec<Vec<Color>>,
-  palette: Palette,
-  self_rect: RectArea,
-  clip_rect: RectArea,
+    width: u32,
+    height: u32,
+    data: Vec<Vec<Color>>,
+    palette: Palette,
+    self_rect: RectArea,
+    clip_rect: RectArea,
 }
 
 impl Image {
-  pub fn new(width: u32, height: u32) -> Image {
-    Image {
-      width: width,
-      height: height,
-      data: vec![vec![0; width as usize]; height as usize],
-      palette: Palette::new(),
-      self_rect: RectArea::with_size(0, 0, width, height),
-      clip_rect: RectArea::with_size(0, 0, width, height),
+    pub fn new(width: u32, height: u32) -> Image {
+        Image {
+            width: width,
+            height: height,
+            data: vec![vec![0; width as usize]; height as usize],
+            palette: Palette::new(),
+            self_rect: RectArea::with_size(0, 0, width, height),
+            clip_rect: RectArea::with_size(0, 0, width, height),
+        }
     }
-  }
 
-  #[inline]
-  pub fn palette(&self) -> &Palette {
-    &self.palette
-  }
+    #[inline]
+    pub fn palette(&self) -> &Palette {
+        &self.palette
+    }
 
-  #[inline]
-  pub fn palette_mut(&mut self) -> &mut Palette {
-    &mut self.palette
-  }
+    #[inline]
+    pub fn palette_mut(&mut self) -> &mut Palette {
+        &mut self.palette
+    }
 
-  pub fn draw_tilemap(
-    &mut self,
-    x: i32,
-    y: i32,
-    src: &Image,
-    u: i32,
-    v: i32,
-    width: i32,
-    height: i32,
-    color_key: Option<Color>,
-  ) {
-    //
-  }
+    pub fn draw_tilemap(
+        &mut self,
+        x: i32,
+        y: i32,
+        src: &Image,
+        u: i32,
+        v: i32,
+        width: i32,
+        height: i32,
+        color_key: Option<Color>,
+    ) {
+        //
+    }
 
-  pub fn draw_text(&mut self, x: i32, y: i32, text: &str, color: Color) {
-    //
-  }
+    pub fn draw_text(&mut self, x: i32, y: i32, text: &str, color: Color) {
+        //
+    }
 }
 
 impl Canvas<Color> for Image {
-  #[inline]
-  fn width(&self) -> u32 {
-    self.width
-  }
+    #[inline]
+    fn width(&self) -> u32 {
+        self.width
+    }
 
-  #[inline]
-  fn height(&self) -> u32 {
-    self.height
-  }
+    #[inline]
+    fn height(&self) -> u32 {
+        self.height
+    }
 
-  #[inline]
-  fn data<'a>(&'a self) -> &'a Vec<Vec<Color>> {
-    &self.data
-  }
+    #[inline]
+    fn data<'a>(&'a self) -> &'a Vec<Vec<Color>> {
+        &self.data
+    }
 
-  #[inline]
-  fn data_mut<'a>(&'a mut self) -> &'a mut Vec<Vec<Color>> {
-    &mut self.data
-  }
+    #[inline]
+    fn data_mut<'a>(&'a mut self) -> &'a mut Vec<Vec<Color>> {
+        &mut self.data
+    }
 
-  #[inline]
-  fn self_rect(&self) -> RectArea {
-    self.self_rect
-  }
+    #[inline]
+    fn self_rect(&self) -> RectArea {
+        self.self_rect
+    }
 
-  #[inline]
-  fn clip_rect(&self) -> RectArea {
-    self.clip_rect
-  }
+    #[inline]
+    fn clip_rect(&self) -> RectArea {
+        self.clip_rect
+    }
 
-  #[inline]
-  fn clip_rect_mut(&mut self) -> &mut RectArea {
-    &mut self.clip_rect
-  }
+    #[inline]
+    fn clip_rect_mut(&mut self) -> &mut RectArea {
+        &mut self.clip_rect
+    }
 
-  #[inline]
-  fn render_color(&self, original_color: Color) -> Color {
-    self.palette.render_color(original_color)
-  }
+    #[inline]
+    fn render_color(&self, original_color: Color) -> Color {
+        self.palette.render_color(original_color)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  //
+    use super::*;
+    //
 }
