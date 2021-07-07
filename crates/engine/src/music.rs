b4e6e939ab0a2a9770a42c9eefb22e8beb558a7f
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-07 15:01:03 +0900

    Changed the indent size

--- crates/engine/src/music.rs
+++ crates/engine/src/music.rs
@@ -1,27 +1,27 @@
 use crate::settings::CHANNEL_COUNT;
 
 pub struct Music {
-  channels: Vec<Vec<u32>>,
+    channels: Vec<Vec<u32>>,
 }
 
 impl Music {
-  pub fn new() -> Music {
-    let mut channels = Vec::new();
+    pub fn new() -> Music {
+        let mut channels = Vec::new();
 
-    for _ in 0..CHANNEL_COUNT {
-      channels.push(Vec::new());
+        for _ in 0..CHANNEL_COUNT {
+            channels.push(Vec::new());
+        }
+
+        Music { channels: channels }
+    }
+}
+
+impl Music {
+    pub fn channel_mut(&mut self, index: u32) -> &mut Vec<u32> {
+        &mut self.channels[index as usize]
     }
 
-    Music { channels: channels }
-  }
-}
-
-impl Music {
-  pub fn channel_mut(&mut self, index: u32) -> &mut Vec<u32> {
-    &mut self.channels[index as usize]
-  }
-
-  pub fn set(&mut self, index: u32, sounds: &Vec<u32>) {
-    self.channels[index as usize] = sounds.clone();
-  }
+    pub fn set(&mut self, index: u32, sounds: &Vec<u32>) {
+        self.channels[index as usize] = sounds.clone();
+    }
 }
