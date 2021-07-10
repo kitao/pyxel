a01f6f0fb80444af74eb1d415130545264a9d464
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-10 23:24:53 +0900

    Changed the way to initialize

--- crates/engine/src/graphics.rs
+++ crates/engine/src/graphics.rs
@@ -12,28 +12,22 @@ pub struct Graphics {
 impl Graphics {
     pub fn new(width: u32, height: u32, colors: Option<&[Rgb24]>) -> Graphics {
         let mut screen = Image::new(width, height);
-        let mut images = Vec::new();
-        let mut tilemaps = Vec::new();
+        let images = (0..IMAGE_COUNT)
+            .map(|_| Image::new(IMAGE_SIZE, IMAGE_SIZE))
+            .collect();
+        let tilemaps = (0..TILEMAP_COUNT)
+            .map(|_| Tilemap::new(TILEMAP_SIZE, TILEMAP_SIZE))
+            .collect();
 
         screen
             .palette_mut()
             .set_display_colors(colors.unwrap_or(&DISPLAY_COLORS));
 
-        for _ in 0..IMAGE_COUNT {
-            images.push(Image::new(IMAGE_SIZE, IMAGE_SIZE));
-        }
-
-        for _ in 0..TILEMAP_COUNT {
-            tilemaps.push(Tilemap::new(TILEMAP_SIZE, TILEMAP_SIZE));
-        }
-
-        let graphics = Graphics {
+        Graphics {
             screen: screen,
             images: images,
             tilemaps: tilemaps,
-        };
-
-        graphics
+        }
     }
 
     pub fn screen(&self) -> &Image {
@@ -44,27 +38,27 @@ impl Graphics {
         &mut self.screen
     }
 
-    pub fn image(&self, no: u32) -> &Image {
-        if no < self.images.len() as u32 {
-            &self.images[no as usize]
+    pub fn image(&self, image_no: u32) -> &Image {
+        if image_no < self.images.len() as u32 {
+            &self.images[image_no as usize]
         } else {
             &self.screen
         }
     }
 
-    pub fn image_mut(&mut self, no: u32) -> &mut Image {
-        if no < self.images.len() as u32 {
-            &mut self.images[no as usize]
+    pub fn image_mut(&mut self, image_no: u32) -> &mut Image {
+        if image_no < self.images.len() as u32 {
+            &mut self.images[image_no as usize]
         } else {
             &mut self.screen
         }
     }
 
-    pub fn tilemap(&self, no: u32) -> &Tilemap {
-        &self.tilemaps[no as usize]
+    pub fn tilemap(&self, tilemap_no: u32) -> &Tilemap {
+        &self.tilemaps[tilemap_no as usize]
     }
 
-    pub fn tilemap_mut(&mut self, no: u32) -> &mut Tilemap {
-        &mut self.tilemaps[no as usize]
+    pub fn tilemap_mut(&mut self, tilemap_no: u32) -> &mut Tilemap {
+        &mut self.tilemaps[tilemap_no as usize]
     }
 }
