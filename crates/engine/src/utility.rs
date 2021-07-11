089f0f30a319f3b018c9ae7c5658d20e89c5c017
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-11 13:26:02 +0900

    Implemented test code

--- crates/engine/src/utility.rs
+++ crates/engine/src/utility.rs
@@ -1,3 +1,13 @@
 pub fn remove_whitespace(s: &str) -> String {
     s.replace(&[' ', '\t', '\r', '\n'][..], "")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn remove_whitespace_() {
+        assert_eq!(remove_whitespace(" a\t b\r c\n d "), "abcd")
+    }
+}
