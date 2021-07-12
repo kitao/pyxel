4dddb0421a279682afa5802584eefc38adfd5197
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-12 21:45:45 +0900

    Added the sound test

--- crates/engine/tests/main.rs
+++ crates/engine/tests/main.rs
@@ -31,5 +31,54 @@ pub fn main() {
     let mut pyxel = Pyxel::new(200, 150, Some("Hello, Pyxel in Rust!"), None, None, None);
     let mut app = App::new();
 
+    pyxel.sound(0).set(
+        &pyxel,
+        "e2e2c2g1 g1g1c2e2 d2d2d2g2 g2g2rr c2c2a1e1 e1e1a1c2 b1b1b1e2 e2e2rr",
+        "p",
+        "6",
+        "vffn fnff vffs vfnn",
+        25,
+    );
+
+    pyxel.sound(1).set(
+        &pyxel,
+        "r a1b1c2 b1b1c2d2 g2g2g2g2 c2c2d2e2 f2f2f2e2 f2e2d2c2 d2d2d2d2 g2g2r r ",
+        "s",
+        "6",
+        "nnff vfff vvvv vfff svff vfff vvvv svnn",
+        25,
+    );
+
+    pyxel.sound(2).set(
+        &pyxel,
+        "c1g1c1g1 c1g1c1g1 b0g1b0g1 b0g1b0g1 a0e1a0e1 a0e1a0e1 g0d1g0d1 g0d1g0d1",
+        "t",
+        "7",
+        "n",
+        25,
+    );
+
+    pyxel.sound(3).set(
+        &pyxel,
+        "f0c1f0c1 g0d1g0d1 c1g1c1g1 a0e1a0e1 f0c1f0c1 f0c1f0c1 g0d1g0d1 g0d1g0d1",
+        "t",
+        "7",
+        "n",
+        25,
+    );
+
+    pyxel.sound(4).set(
+        &pyxel,
+        "f0ra4r f0ra4r f0ra4r f0f0a4r",
+        "n",
+        "6622 6622 6622 6422",
+        "f",
+        25,
+    );
+
+    pyxel.play(0, &[0, 1], true);
+    pyxel.play(1, &[2, 3], true);
+    pyxel.play(2, &[4], true);
+
     pyxel.run(&mut app);
 }
