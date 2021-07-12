9323ff4110382d3abbc8eeb3b55749183b169956
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-12 21:12:50 +0900

    Added the unify_string function

--- crates/engine/src/utility.rs
+++ crates/engine/src/utility.rs
@@ -1,5 +1,9 @@
 pub fn remove_whitespace(s: &str) -> String {
-    s.replace(&[' ', '\t', '\r', '\n'][..], "")
+    s.replace(&[' ', '\n', '\r', '\t'][..], "")
+}
+
+pub fn arrange_string(s: &str) -> String {
+    remove_whitespace(s).to_ascii_lowercase()
 }
 
 #[cfg(test)]
@@ -8,6 +12,11 @@ mod tests {
 
     #[test]
     fn remove_whitespace_() {
-        assert_eq!(remove_whitespace(" a\t b\r c\n d "), "abcd")
+        assert_eq!(remove_whitespace(" a\n b\r c\t d "), "abcd")
+    }
+
+    #[test]
+    fn unify_string_() {
+        assert_eq!(arrange_string(" 0\n 1\r 2\t 3 A\n b\r c\t d "), "0123abcd");
     }
 }
