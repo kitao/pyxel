e91487a40a14fc60e8e91ab79857cc4c408487fe
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-13 22:52:14 +0900

    Added utility functions

--- crates/engine/src/utility.rs
+++ crates/engine/src/utility.rs
@@ -6,6 +6,25 @@ pub fn arrange_string(s: &str) -> String {
     remove_whitespace(s).to_ascii_lowercase()
 }
 
+pub fn parse_hex_string(s: &str) -> Option<u32> {
+    let s = s.to_ascii_lowercase();
+    let mut result: u32 = 0;
+
+    for c in s.chars() {
+        result *= 0x10;
+
+        if c >= '0' && c <= '9' {
+            result += c as u32 - '0' as u32;
+        } else if c >= 'a' && c <= 'f' {
+            result += 10 + c as u32 - 'a' as u32;
+        } else {
+            return None;
+        }
+    }
+
+    Some(result)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,5 +37,13 @@ mod tests {
     #[test]
     fn unify_string_() {
         assert_eq!(arrange_string(" 0\n 1\r 2\t 3 A\n b\r c\t d "), "0123abcd");
+    }
+
+    #[test]
+    fn parse_hex_string_() {
+        assert_eq!(parse_hex_string("100"), Some(256));
+        assert_eq!(parse_hex_string("a2"), Some(162));
+        assert_eq!(parse_hex_string("BC"), Some(188));
+        assert_eq!(parse_hex_string(" "), None);
     }
 }
