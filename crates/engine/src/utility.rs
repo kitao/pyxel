93d82eecd146cc56e6ea49afe9c73634d8aa774c
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-25 15:26:37 +0900

    Added utility functions

--- crates/engine/src/utility.rs
+++ crates/engine/src/utility.rs
@@ -1,3 +1,5 @@
+use crate::rectarea::RectArea;
+
 pub fn remove_whitespace(s: &str) -> String {
     s.replace(&[' ', '\n', '\r', '\t'][..], "")
 }
@@ -25,6 +27,43 @@ pub fn parse_hex_string(s: &str) -> Option<u32> {
     Some(result)
 }
 
+#[inline]
+pub fn data_value<T: Copy>(data: &Vec<Vec<T>>, x: i32, y: i32) -> T {
+    data[y as usize][x as usize]
+}
+
+#[inline]
+pub fn data_value_with_check<T: Copy + Default>(
+    data: &Vec<Vec<T>>,
+    rect: RectArea,
+    x: i32,
+    y: i32,
+) -> T {
+    if rect.contains(x, y) {
+        data[y as usize][x as usize]
+    } else {
+        T::default()
+    }
+}
+
+#[inline]
+pub fn set_data_value<T: Copy>(data: &mut Vec<Vec<T>>, x: i32, y: i32, val: T) {
+    data[y as usize][x as usize] = val;
+}
+
+#[inline]
+pub fn set_data_value_with_check<T: Copy>(
+    data: &mut Vec<Vec<T>>,
+    rect: RectArea,
+    x: i32,
+    y: i32,
+    val: T,
+) {
+    if rect.contains(x, y) {
+        data[y as usize][x as usize] = val;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,5 +84,77 @@ mod tests {
         assert_eq!(parse_hex_string("a2"), Some(162));
         assert_eq!(parse_hex_string("BC"), Some(188));
         assert_eq!(parse_hex_string(" "), None);
+    }
+
+    #[test]
+    fn data_value_() {
+        let data = vec![vec![1, 2], vec![3, 4]];
+
+        assert_eq!(data_value(&data, 0, 0), 1);
+        assert_eq!(data_value(&data, 1, 0), 2);
+        assert_eq!(data_value(&data, 0, 1), 3);
+        assert_eq!(data_value(&data, 1, 1), 4);
+    }
+
+    #[test]
+    fn data_value_with_check_() {
+        let data = vec![vec![1, 2], vec![3, 4]];
+        let rect = RectArea::new(0, 0, 2, 2);
+
+        assert_eq!(data_value_with_check(&data, rect, 0, 0), 1);
+        assert_eq!(data_value_with_check(&data, rect, 1, 0), 2);
+        assert_eq!(data_value_with_check(&data, rect, 0, 1), 3);
+        assert_eq!(data_value_with_check(&data, rect, 1, 1), 4);
+
+        assert_eq!(data_value_with_check(&data, rect, -1, 0), 0);
+        assert_eq!(data_value_with_check(&data, rect, 0, -1), 0);
+        assert_eq!(data_value_with_check(&data, rect, 2, 0), 0);
+        assert_eq!(data_value_with_check(&data, rect, 0, 2), 0);
+    }
+
+    #[test]
+    fn set_data_value_() {
+        let mut data = vec![vec![1, 2], vec![3, 4]];
+
+        assert_eq!(data_value(&data, 0, 0), 1);
+        assert_eq!(data_value(&data, 1, 0), 2);
+        assert_eq!(data_value(&data, 0, 1), 3);
+        assert_eq!(data_value(&data, 1, 1), 4);
+
+        set_data_value(&mut data, 0, 0, 5);
+        set_data_value(&mut data, 1, 0, 6);
+        set_data_value(&mut data, 0, 1, 7);
+        set_data_value(&mut data, 1, 1, 8);
+
+        assert_eq!(data_value(&data, 0, 0), 5);
+        assert_eq!(data_value(&data, 1, 0), 6);
+        assert_eq!(data_value(&data, 0, 1), 7);
+        assert_eq!(data_value(&data, 1, 1), 8);
+    }
+
+    #[test]
+    fn set_data_value_with_check_() {
+        let mut data = vec![vec![1, 2], vec![3, 4]];
+        let rect = RectArea::new(0, 0, 2, 2);
+
+        assert_eq!(data_value(&data, 0, 0), 1);
+        assert_eq!(data_value(&data, 1, 0), 2);
+        assert_eq!(data_value(&data, 0, 1), 3);
+        assert_eq!(data_value(&data, 1, 1), 4);
+
+        set_data_value(&mut data, 0, 0, 5);
+        set_data_value(&mut data, 1, 0, 6);
+        set_data_value(&mut data, 0, 1, 7);
+        set_data_value(&mut data, 1, 1, 8);
+
+        set_data_value_with_check(&mut data, rect, -1, 0, 5);
+        set_data_value_with_check(&mut data, rect, 0, -1, 6);
+        set_data_value_with_check(&mut data, rect, 0, 1, 7);
+        set_data_value_with_check(&mut data, rect, 1, 1, 8);
+
+        assert_eq!(data_value(&data, 0, 0), 5);
+        assert_eq!(data_value(&data, 1, 0), 6);
+        assert_eq!(data_value(&data, 0, 1), 7);
+        assert_eq!(data_value(&data, 1, 1), 8);
     }
 }
