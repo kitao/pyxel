9120e6db5ae51354d145fcdccdb94961bfbfde58
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-26 14:52:09 +0900

    Refactored the code

--- crates/engine/tests/main.rs
+++ crates/engine/tests/main.rs
@@ -6,8 +6,63 @@ pub struct App {
 }
 
 impl App {
-    fn new() -> App {
-        App { x: 0, y: 0 }
+    fn new(pyxel: &mut Pyxel) -> App {
+        let app = App { x: 0, y: 0 };
+
+        pyxel.images[0].borrow_mut().set(
+            0,
+            0,
+            &[
+                "00011000", "00010100", "00010010", "00010010", "00010100", "00010000", "01110000",
+                "01100000",
+            ],
+        );
+
+        pyxel.sounds[0].borrow_mut().set(
+            "e2e2c2g1 g1g1c2e2 d2d2d2g2 g2g2rr c2c2a1e1 e1e1a1c2 b1b1b1e2 e2e2rr",
+            "p",
+            "6",
+            "vffn fnff vffs vfnn",
+            25,
+        );
+
+        pyxel.sounds[1].borrow_mut().set(
+            "r a1b1c2 b1b1c2d2 g2g2g2g2 c2c2d2e2 f2f2f2e2 f2e2d2c2 d2d2d2d2 g2g2r r ",
+            "s",
+            "6",
+            "nnff vfff vvvv vfff svff vfff vvvv svnn",
+            25,
+        );
+
+        pyxel.sounds[2].borrow_mut().set(
+            "c1g1c1g1 c1g1c1g1 b0g1b0g1 b0g1b0g1 a0e1a0e1 a0e1a0e1 g0d1g0d1 g0d1g0d1",
+            "t",
+            "7",
+            "n",
+            25,
+        );
+
+        pyxel.sounds[3].borrow_mut().set(
+            "f0c1f0c1 g0d1g0d1 c1g1c1g1 a0e1a0e1 f0c1f0c1 f0c1f0c1 g0d1g0d1 g0d1g0d1",
+            "t",
+            "7",
+            "n",
+            25,
+        );
+
+        pyxel.sounds[4].borrow_mut().set(
+            "f0ra4r f0ra4r f0ra4r f0f0a4r",
+            "n",
+            "6622 6622 6622 6422",
+            "f",
+            25,
+        );
+
+        pyxel.play(0, &[0, 1], true);
+        pyxel.play(1, &[2, 3], true);
+        pyxel.play(2, &[4], true);
+
+        app
     }
 }
 
@@ -35,60 +90,7 @@ impl PyxelCallback for App {
 
 pub fn main() {
     let mut pyxel = Pyxel::new(200, 150, Some("Hello, Pyxel in Rust!"), None, None, None);
-    let mut app = App::new();
-
-    pyxel.images[0].borrow_mut().set(
-        0,
-        0,
-        &[
-            "00011000", "00010100", "00010010", "00010010", "00010100", "00010000", "01110000",
-            "01100000",
-        ],
-    );
-
-    pyxel.sounds[0].borrow_mut().set(
-        "e2e2c2g1 g1g1c2e2 d2d2d2g2 g2g2rr c2c2a1e1 e1e1a1c2 b1b1b1e2 e2e2rr",
-        "p",
-        "6",
-        "vffn fnff vffs vfnn",
-        25,
-    );
-
-    pyxel.sounds[1].borrow_mut().set(
-        "r a1b1c2 b1b1c2d2 g2g2g2g2 c2c2d2e2 f2f2f2e2 f2e2d2c2 d2d2d2d2 g2g2r r ",
-        "s",
-        "6",
-        "nnff vfff vvvv vfff svff vfff vvvv svnn",
-        25,
-    );
-
-    pyxel.sounds[2].borrow_mut().set(
-        "c1g1c1g1 c1g1c1g1 b0g1b0g1 b0g1b0g1 a0e1a0e1 a0e1a0e1 g0d1g0d1 g0d1g0d1",
-        "t",
-        "7",
-        "n",
-        25,
-    );
-
-    pyxel.sounds[3].borrow_mut().set(
-        "f0c1f0c1 g0d1g0d1 c1g1c1g1 a0e1a0e1 f0c1f0c1 f0c1f0c1 g0d1g0d1 g0d1g0d1",
-        "t",
-        "7",
-        "n",
-        25,
-    );
-
-    pyxel.sounds[4].borrow_mut().set(
-        "f0ra4r f0ra4r f0ra4r f0f0a4r",
-        "n",
-        "6622 6622 6622 6422",
-        "f",
-        25,
-    );
-
-    pyxel.play(0, &[0, 1], true);
-    pyxel.play(1, &[2, 3], true);
-    pyxel.play(2, &[4], true);
+    let mut app = App::new(&mut pyxel);
 
     pyxel.run(&mut app);
 }
