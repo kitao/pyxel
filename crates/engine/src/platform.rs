570e62933f6fe8218a812d7d745586ac94c96093
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-26 23:06:03 +0900

    Renamed methods

--- crates/engine/src/platform.rs
+++ crates/engine/src/platform.rs
@@ -5,17 +5,16 @@ use crate::image::Image;
 use crate::types::Rgb8;
 
 pub trait AudioCallback {
-    fn audio_callback(&mut self, out: &mut [i16]);
+    fn update(&mut self, out: &mut [i16]);
 }
 
 pub trait Platform {
     fn new(title: &str, width: u32, height: u32, scale: u32) -> Self;
     fn set_title(&mut self, title: &str);
     fn set_icon(&mut self, icon: &Image, colors: &[Rgb8], scale: u32);
-    fn is_fullscreen(&mut self) -> bool;
-    fn set_fullscreen(&mut self, fullscreen: bool);
-    fn ticks(&self) -> u32;
-    fn delay(&mut self, ms: u32);
+    fn toggle_fullscreen(&mut self);
+    fn tick_count(&self) -> u32;
+    fn sleep(&mut self, ms: u32);
     fn poll_event(&mut self) -> Option<Event>;
     fn render_screen(&mut self, screen: &Image, colors: &[Rgb8], bg_color: Rgb8);
     fn start_audio(
