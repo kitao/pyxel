4f28f8a6f3e3c2a2956f6042d38a1b4e7f3232f5
Takashi Kitao <takashi.kitao@gmail.com>
2021-07-31 10:39:20 +0900

    Refactored the code

--- crates/engine/src/image.rs
+++ crates/engine/src/image.rs
@@ -21,6 +21,43 @@ pub struct Image {
 
 pub type SharedImage = Rc<RefCell<Image>>;
 
+impl Canvas<Color> for Image {
+    #[inline]
+    fn _width(&self) -> u32 {
+        self._self_rect().width()
+    }
+
+    #[inline]
+    fn _height(&self) -> u32 {
+        self._self_rect().height()
+    }
+
+    #[inline]
+    fn _data<'a>(&'a self) -> &'a Vec<Vec<Color>> {
+        &self.data
+    }
+
+    #[inline]
+    fn _data_mut<'a>(&'a mut self) -> &'a mut Vec<Vec<Color>> {
+        &mut self.data
+    }
+
+    #[inline]
+    fn _self_rect(&self) -> RectArea {
+        self.self_rect
+    }
+
+    #[inline]
+    fn _clip_rect(&self) -> RectArea {
+        self.clip_rect
+    }
+
+    #[inline]
+    fn _clip_rect_mut(&mut self) -> &mut RectArea {
+        &mut self.clip_rect
+    }
+}
+
 impl Image {
     pub fn new(width: u32, height: u32) -> SharedImage {
         Rc::new(RefCell::new(Image {
@@ -78,11 +115,85 @@ impl Image {
         height: i32,
         transparent: Option<Tile>,
     ) {
-        //
+        /*
+        Tilemap* tilemap = GetTilemapBank(tilemap_index);
+        int32_t image_index = tilemap->ImageIndex();
+
+        int32_t left = clip_area_.Left() / TILEMAP_CHIP_WIDTH;
+        int32_t top = clip_area_.Top() / TILEMAP_CHIP_WIDTH;
+        int32_t right =
+            (clip_area_.Right() + TILEMAP_CHIP_WIDTH - 1) / TILEMAP_CHIP_WIDTH;
+        int32_t bottom =
+            (clip_area_.Bottom() + TILEMAP_CHIP_HEIGHT - 1) / TILEMAP_CHIP_HEIGHT;
+        Rectangle dst_rect = Rectangle(left, top, right - left + 1, bottom - top + 1);
+
+        Rectangle::CopyArea copy_area =
+            dst_rect.GetCopyArea(x / TILEMAP_CHIP_WIDTH, y / TILEMAP_CHIP_HEIGHT,
+                                 tilemap->Rectangle(), u, v, width, height);
+
+        if (copy_area.IsEmpty()) {
+          return;
+        }
+
+        int32_t** src_data = tilemap->Data();
+
+        copy_area.x = copy_area.x * TILEMAP_CHIP_WIDTH + x % TILEMAP_CHIP_WIDTH;
+        copy_area.y = copy_area.y * TILEMAP_CHIP_HEIGHT + y % TILEMAP_CHIP_HEIGHT;
+
+        for (int32_t i = 0; i < copy_area.height; i++) {
+          int32_t* src_line = src_data[copy_area.v + i];
+          int32_t dst_y = copy_area.y + TILEMAP_CHIP_HEIGHT * i;
+
+          for (int32_t j = 0; j < copy_area.width; j++) {
+            int32_t chip = src_line[copy_area.u + j];
+            int32_t cu =
+                (chip % (IMAGE_BANK_WIDTH / TILEMAP_CHIP_WIDTH)) * TILEMAP_CHIP_WIDTH;
+            int32_t cv = (chip / (IMAGE_BANK_HEIGHT / TILEMAP_CHIP_HEIGHT)) *
+                         TILEMAP_CHIP_HEIGHT;
+
+            DrawImage(copy_area.x + TILEMAP_CHIP_WIDTH * j, dst_y, image_index, cu,
+                      cv, TILEMAP_CHIP_WIDTH, TILEMAP_CHIP_HEIGHT, color_key);
+          }
+        }
+        */
     }
 
     pub fn text(&mut self, x: i32, y: i32, string: &str, color: Color, font: &Image) {
-        //
+        /*
+        int32_t draw_color = GET_DRAW_COLOR(color);
+        int32_t cur_color = palette_table_[FONT_COLOR];
+        palette_table_[FONT_COLOR] = draw_color;
+
+        int32_t left = x;
+
+        for (const char* ch = text; *ch != '\0'; ch++) {
+          if (*ch == 10) {  // new line
+            x = left;
+            y += FONT_HEIGHT;
+            continue;
+          }
+
+          if (*ch == 32) {  // space
+            x += FONT_WIDTH;
+            continue;
+          }
+
+          if (*ch < MIN_FONT_CODE || *ch > MAX_FONT_CODE) {
+            continue;
+          }
+
+          int32_t code = *ch - MIN_FONT_CODE;
+          int32_t u = (code % FONT_ROW_COUNT) * FONT_WIDTH;
+          int32_t v = (code / FONT_ROW_COUNT) * FONT_HEIGHT;
+
+          DrawImage(x, y, IMAGE_BANK_FOR_SYSTEM, FONT_X + u, FONT_Y + v, FONT_WIDTH,
+                    FONT_HEIGHT, 0);
+
+          x += FONT_WIDTH;
+        }
+
+        palette_table_[FONT_COLOR] = cur_color;
+        */
     }
 
     pub fn load(&mut self, x: i32, y: i32, filename: &str, colors: &[Rgb8]) {
@@ -150,42 +261,5 @@ impl Image {
         let dz = (b1 as f64 - b2 as f64) * 0.11;
 
         dx * dx + dy * dy + dz * dz
-    }
-}
-
-impl Canvas<Color> for Image {
-    #[inline]
-    fn _width(&self) -> u32 {
-        self._self_rect().width()
-    }
-
-    #[inline]
-    fn _height(&self) -> u32 {
-        self._self_rect().height()
-    }
-
-    #[inline]
-    fn _data<'a>(&'a self) -> &'a Vec<Vec<Color>> {
-        &self.data
-    }
-
-    #[inline]
-    fn _data_mut<'a>(&'a mut self) -> &'a mut Vec<Vec<Color>> {
-        &mut self.data
-    }
-
-    #[inline]
-    fn _self_rect(&self) -> RectArea {
-        self.self_rect
-    }
-
-    #[inline]
-    fn _clip_rect(&self) -> RectArea {
-        self.clip_rect
-    }
-
-    #[inline]
-    fn _clip_rect_mut(&mut self) -> &mut RectArea {
-        &mut self.clip_rect
     }
 }
