1054fbd5d341e1276c9dfa0ad436de7704467621
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-01 21:42:24 +0900

    Renamed the extension module

--- crates/extension/src/lib.rs
+++ crates/extension/src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
-use pyxel as engine;
+use pyxel;
 
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
@@ -8,23 +8,8 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pymodule]
-fn pyxel(_py: Python, m: &PyModule) -> PyResult<()> {
+fn pyxel_extension(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use pyo3::py_run;
-    use pyo3::types::PyList;
-
-    #[test]
-    fn test() {
-        let gil = Python::acquire_gil();
-        let py = gil.python();
-        let list = PyList::new(py, &[1, 2, 3]);
-        py_run!(py, list, "assert list == [1, 2, 3]");
-    }
 }
