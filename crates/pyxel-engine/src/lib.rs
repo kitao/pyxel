a06093574227b10f17f09e7c08499c7f475562f6
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-14 14:57:57 +0900

    Changed the way to keep variables

--- crates/pyxel-engine/src/lib.rs
+++ crates/pyxel-engine/src/lib.rs
@@ -21,18 +21,17 @@ mod types;
 mod utility;
 
 use array_macro::array;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-use crate::audio::{AtomicAudio, Audio};
+use crate::audio::Audio;
 use crate::graphics::Graphics;
-use crate::image::{Image, SharedImage};
+use crate::image::Image;
 use crate::input::Input;
-use crate::music::{Music, SharedMusic};
 use crate::platform::Platform;
 use crate::resource::Resource;
 use crate::sdl2::Sdl2;
-use crate::sound::{SharedSound, Sound};
 use crate::system::System;
-use crate::tilemap::{SharedTilemap, Tilemap};
 
 pub use crate::key::*;
 pub use crate::settings::*;
@@ -45,18 +44,14 @@ pub struct Pyxel {
     system: System,
     resource: Resource,
     input: Input,
-    _graphics: Graphics,
-    audio: AtomicAudio,
+    graphics: Graphics,
+    audio: Audio,
 
-    pub colors: [Rgb8; COLOR_COUNT as usize],
-    pub palette: [Color; COLOR_COUNT as usize],
-    pub screen: SharedImage,
-    pub cursor: SharedImage,
-    pub font: SharedImage,
-    pub images: Vec<SharedImage>,
-    pub tilemaps: Vec<SharedTilemap>,
-    pub sounds: Vec<SharedSound>,
-    pub musics: Vec<SharedMusic>,
+    pub colors: [Rgb8; MAX_COLOR_COUNT as usize],
+    pub palette: [Color; MAX_COLOR_COUNT as usize],
+    pub screen: Rc<RefCell<Image>>,
+    pub cursor: Rc<RefCell<Image>>,
+    pub font: Rc<RefCell<Image>>,
 }
 
 pub trait PyxelCallback {
@@ -85,37 +80,28 @@ impl Pyxel {
         let graphics = Graphics::new();
         let audio = Audio::new(&mut platform);
 
-        let colors = DEFAULT_COLORS.clone();
-        let palette = array![i => i as Color; COLOR_COUNT as usize];
-        let screen = Image::new(width, height);
-        let cursor = Graphics::new_cursor_image();
-        let font = Graphics::new_font_image();
-        let images = (0..IMAGE_COUNT)
-            .map(|_| Image::new(IMAGE_SIZE, IMAGE_SIZE))
-            .collect();
-        let tilemaps = (0..TILEMAP_COUNT)
-            .map(|_| Tilemap::new(TILEMAP_SIZE, TILEMAP_SIZE))
-            .collect();
-        let sounds = (0..SOUND_COUNT).map(|_| Sound::new()).collect();
-        let musics = (0..MUSIC_COUNT).map(|_| Music::new()).collect();
+        let mut colors = [0; MAX_COLOR_COUNT as usize];
+        for i in 0..COLOR_COUNT {
+            colors[i as usize] = DEFAULT_COLORS[i as usize];
+        }
+        let palette = array![i => i as Color; MAX_COLOR_COUNT as usize];
+        let screen = Rc::new(RefCell::new(Image::new(width, height)));
+        let cursor = Rc::new(RefCell::new(Graphics::new_cursor_image()));
+        let font = Rc::new(RefCell::new(Graphics::new_font_image()));
 
         Pyxel {
             platform: platform,
             system: system,
             resource: resource,
             input: input,
-            _graphics: graphics,
-            audio: audio.clone(),
+            graphics: graphics,
+            audio: audio,
 
             colors: colors,
             palette: palette,
             screen: screen,
             cursor: cursor,
             font: font,
-            images: images,
-            tilemaps: tilemaps,
-            sounds: sounds,
-            musics: musics,
         }
     }
 }
