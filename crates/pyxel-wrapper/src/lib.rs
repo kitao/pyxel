83571a6d31490618c7d2815dd6b8968131b05081
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-15 17:08:39 +0900

    Moved Colors and Palette to variable_wrapper

--- crates/pyxel-wrapper/src/lib.rs
+++ crates/pyxel-wrapper/src/lib.rs
@@ -1,8 +1,11 @@
-mod colors_wrapper;
+mod channel_wrapper;
 mod constant_wrapper;
 mod graphics_wrapper;
-mod palette_wrapper;
+mod image_wrapper;
+mod music_wrapper;
+mod sound_wrapper;
 mod system_wrapper;
+mod tilemap_wrapper;
 #[allow(non_snake_case)]
 mod variable_wrapper;
 
@@ -11,10 +14,8 @@ use pyxel::Pyxel;
 use std::cmp::max;
 use std::mem::transmute;
 
-use crate::colors_wrapper::add_colors_class;
 use crate::constant_wrapper::add_module_constants;
 use crate::graphics_wrapper::add_graphics_functions;
-use crate::palette_wrapper::add_palette_class;
 use crate::system_wrapper::add_system_functions;
 use crate::variable_wrapper::add_module_variables;
 
@@ -42,8 +43,6 @@ pub fn i32_to_u32(x: i32) -> u32 {
 
 #[pymodule]
 fn pyxel_wrapper(_py: Python, m: &PyModule) -> PyResult<()> {
-    add_colors_class(m)?;
-    add_palette_class(m)?;
     add_module_constants(m)?;
     add_module_variables(m)?;
     add_system_functions(m)?;
