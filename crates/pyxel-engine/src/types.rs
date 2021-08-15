9295434b6c54808f926d1807dbbcc2fcb94588de
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-15 13:19:58 +0900

    Changed enum to const

--- crates/pyxel-engine/src/types.rs
+++ crates/pyxel-engine/src/types.rs
@@ -32,33 +32,8 @@ impl ToIndex for Tile {
 //
 // Audio
 //
-pub type Note = i32;
+pub type Note = i8;
+pub type Tone = u8;
+pub type Volume = u8;
+pub type Effect = u8;
 pub type Speed = u32;
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Tone {
-    Triangle,
-    Square,
-    Pulse,
-    Noise,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
-pub enum Volume {
-    Level0 = 0,
-    Level1 = 1,
-    Level2 = 2,
-    Level3 = 3,
-    Level4 = 4,
-    Level5 = 5,
-    Level6 = 6,
-    Level7 = 7,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Effect {
-    None,
-    Slide,
-    Vibrato,
-    FadeOut,
-}
