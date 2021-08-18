51993e9a912aca1743d8027fb14b6495cdfba01a
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-18 21:18:59 +0900

    Implemented the wrapper for Channel (WIP)

--- crates/pyxel-wrapper/src/channel_wrapper.rs
+++ crates/pyxel-wrapper/src/channel_wrapper.rs
@@ -1,7 +1,66 @@
+use parking_lot::Mutex;
 use pyo3::prelude::*;
+use std::sync::Arc;
+
+use pyxel::Channel as PyxelChannel;
+use pyxel::Volume;
 
 #[pyclass]
-struct Channel;
+#[derive(Clone)]
+pub struct Channel {
+    pyxel_channel: Arc<Mutex<PyxelChannel>>,
+}
+
+pub fn wrap_pyxel_channel(pyxel_channel: Arc<Mutex<PyxelChannel>>) -> Channel {
+    Channel {
+        pyxel_channel: pyxel_channel,
+    }
+}
+
+#[pymethods]
+impl Channel {
+    #[new]
+    pub fn new() -> Channel {
+        wrap_pyxel_channel(PyxelChannel::with_arc_mutex())
+    }
+
+    #[getter]
+    pub fn get_volume(&self) -> Volume {
+        self.pyxel_channel.lock().volume
+    }
+
+    #[setter]
+    pub fn set_volume(&self, volume: Volume) {
+        self.pyxel_channel.lock().volume = volume;
+    }
+
+    #[getter]
+    pub fn is_playing(&self) -> bool {
+        self.pyxel_channel.lock().is_playing()
+    }
+
+    #[getter]
+    pub fn is_looping(&self) -> bool {
+        self.pyxel_channel.lock().is_looping()
+    }
+
+    #[getter]
+    pub fn sound_index(&self) -> u32 {
+        self.pyxel_channel.lock().sound_index()
+    }
+
+    #[getter]
+    pub fn note_index(&self) -> u32 {
+        self.pyxel_channel.lock().note_index()
+    }
+
+    /*pub fn play(&mut self, sounds: Vec<Sound>, is_looping: bool) {
+    }*/
+
+    pub fn stop(&mut self) {
+        self.pyxel_channel.lock().stop();
+    }
+}
 
 pub fn add_channel_class(m: &PyModule) -> PyResult<()> {
     m.add_class::<Channel>()?;
