587e8f117b2da0797a89ef8f0b037747ace7c3a7
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-18 21:40:30 +0900

    Implemented the wrapper for Input

--- crates/pyxel-wrapper/src/input_wrapper.rs
+++ crates/pyxel-wrapper/src/input_wrapper.rs
@@ -1,9 +1,39 @@
 use pyo3::prelude::*;
 
+use pyxel::{Key, KeyValue};
+
 use crate::instance;
 
+#[pyfunction]
+pub fn mouse(is_visible: bool) {
+    instance().mouse(is_visible);
+}
+
+#[pyfunction]
+pub fn btn(key: Key) -> bool {
+    instance().btn(key)
+}
+
+#[pyfunction]
+pub fn btnp(key: Key, hold_frame_count: Option<u32>, repeat_frame_count: Option<u32>) -> bool {
+    instance().btnp(key, hold_frame_count, repeat_frame_count)
+}
+
+#[pyfunction]
+pub fn btnr(key: Key) -> bool {
+    instance().btnr(key)
+}
+
+pub fn btnv(key: Key) -> KeyValue {
+    instance().btnv(key)
+}
+
 pub fn add_input_functions(m: &PyModule) -> PyResult<()> {
-    //m.add_function(wrap_pyfunction!(cls, m)?)?;
+    m.add_function(wrap_pyfunction!(mouse, m)?)?;
+    m.add_function(wrap_pyfunction!(btn, m)?)?;
+    m.add_function(wrap_pyfunction!(btnp, m)?)?;
+    m.add_function(wrap_pyfunction!(btnr, m)?)?;
+    m.add_function(wrap_pyfunction!(btnv, m)?)?;
 
     Ok(())
 }
