bfacc441d99290b6c174df06de66875b875f3c15
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-19 15:25:05 +0900

    Implemented wrappers

--- crates/pyxel-wrapper/src/resource_wrapper.rs
+++ crates/pyxel-wrapper/src/resource_wrapper.rs
@@ -2,8 +2,41 @@ use pyo3::prelude::*;
 
 use crate::instance;
 
+#[pyfunction]
+pub fn load(
+    filename: &str,
+    image: Option<bool>,
+    tilemap: Option<bool>,
+    sound: Option<bool>,
+    music: Option<bool>,
+) {
+    let image = image.unwrap_or(false);
+    let tilemap = tilemap.unwrap_or(false);
+    let sound = sound.unwrap_or(false);
+    let music = music.unwrap_or(false);
+
+    instance().load(filename, image, tilemap, sound, music);
+}
+
+#[pyfunction]
+pub fn save(
+    filename: &str,
+    image: Option<bool>,
+    tilemap: Option<bool>,
+    sound: Option<bool>,
+    music: Option<bool>,
+) {
+    let image = image.unwrap_or(false);
+    let tilemap = tilemap.unwrap_or(false);
+    let sound = sound.unwrap_or(false);
+    let music = music.unwrap_or(false);
+
+    instance().save(filename, image, tilemap, sound, music);
+}
+
 pub fn add_resource_functions(m: &PyModule) -> PyResult<()> {
-    //m.add_function(wrap_pyfunction!(cls, m)?)?;
+    m.add_function(wrap_pyfunction!(load, m)?)?;
+    m.add_function(wrap_pyfunction!(save, m)?)?;
 
     Ok(())
 }
