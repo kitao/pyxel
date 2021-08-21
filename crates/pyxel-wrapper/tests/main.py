25026e50181d945903a7dffa307a59def2bd49ef
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-21 10:11:52 +0900

    Changed test code temporarily

--- crates/pyxel-wrapper/tests/main.py
+++ crates/pyxel-wrapper/tests/main.py
@@ -1,13 +1,14 @@
 import pyxel_debug as pyxel
 
 pyxel.init(300, 300, "hoge")
-print(pyxel.frame_count)
 
 pyxel.title("hoge")
 # pyxel.fullscreen()
 
 
 def update():
+    if pyxel.text_input:
+        print(pyxel.text_input)
     pass
     # print(pyxel.frame_count)
 
@@ -16,4 +17,9 @@ def draw():
     pyxel.cls(3)
 
 
-pyxel.run(update, draw)
+print(pyxel.palette[3])
+pyxel.palette[3] = 5
+print(pyxel.palette[3])
+
+
+# pyxel.run(update, draw)
