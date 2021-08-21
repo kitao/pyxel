6b9974ff9db8dec7afdbce221d15b2e0c67143b3
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-21 15:02:29 +0900

    Implemented the text method

--- crates/pyxel-engine/src/image.rs
+++ crates/pyxel-engine/src/image.rs
@@ -5,9 +5,11 @@ use std::sync::Arc;
 
 use crate::canvas::{Canvas, CopyArea};
 use crate::rectarea::RectArea;
-use crate::settings::{COLOR_COUNT, TILE_SIZE};
+use crate::settings::{
+    COLOR_COUNT, FONT_HEIGHT, FONT_ROW_COUNT, FONT_WIDTH, MAX_FONT_CODE, MIN_FONT_CODE, TILE_SIZE,
+};
 use crate::tilemap::SharedTilemap;
-use crate::types::{Color, Rgb8, Tile};
+use crate::types::{Color, Rgb8};
 use crate::utility::{parse_hex_string, simplify_string};
 
 pub struct Image {
@@ -120,9 +122,10 @@ impl Image {
         tilemap_y: i32,
         width: i32,
         height: i32,
-        transparent: Option<Tile>,
+        transparent: Option<Color>,
+        palette: Option<&[Color]>,
     ) {
-        let tilemap = tilemap.lock();
+        let tilemap_ = tilemap.lock();
         let tile_size = if width < 0 {
             -(TILE_SIZE as i32)
         } else {
@@ -146,7 +149,7 @@ impl Image {
             dst_rect,
             tilemap_x,
             tilemap_y,
-            tilemap._self_rect(),
+            tilemap_._self_rect(),
             width,
             height,
         );
@@ -167,82 +170,65 @@ impl Image {
         for i in 0..height {
             for j in 0..width {
                 let tile =
-                    tilemap._value(src_x + sign_x * j + offset_x, src_y + sign_y * i + offset_y);
+                    tilemap_._value(src_x + sign_x * j + offset_x, src_y + sign_y * i + offset_y);
 
-                if let Some(transparent) = transparent {
-                    if tile == transparent {
-                        continue;
-                    }
-                }
-
-                // self.blt(x + TILE_SIZE as i32 * j, y + TILE_SIZE as i32 * i,
-                // TODO
-                //self._set_value(dst_x + j, dst_y + i, tile);
+                self.blt(
+                    x + TILE_SIZE as i32 * j,
+                    y + TILE_SIZE as i32 * i,
+                    tilemap_.image.clone(),
+                    tile.0 as i32 * TILE_SIZE as i32,
+                    tile.1 as i32 * TILE_SIZE as i32,
+                    tile_size,
+                    tile_size,
+                    transparent,
+                    palette,
+                );
             }
         }
-
-        /*
-        int32_t** src_data = tilemap->Data();
-
-        copy_area.x = copy_area.x * TILEMAP_CHIP_WIDTH + x % TILEMAP_CHIP_WIDTH;
-        copy_area.y = copy_area.y * TILEMAP_CHIP_HEIGHT + y % TILEMAP_CHIP_HEIGHT;
-
-        for (int32_t i = 0; i < copy_area.height; i++) {
-          int32_t* src_line = src_data[copy_area.v + i];
-          int32_t dst_y = copy_area.y + TILEMAP_CHIP_HEIGHT * i;
-
-          for (int32_t j = 0; j < copy_area.width; j++) {
-            int32_t chip = src_line[copy_area.u + j];
-            int32_t cu =
-                (chip % (IMAGE_BANK_WIDTH / TILEMAP_CHIP_WIDTH)) * TILEMAP_CHIP_WIDTH;
-            int32_t cv = (chip / (IMAGE_BANK_HEIGHT / TILEMAP_CHIP_HEIGHT)) *
-                         TILEMAP_CHIP_HEIGHT;
-
-            DrawImage(copy_area.x + TILEMAP_CHIP_WIDTH * j, dst_y, image_index, cu,
-                      cv, TILEMAP_CHIP_WIDTH, TILEMAP_CHIP_HEIGHT, color_key);
-          }
-        }
-        */
     }
 
     pub fn text(&mut self, x: i32, y: i32, string: &str, color: Color, font: SharedImage) {
-        let _ = (x, y, string, color, font); // dummy
+        let palette = [0, color];
+        let start_x = x;
+        let mut x = x;
+        let mut y = y;
 
-        /*
-        int32_t draw_color = GET_DRAW_COLOR(color);
-        int32_t cur_color = palette_table_[FONT_COLOR];
-        palette_table_[FONT_COLOR] = draw_color;
+        for c in string.chars() {
+            if c < MIN_FONT_CODE || c > MAX_FONT_CODE {
+                continue;
+            }
 
-        int32_t left = x;
+            // new line
+            if c == 10 as char {
+                x = start_x;
+                y += FONT_HEIGHT as i32;
+                continue;
+            }
 
-        for (const char* ch = text; *ch != '\0'; ch++) {
-          if (*ch == 10) {  // new line
-            x = left;
-            y += FONT_HEIGHT;
-            continue;
-          }
+            // space
+            if c == 32 as char {
+                x += FONT_WIDTH as i32;
+                continue;
+            }
 
-          if (*ch == 32) {  // space
-            x += FONT_WIDTH;
-            continue;
-          }
+            let code = c as i32 - MIN_FONT_CODE as i32;
+            let src_x = (code % FONT_ROW_COUNT as i32) * FONT_WIDTH as i32;
+            let src_y = (code / FONT_ROW_COUNT as i32) * FONT_HEIGHT as i32;
 
-          if (*ch < MIN_FONT_CODE || *ch > MAX_FONT_CODE) {
-            continue;
-          }
+            self.blt(
+                x,
+                y,
+                font.clone(),
+                src_x,
+                src_y,
+                FONT_WIDTH as i32,
+                FONT_HEIGHT as i32,
+                Some(0),
+                Some(&palette),
+            );
 
-          int32_t code = *ch - MIN_FONT_CODE;
-          int32_t u = (code % FONT_ROW_COUNT) * FONT_WIDTH;
-          int32_t v = (code / FONT_ROW_COUNT) * FONT_HEIGHT;
-
-          DrawImage(x, y, IMAGE_BANK_FOR_SYSTEM, FONT_X + u, FONT_Y + v, FONT_WIDTH,
-                    FONT_HEIGHT, 0);
-
-          x += FONT_WIDTH;
+            x += FONT_WIDTH as i32;
         }
-
-        palette_table_[FONT_COLOR] = cur_color;
-        */
     }
 }
 
