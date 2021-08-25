2f14d55c16bb0d230f954b3849de01a0817102ac
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-25 23:42:52 +0900

    Added wrapper methods for Tilemap

--- crates/wrapper/src/tilemap_wrapper.rs
+++ crates/wrapper/src/tilemap_wrapper.rs
@@ -3,7 +3,7 @@ use pyo3::prelude::*;
 use pyxel::SharedTilemap as PyxelSharedTilemap;
 use pyxel::Tilemap as PyxelTilemap;
 
-use crate::image_wrapper::Image;
+use crate::image_wrapper::{wrap_pyxel_image, Image};
 
 #[pyclass]
 #[derive(Clone)]
@@ -22,6 +22,20 @@ impl Tilemap {
     #[new]
     pub fn new(width: u32, height: u32, image: Image) -> Tilemap {
         wrap_pyxel_tilemap(PyxelTilemap::new(width, height, image.pyxel_image))
+    }
+
+    #[getter]
+    pub fn image(&self) -> Image {
+        wrap_pyxel_image(self.pyxel_tilemap.lock().image.clone())
+    }
+
+    #[setter]
+    pub fn set_image(&self, image: Image) {
+        self.pyxel_tilemap.lock().image = image.pyxel_image.clone();
+    }
+
+    pub fn set(&mut self, x: i32, y: i32, data_str: Vec<&str>) {
+        self.pyxel_tilemap.lock().set(x, y, &data_str);
     }
 }
 
