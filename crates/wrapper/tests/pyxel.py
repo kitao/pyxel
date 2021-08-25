44561228d9b42e3a7e645a80729fa8c596cfc09d
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-25 23:02:41 +0900

    Changed the way to set the current directory

--- crates/wrapper/tests/pyxel.py
+++ crates/wrapper/tests/pyxel.py
@@ -1,9 +1,6 @@
-import inspect
 import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../target/debug"))
 
 from pyxel_wrapper import *  # type: ignore  # noqa: E402,F401,F403
-
-os.chdir(os.path.dirname(inspect.stack()[-1].filename))
