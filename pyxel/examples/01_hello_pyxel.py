30e682eaa59eb02aa7c320804c56a0291311a128
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-25 13:51:42 +0900

    Changed an option name

--- pyxel/examples/01_hello_pyxel.py
+++ pyxel/examples/01_hello_pyxel.py
@@ -3,7 +3,7 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, caption="Hello Pyxel")
+        pyxel.init(160, 120, title="Hello Pyxel")
         pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
         pyxel.run(self.update, self.draw)
 
