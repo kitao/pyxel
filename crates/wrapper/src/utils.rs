fe44978e6bb3f84f7ff65988bf7876169f7b444b
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-26 21:31:08 +0900

    Changed to use absolute function path

--- crates/wrapper/src/utils.rs
+++ crates/wrapper/src/utils.rs
@@ -1,9 +1,9 @@
 macro_rules! as_int {
     ($type: ident, $name: ident) => {
-        if let Ok($name) = $name.cast_as::<PyInt>() {
-            $name.extract::<$type>().unwrap()
-        } else {
+        if let Ok($name) = $name.cast_as::<pyo3::types::PyFloat>() {
             $name.extract::<f64>().unwrap() as $type
+        } else {
+            $name.extract::<$type>().unwrap()
         }
     };
 }
