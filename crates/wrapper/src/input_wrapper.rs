280b98b64cc7c5b3e4fb26729fcd26998af7f2ef
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-27 09:39:31 +0900

    Removed unnecessary public declarations

--- crates/wrapper/src/input_wrapper.rs
+++ crates/wrapper/src/input_wrapper.rs
@@ -5,27 +5,27 @@ use pyxel::{Key, KeyValue};
 use crate::instance;
 
 #[pyfunction]
-pub fn mouse(is_visible: bool) {
+fn mouse(is_visible: bool) {
     instance().mouse(is_visible);
 }
 
 #[pyfunction]
-pub fn btn(key: Key) -> bool {
+fn btn(key: Key) -> bool {
     instance().btn(key)
 }
 
 #[pyfunction]
-pub fn btnp(key: Key, hold_frame_count: Option<u32>, repeat_frame_count: Option<u32>) -> bool {
+fn btnp(key: Key, hold_frame_count: Option<u32>, repeat_frame_count: Option<u32>) -> bool {
     instance().btnp(key, hold_frame_count, repeat_frame_count)
 }
 
 #[pyfunction]
-pub fn btnr(key: Key) -> bool {
+fn btnr(key: Key) -> bool {
     instance().btnr(key)
 }
 
 #[pyfunction]
-pub fn btnv(key: Key) -> KeyValue {
+fn btnv(key: Key) -> KeyValue {
     instance().btnv(key)
 }
 
