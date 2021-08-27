767c249b88f05e7a3ef643d4ff3e8f1ed63cf117
Takashi Kitao <takashi.kitao@gmail.com>
2021-08-27 09:37:53 +0900

    Added macros for pyo3

--- crates/wrapper/src/utils.rs
+++ crates/wrapper/src/utils.rs
@@ -1,9 +1,32 @@
 macro_rules! as_int {
-    ($type: ident, $name: ident) => {
-        if let Ok($name) = $name.cast_as::<pyo3::types::PyFloat>() {
-            $name.extract::<f64>().unwrap() as $type
+    ($type: ty, $var: ident) => {
+        if let Ok($var) = $var.cast_as::<pyo3::types::PyInt>() {
+            $var.extract::<$type>().unwrap()
         } else {
-            $name.extract::<$type>().unwrap()
+            $var.extract::<f64>().unwrap() as $type
+        }
+    };
+}
+
+macro_rules! as_i32 {
+    ($var: ident) => {
+        as_int!(i32, $var)
+    };
+}
+
+macro_rules! as_u32 {
+    ($var: ident) => {
+        as_int!(u32, $var)
+    };
+}
+
+macro_rules! type_switch {
+    ($var: ident, $type1: ty, $block1: block, $type2: ty, $block2: block) => {
+        if let Ok($var) = $var.extract::<$type1>() {
+            $block1
+        } else {
+            let $var = $var.extract::<$type2>().unwrap();
+            $block2
         }
     };
 }
