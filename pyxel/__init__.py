14cdcf0c1f325155ea7d6192f4ea44a31702a3bc
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-01 10:10:17 +0900

    Fixed import path

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -3,10 +3,10 @@ import platform
 system = platform.system()
 
 if system == "Darwin":
-    from .bin.mac.pyxel_extension import *  # type: ignore  # noqa F403
+    from .lib.mac.pyxel_extension import *  # type: ignore  # noqa F403
 elif system == "Windows":
-    from .bin.windows.pyxel_extension import *  # type: ignore  # noqa F403
+    from .lib.windows.pyxel_extension import *  # type: ignore  # noqa F403
 elif system == "Linux":
-    from .bin.linux.pyxel_extension import *  # type: ignore  # noqa F403
+    from .lib.linux.pyxel_extension import *  # type: ignore  # noqa F403
 else:
     raise Exception("unsupported platform")
