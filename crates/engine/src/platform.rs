903033aef17110ef6a89eec331c62ab321494b10
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-04 13:20:42 +0900

    Added the set_btnp, set_btnr, and set_btnv methods

--- crates/engine/src/platform.rs
+++ crates/engine/src/platform.rs
@@ -14,6 +14,7 @@ pub trait Platform {
     fn set_title(&mut self, title: &str);
     fn set_icon(&mut self, icon: SharedImage, colors: &[Rgb8], scale: u32);
     fn show_cursor(&self, show: bool);
+    fn move_cursor(&self, x: i32, y: i32);
     fn toggle_fullscreen(&mut self);
     fn tick_count(&self) -> u32;
     fn sleep(&mut self, ms: u32);
