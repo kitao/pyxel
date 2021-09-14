086545068af51dc6e6e7deb0df942c629ae20495
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-15 00:10:51 +0900

    Added the resource editor launcher

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -1,7 +1,17 @@
-from os.path import abspath
+import os.path
 
-from pyxel.editor.app import App
+import pyxel
+
+from .app import App
 
 
 def run(filename):
-    App(abspath(filename))
+    if filename:
+        if os.path.splitext(filename)[1] == "." + pyxel.RESOURCE_FILE_EXTENSION:
+            App(os.path.abspath(filename))
+        else:
+            print("invalid resource file type")
+            exit(1)
+    else:
+        print("filename is not specified")
+        exit(1)
--- pyxel/editor/__main__.py
+++ pyxel/editor/__main__.py
@@ -2,4 +2,4 @@ import sys
 
 from . import run
 
-run(sys.argv[1])
+run(sys.argv[1] if len(sys.argv) >= 2 else None)
