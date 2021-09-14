0b0c5f6318f5e1ba0a3a3b2317249b136063b1fe
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-14 13:25:33 +0900

    Implemented the editor launcher

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -1,28 +1,7 @@
-import os
-import sys
+from os.path import abspath
 
-import pyxel
 from pyxel.editor.app import App
 
 
-def run():
-    arg = sys.argv[1] if len(sys.argv) >= 2 else ""
-
-    if arg.startswith("-"):
-        if arg == "-v" or arg == "--version":
-            print("Pyxel Editor {}".format(pyxel.VERSION))
-        else:
-            print("Usage: pyxeleditor [option] [pyxel_resource_file]")
-            print("Options:")
-            print(" -h, --help     This help text")
-            print(" -v, --version  Show version number and quit")
-    else:
-        filename = arg or os.path.join(os.getcwd(), "my_resource")
-        App(filename)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.argv[1:] = [os.path.join(os.path.dirname(__file__), "assets/test_resource")]
-
-    run()
+def run(filename):
+    App(abspath(filename))
