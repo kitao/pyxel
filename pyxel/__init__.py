e1966fca2a0bc94ce3ae8e3dde6cb83748cafa8d
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-19 12:34:28 +0900

    Renamed crates

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -7,13 +7,13 @@ _system = platform.system()
 
 if _system == "Darwin":
     _cli_dir = "bin/macos"
-    from .lib.macos.pyxel_extension import *  # type: ignore  # noqa F403
+    from .lib.macos.pyxel_wrapper import *  # type: ignore  # noqa F403
 elif _system == "Windows":
     _cli_dir = "bin/windows"
-    from .lib.windows.pyxel_extension import *  # type: ignore  # noqa F403
+    from .lib.windows.pyxel_wrapper import *  # type: ignore  # noqa F403
 elif _system == "Linux":
     _cli_dir = "bin/linux"
-    from .lib.linux.pyxel_extension import *  # type: ignore  # noqa F403
+    from .lib.linux.pyxel_wrapper import *  # type: ignore  # noqa F403
 else:
     raise Exception("unsupported platform")
 
