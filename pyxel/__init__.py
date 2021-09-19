8c39232ab73d70e02c320a7437930be2ec92c7c5
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-19 19:55:20 +0900

    Removed the cli crate

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -1,7 +1,4 @@
-import os
 import platform
-import subprocess
-import sys
 
 _system = platform.system()
 
@@ -16,9 +13,3 @@ elif _system == "Linux":
     from .lib.linux.pyxel_wrapper import *  # type: ignore  # noqa F403
 else:
     raise Exception("unsupported platform")
-
-
-def cli():
-    cmd = os.path.join(_cli_dir, os.path.dirname(__file__), "pyxel")
-    result = subprocess.run(cmd, *sys.argv)
-    exit(result.returncode)
