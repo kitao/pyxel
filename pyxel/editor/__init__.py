e1b10ac4d961b353d76d2ec3fe7c94775148117f
Takashi Kitao <takashi.kitao@gmail.com>
2021-09-25 18:09:04 +0900

    Suppressed warnings

--- pyxel/editor/__init__.py
+++ pyxel/editor/__init__.py
@@ -1,17 +1,1 @@
-import os.path
-
-import pyxel
-
-from .app import App
-
-
-def run(filename):
-    if filename:
-        if os.path.splitext(filename)[1] == "." + pyxel.RESOURCE_FILE_EXTENSION:
-            App(os.path.abspath(filename))
-        else:
-            print("invalid resource file type")
-            exit(1)
-    else:
-        print("filename is not specified")
-        exit(1)
+from .app import App  # noqa: F401
