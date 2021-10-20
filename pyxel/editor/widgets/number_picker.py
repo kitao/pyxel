bb080fd786e190a3b65e5d1f20aeb309a9d8442c
Takashi Kitao <takashi.kitao@gmail.com>
2021-10-20 09:44:03 +0900

    Added the variable feature to the Widget class

--- pyxel/editor/widgets/number_picker.py
+++ pyxel/editor/widgets/number_picker.py
@@ -3,7 +3,6 @@ import pyxel
 from .settings import INPUT_FIELD_COLOR, INPUT_TEXT_COLOR
 from .text_button import TextButton
 from .widget import Widget
-from .widget_variable import WidgetVariable
 
 
 class NumberPicker(Widget):
@@ -25,10 +24,15 @@ class NumberPicker(Widget):
         self._min_value = min_value
         self._max_value = max_value
 
+        self.add_event_listener("draw", self.__on_draw)
+
         # value_var
-        self.value_var = WidgetVariable(value)
-        self.value_var.add_event_listener("set", self.__on_value_set)
-        self.value_var.add_event_listener("change", self.__on_value_change)
+        self.make_variable(
+            "value_var",
+            value,
+            on_set=self.__on_value_set,
+            on_change=self.__on_value_change,
+        )
 
         # dec button
         self.dec_button = TextButton(self, 0, 0, "-")
@@ -38,28 +42,26 @@ class NumberPicker(Widget):
         self.inc_button = TextButton(self, width - 7, 0, "+")
         self.inc_button.add_event_listener("press", self.__on_inc_button_press)
 
-        self.add_event_listener("draw", self.__on_draw)
-
-    def __on_value_set(self, value):
-        return min(max(value, self._min_value), self._max_value)
-
-    def __on_value_change(self, value):
-        self.dec_button.is_enabled_var.v = self.value_var.v > self._min_value
-        self.inc_button.is_enabled_var.v = self.value_var.v < self._max_value
-
-        self.trigger_event("change", value)
-
-    def __on_dec_button_press(self):
-        self.value_var.v -= 10 if pyxel.btn(pyxel.KEY_SHIFT) else 1
-
-    def __on_inc_button_press(self):
-        self.value_var.v += 10 if pyxel.btn(pyxel.KEY_SHIFT) else 1
-
     def __on_draw(self):
         pyxel.rect(self.x + 9, self.y, self.width - 18, self.height, INPUT_FIELD_COLOR)
         pyxel.text(
             self.x + 11,
             self.y + 1,
-            ("{:>" + str(self._number_len) + "}").format(self.value_var.v),
+            ("{:>" + str(self._number_len) + "}").format(self.value_var),
             INPUT_TEXT_COLOR,
         )
+
+    def __on_value_set(self, value):
+        return min(max(value, self._min_value), self._max_value)
+
+    def __on_value_change(self, value):
+        self.dec_button.is_enabled_var = self.value_var > self._min_value
+        self.inc_button.is_enabled_var = self.value_var < self._max_value
+
+        self.trigger_event("change", value)
+
+    def __on_dec_button_press(self):
+        self.value_var -= 10 if pyxel.btn(pyxel.KEY_SHIFT) else 1
+
+    def __on_inc_button_press(self):
+        self.value_var += 10 if pyxel.btn(pyxel.KEY_SHIFT) else 1
--- pyxel/editor/widgets/toggle_button.py
+++ pyxel/editor/widgets/toggle_button.py
@@ -1,7 +1,7 @@
 import pyxel
 
 from .settings import BUTTON_DISABLED_COLOR, BUTTON_ENABLED_COLOR, BUTTON_PRESSED_COLOR
-from .widget import Widget, WidgetVariable
+from .widget import Widget
 
 
 class ToggleButton(Widget):
@@ -17,28 +17,27 @@ class ToggleButton(Widget):
     def __init__(self, parent, left, top, width, height, **kwargs):
         super().__init__(parent, left, top, width, height, **kwargs)
 
-        # is_checked_var
-        self.is_checked_var = WidgetVariable(False)
-        self.is_checked_var.add_event_listener("change", self.__on_is_checked_change)
-
         self.add_event_listener("mouse_down", self.__on_mouse_down)
+
+        # is_checked_var
+        self.make_variable("is_checked_var", on_change=self.__on_is_checked_change)
 
     @property
     def button_color(self):
         return (
-            (BUTTON_PRESSED_COLOR if self.is_checked_var.v else BUTTON_ENABLED_COLOR)
-            if self.is_enabled_var.v
+            (BUTTON_PRESSED_COLOR if self.is_checked_var else BUTTON_ENABLED_COLOR)
+            if self.is_enabled_var
             else BUTTON_DISABLED_COLOR
         )
+
+    def __on_mouse_down(self, key, x, y):
+        if key != pyxel.MOUSE_BUTTON_LEFT:
+            return
+
+        self.is_checked_var = not self.is_checked_var
 
     def __on_is_checked_change(self, value):
         if value:
             self.trigger_event("checked")
         else:
             self.trigger_event("unchecked")
-
-    def __on_mouse_down(self, key, x, y):
-        if key != pyxel.MOUSE_BUTTON_LEFT:
-            return
-
-        self.is_checked_var.v = not self.is_checked_var.v
