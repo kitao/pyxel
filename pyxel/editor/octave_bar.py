2eec1244d015624c75d70a03d6c9e390345b92d9
Takashi Kitao <takashi.kitao@gmail.com>
2021-10-24 07:14:46 +0900

    Adapted to new widget system

--- pyxel/editor/octave_bar.py
+++ pyxel/editor/octave_bar.py
@@ -5,9 +5,19 @@ from .widgets import Widget
 
 
 class OctaveBar(Widget):
+    """
+    Variables:
+        octave_var
+        help_message_var
+    """
+
     def __init__(self, parent, x, y):
         super().__init__(parent, x, y, 4, 123)
 
+        self.copy_var("octave_var", parent)
+        self.copy_var("help_message_var", parent)
+
+        # event listeners
         self.add_event_listener("mouse_down", self.__on_mouse_down)
         self.add_event_listener("mouse_drag", self.__on_mouse_drag)
         self.add_event_listener("mouse_hover", self.__on_mouse_hover)
@@ -17,10 +27,7 @@ class OctaveBar(Widget):
         if key != pyxel.MOUSE_BUTTON_LEFT:
             return
 
-        x -= self.x
-        y -= self.y
-
-        self.parent.octave = min(max(3 - ((y - 12) // 24), 0), 3)
+        self.octave_var = min(max(3 - ((y - self.y - 12) // 24), 0), 3)
 
     def __on_mouse_drag(self, key, x, y, dx, dy):
         self.__on_mouse_down(key, x, y)
@@ -29,8 +36,8 @@ class OctaveBar(Widget):
         self.parent.help_message = "OCTAVE:PAGEUP/PAGEDOWN"
 
     def __on_draw(self):
-        pyxel.rect(self.x, self.y, self.width, self.height, OCTAVE_BAR_BACKGROUND_COLOR)
-
         x = self.x + 1
-        y = self.y + 1 + (3 - self.parent.octave) * 24
+        y = self.y + 1 + (3 - self.octave_var) * 24
+
+        pyxel.rect(self.x, self.y, self.width, self.height, OCTAVE_BAR_BACKGROUND_COLOR)
         pyxel.rect(x, y, 2, 47, OCTAVE_BAR_COLOR)
