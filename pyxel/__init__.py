3a1125017ecd536b152399106538cb3be57f43fd
Takashi Kitao <takashi.kitao@gmail.com>
2021-11-27 00:43:12 +0900

    Change the library loading path for Windows

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -1,16 +1,21 @@
+import os
 import platform
 
 _system = platform.system()
 
 if _system == "Darwin":
-    _cli_dir = "bin/macos"
     from .lib.macos.pyxel_wrapper import *  # type: ignore  # noqa F403
+
 elif _system == "Windows":
-    _cli_dir = "bin/windows"
+    sdl2_dir = os.path.join(os.path.dirname(__file__), "lib/windows")
+    if hasattr(os, "add_dll_directory"):  # for Python 3.8 or later
+        os.add_dll_directory(sdl2_dir)
+    os.environ["PATH"] = os.pathsep.join([sdl2_dir, os.environ["PATH"]])
     from .lib.windows.pyxel_wrapper import *  # type: ignore  # noqa F403
+
 elif _system == "Linux":
-    _cli_dir = "bin/linux"
     from .lib.linux.pyxel_wrapper import *  # type: ignore  # noqa F403
+
 else:
     raise Exception("unsupported platform")
 
