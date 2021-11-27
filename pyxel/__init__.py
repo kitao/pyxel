9bd5aa5e91c85a97204e22754c1076cd812aa4a4
Takashi Kitao <takashi.kitao@gmail.com>
2021-11-27 17:50:36 +0900

    Removed unnecessary code

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -5,17 +5,10 @@ _system = platform.system()
 
 if _system == "Darwin":
     from .lib.macos.pyxel_wrapper import *  # type: ignore  # noqa F403
-
 elif _system == "Windows":
-    sdl2_dir = os.path.join(os.path.dirname(__file__), "lib/windows")
-    if hasattr(os, "add_dll_directory"):  # for Python 3.8 or later
-        os.add_dll_directory(sdl2_dir)
-    os.environ["PATH"] = os.pathsep.join([sdl2_dir, os.environ["PATH"]])
     from .lib.windows.pyxel_wrapper import *  # type: ignore  # noqa F403
-
 elif _system == "Linux":
     from .lib.linux.pyxel_wrapper import *  # type: ignore  # noqa F403
-
 else:
     raise Exception("unsupported platform")
 
