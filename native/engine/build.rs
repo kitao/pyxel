480a22271827fe47d89ad854bcddf9d3ef8ccb94
Takashi Kitao <takashi.kitao@gmail.com>
2021-12-08 15:42:38 +0900

    Removed unnecessary settings

--- native/engine/build.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(target_os = "linux")]
-fn main() {
-    println!("cargo:rustc-link-search=native=/home/linuxbrew/.linuxbrew/lib");
-    println!("cargo:rustc-link-arg=-Wl,-rpath,/home/linuxbrew/.linuxbrew/lib");
-}
