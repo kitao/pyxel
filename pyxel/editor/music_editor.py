377f8562baa2bb89a1fd550f6a13c453a59ba039
Takashi Kitao <takashi.kitao@gmail.com>
2022-01-21 08:04:42 +0900

    Revert "Added a partial playback feature to the Music Editor"
    This reverts commit 207be51270fc5da4431ce6c26aaa13ad12b867c4.

--- pyxel/editor/music_editor.py
+++ pyxel/editor/music_editor.py
@@ -107,19 +107,13 @@ class MusicEditor(EditorBase):
         if data["old_field"] != data["new_field"]:
             self.add_history(self._history_data)
 
-    def _play(self, is_partial):
+    def _play(self):
         self.is_playing_var = True
         self._music_picker.is_enabled_var = False
         self._play_button.is_enabled_var = False
         self._stop_button.is_enabled_var = True
         self._loop_button.is_enabled_var = False
-        pos = 0
-        if is_partial:
-            for i in range(self.field_cursor.x):
-                music = pyxel.music(self.music_no_var)
-                sound = pyxel.sound(music.sequences[self.field_cursor.y][i])
-                pos += len(sound.notes)
-        pyxel.playm(self.music_no_var, pos=pos, loop=self.should_loop_var)
+        pyxel.playm(self.music_no_var, loop=self.should_loop_var)
 
     def _stop(self):
         self.is_playing_var = False
@@ -169,13 +163,13 @@ class MusicEditor(EditorBase):
         pyxel.text(23, 18, "MUSIC", TEXT_LABEL_COLOR)
 
     def __on_play_button_press(self):
-        self._play(pyxel.btn(pyxel.KEY_SHIFT))
+        self._play()
 
     def __on_stop_button_press(self):
         self._stop()
 
     def __on_play_button_mouse_hover(self, x, y):
-        self.help_message_var = "PLAY:SPACE PART-PLAY:SHIFT+SPACE"
+        self.help_message_var = "PLAY:SPACE"
 
     def __on_stop_button_mouse_hover(self, x, y):
         self.help_message_var = "STOP:SPACE"
