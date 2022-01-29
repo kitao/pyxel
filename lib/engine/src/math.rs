f7d9b3657fb4cd10e84d6f2f5088629ed0aa3853
Takashi Kitao <takashi.kitao@gmail.com>
2022-01-29 15:42:13 +0900

    Revert "Added the nseed function"
    This reverts commit 6ce90cbdd1fcac091d269c891db77fd558a56963.

--- lib/engine/src/math.rs
+++ lib/engine/src/math.rs
@@ -14,10 +14,8 @@ pub struct Math {
 
 impl Math {
     pub fn new<T: Platform>(platform: &mut T) -> Self {
-        let seed = platform.tick_count();
-        let rng = Xoshiro256StarStar::seed_from_u64(seed as u64);
+        let rng = Xoshiro256StarStar::seed_from_u64(platform.tick_count() as u64);
         let perlin = Perlin::new();
-        perlin.set_seed(seed);
         Self { rng, perlin }
     }
 }
@@ -55,6 +53,7 @@ impl Pyxel {
 
     pub fn srand(&mut self, seed: u32) {
         self.math.rng = Xoshiro256StarStar::seed_from_u64(seed as u64);
+        self.math.perlin.set_seed(seed);
     }
 
     pub fn rnd(&mut self) -> f64 {
@@ -64,10 +63,6 @@ impl Pyxel {
     pub fn rndi(&mut self, a: i32, b: i32) -> i32 {
         let (a, b) = if a < b { (a, b) } else { (b, a) };
         self.math.rng.gen_range(a..=b)
-    }
-
-    pub fn nseed(&mut self, seed: u32) {
-        self.math.perlin.set_seed(seed);
     }
 
     pub fn noise(&self, x: f64, y: f64, z: f64) -> f64 {
