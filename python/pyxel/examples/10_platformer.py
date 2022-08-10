dfde7fac94aeab3afd712a6c213e14d2dc32d024
Takashi Kitao <takashi.kitao@gmail.com>
2022-08-10 22:08:11 +0900

    Added fall death to the example #10

--- python/pyxel/examples/10_platformer.py
+++ python/pyxel/examples/10_platformer.py
@@ -124,6 +124,8 @@ class Player:
             last_scroll_x = scroll_x
             scroll_x = min(self.x - SCROLL_BORDER_X, 240 * 8)
             spawn_enemy(last_scroll_x + 128, scroll_x + 127)
+        if self.y > pyxel.height:
+            game_over()
 
     def draw(self):
         u = (2 if self.is_falling else pyxel.frame_count // 3 % 2) * 8
