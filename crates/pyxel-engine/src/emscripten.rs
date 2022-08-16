007b521bb4b07b101d1e3529362aa14eb506bfb6
Takashi Kitao <takashi.kitao@gmail.com>
2022-08-16 18:00:02 +0900

    Split the Emscripten code

---
+++ crates/pyxel-engine/src/emscripten.rs
@@ -0,0 +1,46 @@
+use std::cell::RefCell;
+use std::os::raw::c_int;
+
+#[allow(non_camel_case_types)]
+type em_callback_func = unsafe extern "C" fn();
+
+extern "C" {
+    pub fn emscripten_set_main_loop(
+        func: em_callback_func,
+        fps: c_int,
+        simulate_infinite_loop: c_int,
+    );
+    pub fn emscripten_cancel_main_loop();
+}
+
+thread_local! {
+    static MAIN_LOOP_CLOSURE: RefCell<Option<Box<dyn FnMut()>>> = RefCell::new(None);
+}
+
+pub fn set_main_loop_callback<F: FnMut()>(callback: F) {
+    MAIN_LOOP_CLOSURE.with(|d| {
+        *d.borrow_mut() = Some(Box::new(callback));
+    });
+
+    unsafe extern "C" fn wrapper<F: FnMut()>() {
+        MAIN_LOOP_CLOSURE.with(|z| {
+            if let Some(closure) = &mut *z.borrow_mut() {
+                (*closure)();
+            }
+        });
+    }
+
+    unsafe {
+        emscripten_set_main_loop(wrapper::<F>, 0, 1);
+    }
+}
+
+pub fn cancel_main_loop() {
+    unsafe {
+        emscripten_cancel_main_loop();
+    }
+
+    MAIN_LOOP_CLOSURE.with(|d| {
+        *d.borrow_mut() = None;
+    });
+}
