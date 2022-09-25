21d8baeff7958bb5ca257ba40b0a65b6306c0c17
Takashi Kitao <takashi.kitao@gmail.com>
2022-09-25 13:51:06 +0900

    Auto layout test

--- wasm/pyxel.js
+++ wasm/pyxel.js
@@ -101,9 +101,10 @@ function _addElements() {
         img.tabindex = -1;
         body.appendChild(img);
     }
-    addEventListener('resize', () => {
-        document.body.style.height = `{window.innerHeight}px`;
-    });
+    /*addEventListener('resize', () => {
+        body.style.height = `{window.innerHeight}px`;
+    });*/
+    //body.scrollTo((body.scrollWidth - body.clientWidth) / 2, (body.scrollHeight - body.clientHeight) / 2);
 }
 
 function _isMobileDevice() {
