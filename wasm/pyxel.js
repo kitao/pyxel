f1c14b97f3754871dc6550930cd841f6637c1e48
Takashi Kitao <takashi.kitao@gmail.com>
2023-03-22 01:50:06 +0900

    Updated the WASM wheel

--- wasm/pyxel.js
+++ wasm/pyxel.js
@@ -2,7 +2,7 @@ const NO_SLEEP_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/nosleep/0.12.0/NoSleep.min.js";
 const PYODIDE_SDL2_URL =
   "https://cdn.jsdelivr.net/gh/kitao/pyodide-sdl2@0.22.1/pyodide.js";
-const PYXEL_WHEEL_PATH = "pyxel-1.9.12-cp37-abi3-emscripten_3_1_31_wasm32.whl";
+const PYXEL_WHEEL_PATH = "pyxel-1.9.13-cp37-abi3-emscripten_3_1_34_wasm32.whl";
 const PYXEL_LOGO_PATH = "../docs/images/pyxel_logo_76x32.png";
 const TOUCH_TO_START_PATH = "../docs/images/touch_to_start_114x14.png";
 const CLICK_TO_START_PATH = "../docs/images/click_to_start_114x14.png";
