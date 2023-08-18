dd911efe1b31c50dd6c54b5dd2c859564148dbe7
Takashi Kitao <takashi.kitao@gmail.com>
2023-08-18 12:33:21 +0900

    Refined the code

--- crates/pyxel-platform/src/lib.rs
+++ crates/pyxel-platform/src/lib.rs
@@ -1,11 +1,15 @@
 mod event;
 mod platform;
 mod sdl2_sys;
+mod window;
 
 pub mod keys;
 
-pub use crate::event::Event;
+pub use crate::event::{poll_events, Event};
 pub use crate::platform::{
-    elapsed_time, gl, init, is_fullscreen, poll_events, quit, run, set_audio_enabled,
-    set_fullscreen, set_mouse_pos, set_mouse_visible, set_window_icon, set_window_title, sleep,
+    display_size, elapsed_time, init, quit, run, set_audio_enabled, set_mouse_pos,
+    set_mouse_visible, sleep,
+};
+pub use crate::window::{
+    create_window, gl, is_fullscreen, set_fullscreen, set_window_icon, set_window_title,
 };
