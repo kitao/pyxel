878a5c236ed1a469ff53ef04ece00ff160190a1f
Takashi Kitao <takashi.kitao@gmail.com>
2023-08-19 17:38:11 +0900

    Refactored code

--- crates/pyxel-platform/src/lib.rs
+++ crates/pyxel-platform/src/lib.rs
@@ -6,10 +6,9 @@ mod window;
 
 pub use crate::event::{poll_events, Event};
 pub use crate::platform::{
-    display_size, elapsed_time, init, quit, run, set_audio_enabled, set_mouse_pos,
-    set_mouse_visible, sleep,
+    elapsed_time, init, quit, run, set_audio_enabled, set_mouse_pos, set_mouse_visible, sleep,
 };
 pub use crate::window::{
-    create_window, gl, is_fullscreen, set_fullscreen, set_window_icon, set_window_pos,
-    set_window_size, set_window_title, window_pos, window_size,
+    display_size, gl, is_fullscreen, set_fullscreen, set_window_icon, set_window_pos,
+    set_window_size, set_window_title, swap_window, window_pos, window_size,
 };
