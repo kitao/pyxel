2717da7b4907050012e66d4a5a9f4f81fded264f
Takashi Kitao <takashi.kitao@gmail.com>
2023-09-06 19:08:08 +0900

    Added a prelude module

---
+++ crates/pyxel-engine/src/prelude.rs
@@ -0,0 +1,11 @@
+pub use pyxel_platform::keys::*;
+
+pub use crate::channel::{Channel, Note, SharedChannel, Speed, Volume};
+pub use crate::image::{Color, Image, Rgb8, SharedImage};
+pub use crate::music::{Music, SharedMusic};
+pub use crate::oscillator::{Effect, Tone};
+pub use crate::pyxel::Pyxel;
+pub use crate::settings::*;
+pub use crate::sound::{SharedSound, Sound};
+pub use crate::system::PyxelCallback;
+pub use crate::tilemap::{SharedTilemap, Tile, Tilemap};
