382be6d5dac78d43bfc779e03711fc7a6976c523
Takashi Kitao <takashi.kitao@gmail.com>
2023-09-09 17:35:27 +0900

    Refactored code

--- crates/pyxel-wrapper/src/input_wrapper.rs
+++ crates/pyxel-wrapper/src/input_wrapper.rs
@@ -1,35 +1,37 @@
 use pyo3::prelude::*;
-use pyxel_engine::{Key, KeyValue};
+use pyxel_engine as pyxel;
+
+use crate::pyxel_singleton::pyxel;
 
 #[pyfunction]
-fn btn(key: Key) -> bool {
-    pyxel_engine::btn(key)
+fn btn(key: pyxel::Key) -> bool {
+    pyxel().btn(key)
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(key, *, hold, repeat)")]
-fn btnp(key: Key, hold: Option<u32>, repeat: Option<u32>) -> bool {
-    pyxel_engine::btnp(key, hold, repeat)
+fn btnp(key: pyxel::Key, hold: Option<u32>, repeat: Option<u32>) -> bool {
+    pyxel().btnp(key, hold, repeat)
 }
 
 #[pyfunction]
-fn btnr(key: Key) -> bool {
-    pyxel_engine::btnr(key)
+fn btnr(key: pyxel::Key) -> bool {
+    pyxel().btnr(key)
 }
 
 #[pyfunction]
-fn btnv(key: Key) -> KeyValue {
-    pyxel_engine::btnv(key)
+fn btnv(key: pyxel::Key) -> pyxel::KeyValue {
+    pyxel().btnv(key)
 }
 
 #[pyfunction]
 fn mouse(visible: bool) {
-    pyxel_engine::mouse(visible);
+    pyxel().mouse(visible);
 }
 
 #[pyfunction]
 pub fn set_mouse_pos(x: f64, y: f64) {
-    pyxel_engine::set_mouse_pos(x, y);
+    pyxel().set_mouse_pos(x, y);
 }
 
 pub fn add_input_functions(m: &PyModule) -> PyResult<()> {
--- crates/pyxel-wrapper/src/math_wrapper.rs
+++ crates/pyxel-wrapper/src/math_wrapper.rs
@@ -1,65 +1,67 @@
 use pyo3::prelude::*;
 
+use crate::pyxel_singleton::pyxel;
+
 #[pyfunction]
 fn ceil(x: f64) -> i32 {
-    pyxel_engine::ceil(x)
+    pyxel().ceil(x)
 }
 
 #[pyfunction]
 fn floor(x: f64) -> i32 {
-    pyxel_engine::floor(x)
+    pyxel().floor(x)
 }
 
 #[pyfunction]
 fn sgn(x: f64) -> f64 {
-    pyxel_engine::sgn(x)
+    pyxel().sgn(x)
 }
 
 #[pyfunction]
 fn sqrt(x: f64) -> f64 {
-    pyxel_engine::sqrt(x)
+    pyxel().sqrt(x)
 }
 
 #[pyfunction]
 fn sin(deg: f64) -> f64 {
-    pyxel_engine::sin(deg)
+    pyxel().sin(deg)
 }
 
 #[pyfunction]
 fn cos(deg: f64) -> f64 {
-    pyxel_engine::cos(deg)
+    pyxel().cos(deg)
 }
 
 #[pyfunction]
 fn atan2(y: f64, x: f64) -> f64 {
-    pyxel_engine::atan2(y, x)
+    pyxel().atan2(y, x)
 }
 
 #[pyfunction]
 fn rseed(seed: u32) {
-    pyxel_engine::rseed(seed);
+    pyxel().rseed(seed);
 }
 
 #[pyfunction]
 fn rndi(a: i32, b: i32) -> i32 {
-    pyxel_engine::rndi(a, b)
+    pyxel().rndi(a, b)
 }
 
 #[pyfunction]
 fn rndf(a: f64, b: f64) -> f64 {
-    pyxel_engine::rndf(a, b)
+    pyxel().rndf(a, b)
 }
 
 #[pyfunction]
 fn nseed(seed: u32) {
-    pyxel_engine::nseed(seed);
+    pyxel().nseed(seed);
 }
 
 #[pyfunction]
 fn noise(x: f64, y: Option<f64>, z: Option<f64>) -> f64 {
     let y = y.unwrap_or(0.0);
     let z = z.unwrap_or(0.0);
-    pyxel_engine::noise(x, y, z)
+    pyxel().noise(x, y, z)
 }
 
 pub fn add_math_functions(m: &PyModule) -> PyResult<()> {
