e3811696bea98f7ce0829ad6cb50f82ce2f3d184
Takashi Kitao <takashi.kitao@gmail.com>
2023-10-15 11:09:29 +0900

    Refactored code

--- crates/pyxel-wrapper/src/audio_wrapper.rs
+++ crates/pyxel-wrapper/src/audio_wrapper.rs
@@ -21,11 +21,6 @@ fn music(msc: u32) -> Music {
 }
 
 #[pyfunction]
-fn play_pos(ch: u32) -> Option<(u32, u32)> {
-    pyxel().play_pos(ch)
-}
-
-#[pyfunction]
 #[pyo3(text_signature = "(ch, snd, *, tick, loop)")]
 fn play(ch: u32, snd: &PyAny, tick: Option<u32>, r#loop: Option<bool>) -> PyResult<()> {
     type_switch! {
@@ -62,13 +57,18 @@ fn stop(ch: Option<u32>) {
     }
 }
 
+#[pyfunction]
+fn play_pos(ch: u32) -> Option<(u32, u32)> {
+    pyxel().play_pos(ch)
+}
+
 pub fn add_audio_functions(m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(channel, m)?)?;
     m.add_function(wrap_pyfunction!(sound, m)?)?;
     m.add_function(wrap_pyfunction!(music, m)?)?;
-    m.add_function(wrap_pyfunction!(play_pos, m)?)?;
     m.add_function(wrap_pyfunction!(play, m)?)?;
     m.add_function(wrap_pyfunction!(playm, m)?)?;
     m.add_function(wrap_pyfunction!(stop, m)?)?;
+    m.add_function(wrap_pyfunction!(play_pos, m)?)?;
     Ok(())
 }
