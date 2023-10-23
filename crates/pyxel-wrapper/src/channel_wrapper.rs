2fe0d824a85d8cabf67693f22b50d1251da23c14
Takashi Kitao <takashi.kitao@gmail.com>
2023-10-23 22:13:04 +0900

    Refactored code

--- crates/pyxel-wrapper/src/channel_wrapper.rs
+++ crates/pyxel-wrapper/src/channel_wrapper.rs
@@ -6,23 +6,19 @@ use crate::sound_wrapper::Sound;
 #[pyclass]
 #[derive(Clone)]
 pub struct Channel {
-    pyxel_channel: pyxel::SharedChannel,
-}
-
-pub const fn wrap_pyxel_channel(pyxel_channel: pyxel::SharedChannel) -> Channel {
-    Channel { pyxel_channel }
+    pub(crate) inner: pyxel::SharedChannel,
 }
 
 #[pymethods]
 impl Channel {
     #[getter]
     pub fn get_gain(&self) -> pyxel::Volume {
-        self.pyxel_channel.lock().gain
+        self.inner.lock().gain
     }
 
     #[setter]
     pub fn set_gain(&self, gain: u8) {
-        self.pyxel_channel.lock().gain = gain;
+        self.inner.lock().gain = gain;
     }
 
     #[pyo3(text_signature = "($self, snd, *, tick, loop)")]
@@ -32,29 +28,29 @@ impl Channel {
             snd,
             u32, {
                 let sound = pyxel().sounds[snd as usize].clone();
-                self.pyxel_channel.lock().play1(sound, tick, loop_);
+                self.inner.lock().play1(sound, tick, loop_);
             },
             Vec<u32>, {
                 let sounds = snd.iter().map(|snd| pyxel().sounds[*snd as usize].clone()).collect();
-                self.pyxel_channel.lock().play(sounds, tick, loop_);
+                self.inner.lock().play(sounds, tick, loop_);
             },
             Sound, {
-                self.pyxel_channel.lock().play1(snd.pyxel_sound, tick, loop_);
+                self.inner.lock().play1(snd.inner, tick, loop_);
             },
             Vec<Sound>, {
-                let sounds = snd.iter().map(|sound| sound.pyxel_sound.clone()).collect();
-                self.pyxel_channel.lock().play(sounds, tick, loop_);
+                let sounds = snd.iter().map(|sound| sound.inner.clone()).collect();
+                self.inner.lock().play(sounds, tick, loop_);
             }
         }
         Ok(())
     }
 
     pub fn stop(&mut self) {
-        self.pyxel_channel.lock().stop();
+        self.inner.lock().stop();
     }
 
     pub fn play_pos(&self) -> Option<(u32, u32)> {
-        self.pyxel_channel.lock().play_pos()
+        self.inner.lock().play_pos()
     }
 }
 
