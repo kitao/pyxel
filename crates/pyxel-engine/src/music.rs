b310d17004b79f92965cbd6ada9f611b093d5cb9
Takashi Kitao <takashi.kitao@gmail.com>
2023-10-24 19:05:45 +0900

    Renamed Chains to Seqs

--- crates/pyxel-engine/src/music.rs
+++ crates/pyxel-engine/src/music.rs
@@ -6,11 +6,9 @@ use crate::settings::NUM_CHANNELS;
 use crate::settings::RESOURCE_ARCHIVE_DIRNAME;
 use crate::utils::parse_hex_string;
 
-pub type SharedChain = shared_type!(Vec<u32>);
-
 #[derive(Clone)]
 pub struct Music {
-    pub chains: Vec<SharedChain>,
+    pub seqs: Vec<Vec<u32>>,
 }
 
 pub type SharedMusic = shared_type!(Music);
@@ -18,17 +16,12 @@ pub type SharedMusic = shared_type!(Music);
 impl Music {
     pub fn new() -> SharedMusic {
         new_shared_type!(Self {
-            chains: (0..NUM_CHANNELS)
-                .map(|_| new_shared_type!(Vec::new()))
-                .collect()
+            seqs: (0..NUM_CHANNELS).map(|_| Vec::new()).collect()
         })
     }
 
-    pub fn set(&mut self, sounds0: &[u32], sounds1: &[u32], sounds2: &[u32], sounds3: &[u32]) {
-        self.chains[0] = new_shared_type!(sounds0.to_vec());
-        self.chains[1] = new_shared_type!(sounds1.to_vec());
-        self.chains[2] = new_shared_type!(sounds2.to_vec());
-        self.chains[3] = new_shared_type!(sounds3.to_vec());
+    pub fn set(&mut self, seqs: &[Vec<u32>]) {
+        self.seqs = seqs.to_vec();
     }
 }
 
@@ -38,25 +31,21 @@ impl ResourceItem for Music {
     }
 
     fn is_modified(&self) -> bool {
-        self.chains
-            .iter()
-            .any(|sound_nums| !sound_nums.lock().is_empty())
+        self.seqs.iter().any(|seq| !seq.is_empty())
     }
 
     fn clear(&mut self) {
-        self.chains = (0..NUM_CHANNELS)
-            .map(|_| new_shared_type!(Vec::new()))
-            .collect();
+        self.seqs = (0..NUM_CHANNELS).map(|_| Vec::new()).collect();
     }
 
     fn serialize(&self, _pyxel: &Pyxel) -> String {
         let mut output = String::new();
-        for sound_nums in &self.chains {
-            if sound_nums.lock().is_empty() {
+        for seq in &self.seqs {
+            if seq.is_empty() {
                 output += "none";
             } else {
-                for sound_num in &*sound_nums.lock() {
-                    let _guard = write!(output, "{sound_num:02x}");
+                for sound_index in seq {
+                    let _guard = write!(output, "{sound_index:02x}");
                 }
             }
             output += "\n";
@@ -71,9 +60,7 @@ impl ResourceItem for Music {
                 continue;
             }
             string_loop!(j, value, line, 2, {
-                self.chains[i]
-                    .lock()
-                    .push(parse_hex_string(&value).unwrap());
+                self.seqs[i].push(parse_hex_string(&value).unwrap());
             });
         }
     }
@@ -87,7 +74,7 @@ mod tests {
     fn new() {
         let music = Music::new();
         for i in 0..NUM_CHANNELS {
-            assert_eq!(music.lock().chains[i as usize].lock().len(), 0);
+            assert_eq!(music.lock().seqs[i as usize].len(), 0);
         }
     }
 
@@ -96,12 +83,9 @@ mod tests {
         let music = Music::new();
         music
             .lock()
-            .set(&[0, 1, 2], &[1, 2, 3], &[2, 3, 4], &[3, 4, 5]);
+            .set(&[vec![0, 1, 2], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
         for i in 0..NUM_CHANNELS {
-            assert_eq!(
-                &*music.lock().chains[i as usize].lock(),
-                &vec![i, i + 1, i + 2]
-            );
+            assert_eq!(&music.lock().seqs[i as usize], &vec![i, i + 1, i + 2]);
         }
     }
 }
