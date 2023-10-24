796b556186d6324e3380a55f8fa065bc41e12558
Takashi Kitao <takashi.kitao@gmail.com>
2023-10-24 09:22:32 +0900

    Refactored code

--- crates/pyxel-engine/src/watch_info.rs
+++ crates/pyxel-engine/src/watch_info.rs
@@ -15,26 +15,24 @@ impl WatchInfo {
     pub fn new() -> Self {
         let watch_info_file =
             var(WATCH_INFO_FILE_ENVVAR).map_or(None, |watch_info_file| Some(watch_info_file));
-        let (mut window_x, mut window_y) = pyxel_platform::window_pos();
-        let (mut window_width, mut window_height) = pyxel_platform::window_size();
         if let Some(ref watch_info_file) = watch_info_file {
             let watch_info = read_to_string(watch_info_file).unwrap();
             let watch_info: Vec<&str> = watch_info.split(' ').collect();
             if watch_info.len() == 4 {
-                window_x = watch_info[0].parse::<i32>().unwrap();
-                window_y = watch_info[1].parse::<i32>().unwrap();
-                window_width = watch_info[2].parse::<u32>().unwrap();
-                window_height = watch_info[3].parse::<u32>().unwrap();
+                let window_x = watch_info[0].parse::<i32>().unwrap();
+                let window_y = watch_info[1].parse::<i32>().unwrap();
+                let window_width = watch_info[2].parse::<u32>().unwrap();
+                let window_height = watch_info[3].parse::<u32>().unwrap();
                 pyxel_platform::set_window_pos(window_x, window_y);
                 pyxel_platform::set_window_size(window_width, window_height);
             }
         }
         Self {
             watch_info_file,
-            window_x,
-            window_y,
-            window_width,
-            window_height,
+            window_x: i32::MIN,
+            window_y: i32::MIN,
+            window_width: u32::MAX,
+            window_height: u32::MAX,
         }
     }
 
