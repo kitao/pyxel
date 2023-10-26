53013be7ab5a4793c0502d1bd1a0f508cfe71a58
Takashi Kitao <takashi.kitao@gmail.com>
2023-10-26 18:51:26 +0900

    Added SharedSeq to handle a Sound sequence

--- crates/pyxel-engine/src/music.rs
+++ crates/pyxel-engine/src/music.rs
@@ -6,9 +6,11 @@ use crate::settings::NUM_CHANNELS;
 use crate::settings::RESOURCE_ARCHIVE_DIRNAME;
 use crate::utils::parse_hex_string;
 
+pub type SharedSeq = shared_type!(Vec<u32>);
+
 #[derive(Clone)]
 pub struct Music {
-    pub seqs: Vec<Vec<u32>>,
+    pub seqs: Vec<SharedSeq>,
 }
 
 pub type SharedMusic = shared_type!(Music);
@@ -16,12 +18,17 @@ pub type SharedMusic = shared_type!(Music);
 impl Music {
     pub fn new() -> SharedMusic {
         new_shared_type!(Self {
-            seqs: (0..NUM_CHANNELS).map(|_| Vec::new()).collect()
+            seqs: (0..NUM_CHANNELS)
+                .map(|_| new_shared_type!(Vec::new()))
+                .collect()
         })
     }
 
     pub fn set(&mut self, seqs: &[Vec<u32>]) {
-        self.seqs = seqs.to_vec();
+        self.seqs = seqs
+            .iter()
+            .map(|seq| new_shared_type!(seq.clone()))
+            .collect();
     }
 }
 
@@ -31,20 +38,22 @@ impl ResourceItem for Music {
     }
 
     fn is_modified(&self) -> bool {
-        self.seqs.iter().any(|seq| !seq.is_empty())
+        self.seqs.iter().any(|seq| !seq.lock().is_empty())
     }
 
     fn clear(&mut self) {
-        self.seqs = (0..NUM_CHANNELS).map(|_| Vec::new()).collect();
+        self.seqs = (0..NUM_CHANNELS)
+            .map(|_| new_shared_type!(Vec::new()))
+            .collect();
     }
 
     fn serialize(&self, _pyxel: &Pyxel) -> String {
         let mut output = String::new();
         for seq in &self.seqs {
-            if seq.is_empty() {
+            if seq.lock().is_empty() {
                 output += "none";
             } else {
-                for sound_index in seq {
+                for sound_index in &*seq.lock() {
                     let _guard = write!(output, "{sound_index:02x}");
                 }
             }
@@ -60,7 +69,7 @@ impl ResourceItem for Music {
                 continue;
             }
             string_loop!(j, value, line, 2, {
-                self.seqs[i].push(parse_hex_string(&value).unwrap());
+                self.seqs[i].lock().push(parse_hex_string(&value).unwrap());
             });
         }
     }
@@ -74,7 +83,7 @@ mod tests {
     fn new() {
         let music = Music::new();
         for i in 0..NUM_CHANNELS {
-            assert_eq!(music.lock().seqs[i as usize].len(), 0);
+            assert_eq!(music.lock().seqs[i as usize].lock().len(), 0);
         }
     }
 
@@ -85,7 +94,10 @@ mod tests {
             .lock()
             .set(&[vec![0, 1, 2], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
         for i in 0..NUM_CHANNELS {
-            assert_eq!(&music.lock().seqs[i as usize], &vec![i, i + 1, i + 2]);
+            assert_eq!(
+                &*music.lock().seqs[i as usize].lock(),
+                &vec![i, i + 1, i + 2]
+            );
         }
     }
 }
