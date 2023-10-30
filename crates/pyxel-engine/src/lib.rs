2de4e68519fbc699cdae1d9f6a11369b04f0ca9b
Takashi Kitao <takashi.kitao@gmail.com>
2023-10-30 11:54:15 +0900

    Suppressed clippy warnings

--- crates/pyxel-engine/src/lib.rs
+++ crates/pyxel-engine/src/lib.rs
@@ -1,30 +1,18 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![warn(clippy::pedantic, clippy::cargo)]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
     clippy::cast_possible_wrap,
     clippy::cast_precision_loss,
     clippy::cast_sign_loss,
-    clippy::float_cmp,
     clippy::fn_params_excessive_bools,
-    clippy::items_after_statements,
-    clippy::match_same_arms,
-    clippy::missing_const_for_fn,
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
     clippy::must_use_candidate,
     clippy::needless_pass_by_value,
-    clippy::range_plus_one,
-    clippy::redundant_closure,
-    clippy::significant_drop_in_scrutinee,
-    clippy::significant_drop_tightening,
-    clippy::suboptimal_flops,
     clippy::too_many_arguments,
-    clippy::too_many_lines,
     clippy::unreadable_literal,
-    clippy::unused_self,
-    clippy::wildcard_imports,
-    clippy::zero_ptr
+    clippy::unused_self
 )]
 
 #[macro_use]
