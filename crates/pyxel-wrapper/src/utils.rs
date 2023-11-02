26a594716bf18366a50b1b8d07be3f3bd76b34e0
Takashi Kitao <takashi.kitao@gmail.com>
2023-11-02 20:15:07 +0900

    Changed to display file and line for the deprecated warning

--- crates/pyxel-wrapper/src/utils.rs
+++ crates/pyxel-wrapper/src/utils.rs
@@ -1,3 +1,6 @@
+use pyo3::types::PyDict;
+use pyo3::Python;
+
 macro_rules! type_error {
     ($msg: expr) => {
         return Err(pyo3::exceptions::PyTypeError::new_err($msg))
@@ -120,4 +123,21 @@ macro_rules! wrap_as_python_list {
             }
         }
     };
+}
+
+pub fn pyxel_warn(message: &str) {
+    Python::with_gil(|py| {
+        let locals = PyDict::new(py);
+        py.run(
+            "import traceback; stack_info = traceback.extract_stack()[-2][:2]",
+            None,
+            Some(locals),
+        )
+        .unwrap();
+        let stack_info = locals.get_item("stack_info").unwrap();
+        if let Ok((file, line)) = stack_info.extract::<(String, i64)>() {
+            print!("{file}:{line}: ");
+        }
+        println!("PyxelWarning: {message}");
+    });
 }
