595e82c0db6106f1fe77d3a4c90f435bed87b083
Takashi Kitao <takashi.kitao@gmail.com>
2023-11-04 11:23:27 +0900

    Added support for WASM build

--- crates/pyxel-platform/build.rs
+++ crates/pyxel-platform/build.rs
@@ -1,137 +1,197 @@
 use std::env::var;
-use std::fs::{write, File};
+use std::fs::File;
 use std::io::{copy, BufWriter};
 use std::path::Path;
+use std::process::Command;
+use std::str;
 
 use tar::Archive;
 
 const SDL2_VERSION: &str = "2.28.2";
 
-fn main() {
-    let target = var("TARGET").unwrap();
-    let target_os = target.splitn(3, '-').nth(2).unwrap();
-    let out_dir = &var("OUT_DIR").unwrap();
-    let sdl2_dir = &format!("{}/SDL2-{}", out_dir, SDL2_VERSION);
-
-    download_sdl2(sdl2_dir, out_dir);
-    build_sdl2(sdl2_dir, target_os);
-    generate_bindings(sdl2_dir, out_dir);
+struct SDL2BindingsBuilder {
+    target: String,
+    target_os: String,
+    sdl2_dir: String,
+    out_dir: String,
 }
 
-fn download_sdl2(sdl2_dir: &str, out_dir: &str) {
-    if Path::new(&sdl2_dir).exists() {
-        return;
-    }
-    let sdl2_archive_url = format!(
-        "https://www.libsdl.org/release/SDL2-{}.tar.gz",
-        SDL2_VERSION
-    );
-    let sdl2_archive_path = format!("{}/SDL2-{}.tar.gz", out_dir, SDL2_VERSION);
-
-    // Download SDL2
-    let mut resp =
-        reqwest::blocking::get(sdl2_archive_url).expect("Failed to download SDL2 source code");
-    let file = File::create(&sdl2_archive_path).unwrap();
-    let mut writer = BufWriter::new(file);
-    copy(&mut resp, &mut writer).expect("Failed to write SDL2 source code to file");
-
-    // Extract SDL2
-    let tar_gz = File::open(&sdl2_archive_path).unwrap();
-    let tar = flate2::read::GzDecoder::new(tar_gz);
-    let mut archive = Archive::new(tar);
-    if let Err(_err) = archive.unpack(out_dir) {
-        if Path::new(&sdl2_dir).exists() {
-            std::fs::remove_dir_all(sdl2_dir).expect("Failed to remove SDL2 directory");
+impl SDL2BindingsBuilder {
+    fn new() -> Self {
+        let target = var("TARGET").unwrap().to_string();
+        let target_os = target.splitn(3, '-').nth(2).unwrap().to_string();
+        let out_dir = var("OUT_DIR").unwrap().to_string();
+        let sdl2_dir = format!("{}/SDL2-{}", out_dir, SDL2_VERSION);
+        Self {
+            target,
+            target_os,
+            sdl2_dir,
+            out_dir,
         }
-        panic!("Failed to extract SDL2 source code");
     }
-}
 
-fn build_sdl2(sdl2_dir: &str, target_os: &str) {
-    // Compile SDL2
-    let mut cfg = cmake::Config::new(sdl2_dir);
-    cfg.profile("release");
-    if target_os == "linux" {
-        /*
-        use version_compare::{compare_to, Cmp};
-        if let Ok(version) = std::process::Command::new("cc")
-            .arg("-dumpversion")
-            .output()
-        {
-            let affected =
-                compare_to(std::str::from_utf8(&version.stdout).unwrap(), "10", Cmp::Ge)
-                    .unwrap_or(true);
-            if affected {
-                cfg.cflag("-fcommon");
+    fn should_bundle_sdl2(&self) -> bool {
+        self.target_os.contains("windows") || self.target_os == "darwin"
+    }
+
+    fn download_sdl2(&self) {
+        if Path::new(&self.sdl2_dir).exists() {
+            return;
+        }
+        let sdl2_archive_url = format!(
+            "https://www.libsdl.org/release/SDL2-{}.tar.gz",
+            SDL2_VERSION
+        );
+        let sdl2_archive_path = format!("{}/SDL2-{}.tar.gz", self.out_dir, SDL2_VERSION);
+
+        // Download SDL2
+        let mut resp =
+            reqwest::blocking::get(sdl2_archive_url).expect("Failed to download SDL2 source code");
+        let file = File::create(&sdl2_archive_path).unwrap();
+        let mut writer = BufWriter::new(file);
+        copy(&mut resp, &mut writer).expect("Failed to write SDL2 source code to file");
+
+        // Extract SDL2
+        let tar_gz = File::open(&sdl2_archive_path).unwrap();
+        let tar = flate2::read::GzDecoder::new(tar_gz);
+        let mut archive = Archive::new(tar);
+        if let Err(_err) = archive.unpack(&self.out_dir) {
+            if Path::new(&self.sdl2_dir).exists() {
+                std::fs::remove_dir_all(&self.sdl2_dir).expect("Failed to remove SDL2 directory");
             }
+            panic!("Failed to extract SDL2 source code");
         }
-        */
-    } else {
-        cfg.cflag("-D__FLTUSED__");
     }
-    if target_os == "windows-gnu" {
-        cfg.define("VIDEO_OPENGLES", "OFF");
-    }
-    cfg.define("SDL_SHARED", "OFF");
-    cfg.define("SDL_STATIC", "ON");
-    cfg.define("SDL_MAIN_HANDLED", "ON");
-    let sdl2_compiled_path = cfg.build();
 
-    // Link SDL2
-    println!(
-        "cargo:rustc-link-search={}",
-        sdl2_compiled_path.join("lib64").display()
-    );
-    println!(
-        "cargo:rustc-link-search={}",
-        sdl2_compiled_path.join("lib").display()
-    );
-    println!("cargo:rustc-link-lib=static=SDL2main");
-    if target_os.contains("windows") {
-        println!("cargo:rustc-link-lib=static=SDL2-static");
-    } else {
-        println!("cargo:rustc-link-lib=static=SDL2");
+    fn build_sdl2(&self) {
+        let mut cfg = cmake::Config::new(&self.sdl2_dir);
+        cfg.profile("release")
+            .cflag("-D__FLTUSED__")
+            .define("SDL_SHARED", "OFF")
+            .define("SDL_STATIC", "ON")
+            .define("SDL_MAIN_HANDLED", "ON");
+        if self.target_os == "windows-gnu" {
+            cfg.define("VIDEO_OPENGLES", "OFF");
+        }
+        let cmake_out_dir = cfg.build();
+        println!(
+            "cargo:rustc-link-search={}",
+            cmake_out_dir.join("lib64").display()
+        );
+        println!(
+            "cargo:rustc-link-search={}",
+            cmake_out_dir.join("lib").display()
+        );
     }
-    if target_os.contains("windows") {
-        println!("cargo:rustc-link-lib=shell32");
-        println!("cargo:rustc-link-lib=user32");
-        println!("cargo:rustc-link-lib=gdi32");
-        println!("cargo:rustc-link-lib=winmm");
-        println!("cargo:rustc-link-lib=imm32");
-        println!("cargo:rustc-link-lib=ole32");
-        println!("cargo:rustc-link-lib=oleaut32");
-        println!("cargo:rustc-link-lib=version");
-        println!("cargo:rustc-link-lib=uuid");
-        println!("cargo:rustc-link-lib=dinput8");
-        println!("cargo:rustc-link-lib=dxguid");
-        println!("cargo:rustc-link-lib=setupapi");
-    } else if target_os == "darwin" {
-        println!("cargo:rustc-link-lib=framework=Cocoa");
-        println!("cargo:rustc-link-lib=framework=IOKit");
-        println!("cargo:rustc-link-lib=framework=Carbon");
-        println!("cargo:rustc-link-lib=framework=ForceFeedback");
-        println!("cargo:rustc-link-lib=framework=GameController");
-        println!("cargo:rustc-link-lib=framework=CoreHaptics");
-        println!("cargo:rustc-link-lib=framework=CoreVideo");
-        println!("cargo:rustc-link-lib=framework=CoreAudio");
-        println!("cargo:rustc-link-lib=framework=AudioToolbox");
-        println!("cargo:rustc-link-lib=framework=Metal");
-        println!("cargo:rustc-link-lib=iconv");
+
+    fn link_sdl2(&self) {
+        if self.should_bundle_sdl2() {
+            println!("cargo:rustc-link-lib=static=SDL2main");
+            if self.target_os.contains("windows") {
+                println!("cargo:rustc-link-lib=static=SDL2-static");
+            } else {
+                println!("cargo:rustc-link-lib=static=SDL2");
+            }
+            if self.target_os.contains("windows") {
+                println!("cargo:rustc-link-lib=shell32");
+                println!("cargo:rustc-link-lib=user32");
+                println!("cargo:rustc-link-lib=gdi32");
+                println!("cargo:rustc-link-lib=winmm");
+                println!("cargo:rustc-link-lib=imm32");
+                println!("cargo:rustc-link-lib=ole32");
+                println!("cargo:rustc-link-lib=oleaut32");
+                println!("cargo:rustc-link-lib=version");
+                println!("cargo:rustc-link-lib=uuid");
+                println!("cargo:rustc-link-lib=dinput8");
+                println!("cargo:rustc-link-lib=dxguid");
+                println!("cargo:rustc-link-lib=setupapi");
+            } else if self.target_os == "darwin" {
+                println!("cargo:rustc-link-lib=framework=Cocoa");
+                println!("cargo:rustc-link-lib=framework=IOKit");
+                println!("cargo:rustc-link-lib=framework=Carbon");
+                println!("cargo:rustc-link-lib=framework=ForceFeedback");
+                println!("cargo:rustc-link-lib=framework=GameController");
+                println!("cargo:rustc-link-lib=framework=CoreHaptics");
+                println!("cargo:rustc-link-lib=framework=CoreVideo");
+                println!("cargo:rustc-link-lib=framework=CoreAudio");
+                println!("cargo:rustc-link-lib=framework=AudioToolbox");
+                println!("cargo:rustc-link-lib=framework=Metal");
+                println!("cargo:rustc-link-lib=iconv");
+            }
+        } else if self.target_os != "emscripten" {
+            println!("cargo:rustc-flags=-l SDL2");
+        }
+    }
+
+    fn get_include_paths(&self) -> Vec<String> {
+        let mut include_paths = Vec::new();
+        if self.should_bundle_sdl2() {
+            include_paths.push(format!("{}/include", self.sdl2_dir));
+        } else if self.target_os == "emscripten" {
+            let output = Command::new("emcc")
+                .args(&["--cflags", "-s", "USE_SDL=2"])
+                .output()
+                .unwrap();
+            let cflags_str = str::from_utf8(&output.stdout).unwrap();
+            let sdl2_include_paths = cflags_str
+                .split_whitespace()
+                .filter(|flag| flag.starts_with("-I"))
+                .collect::<Vec<&str>>();
+            let sdl2_include_path = sdl2_include_paths
+                .iter()
+                .find(|&&path| path.contains("SDL2"))
+                .map(|&path| path[2..].to_string())
+                .unwrap();
+            include_paths.push(sdl2_include_path.clone());
+            include_paths.push(sdl2_include_path + "/..");
+        } else {
+            include_paths.push(var("SDL2_INCLUDE_PATH").unwrap());
+        }
+        include_paths
+    }
+
+    fn generate_bindings(&self) {
+        let mut builder = bindgen::Builder::default()
+            .use_core()
+            .clang_arg(format!("--target={}", self.target.clone()))
+            .header("sdl2_proxy.h")
+            .prepend_enum_name(false)
+            .derive_debug(false);
+        for include_path in self.get_include_paths() {
+            builder = builder.clang_arg(format!("-I{include_path}"));
+        }
+        if self.target_os == "windows-msvc" {
+            builder = builder
+                .clang_arg("-IC:/Program Files (x86)/Windows Kits/8.1/Include/shared")
+                .clang_arg("-IC:/Program Files/LLVM/lib/clang/5.0.0/include")
+                .clang_arg("-IC:/Program Files (x86)/Windows Kits/10/Include/10.0.10240.0/ucrt")
+                .clang_arg("-IC:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/include")
+                .clang_arg("-IC:/Program Files (x86)/Windows Kits/8.1/Include/um");
+        }
+        if self.target_os == "linux-gnu" {
+            builder = builder
+                .clang_arg("-DSDL_VIDEO_DRIVER_X11")
+                .clang_arg("-DSDL_VIDEO_DRIVER_WAYLAND");
+        }
+        builder
+            .generate()
+            .expect("Failed to generate bindings")
+            .write_to_file(Path::new(&self.out_dir).join("bindings.rs"))
+            .unwrap();
+    }
+
+    fn build(&self) {
+        if self.should_bundle_sdl2() {
+            self.download_sdl2();
+            self.build_sdl2();
+        }
+        self.link_sdl2();
+        self.generate_bindings()
     }
 }
 
-fn generate_bindings(sdl2_dir: &str, out_dir: &str) {
-    let bindings = bindgen::Builder::default()
-        .header(format!("{}/include/SDL.h", sdl2_dir))
-        .generate()
-        .expect("Failed to generate bindings")
-        .to_string()
-        .replace("SDL_EventType_", "")
-        .replace("SDL_GLattr_", "")
-        .replace("SDL_GLprofile_", "")
-        .replace("SDL_KeyCode_", "")
-        .replace("SDL_PixelFormatEnum_", "")
-        .replace("SDL_WindowEventID_", "")
-        .replace("SDL_WindowFlags_", "");
-    write(Path::new(&out_dir).join("bindings.rs"), bindings).expect("Failed to write bindings");
+fn main() {
+    std::env::set_var("RUST_LOG", "bindgen=debug");
+    std::env::set_var("BINDGEN_EXTRA_CLANG_ARGS", "-v");
+    SDL2BindingsBuilder::new().build();
 }
