a303f458c1ffb0a58002d294c88d369ee1baff4a
Takashi Kitao <takashi.kitao@gmail.com>
2023-11-25 18:47:33 +0900

    Implemented Waveform class

--- crates/pyxel-engine/src/oscillator.rs
+++ crates/pyxel-engine/src/oscillator.rs
@@ -1,12 +1,12 @@
 use crate::blip_buf::BlipBuf;
+use crate::pyxel::WAVEFORMS;
 use crate::settings::{
-    CLOCK_RATE, EFFECT_FADEOUT, EFFECT_NONE, EFFECT_SLIDE, EFFECT_VIBRATO, NOISE_VOLUME_FACTOR,
-    NUM_CLOCKS_PER_TICK, OSCILLATOR_RESOLUTION, PULSE_VOLUME_FACTOR, SQUARE_VOLUME_FACTOR,
-    TONE_NOISE, TONE_PULSE, TONE_SQUARE, TONE_TRIANGLE, TRIANGLE_VOLUME_FACTOR, VIBRATO_DEPTH,
-    VIBRATO_FREQUENCY,
+    CLOCK_RATE, EFFECT_FADEOUT, EFFECT_NONE, EFFECT_SLIDE, EFFECT_VIBRATO, INITIAL_NOISE_REG,
+    NUM_CLOCKS_PER_TICK, OSCILLATOR_RESOLUTION, TONE_TRIANGLE, VIBRATO_DEPTH, VIBRATO_FREQUENCY,
 };
 
 pub type Tone = u8;
+pub type Gain = f64;
 pub type Effect = u8;
 
 const VIBRATO_PERIOD: u32 =
@@ -22,19 +22,19 @@ struct Vibrato {
 }
 
 struct FadeOut {
-    volume: f64,
+    gain: Gain,
 }
 
 pub struct Oscillator {
     pitch: f64,
     tone: Tone,
-    volume: f64,
+    gain: Gain,
     effect: Effect,
     duration: u32,
     time: u32,
     phase: u32,
     amplitude: i16,
-    noise: u32,
+    noise_reg: u16,
     slide: Slide,
     vibrato: Vibrato,
     fadeout: FadeOut,
@@ -45,31 +45,31 @@ impl Oscillator {
         Self {
             pitch: Self::note_to_pitch(0.0),
             tone: TONE_TRIANGLE,
-            volume: 0.0,
+            gain: 0.0,
             effect: EFFECT_NONE,
             duration: 0,
             time: 0,
             phase: 0,
             amplitude: 0,
-            noise: 1,
+            noise_reg: INITIAL_NOISE_REG,
             slide: Slide { pitch: 0.0 },
             vibrato: Vibrato { time: 0, phase: 0 },
-            fadeout: FadeOut { volume: 0.0 },
+            fadeout: FadeOut { gain: 0.0 },
         }
     }
 
-    pub fn play(&mut self, note: f64, tone: Tone, volume: f64, effect: Effect, duration: u32) {
+    pub fn play(&mut self, note: f64, tone: Tone, gain: Gain, effect: Effect, duration: u32) {
         let last_pitch = self.pitch;
         self.pitch = Self::note_to_pitch(note);
         self.tone = tone;
-        self.volume = volume;
+        self.gain = gain;
         self.effect = effect;
         self.duration = duration;
         if effect == EFFECT_SLIDE {
             self.slide.pitch = (self.pitch - last_pitch) / self.duration as f64;
             self.pitch = last_pitch;
         } else if effect == EFFECT_FADEOUT {
-            self.fadeout.volume = -self.volume / self.duration as f64;
+            self.fadeout.gain = -self.gain / self.duration as f64;
         }
     }
 
@@ -88,22 +88,24 @@ impl Oscillator {
         }
         let pitch = self.pitch
             + if self.effect == EFFECT_VIBRATO {
-                self.pitch * Self::triangle(self.vibrato.phase) * VIBRATO_DEPTH
+                self.pitch
+                    * (if self.vibrato.phase < OSCILLATOR_RESOLUTION / 2 {
+                        self.vibrato.phase as f64 / (OSCILLATOR_RESOLUTION / 4) as f64 - 1.0
+                    } else {
+                        3.0 - self.vibrato.phase as f64 / (OSCILLATOR_RESOLUTION / 4) as f64
+                    })
+                    * VIBRATO_DEPTH
             } else {
                 0.0
             };
         let period = (CLOCK_RATE as f64 / pitch / OSCILLATOR_RESOLUTION as f64) as u32;
-
+        let waveforms = WAVEFORMS.lock();
+        let waveform = waveforms[self.tone as usize].lock();
         while self.time < NUM_CLOCKS_PER_TICK {
             let last_amplitude = self.amplitude;
             self.phase = (self.phase + 1) % OSCILLATOR_RESOLUTION;
-            self.amplitude = (match self.tone {
-                TONE_TRIANGLE => Self::triangle(self.phase) * TRIANGLE_VOLUME_FACTOR,
-                TONE_SQUARE => Self::square(self.phase) * SQUARE_VOLUME_FACTOR,
-                TONE_PULSE => Self::pulse(self.phase) * PULSE_VOLUME_FACTOR,
-                TONE_NOISE => self.noise(self.phase) * NOISE_VOLUME_FACTOR,
-                _ => panic!("Invalid tone '{}'", self.tone),
-            } * self.volume
+            self.amplitude = (waveform.amplitude(self.phase, &mut self.noise_reg)
+                * self.gain
                 * i16::MAX as f64) as i16;
             blip_buf.add_delta(
                 self.time as u64,
@@ -111,7 +113,6 @@ impl Oscillator {
             );
             self.time += period;
         }
-
         match self.effect {
             EFFECT_NONE => {}
             EFFECT_SLIDE => {
@@ -124,49 +125,15 @@ impl Oscillator {
                 self.vibrato.time %= VIBRATO_PERIOD;
             }
             EFFECT_FADEOUT => {
-                self.volume += self.fadeout.volume;
+                self.gain += self.fadeout.gain;
             }
             _ => panic!("Invalid effect '{}'", self.effect),
         }
-
         self.duration -= 1;
         self.time -= NUM_CLOCKS_PER_TICK;
     }
 
     fn note_to_pitch(note: f64) -> f64 {
         440.0 * ((note - 33.0) / 12.0).exp2()
-    }
-
-    fn triangle(phase: u32) -> f64 {
-        if phase < OSCILLATOR_RESOLUTION / 2 {
-            phase as f64 / (OSCILLATOR_RESOLUTION / 4) as f64 - 1.0
-        } else {
-            3.0 - phase as f64 / (OSCILLATOR_RESOLUTION / 4) as f64
-        }
-    }
-
-    const fn square(phase: u32) -> f64 {
-        if phase < OSCILLATOR_RESOLUTION / 2 {
-            1.0
-        } else {
-            -1.0
-        }
-    }
-
-    const fn pulse(phase: u32) -> f64 {
-        if phase < OSCILLATOR_RESOLUTION / 4 {
-            1.0
-        } else {
-            -1.0
-        }
-    }
-
-    fn noise(&mut self, phase: u32) -> f64 {
-        if phase % 8 == 0 {
-            let feedback = (self.noise & 1) ^ (self.noise >> 1 & 1);
-            self.noise >>= 1;
-            self.noise |= feedback << 14;
-        }
-        (self.noise & 1) as f64 * 2.0 - 1.0
     }
 }
