998c7396e9b0d994d12f594053421d4aa2689fce
Takashi Kitao <takashi.kitao@gmail.com>
2023-12-04 22:21:01 +0900

    Refactored the list wrapper macro

--- crates/pyxel-wrapper/src/utils.rs
+++ crates/pyxel-wrapper/src/utils.rs
@@ -43,3 +43,56 @@ macro_rules! cast_pyany {
         }
     };
 }
+
+macro_rules! wrap_as_python_list {
+    ($wrapper_name:ident, $inner_type:ty, $len:expr, $value_type:ty, $get:expr, $set:expr, $list_type:ty, $from_list:expr, $to_list:expr) => {
+        #[pyclass]
+        #[derive(Clone)]
+        pub struct $wrapper_name {
+            inner: $inner_type,
+        }
+
+        impl $wrapper_name {
+            pub const fn wrap(inner: $inner_type) -> Self {
+                Self { inner }
+            }
+        }
+
+        #[pymethods]
+        impl $wrapper_name {
+            fn __len__(&self) -> PyResult<usize> {
+                Ok($len(&self.inner))
+            }
+
+            fn __getitem__(&self, idx: isize) -> PyResult<$value_type> {
+                if idx < $len(&self.inner) as isize {
+                    Ok($get(&self.inner, idx as usize))
+                } else {
+                    Err(pyo3::exceptions::PyIndexError::new_err(
+                        "list index out of range",
+                    ))
+                }
+            }
+
+            fn __setitem__(&mut self, idx: isize, value: $value_type) -> PyResult<()> {
+                if idx < $len(&self.inner) as isize {
+                    $set(&self.inner, idx as usize, value);
+                    Ok(())
+                } else {
+                    Err(pyo3::exceptions::PyIndexError::new_err(
+                        "list assignment index out of range",
+                    ))
+                }
+            }
+
+            pub fn from_list(&mut self, lst: $list_type) -> PyResult<()> {
+                $from_list(&self.inner, lst);
+                Ok(())
+            }
+
+            pub fn to_list(&self) -> PyResult<$list_type> {
+                Ok($to_list(&self.inner))
+            }
+        }
+    };
+}
