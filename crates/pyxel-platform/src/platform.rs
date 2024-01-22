a253c0adf4d5aeaa748b84bc257252d312d6d716
Takashi Kitao <takashi.kitao@gmail.com>
2024-01-22 15:53:18 +0900

    Added a cast for Windows

--- crates/pyxel-platform/src/platform.rs
+++ crates/pyxel-platform/src/platform.rs
@@ -29,7 +29,7 @@ pub fn init<'a, F: FnOnce(u32, u32) -> (&'a str, u32, u32)>(window_params: F) {
     assert!(
         unsafe {
             SDL_Init(
-                SDL_WINDOW_ALLOW_HIGHDPI
+                SDL_WINDOW_ALLOW_HIGHDPI as Uint32
                     | SDL_INIT_VIDEO
                     | SDL_INIT_AUDIO
                     | SDL_INIT_GAMECONTROLLER,
