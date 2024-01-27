f709d47e5df16ca327963a4c24db8ce41d489b46
Takashi Kitao <takashi.kitao@gmail.com>
2024-01-27 13:04:15 +0900

    Refined error messages

--- crates/pyxel-engine/src/tiled_map_file.rs
+++ crates/pyxel-engine/src/tiled_map_file.rs
@@ -41,67 +41,73 @@ struct TiledMapFile {
 
 impl Tilemap {
     pub fn from_tmx(filename: &str, layer_index: u32) -> SharedTilemap {
-        let file = File::open(filename);
-        if file.is_err() {
-            println!("Failed to open '{filename}'");
-            return Self::new(1, 1, ImageSource::Index(0));
+        macro_rules! check_or_break {
+            ($condition:expr, $fmt:expr $(,$arg:tt)*) => {
+                if !$condition {
+                    println!($fmt, $($arg)*);
+                    break;
+                }
+            };
         }
-        let mut file = file.unwrap();
-        let mut tmx_text = String::new();
-        let blank_tilemap = Self::new(1, 1, ImageSource::Index(0));
-        if file.read_to_string(&mut tmx_text).is_err() {
-            println!("Failed to read TMX file");
-            return blank_tilemap;
-        }
-        let tmx = serde_xml_rs::from_str(&tmx_text);
-        if tmx.is_err() {
-            println!("Failed to parse TMX file");
-            return blank_tilemap;
-        }
-        let tmx: TiledMapFile = tmx.unwrap();
-        if tmx.tilewidth != TILE_SIZE || tmx.tileheight != TILE_SIZE {
-            println!("TMX file's tile size is not {TILE_SIZE}x{TILE_SIZE}");
-            return blank_tilemap;
-        }
-        if tmx.tilesets.is_empty() {
-            println!("Tileset not found in TMX file");
-            return blank_tilemap;
-        }
-        let tileset = &tmx.tilesets[0];
-        if tileset.columns.is_none() {
-            println!("Tileset is not embedded in TMX file");
-            return blank_tilemap;
-        }
-        let tileset_columns = tileset.columns.unwrap();
-        if layer_index >= tmx.layers.len() as u32 {
-            println!("Layer {layer_index} not found in TMX file");
-            return blank_tilemap;
-        };
-        let layer = &tmx.layers[layer_index as usize];
-        if layer.data.encoding != "csv" {
-            println!("TMX file's encoding is not CSV");
-            return blank_tilemap;
-        }
-        let layer_data: Vec<u32> = remove_whitespace(&layer.data.tiles)
-            .split(',')
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect();
-        let tilemap = Self::new(layer.width, layer.height, ImageSource::Index(0));
-        {
-            let mut tilemap = tilemap.lock();
-            for (i, tile_id) in layer_data.iter().enumerate() {
-                let x = i % layer.width as usize;
-                let y = i / layer.width as usize;
-                let tile_id = if *tile_id > tileset.firstgid {
-                    tile_id - tileset.firstgid
-                } else {
-                    0
-                };
-                let tile_x = (tile_id % tileset_columns) as u8;
-                let tile_y = (tile_id / tileset_columns) as u8;
-                tilemap.canvas.write_data(x, y, (tile_x, tile_y));
+        #[allow(clippy::never_loop)]
+        loop {
+            let file = File::open(filename);
+            check_or_break!(file.is_err(), "Failed to open TMX file '{filename}'");
+            let mut file = file.unwrap();
+            let mut tmx_text = String::new();
+            check_or_break!(
+                file.read_to_string(&mut tmx_text).is_err(),
+                "Failed to read TMX file '{filename}'"
+            );
+            let tmx = serde_xml_rs::from_str(&tmx_text);
+            check_or_break!(tmx.is_err(), "Failed to parse TMX file '{filename}'");
+            let tmx: TiledMapFile = tmx.unwrap();
+            check_or_break!(
+                tmx.tilewidth != TILE_SIZE || tmx.tileheight != TILE_SIZE,
+                "TMX file's tile size is not {TILE_SIZE}x{TILE_SIZE}"
+            );
+            check_or_break!(
+                tmx.tilesets.is_empty(),
+                "Tileset not found in TMX file '{filename}'"
+            );
+            let tileset = &tmx.tilesets[0];
+            check_or_break!(
+                tileset.columns.is_none(),
+                "Tileset is not embedded in TMX file '{filename}'"
+            );
+            let tileset_columns = tileset.columns.unwrap();
+            check_or_break!(
+                layer_index >= tmx.layers.len() as u32,
+                "Layer {layer_index} not found in TMX file '{filename}'"
+            );
+            let layer = &tmx.layers[layer_index as usize];
+            check_or_break!(
+                layer.data.encoding != "csv",
+                "TMX file '{filename}''s encoding is not CSV"
+            );
+            let layer_data: Vec<u32> = remove_whitespace(&layer.data.tiles)
+                .split(',')
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect();
+            let tilemap = Self::new(layer.width, layer.height, ImageSource::Index(0));
+            {
+                let mut tilemap = tilemap.lock();
+                for (i, tile_id) in layer_data.iter().enumerate() {
+                    let x = i % layer.width as usize;
+                    let y = i / layer.width as usize;
+                    let tile_id = if *tile_id > tileset.firstgid {
+                        tile_id - tileset.firstgid
+                    } else {
+                        0
+                    };
+                    let tile_x = (tile_id % tileset_columns) as u8;
+                    let tile_y = (tile_id / tileset_columns) as u8;
+                    tilemap.canvas.write_data(x, y, (tile_x, tile_y));
+                }
             }
+            return tilemap;
         }
-        tilemap
+        // Return a blank tilemap due to an error
+        Self::new(1, 1, ImageSource::Index(0))
     }
 }
