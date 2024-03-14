c6d49331c290e87369846a2cfadea961736a11d9
Cookie <1549029765@qq.com>
2024-03-14 11:23:34 +0800

    简单无动态, 用show()函数

---
+++ docs/README.yang.md
@@ -0,0 +1,6 @@
+# Tips
+- update()函数里检测按钮if pyxel.btnp(pyxel.KEY_R); --[画实体方框]
+- pyxel.cls(2) 用来创建背景板颜色; --[画实体方框]
+- pyxel.circb(60, 60, 40, 7);  --[画空心圆]
+- 
+
--- src/demo0.py
+++ src/demo0.py
@@ -3,17 +3,7 @@ Copyright (c) Cookie Yang. All right reserved.
 """
 import pyxel
 
-class App:
-    def __init__(self):
-        pyxel.init(160, 120)
-        self.x = 0
-        pyxel.run(self.update, self.draw)
-
-    def update(self):
-        self.x = (self.x + 1) % pyxel.width
-
-    def draw(self):
-        pyxel.cls(2)
-        pyxel.rect(self.x, 0, 8, 8, 9)
-
-App()
+pyxel.init(120, 120)
+pyxel.cls(11)
+pyxel.circb(60, 60, 40, 7)
+pyxel.show()
