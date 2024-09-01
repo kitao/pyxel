4656311aceede90f546a55a1ce735b5067795a8c
Takashi Kitao <takashi.kitao@gmail.com>
2024-09-01 13:09:21 +0900

    Added the Font class

---
+++ rust/pyxel-wrapper/src/font_wrapper.rs
@@ -0,0 +1,30 @@
+use pyo3::prelude::*;
+
+#[pyclass]
+#[derive(Clone)]
+pub struct Font {
+    pub(crate) inner: pyxel::SharedFont,
+}
+
+impl Font {
+    pub fn wrap(inner: pyxel::SharedFont) -> Self {
+        Self { inner }
+    }
+}
+
+#[pymethods]
+impl Font {
+    #[new]
+    pub fn new(filename: &str) -> Self {
+        Self::wrap(pyxel::Font::new(filename))
+    }
+
+    pub fn draw_width(&self, s: &str) -> i32 {
+        self.inner.lock().draw_width(s)
+    }
+}
+
+pub fn add_font_class(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Font>()?;
+    Ok(())
+}
