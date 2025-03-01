d1eb0c812f6319124d4ce7e3329109bade5ed13f
Takashi Kitao <takashi.kitao@gmail.com>
2025-03-02 05:36:12 +0900

    Enabled importing local modules in the web version

--- wasm/pyxel.js
+++ wasm/pyxel.js
@@ -147,6 +147,60 @@ async function _loadPyodideAndPyxel(canvas) {
   let FS = pyodide.FS;
   FS.mkdir(PYXEL_WORKING_DIRECTORY);
   FS.chdir(PYXEL_WORKING_DIRECTORY);
+  pyodide.runPython(`
+    import importlib.util
+    import inspect
+    import os.path
+    import sys
+
+
+    class ImportHook:
+        def __init__(self):
+            self.imported_modules = set()
+            self.main_dir = None
+
+        def find_spec(self, fullname, path, target=None):
+            if fullname in self.imported_modules:
+                return None
+            self.imported_modules.add(fullname)
+            if fullname in sys.builtin_module_names or fullname in sys.modules:
+                return None
+            spec = importlib.util.find_spec(fullname)
+            if spec and spec.origin:
+                origin = os.path.realpath(spec.origin)
+                if (
+                    origin.startswith(os.path.realpath(sys.base_prefix))
+                    or "site-packages" in origin
+                    or "dist-packages" in origin
+                ):
+                    return None
+            caller_file = next(
+                (
+                    frame.filename
+                    for frame in inspect.stack()
+                    if not frame.filename.startswith("<")
+                ),
+                None,
+            )
+            if not caller_file:
+                return None
+            caller_dir = os.path.dirname(os.path.abspath(caller_file))
+            module_name = fullname.rsplit(".", 1)[-1]
+            module_path = os.path.join(caller_dir, f"{module_name}.py")
+            package_path = os.path.join(caller_dir, module_name, "__init__.py")
+            is_found = os.path.exists(module_path) or os.path.exists(package_path)
+            if is_found and self.main_dir is None:
+                self.main_dir = caller_dir
+            if self.main_dir and self.main_dir != caller_dir:
+                main_module_path = os.path.join(self.main_dir, f"{module_name}.py")
+                main_package_path = os.path.join(self.main_dir, module_name, "__init__.py")
+                if os.path.exists(main_module_path) or os.path.exists(main_package_path):
+                    return None
+            return None
+
+
+    sys.meta_path.insert(0, ImportHook())
+  `);
   return pyodide;
 }
 
