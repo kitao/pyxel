00c27c84f2878c9f8666ee25afc6d74df84c62f0
Takashi Kitao <takashi.kitao@gmail.com>
2025-03-30 19:49:30 +0900

    Formatted the code

--- python/pyxel/examples/05_color_palette.py
+++ python/pyxel/examples/05_color_palette.py
@@ -5,10 +5,12 @@ def draw_palette(x, y, col):
     rgb = pyxel.colors[col]
     hex = f"#{rgb:06X}"
     dec = f"{rgb >> 16},{(rgb >> 8) & 0xFF},{rgb & 0xFF}"
+
     pyxel.rect(x, y, 13, 13, col)
     pyxel.text(x + 16, y + 1, hex, 7)
     pyxel.text(x + 16, y + 8, dec, 7)
     pyxel.text(x + 5 - (col // 10) * 2, y + 4, f"{col}", 7 if col < 6 else 0)
+
     if col == 0:
         pyxel.rectb(x, y, 13, 13, 13)
 
