acf1524a00c5fe073345368c8907809f5bde4817
Takashi Kitao <takashi.kitao@gmail.com>
2025-04-02 17:32:25 +0900

    Revert "Reapply "Made math functions static""
    This reverts commit b68b256794bc4fe4df1c717a498a0cee3d856073.

--- rust/pyxel-wrapper/src/math_wrapper.rs
+++ rust/pyxel-wrapper/src/math_wrapper.rs
@@ -1,59 +1,60 @@
 use pyo3::prelude::*;
-use pyxel::Pyxel;
+
+use crate::pyxel_singleton::pyxel;
 
 #[pyfunction]
 fn ceil(x: f64) -> i32 {
-    Pyxel::ceil(x)
+    pyxel().ceil(x)
 }
 
 #[pyfunction]
 fn floor(x: f64) -> i32 {
-    Pyxel::floor(x)
+    pyxel().floor(x)
 }
 
 #[pyfunction]
 fn sgn(x: f64) -> i32 {
-    Pyxel::sgn(x)
+    pyxel().sgn(x)
 }
 
 #[pyfunction]
 fn sqrt(x: f64) -> f64 {
-    Pyxel::sqrt(x)
+    pyxel().sqrt(x)
 }
 
 #[pyfunction]
 fn sin(deg: f64) -> f64 {
-    Pyxel::sin(deg)
+    pyxel().sin(deg)
 }
 
 #[pyfunction]
 fn cos(deg: f64) -> f64 {
-    Pyxel::cos(deg)
+    pyxel().cos(deg)
 }
 
 #[pyfunction]
 fn atan2(y: f64, x: f64) -> f64 {
-    Pyxel::atan2(y, x)
+    pyxel().atan2(y, x)
 }
 
 #[pyfunction]
 fn rseed(seed: u32) {
-    Pyxel::rseed(seed);
+    pyxel().rseed(seed);
 }
 
 #[pyfunction]
 fn rndi(a: i32, b: i32) -> i32 {
-    Pyxel::rndi(a, b)
+    pyxel().rndi(a, b)
 }
 
 #[pyfunction]
 fn rndf(a: f64, b: f64) -> f64 {
-    Pyxel::rndf(a, b)
+    pyxel().rndf(a, b)
 }
 
 #[pyfunction]
 fn nseed(seed: u32) {
-    Pyxel::nseed(seed);
+    pyxel().nseed(seed);
 }
 
 #[pyfunction]
@@ -61,7 +62,7 @@ fn nseed(seed: u32) {
 fn noise(x: f64, y: Option<f64>, z: Option<f64>) -> f64 {
     let y = y.unwrap_or(0.0);
     let z = z.unwrap_or(0.0);
-    Pyxel::noise(x, y, z)
+    pyxel().noise(x, y, z)
 }
 
 pub fn add_math_functions(m: &Bound<PyModule>) -> PyResult<()> {
