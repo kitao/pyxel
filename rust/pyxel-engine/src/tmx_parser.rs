21b41381a4fe2bc17c019431d2efbbbc3487cc20
Takashi Kitao <takashi.kitao@gmail.com>
2025-06-30 06:53:56 +0900

    Refactored code

--- rust/pyxel-engine/src/tmx_parser.rs
+++ rust/pyxel-engine/src/tmx_parser.rs
@@ -46,9 +46,8 @@ struct TiledMapFile {
     layers: Vec<Layer>,
 }
 
-impl Tilemap {
-    pub fn from_tmx(filename: &str, layer_index: u32) -> SharedTilemap {
-        macro_rules! assert_or_break {
+pub fn parse_tmx(filename: &str, layer_index: u32) -> SharedTilemap {
+    macro_rules! assert_or_break {
             ($condition:expr, $fmt:expr $(,$arg:tt)*) => {
                 if !$condition {
                     println!($fmt, $($arg)*);
@@ -57,71 +56,70 @@ impl Tilemap {
             };
         }
 
-        #[allow(clippy::never_loop)]
-        loop {
-            let file = File::open(filename);
-            assert_or_break!(file.is_ok(), "Failed to open file '{filename}'");
-            let mut file = file.unwrap();
+    #[allow(clippy::never_loop)]
+    loop {
+        let file = File::open(filename);
+        assert_or_break!(file.is_ok(), "Failed to open file '{filename}'");
+        let mut file = file.unwrap();
 
-            let mut tmx_text = String::new();
-            assert_or_break!(
-                file.read_to_string(&mut tmx_text).is_ok(),
-                "Failed to read TMX file"
-            );
+        let mut tmx_text = String::new();
+        assert_or_break!(
+            file.read_to_string(&mut tmx_text).is_ok(),
+            "Failed to read TMX file"
+        );
 
-            let tmx = serde_xml_rs::from_str(&tmx_text);
-            assert_or_break!(tmx.is_ok(), "Failed to parse TMX file");
-            let tmx: TiledMapFile = tmx.unwrap();
+        let tmx = serde_xml_rs::from_str(&tmx_text);
+        assert_or_break!(tmx.is_ok(), "Failed to parse TMX file");
+        let tmx: TiledMapFile = tmx.unwrap();
 
-            assert_or_break!(
-                tmx.tilewidth == TILE_SIZE && tmx.tileheight == TILE_SIZE,
-                "TMX file's tile size is not {TILE_SIZE}x{TILE_SIZE}"
-            );
+        assert_or_break!(
+            tmx.tilewidth == TILE_SIZE && tmx.tileheight == TILE_SIZE,
+            "TMX file's tile size is not {TILE_SIZE}x{TILE_SIZE}"
+        );
 
-            assert_or_break!(!tmx.tilesets.is_empty(), "Tileset not found in TMX file");
-            let tileset = &tmx.tilesets[0];
-            assert_or_break!(
-                tileset.columns.is_some(),
-                "Tileset is not embedded in TMX file"
-            );
-            let tileset_columns = tileset.columns.unwrap();
+        assert_or_break!(!tmx.tilesets.is_empty(), "Tileset not found in TMX file");
+        let tileset = &tmx.tilesets[0];
+        assert_or_break!(
+            tileset.columns.is_some(),
+            "Tileset is not embedded in TMX file"
+        );
+        let tileset_columns = tileset.columns.unwrap();
 
-            assert_or_break!(
-                layer_index < tmx.layers.len() as u32,
-                "Layer {layer_index} not found in TMX file"
-            );
-            let layer = &tmx.layers[layer_index as usize];
-            assert_or_break!(
-                layer.data.encoding == "csv",
-                "TMX file's encoding is not CSV"
-            );
+        assert_or_break!(
+            layer_index < tmx.layers.len() as u32,
+            "Layer {layer_index} not found in TMX file"
+        );
+        let layer = &tmx.layers[layer_index as usize];
+        assert_or_break!(
+            layer.data.encoding == "csv",
+            "TMX file's encoding is not CSV"
+        );
 
-            let layer_data: Vec<u32> = remove_whitespace(&layer.data.tiles)
-                .split(',')
-                .map(|s| s.parse::<u32>().unwrap())
-                .collect();
+        let layer_data: Vec<u32> = remove_whitespace(&layer.data.tiles)
+            .split(',')
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
 
-            let tilemap = Self::new(layer.width, layer.height, ImageSource::Index(0));
-            {
-                let mut tilemap = tilemap.lock();
-                for (i, tile_id) in layer_data.iter().enumerate() {
-                    let x = i % layer.width as usize;
-                    let y = i / layer.width as usize;
-                    let tile_id = if *tile_id > tileset.firstgid {
-                        tile_id - tileset.firstgid
-                    } else {
-                        0
-                    };
-                    let tile_x = (tile_id % tileset_columns) as u8;
-                    let tile_y = (tile_id / tileset_columns) as u8;
-                    tilemap.canvas.write_data(x, y, (tile_x, tile_y));
-                }
+        let tilemap = Tilemap::new(layer.width, layer.height, ImageSource::Index(0));
+        {
+            let mut tilemap = tilemap.lock();
+            for (i, tile_id) in layer_data.iter().enumerate() {
+                let x = i % layer.width as usize;
+                let y = i / layer.width as usize;
+                let tile_id = if *tile_id > tileset.firstgid {
+                    tile_id - tileset.firstgid
+                } else {
+                    0
+                };
+                let tile_x = (tile_id % tileset_columns) as u8;
+                let tile_y = (tile_id / tileset_columns) as u8;
+                tilemap.canvas.write_data(x, y, (tile_x, tile_y));
             }
-
-            return tilemap;
         }
 
-        // Return a blank tilemap due to an error
-        Self::new(1, 1, ImageSource::Index(0))
+        return tilemap;
     }
+
+    // Return a blank tilemap due to an error
+    Tilemap::new(1, 1, ImageSource::Index(0))
 }
