a63ce91c0cc45a2dcc3c002a78a18f46e55ac100
Takashi Kitao <takashi.kitao@gmail.com>
2025-07-25 11:05:00 +0900

    Changed modules

--- rust/pyxel-platform/src/lib.rs
+++ rust/pyxel-platform/src/lib.rs
@@ -1,35 +1,11 @@
-#![warn(clippy::pedantic, clippy::cargo)]
-#![allow(
-    clippy::cast_lossless,
-    clippy::cast_possible_truncation,
-    clippy::cast_possible_wrap,
-    clippy::cast_ptr_alignment,
-    clippy::cast_sign_loss,
-    clippy::missing_panics_doc,
-    clippy::must_use_candidate,
-    clippy::unreadable_literal,
-    clippy::wildcard_imports
-)]
-
-mod audio;
 mod event;
-mod gamepad;
-mod keyboard;
-mod mouse;
 mod platform;
-mod sdl2_sys;
-mod window;
 
-pub mod keys;
+mod sdl2;
 
-#[cfg(target_os = "emscripten")]
-pub mod emscripten;
+mod web;
 
-pub use crate::audio::{set_audio_enabled, start_audio, AudioCallback};
-pub use crate::event::{poll_events, Event};
-pub use crate::platform::{elapsed_time, init, quit, run, sleep};
-pub use crate::window::{
-    glow_context, is_fullscreen, is_gles_enabled, set_fullscreen, set_mouse_pos, set_mouse_visible,
-    set_window_icon, set_window_pos, set_window_size, set_window_title, swap_window, window_pos,
-    window_size,
-};
+pub mod key;
+
+pub use event::Event;
+pub use platform::{init, quit, set_window_size, AudioCallback};
