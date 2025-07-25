48d07de23835c3fb1836ef18ca912cce5c35106d
Takashi Kitao <takashi.kitao@gmail.com>
2025-07-25 11:03:49 +0900

    Updated the platform I/F and key definitions

--- rust/pyxel-platform/src/platform.rs
+++ rust/pyxel-platform/src/platform.rs
@@ -1,107 +1,63 @@
-use std::mem::transmute;
-use std::ptr::{addr_of_mut, null_mut};
+use std::sync::Arc;
 
-use cfg_if::cfg_if;
-use glow::Context as GlowContext;
+use parking_lot::Mutex;
 
-use crate::gamepad::{init_gamepads, Gamepad};
-use crate::sdl2_sys::*;
-use crate::window::{init_glow, init_window};
+use crate::event::Event;
 
-pub struct Platform {
-    pub window: *mut SDL_Window,
-    pub glow_context: *mut GlowContext,
-    pub audio_device_id: SDL_AudioDeviceID,
-    pub mouse_x: i32,
-    pub mouse_y: i32,
-    pub gamepads: Vec<Gamepad>,
-    #[cfg(target_os = "emscripten")]
-    pub virtual_gamepad_states: [bool; 8],
+pub trait LoopCallback {
+    fn update(&mut self);
 }
 
-static mut PLATFORM: *mut Platform = null_mut();
-
-pub fn platform() -> &'static mut Platform {
-    unsafe { &mut *PLATFORM }
+pub trait AudioCallback {
+    fn update(&mut self, out: &mut [i16]);
 }
 
-pub fn init<'a, F: FnOnce(u32, u32) -> (&'a str, u32, u32)>(window_params: F) {
-    assert!(
-        unsafe { SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER,) } >= 0,
-        "Failed to initialize SDL2"
-    );
+pub(crate) trait Platform {
+    // Core
+    fn init(&mut self);
+    fn quit(&mut self);
 
-    let mut display_mode = SDL_DisplayMode {
-        format: 0,
-        w: 0,
-        h: 0,
-        refresh_rate: 0,
-        driverdata: null_mut(),
-    };
-    assert!(
-        unsafe { SDL_GetCurrentDisplayMode(0, addr_of_mut!(display_mode)) } == 0,
-        "Failed to get display size"
-    );
+    // Window
+    fn init_window(&mut self, title: &str, width: u32, height: u32);
+    fn window_pos(&self) -> (i32, i32);
+    fn set_window_pos(&mut self, x: i32, y: i32);
+    fn window_size(&self) -> (u32, u32);
+    fn set_window_size(&mut self, width: u32, height: u32);
+    fn set_window_title(&mut self, title: &str);
+    fn set_window_icon(&mut self, pixels: &[u32]);
+    fn is_fullscreen(&self) -> bool;
+    fn set_fullscreen(&mut self, enabled: bool);
+    fn set_mouse_pos(&mut self, x: i32, y: i32);
+    fn set_mouse_visible(&mut self, visible: bool);
 
-    let (title, width, height) = window_params(display_mode.w as u32, display_mode.h as u32);
+    // Audio
+    fn init_audio(&mut self, sample_rate: u32, buffer_size: u32);
+    fn start_audio(&mut self, callback: Arc<Mutex<dyn AudioCallback>>);
+    fn pause_audio(&mut self, paused: bool);
 
-    let window = init_window(title, width, height);
-    let glow_context = init_glow(window);
-    let gamepads = init_gamepads();
-
-    unsafe {
-        PLATFORM = transmute::<Box<Platform>, *mut Platform>(Box::new(Platform {
-            window,
-            glow_context,
-            audio_device_id: 0,
-            mouse_x: i32::MIN,
-            mouse_y: i32::MIN,
-            gamepads,
-            #[cfg(target_os = "emscripten")]
-            virtual_gamepad_states: [false; 8],
-        }));
-    }
+    // Frame
+    fn start_loop(&mut self, callback: Arc<Mutex<dyn LoopCallback>>);
+    fn step_loop(&mut self);
+    fn poll_events(&mut self) -> Vec<Event>;
+    fn gl_context(&self) -> Option<glow::Context>;
+    fn update_screen(&mut self, pixels: &[u32]);
+    fn elapsed_time(&self) -> u32;
 }
 
-#[allow(unused_mut)]
-pub fn run<F: FnMut()>(mut main_loop: F) {
-    cfg_if! {
-        if #[cfg(target_os = "emscripten")] {
-            crate::emscripten::run(main_loop);
-        } else {
-            loop {
-                let start_ms = elapsed_time() as f64;
-                main_loop();
-                let elapsed_ms = elapsed_time() as f64 - start_ms;
-                let wait_ms = 1000.0 / 60.0 - elapsed_ms;
-                if wait_ms > 0.0 {
-                    sleep((wait_ms / 2.0) as u32);
-                }
-            }
-        }
-    }
+static mut PLATFORM: Option<*mut dyn Platform> = None;
+
+fn platform() -> &'static mut dyn Platform {
+    unsafe { &mut *(PLATFORM.unwrap()) }
+}
+
+pub fn init() {
+    // TODO
 }
 
 pub fn quit() {
-    unsafe {
-        SDL_Quit();
-    }
-
-    cfg_if! {
-        if #[cfg(target_os = "emscripten")] {
-            crate::emscripten::exit(0);
-        } else {
-            std::process::exit(0);
-        }
-    }
+    platform().quit()
 }
 
-pub fn elapsed_time() -> u32 {
-    unsafe { SDL_GetTicks() }
-}
-
-pub fn sleep(ms: u32) {
-    unsafe {
-        SDL_Delay(ms);
-    }
+pub fn set_window_size(width: u32, height: u32) {
+    platform().set_window_size(width, height);
 }
