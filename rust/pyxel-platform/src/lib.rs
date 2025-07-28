4f95e376c2e37da85ce2a123f51295497ad7960f
Takashi Kitao <takashi.kitao@gmail.com>
2025-07-28 10:07:36 +0900

    Implemented new Platform layer

--- rust/pyxel-platform/src/lib.rs
+++ rust/pyxel-platform/src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(clippy::pedantic, clippy::cargo)]
+#![allow(
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_ptr_alignment,
+    clippy::cast_sign_loss,
+    clippy::must_use_candidate,
+    clippy::too_many_lines,
+    clippy::unreadable_literal,
+    clippy::wildcard_imports
+)]
+
 mod event;
 mod platform;
 
@@ -9,8 +22,8 @@ pub mod key;
 
 pub use event::Event;
 pub use platform::{
-    delay, gl_context, gl_profile, init, init_audio, init_window, is_fullscreen, pause_audio,
-    poll_events, quit, set_fullscreen, set_mouse_pos, set_mouse_visible, set_window_icon,
-    set_window_pos, set_window_size, set_window_title, start_loop, step_loop, ticks, window_pos,
-    window_size, GlProfile,
+    delay, display_size, gl_context, gl_profile, gl_swap_buffers, init, init_audio, init_window,
+    is_fullscreen, pause_audio, poll_events, quit, set_fullscreen, set_mouse_pos,
+    set_mouse_visible, set_window_icon, set_window_pos, set_window_size, set_window_title,
+    start_loop, step_loop, ticks, window_pos, window_size, AudioCallback, GlProfile, LoopCallback,
 };
