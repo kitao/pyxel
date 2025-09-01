9e38f6eb9a05b6dc92432ee2f2b25c244c5bc4a3
Takashi Kitao <takashi.kitao@gmail.com>
2025-09-01 10:52:59 +0900

    Changed a button assignment

--- python/pyxel/examples/07_snake.py
+++ python/pyxel/examples/07_snake.py
@@ -88,7 +88,7 @@ class Snake:
         if pyxel.btn(pyxel.KEY_Q):
             pyxel.quit()
 
-        if pyxel.btnp(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A):
+        if pyxel.btnp(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_X):
             self.reset()
 
     def update_direction(self):
