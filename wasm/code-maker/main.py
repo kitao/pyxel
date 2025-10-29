e32a8d9a8bbe185716bd477e6e162d3aabf9ecc0
Takashi Kitao <takashi.kitao@gmail.com>
2025-10-29 09:31:11 +0900

    Added initial files

---
+++ wasm/code-maker/main.py
@@ -0,0 +1,18 @@
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, title="Hello Pyxel")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+
+
+App()
