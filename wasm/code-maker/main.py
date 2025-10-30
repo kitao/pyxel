81eaf42f3d5523e366d038238c6e151521db6de7
Takashi Kitao <takashi.kitao@gmail.com>
2025-10-30 23:29:27 +0900

    Refined the UI

--- wasm/code-maker/main.py
+++ wasm/code-maker/main.py
@@ -4,6 +4,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="Hello Pyxel")
+        pyxel.load("my_resource.pyxres")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,6 +13,7 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
+        pyxel.blt(10, 10, 0, 0, 0, 16, 16)
         pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
 
 
