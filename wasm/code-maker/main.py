bc5eee4441b15fb46aa0a5f1c6cab48d3c4c8cd1
Takashi Kitao <takashi.kitao@gmail.com>
2025-11-06 22:05:57 +0900

    Refined the default code and resource

--- wasm/code-maker/main.py
+++ wasm/code-maker/main.py
@@ -5,16 +5,18 @@ class App:
     def __init__(self):
         pyxel.init(160, 120, title="Hello Pyxel")
         pyxel.load("my_resource.pyxres")
+        self.x = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
+        self.x += 1
+        if self.x >= pyxel.width:
+            self.x = -8
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.blt(10, 10, 0, 0, 0, 16, 16)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.blt(self.x, 60, 0, 0, 0, 16, 16, 2)
+        pyxel.text(48, 53, "Pyxel Code Maker", pyxel.rndi(1, 15))
 
 
 App()
