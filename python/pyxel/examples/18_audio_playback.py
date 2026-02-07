ca775c3872238ec833e3db813af2c84637ce9a9c
Takashi Kitao <takashi.kitao@gmail.com>
2026-02-07 10:00:24 +0900

    Replace audio files

--- python/pyxel/examples/18_audio_playback.py
+++ python/pyxel/examples/18_audio_playback.py
@@ -1,9 +1,9 @@
 import pyxel
 
-TITLE_IMAGE_PATH = "assets/audio_title.png"
-TITLE_AUDIO_PATH = "assets/audio_title.ogg"
-PLAY_IMAGE_PATH = "assets/audio_play.png"
-PLAY_AUDIO_PATH = "assets/audio_play.ogg"
+BGM1_IMAGE_PATH = "assets/audio_title.png"
+BGM1_AUDIO_PATH = "assets/audio_bgm1.ogg"
+BGM2_IMAGE_PATH = "assets/audio_play.png"
+BGM2_AUDIO_PATH = "assets/audio_bgm2.ogg"
 
 
 class App:
@@ -12,34 +12,35 @@ class App:
         pyxel.integer_scale(True)
 
         self.images = [
-            pyxel.Image.from_image(TITLE_IMAGE_PATH),
-            pyxel.Image.from_image(PLAY_IMAGE_PATH),
+            pyxel.Image.from_image(BGM1_IMAGE_PATH),
+            pyxel.Image.from_image(BGM2_IMAGE_PATH),
         ]
 
-        pyxel.sounds[0].pcm(TITLE_AUDIO_PATH)
-        pyxel.sounds[1].pcm(PLAY_AUDIO_PATH)
+        pyxel.sounds[0].pcm(BGM1_AUDIO_PATH)
+        pyxel.sounds[1].pcm(BGM2_AUDIO_PATH)
 
-        self.state = 0
-        self.play_current()
+        # To avoid quality loss, use audio files pre-converted to 22.05kHz.
+        # Change gain from the default 0.125 to adjust volume.
+        pyxel.channels[0].gain = 0.8
+
+        self.bgm_index = 0
+        pyxel.play(0, 0, loop=True)
 
         pyxel.run(self.update, self.draw)
-
-    def play_current(self):
-        pyxel.play(0, pyxel.sounds[self.state], loop=True)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
         if pyxel.btnp(pyxel.KEY_RETURN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A):
-            self.state = 1 - self.state
-            self.play_current()
+            self.bgm_index = 1 - self.bgm_index
+            pyxel.play(0, self.bgm_index, loop=True)
 
     def draw(self):
         pyxel.blt(
             0,
             0,
-            self.images[self.state],
+            self.images[self.bgm_index],
             0,
             0,
             pyxel.width,
@@ -48,7 +49,7 @@ class App:
 
         x = 26 - ((pyxel.frame_count // 2) % pyxel.width)
         s = "Pyxel Audio Playback Sample - Press Enter to Toggle"
-        c = 15 if self.state == 0 else 8
+        c = 15 if self.bgm_index == 0 else 8
         for i in range(2):
             pyxel.text(x + i * pyxel.width, 4, s, c)
 
