f59276f39344817055992b58b859475b40250c11
Takashi Kitao <takashi.kitao@gmail.com>
2026-02-16 16:04:04 +0900

    Change parameter range

--- python/pyxel/examples/15_tiled_map_file.py
+++ python/pyxel/examples/15_tiled_map_file.py
@@ -46,7 +46,10 @@ class App:
             pyxel.quit()
 
         if pyxel.btnp(pyxel.KEY_X):
-            pyxel.gen_bgm(pyxel.rndi(0, 7), 0, 3, play=True)
+            preset = pyxel.rndi(0, 7)
+            transpose = pyxel.rndi(-5, 6)
+            instr = pyxel.rndi(0, 3)
+            pyxel.gen_bgm(preset, transpose, instr, play=True)
 
         # Update player
         x, y, u, v = self.player
