af172599c210a3210cdd38b08849d961cf543260
Takashi Kitao <takashi.kitao@gmail.com>
2026-02-17 19:53:14 +0900

    Add comment

--- python/pyxel/examples/15_tiled_map_file.py
+++ python/pyxel/examples/15_tiled_map_file.py
@@ -45,9 +45,14 @@ class App:
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
-        if pyxel.frame_count % 240 == 0:
+        if pyxel.btnp(pyxel.KEY_X):
             pyxel.stop()
 
+            # gen_bgm generates a list of MML strings.
+            # Normally, assign them to sounds:
+            #     mml_list = pyxel.gen_bgm(preset, instr)
+            #     pyxel.sounds[0].mml(mml_list[0])
+            # But here, play=True is used for quick playback.
             preset = pyxel.rndi(0, 7)
             instr = pyxel.rndi(0, 3)
             pyxel.gen_bgm(preset, instr, play=True)
