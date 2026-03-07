735d0b8c487c94be7a4ce2f3bef52e90737cf3ad
Takashi Kitao <takashi.kitao@gmail.com>
2026-03-07 18:23:17 +0900

    Make perspective views responsive with HUD
    Reserve a HUD area and compute view dimensions dynamically. Added HUD_HEIGHT constant, derive hw from pyxel.width and vh from pyxel.height - HUD_HEIGHT, and use those for blt3d/bltm3d calls and divider lines. Also updated HUD text (QE:Roll -> QE:Tilt). Corresponding example GIF image updated.

--- python/pyxel/examples/19_perspective.py
+++ python/pyxel/examples/19_perspective.py
@@ -1,5 +1,7 @@
 import pyxel
 
+HUD_HEIGHT = 16
+
 
 class App:
     def __init__(self):
@@ -76,18 +78,19 @@ class App:
         # Perspective views
         cam = (self.cam_x, self.cam_y, self.cam_z)
         rot = (self.rot_x, self.rot_y, self.rot_z)
-        hw = 99
-        pyxel.blt3d(0, 16, hw, 134, 0, cam, rot, fov=self.fov)
-        pyxel.bltm3d(hw + 2, 16, hw, 134, 0, cam, rot, fov=self.fov)
+        hw = pyxel.width // 2
+        vh = pyxel.height - HUD_HEIGHT
+        pyxel.blt3d(0, HUD_HEIGHT, hw, vh, 0, cam, rot, fov=self.fov)
+        pyxel.bltm3d(hw + 2, HUD_HEIGHT, hw, vh, 0, cam, rot, fov=self.fov)
 
         # Divider
-        pyxel.line(hw, 16, hw, 149, 0)
-        pyxel.line(hw + 1, 16, hw + 1, 149, 0)
+        pyxel.line(hw, HUD_HEIGHT, hw, pyxel.height - 1, 0)
+        pyxel.line(hw + 1, HUD_HEIGHT, hw + 1, pyxel.height - 1, 0)
 
         # HUD
         pyxel.text(4, 2, "blt3d", 10)
         pyxel.text(hw + 6, 2, "bltm3d", 10)
-        pyxel.text(4, 9, "WASD:Move Arrows:Look RF:Alt QE:Roll TG:FOV", 13)
+        pyxel.text(4, 9, "WASD:Move Arrows:Look RF:Alt QE:Tilt TG:FOV", 13)
 
 
 App()
